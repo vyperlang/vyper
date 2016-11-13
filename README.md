275660e85650eed3ced18d49591fdf976b3b11fb
Jelle Gerbrandy <github@gerbrandy.com>
2016-11-13 14:42:59 +0100

    lay an egg

---
+++ README.md
@@ -0,0 +1,13 @@
+# Viper 
+
+
+## Installation
+
+Requires python3
+
+	python setup.py install
+
+## Testing
+
+	python setup.py test
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='viper',
+    version='0.0.1',
+    description='Viper Programming Language for Ethereum',
+    long_description=readme,
+    author='Vitalik Buterin',
+    author_email='',
+    url='https://github.com/ethereum/viper',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        'ethereum == 1.3.7',
+        'serpent',
+        'nose',
+    ]
+)
