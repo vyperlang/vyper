acd76b2cee7f358cb8b252dcd838b9536f78c5c4
Bryant <fubuloubu@gmail.com>
2017-08-09 14:43:50 -0700

    Added two more tests for new methods in contract, rewrote previous tests to address some comments on review

--- examples/stock/test_company.py
+++ examples/stock/test_company.py
@@ -13,11 +13,9 @@ def tester():
     tester.languages['viper'] = compiler.Compiler()
     contract_code = open('examples/stock/company.v.py').read()
     tester.company_address = t.a0
-    # 100 shares @ 10^6 wei / share
-    tester.total_shares = 100
-    tester.share_price = 10**6
+    # Company with 1000 shares @ 10^6 wei / share
     tester.c = tester.s.contract(contract_code, language='viper', \
-            args=[tester.company_address, tester.total_shares, tester.share_price])
+            args=[tester.company_address, 1000, 10**6])
     return tester
 
 @pytest.fixture
@@ -28,27 +26,68 @@ def assert_tx_failed(tester, function_to_test, exception = t.TransactionFailed):
     tester.s.revert(initial_state)
 
 def test_overbuy(tester):
-    # If all the stock has been bought, you can't buy more
-    test_shares = tester.total_shares / 2
-    tester.c.buy_stock(sender=t.k1, value=int(test_shares * tester.share_price))
-    tester.c.buy_stock(sender=t.k1, value=int(test_shares * tester.share_price))
-    assert_tx_failed(tester, lambda: tester.c.buy_stock(sender=t.k1, value=tester.share_price))
+    # If all the stock has been bought, no one can buy more
+    test_shares = int(tester.c.get_total_shares() / 2)
+    test_value = int(test_shares * tester.c.get_price())
+    tester.c.buy_stock(sender=t.k1, value=test_value)
+    tester.c.buy_stock(sender=t.k1, value=test_value)
+    assert tester.c.stock_available() == 0
+    assert tester.c.get_holding(sender=t.k1) == (test_shares * 2)
+    one_stock = tester.c.get_price()
+    assert_tx_failed(tester, lambda: tester.c.buy_stock(sender=t.k1, value=one_stock))
+    assert_tx_failed(tester, lambda: tester.c.buy_stock(sender=t.k2, value=one_stock))
 
 def test_sell_without_stock(tester):
     # If you don't have any stock, you can't sell
     assert_tx_failed(tester, lambda: tester.c.sell_stock(1, sender=t.k1))
-    tester.c.buy_stock(sender=t.k1, value=int(tester.total_shares * tester.share_price))
-    tester.c.sell_stock(tester.total_shares, sender=t.k1)
+    assert_tx_failed(tester, lambda: tester.c.sell_stock(1, sender=t.k2))
+    # But if you do, you can!
+    test_shares = int(tester.c.get_total_shares())
+    test_value = int(test_shares * tester.c.get_price())
+    tester.c.buy_stock(sender=t.k1, value=test_value)
+    assert tester.c.get_holding(sender=t.k1) == test_shares
+    tester.c.sell_stock(test_shares, sender=t.k1)
+    # But only until you run out
     assert_tx_failed(tester, lambda: tester.c.sell_stock(1, sender=t.k1))
 
 def test_oversell(tester):
     # You can't sell more than you own
-    test_shares = tester.total_shares / 10
-    tester.c.buy_stock(sender=t.k1, value=int(test_shares * tester.share_price))
-    assert_tx_failed(tester, lambda: tester.c.sell_stock(tester.total_shares, sender=t.k1))
+    test_shares = int(tester.c.get_total_shares())
+    test_value = int(test_shares * tester.c.get_price())
+    tester.c.buy_stock(sender=t.k1, value=test_value)
+    assert_tx_failed(tester, lambda: tester.c.sell_stock(test_shares + 1, sender=t.k1))
+
+def test_transfer(tester):
+    # If you don't have any stock, you can't transfer
+    assert_tx_failed(tester, lambda: tester.c.transfer_stock(t.a2, 1, sender=t.k1))
+    assert_tx_failed(tester, lambda: tester.c.transfer_stock(t.a1, 1, sender=t.k2))
+    # If you transfer, you don't have the stock anymore
+    test_shares = int(tester.c.get_total_shares())
+    test_value = int(test_shares * tester.c.get_price())
+    tester.c.buy_stock(sender=t.k1, value=test_value)
+    assert tester.c.get_holding(sender=t.k1) == test_shares
+    tester.c.transfer_stock(t.a2, test_shares, sender=t.k1)
+    assert_tx_failed(tester, lambda: tester.c.sell_stock(1, sender=t.k1))
+    # But the other person does
+    tester.c.sell_stock(test_shares, sender=t.k2)
+
+def test_paybill(tester):
+    # Only the company can authorize payments
+    assert_tx_failed(tester, lambda: tester.c.pay_bill(t.a2, 1, sender=t.k1))
+    # A company can only pay someone if it has the money
+    assert_tx_failed(tester, lambda: tester.c.pay_bill(t.a2, 1, sender=t.k0))
+    # If it has the money, it can pay someone
+    test_value = int(tester.c.get_total_shares() * tester.c.get_price())
+    tester.c.buy_stock(sender=t.k1, value=test_value)
+    tester.c.pay_bill(t.a2, test_value, sender=t.k0)
+    # Until it runs out of money
+    assert_tx_failed(tester, lambda: tester.c.pay_bill(t.a3, 1, sender=t.k0))
+    # Then no stockholders can sell their stock either
+    assert_tx_failed(tester, lambda: tester.c.sell_stock(1, sender=t.k1))
 
 def test_valuation(tester):
     # Valuation is number of shares held times price
-    assert tester.c.valuation() == 0
-    tester.c.buy_stock(sender=t.k1, value=int(tester.total_shares * tester.share_price))
-    assert tester.c.valuation() == (tester.total_shares * tester.share_price)
+    assert tester.c.worth() == 0
+    test_value = int(tester.c.get_total_shares() * tester.c.get_price())
+    tester.c.buy_stock(sender=t.k1, value=test_value)
+    assert tester.c.worth() == test_value
