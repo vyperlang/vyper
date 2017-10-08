b047aee65f95fd1175da294fcb585ca49fa7d3fd
chgue <chris.gue@gmx.at>
2017-10-08 12:36:12 +0200

    Added multiple of two check again; Made locked check syntax more pythonic

--- examples/safe_remote_purchase/safe_remote_purchase.v.py
+++ examples/safe_remote_purchase/safe_remote_purchase.v.py
@@ -17,22 +17,23 @@ def unlocked() -> bool: #Is a refund possible for the seller?
     
 @payable
 def __init__():
+    assert (msg.value % 2) == 0
     self.value = msg.value / 2 #Seller initializes contract by posting a safety deposit of 2*value of the item up for sale
     self.seller = msg.sender
     
 def abort():
-    assert self.unlocked() == true #Is the contract still refundable
+    assert self.unlocked() #Is the contract still refundable
     assert msg.sender == self.seller #Only seller can refund his deposit before any buyer purchases the item
     selfdestruct(self.seller) #Refunds seller, deletes contract
 
 @payable
 def purchase():
-    assert self.unlocked() == true #Contract still open (item still up for sale)?
+    assert self.unlocked() #Contract still open (item still up for sale)?
     assert msg.value == (2*self.value) #Is the deposit of correct value?
     self.buyer = msg.sender
     
 def recieved():
-    assert self.unlocked() == false #Is the item already purchased and pending confirmation of buyer
+    assert not self.unlocked() #Is the item already purchased and pending confirmation of buyer
     assert msg.sender == self.buyer 
     send(self.buyer, self.value) #Return deposit (=value) to buyer
-selfdestruct(self.seller) #Returns deposit (=2*value) and the purchase price (=value)
+    selfdestruct(self.seller) #Returns deposit (=2*value) and the purchase price (=value)
