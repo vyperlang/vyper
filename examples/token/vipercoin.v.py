e38f1a7be73a6b9c38dc6b7354769c794f67c4e8
Jacques Wagener <jacques@dilectum.co.za>
2017-10-09 13:12:47 +0200

    Adds token example tests for initial state, transfer, allowance and transferFrom.

--- examples/token/vipercoin.v.py
+++ examples/token/vipercoin.v.py
@@ -1,5 +1,5 @@
 # Viper Port of MyToken
-# THIS HAS NOT BEEN AUDITED!
+# THIS CONTRACT HAS NOT BEEN AUDITED!
 # ERC20 details at:
 # https://theethereum.wiki/w/index.php/ERC20_Token_Standard
 # https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
@@ -14,8 +14,7 @@ balances: num[address]
 allowed: num[address][address]
 
 
-def __init__(
-        _name: bytes32, _symbol: bytes32, _decimals: num, initialSupply: num):
+def __init__(_name: bytes32, _symbol: bytes32, _decimals: num, initialSupply: num):
 
     self.name = _name
     self.symbol = _symbol
@@ -24,21 +23,31 @@ def __init__(
     self.balances[msg.sender] = initialSupply
 
 
+@constant
+def symbol() -> bytes32:
+
+    return self.symbol
+
+
 # What is the balance of a particular account?
+@constant
 def balanceOf(_owner: address) -> num256:
+
     return as_num256(self.balances[_owner])
 
 
 # Return total supply of token.
-def totalSupply():
+@constant
+def totalSupply() -> num256:
+
     return as_num256(self.totalSupply)
 
 
 # Send `_value` tokens to `_to` from your account
 def transfer(_to: address, _amount: num(num256)) -> bool:
 
-    if self.balances[msg.sender] > _amount and \
-       self.balances[_to] + _amount > self.balances[_to]:
+    if self.balances[msg.sender] >= _amount and \
+       self.balances[_to] + _amount >= self.balances[_to]:
 
         self.balances[msg.sender] -= _amount  # Subtract from the sender
         self.balances[_to] += _amount  # Add the same to the recipient
@@ -48,13 +57,30 @@ def transfer(_to: address, _amount: num(num256)) -> bool:
         return False
 
 
+# Transfer allowed tokens from a specific account to another.
+def transferFrom(_from: address, _to: address, _value: num(num256)) -> bool:
+
+    if _value <= self.allowed[_from][msg.sender] and \
+       _value <= self.balances[_from]:
+
+        self.balances[_from] -= _value  # decrease balance of from address.
+        self.allowed[_from][msg.sender] -= _value  # decrease allowance.
+        self.balances[_to] += _value  # incease balance of to address.
+
+        return True
+    else:
+        return False
+
+
 # Allow _spender to withdraw from your account, multiple times, up to the _value amount.
 # If this function is called again it overwrites the current allowance with _value.
 def approve(_spender: address, _amount: num(num256)) -> bool:
+
     self.allowed[msg.sender][_spender] = _amount
     return True
 
 
 # Get the allowence an address has to spend anothers' token.
 def allowance(_owner: address, _spender: address) -> num256:
+
     return as_num256(self.allowed[_owner][_spender])
--- tests/examples/token/test_vipercoin.py
+++ tests/examples/token/test_vipercoin.py
@@ -1,14 +1,111 @@
 import pytest
+
+from ethereum.abi import ValueOutOfBounds
 from ethereum.tools import tester
-import ethereum.utils as utils
 
 
+TOKEN_NAME = "Vipercoin"
+TOKEN_SYMBOL = "FANG"
+TOKEN_DECIMALS = 18
+TOKEN_INITIAL_SUPPLY = 21 * 10 ** 6
 
+
+@pytest.fixture
 def token_tester():
     t = tester
     tester.s = t.Chain()
     from viper import compiler
     t.languages['viper'] = compiler.Compiler()
     contract_code = open('examples/token/vipercoin.v.py').read()
-    tester.c = tester.s.contract(contract_code, language='viper', args=[tester.accounts[0], FIVE_DAYS])
+    tester.c = tester.s.contract(
+        contract_code,
+        language='viper',
+        args=[TOKEN_NAME, TOKEN_SYMBOL, TOKEN_DECIMALS, TOKEN_INITIAL_SUPPLY]
+    )
     return tester
+
+
+@pytest.fixture
+def assert_tx_failed():
+    def assert_tx_failed(tester, function_to_test, exception=tester.TransactionFailed):
+        initial_state = tester.s.snapshot()
+        with pytest.raises(exception):
+            function_to_test()
+        tester.s.revert(initial_state)
+    return assert_tx_failed
+
+
+def test_initial_state(token_tester):
+    assert token_tester.c.totalSupply() == TOKEN_INITIAL_SUPPLY == token_tester.c.balanceOf(token_tester.accounts[0])
+    assert token_tester.c.balanceOf(token_tester.accounts[1]) == 0
+
+
+def test_transfer(token_tester, assert_tx_failed):
+
+    # Basic transfer.
+    assert token_tester.c.transfer(token_tester.accounts[1], 1) is True
+    assert token_tester.c.balanceOf(token_tester.accounts[1]) == 1
+    assert token_tester.c.balanceOf(token_tester.accounts[0]) == TOKEN_INITIAL_SUPPLY - 1
+
+    # Some edge cases:
+
+    # more than allowed
+    assert token_tester.c.transfer(token_tester.accounts[1], TOKEN_INITIAL_SUPPLY) is False
+
+    # Negative transfer value.
+    assert_tx_failed(
+        tester=token_tester,
+        function_to_test=lambda: tester.c.transfer(token_tester.accounts[1], -1),
+        exception=ValueOutOfBounds
+    )
+
+
+def test_approve_allowance(token_tester):
+    assert token_tester.c.allowance(token_tester.accounts[0], token_tester.accounts[1]) == 0
+    assert token_tester.c.approve(token_tester.accounts[1], 10) is True
+    assert token_tester.c.allowance(token_tester.accounts[0], token_tester.accounts[1]) == 10
+
+
+def test_transferFrom(token_tester, assert_tx_failed):
+    a0 = token_tester.accounts[0]
+    a1 = token_tester.accounts[1]
+    k1 = token_tester.k1
+    a2 = token_tester.accounts[2]
+    k2 = token_tester.k2
+    contract = token_tester.c
+
+    # Allow 10 token transfers. Account a1 is allowed to spend 10 tokens of a0's account.
+    ALLOWANCE = 10
+    assert contract.approve(a1, 10) is True
+    assert contract.allowance(a0, a1) == ALLOWANCE
+
+    assert contract.transferFrom(a0, a2, 3, sender=k1) is True  # a1 may transfer.
+    assert contract.balanceOf(a0) == TOKEN_INITIAL_SUPPLY - 3
+    assert contract.balanceOf(a1) == 0
+    assert contract.balanceOf(a2) == 3
+    assert contract.allowance(a0, a1) == ALLOWANCE - 3
+
+    # a2 may not transfer.
+    assert contract.transferFrom(a0, a2, ALLOWANCE, sender=k2) is False
+
+    # Negative transfer value.
+    assert_tx_failed(
+        tester=token_tester,
+        function_to_test=lambda: contract.transferFrom(a0, a2, -1, sender=k1),
+        exception=ValueOutOfBounds
+    )
+
+    # Transfer more than allowance:
+    assert contract.transferFrom(a0, a2, 8, sender=k1) is False
+    assert contract.balanceOf(a0) == TOKEN_INITIAL_SUPPLY - 3
+    assert contract.balanceOf(a1) == 0
+    assert contract.balanceOf(a2) == 3
+    assert contract.allowance(a0, a1) == ALLOWANCE - 3
+
+    # Transfer exact amount left in allowance:
+    allowance_left = contract.allowance(a0, a1)
+    assert contract.transferFrom(a0, a2, allowance_left, sender=k1) is True
+    assert contract.balanceOf(a0) == TOKEN_INITIAL_SUPPLY - ALLOWANCE
+    assert contract.balanceOf(a1) == 0
+    assert contract.balanceOf(a2) == ALLOWANCE
+    assert contract.allowance(a0, a1) == 0
