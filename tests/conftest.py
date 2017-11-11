5f7ee7561d5405fc3cae682cb757f1836d62d4c3
David Knott <david1k1nott@gmail.com>
2017-11-11 14:09:44 -0700

    Add fixture to directly test LLL

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,28 @@
 import pytest
+from ethereum.tools import tester
+from viper.parser.parser_utils import (
+    LLLnode
+)
+from viper import (
+    compile_lll,
+    optimizer
+)
 
 @pytest.fixture
 def bytes_helper():
     def bytes_helper(str, length):
         return bytes(str, 'utf-8') + bytearray(length-len(str))
     return bytes_helper
+
+@pytest.fixture
+def t():
+    tester.s = tester.Chain()
+    return tester
+
+@pytest.fixture
+def get_contract_from_lll(t):
+    def lll_compiler(lll):
+        lll = optimizer.optimize(LLLnode.from_list(lll))
+        byte_code = compile_lll.assembly_to_evm(compile_lll.compile_to_assembly(lll))
+        t.s.tx(to=b'', data=byte_code)
+    return lll_compiler
