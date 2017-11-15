48301a377ca442b1fbd1419b6dd35f1beb7d1807
Daejun Park <daejunpark@gmail.com>
2017-11-14 22:45:34 -0600

    add sha3_32 compilation test

---
+++ tests/compiler/test_sha3_32.py
@@ -0,0 +1,9 @@
+import pytest
+from viper.parser.parser_utils import LLLnode
+from viper import compile_lll, optimizer
+
+def test_sha3_32():
+    lll = ['sha3_32', 0]
+    evm = ['PUSH1', 0, 'PUSH1', 192, 'MSTORE', 'PUSH1', 32, 'PUSH1', 192, 'SHA3']
+    assert compile_lll.compile_to_assembly(LLLnode.from_list(lll)) == evm
+    assert compile_lll.compile_to_assembly(optimizer.optimize(LLLnode.from_list(lll))) == evm
