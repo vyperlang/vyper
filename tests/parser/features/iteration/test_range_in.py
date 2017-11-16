35a915b41c27c7eff2c9fd13dd34800525842440
DavidKnott <david1k1nott@gmail.com>
2017-11-15 21:40:40 -0700

    Add public decorator to tests (#474)

--- tests/parser/features/iteration/test_range_in.py
+++ tests/parser/features/iteration/test_range_in.py
@@ -82,13 +82,16 @@ def test_ownership(assert_tx_failed):
 
 owners: address[2]
 
+@public
 def __init__():
     self.owners[0] = msg.sender
 
+@public
 def set_owner(i: num, new_owner: address):
     assert msg.sender in self.owners
     self.owners[i] = new_owner
 
+@public
 def is_owner() -> bool:
     return msg.sender in self.owners
     """
