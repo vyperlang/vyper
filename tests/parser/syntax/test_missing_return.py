d898f0cc14fb224101b170deeba39efb6be7336b
Jacques Wagener <jacques@dilectum.co.za>
2017-12-20 15:38:49 +0200

    Adds example of return checker behaving incorrectly.

--- tests/parser/syntax/test_missing_return.py
+++ tests/parser/syntax/test_missing_return.py
@@ -18,3 +18,23 @@ def foo() -> num:
 def test_missing_return(bad_code):
     with raises(StructureException):
         compiler.compile(bad_code)
+
+
+
+valid_list = [
+    """
+@public
+def foo() -> num:
+    return 123
+    """,
+    """
+@public
+def foo() -> num:
+    if false:
+        return 123
+    """,  # For the time being this is valid code, even though it should not be.
+]
+
+@pytest.mark.parametrize('good_code', valid_list)
+def test_return_success(good_code):
+    assert compiler.compile(good_code) is not None
