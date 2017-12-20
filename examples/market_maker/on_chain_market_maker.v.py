d6c60bdc53920419d80d79fa7d017dd8ba053729
David Knott <david1k1nott@gmail.com>
2017-12-20 16:55:19 +0700

    Fix on chain market maker

--- examples/market_maker/on_chain_market_maker.v.py
+++ examples/market_maker/on_chain_market_maker.v.py
@@ -1,19 +1,19 @@
 total_eth_qty: public(wei_value)
-total_token_qty: public(num)
-invariant: public(wei_value)
+total_token_qty: public(currency_value)
+invariant: public(num(wei * currency))
 token_address: address(ERC20)
 owner: public(address)
 
 @public
 @payable
-def initiate(token_addr: address, token_quantity: num):
+def initiate(token_addr: address, token_quantity: currency_value):
     assert self.invariant == 0
     self.token_address = token_addr
     self.token_address.transferFrom(msg.sender, self, as_num256(token_quantity))
     self.owner = msg.sender
     self.total_eth_qty = msg.value
     self.total_token_qty = token_quantity
-    self.invariant = msg.value
+    self.invariant = msg.value * token_quantity
     assert self.invariant > 0
 
 @public
@@ -25,16 +25,18 @@ def eth_to_tokens():
     new_total_tokens = self.invariant / new_total_eth
     self.token_address.transfer(msg.sender,
                                 as_num256(self.total_token_qty - new_total_tokens))
+    self.total_eth_qty = new_total_eth
     self.total_token_qty = new_total_tokens
 
 @public
-def tokens_to_eth(sell_quantity: num):
-    # self.token_address.transferFrom(msg.sender, self, as_num256(sell_quantity))
+def tokens_to_eth(sell_quantity: currency_value):
+    self.token_address.transferFrom(msg.sender, self, as_num256(sell_quantity))
     new_total_tokens = self.total_token_qty + sell_quantity
     new_total_eth = self.invariant / new_total_tokens
     eth_to_send = self.total_eth_qty - new_total_eth
     send(msg.sender, eth_to_send)
     self.total_eth_qty = new_total_eth
+    self.total_token_qty = new_total_tokens
 
 @public
 def owner_withdraw():
