7ed7d9b34e32f2fe4dc492cc38d89c00bb3013cb
Jacques Wagener <jacques@dilectum.co.za>
2018-01-02 13:14:32 +0200

    Allow RLPList to be the one exception were types do not have to be specified on assignment.

--- viper/parser/stmt.py
+++ viper/parser/stmt.py
@@ -92,7 +92,13 @@ class Stmt(object):
         if len(self.stmt.targets) != 1:
             raise StructureException("Assignment statement must have one target", self.stmt)
         sub = Expr(self.stmt.value, self.context).lll_node
-        if isinstance(self.stmt.targets[0], ast.Name) and self.stmt.targets[0].id not in self.context.vars:
+        # Determine if it's an RLPList assignment.
+        if isinstance(self.stmt.value, ast.Call) and self.stmt.value.func.id is 'RLPList':
+            pos = self.context.new_variable(self.stmt.targets[0].id, set_default_units(sub.typ))
+            variable_loc = LLLnode.from_list(pos, typ=sub.typ, location='memory', pos=getpos(self.stmt), annotation=self.stmt.targets[0].id)
+            o = make_setter(variable_loc, sub, 'memory', pos=getpos(self.stmt))
+        # All other assignments are forbidden.
+        elif isinstance(self.stmt.targets[0], ast.Name) and self.stmt.targets[0].id not in self.context.vars:
             raise VariableDeclarationException("Variable not defined", self.stmt)
         else:
             # Checks to see if assignment is valid
