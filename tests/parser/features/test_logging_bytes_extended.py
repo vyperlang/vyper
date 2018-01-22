019a4a75c28eaeecf9a46a0366829a33e0bb8cb5
Jacques Wagener <jacques@dilectum.co.za>
2018-01-22 12:54:36 +0200

    Adds extended bytes tests.

--- tests/parser/features/test_logging_bytes_extended.py
+++ tests/parser/features/test_logging_bytes_extended.py
@@ -10,12 +10,86 @@ def foo():
     """
 
     c = get_contract_with_gas_estimation(code)
-    a = c.foo()
+    c.foo()
     receipt = chain.head_state.receipts[-1]
     log = get_logs(receipt, c)
     assert log[0]['arg1'] == 667788
     assert log[0]['arg2'] == b"hello" * 9
     assert log[0]['arg3'] == 334455
+
+
+def test_bytes_logging_extended_variables(t, get_contract_with_gas_estimation, get_logs, chain):
+    code = """
+MyLog: __log__({arg1: bytes <= 64, arg2: bytes <= 64, arg3: bytes <= 64})
+
+@public
+def foo():
+    a = 'hellohellohellohellohellohellohellohellohello'
+    b = 'hellohellohellohellohellohellohellohello'
+    c = 'hellohellohellohellohellohellohello'
+    log.MyLog(a, b, c)
+    """
+
+    c = get_contract_with_gas_estimation(code)
+    c.foo()
+    receipt = chain.head_state.receipts[-1]
+    log = get_logs(receipt, c)
+    assert log[0]['arg1'] == b"hello" * 9
+    assert log[0]['arg2'] == b"hello" * 8
+    assert log[0]['arg3'] == b"hello" * 7
+
+
+def test_bytes_logging_extended_passthrough(t, get_contract_with_gas_estimation, get_logs, chain):
+    code = """
+MyLog: __log__({arg1: num, arg2: bytes <= 64, arg3: num})
+
+@public
+def foo(a: num, b: bytes <= 64, c: num):
+    log.MyLog(a, b, c)
+    """
+
+    c = get_contract_with_gas_estimation(code)
+    c.foo(333, b"flower" * 8, 444)
+    receipt = chain.head_state.receipts[-1]
+    log = get_logs(receipt, c)
+    assert log[0]['arg1'] == 333
+    assert log[0]['arg2'] == b"flower" * 8
+    assert log[0]['arg3'] == 444
+
+
+def test_bytes_logging_extended_storage(t, get_contract_with_gas_estimation, get_logs, chain):
+    code = """
+MyLog: __log__({arg1: num, arg2: bytes <= 64, arg3: num})
+a: num
+b: bytes <= 64
+c: num
+
+@public
+def foo():
+    log.MyLog(self.a, self.b, self.c)
+
+@public
+def set(x: num, y: bytes <= 64, z: num):
+    self.a = x
+    self.b = y
+    self.c = z
+    """
+
+    c = get_contract_with_gas_estimation(code)
+    c.foo()
+    receipt = chain.head_state.receipts[-1]
+    log = get_logs(receipt, c)
+    assert log[0]['arg1'] == 0
+    assert log[0]['arg2'] == b""
+    assert log[0]['arg3'] == 0
+
+    c.set(333, b"flower" * 8, 444)
+    c.foo()
+    receipt = chain.head_state.receipts[-1]
+    log = get_logs(receipt, c)
+    assert log[0]['arg1'] == 333
+    assert log[0]['arg2'] == b"flower" * 8
+    assert log[0]['arg3'] == 444
 
 
 def test_bytes_logging_extended_mixed_with_lists(t, get_contract_with_gas_estimation, get_logs, chain):
@@ -38,7 +112,7 @@ def foo():
     """
 
     c = get_contract_with_gas_estimation(code)
-    a = c.foo()
+    c.foo()
     receipt = chain.head_state.receipts[-1]
     log = get_logs(receipt, c)
     assert log[0]['arg1'] == [[24, 26], [12, 10]]
