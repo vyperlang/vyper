ab52588ada81af50c70ceb4bf0f8be7676854758
Jacques Wagener <jacques@dilectum.co.za>
2018-02-09 19:20:58 +0200

    Adds tests for compile_lll stage.

--- vyper/__init__.py
+++ vyper/__init__.py
@@ -2,7 +2,7 @@ import sys
 
 from pkg_resources import get_distribution
 if (sys.version_info.major, sys.version_info.minor) < (3, 6):
-    raise Exception("Requires python3.6+")
+    raise Exception("Requires python3.6+")  # pragma: no cover
 
 
 __version__ = get_distribution('vyper').version
