13c780b04fa65ac7a629e6e39eb7656aabe0568b
David Knott <david1k1nott@gmail.com>
2018-02-13 21:37:05 -0700

    Ban and test functions having payable and constant decorators / Improve error messages

--- tests/parser/features/decorators/test_constant.py
+++ tests/parser/features/decorators/test_constant.py
@@ -1,3 +1,6 @@
+from vyper.exceptions import StructureException
+
+
 def test_constant_test(get_contract_with_gas_estimation_for_constants):
     constant_test = """
 @public
@@ -10,3 +13,14 @@ def foo() -> int128:
     assert c.foo() == 5
 
     print("Passed constant function test")
+
+
+def test_invalid_constant_and_payable(get_contract_with_gas_estimation_for_constants, assert_compile_failed):
+    code = """
+@public
+@payable
+@constant
+def foo() -> num:
+    return 5
+"""
+    assert_compile_failed(lambda: get_contract_with_gas_estimation_for_constants(code), StructureException)
