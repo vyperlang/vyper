f422034d63793c6d71468be1907b7efc18f67de6
Jacques Wagener <jacques@dilectum.co.za>
2018-02-20 15:49:21 +0200

    Add comment to __init__.py test exception.

--- vyper/__init__.py
+++ vyper/__init__.py
@@ -2,6 +2,7 @@ import sys
 
 from pkg_resources import get_distribution
 if (sys.version_info.major, sys.version_info.minor) < (3, 6):
+    # Can't be tested, as our test harness is using python3.6.
     raise Exception("Requires python3.6+")  # pragma: no cover
 
 
