0896b7aeb43a870bdbee544cde649d03b083dca1
Jacques Wagener <jacques@dilectum.co.za>
2018-02-22 17:36:28 +0200

    Rename the integer types:
        The signed integer name of 'num' replaced with 'int128'.
        The unsigend integer name of 'num256' replaced with 'uint128'.

--- vyper/premade_contracts.py
+++ vyper/premade_contracts.py
@@ -3,12 +3,12 @@ import ast
 erc20 = """
 class ERC20():
     def symbol() -> bytes32: pass
-    def balanceOf(_owner: address) -> num256: pass
-    def totalSupply() -> num256: pass
-    def transfer(_to: address, _amount: num256) -> bool: pass
-    def transferFrom(_from: address, _to: address, _value: num(num256)) -> bool: pass
-    def approve(_spender: address, _amount: num(num256)) -> bool: pass
-    def allowance(_owner: address, _spender: address) -> num256: pass
+    def balanceOf(_owner: address) -> uint256: pass
+    def totalSupply() -> uint256: pass
+    def transfer(_to: address, _amount: uint256) -> bool: pass
+    def transferFrom(_from: address, _to: address, _value: int128(uint256)) -> bool: pass
+    def approve(_spender: address, _amount: int128(uint256)) -> bool: pass
+    def allowance(_owner: address, _spender: address) -> uint256: pass
 """
 
 
