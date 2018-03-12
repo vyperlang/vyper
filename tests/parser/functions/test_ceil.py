aba7aff0ff7570d28f9baa0fa6152b241ecb8cd8
Michael Sproul <micsproul@gmail.com>
2018-03-12 22:28:55 +1100

    Fix floor and ceil for negative inputs
    Previously floor and ceil would attempt to round towards 0,
    rather than towards negative and positive infinity, respectively.
    
    The functions now satisfy the following definitions:
    
    floor(x) := the greatest integer less than or equal to x
    ceil(x) := the least integer greater than or equal to x

--- tests/parser/functions/test_ceil.py
+++ tests/parser/functions/test_ceil.py
@@ -11,8 +11,44 @@ def fop() -> int128:
 @public
 def foq() -> int128:
     return ceil(170141183460469231731687303715884105723.0000000001)
+
+@public
+def fos() -> int128:
+    return ceil(0.0)
 """
     c = get_contract_with_gas_estimation(code)
     assert c.foo() == 1
     assert c.fop() == 1
     assert c.foq() == 170141183460469231731687303715884105724
+    assert c.fos() == 0
+
+
+# ceil(x) should yeild the smallest integer greater than or equal to x
+def test_ceil_negative(get_contract_with_gas_estimation):
+    code = """
+@public
+def foo() -> int128:
+    return ceil(-11.01)
+
+@public
+def fop() -> int128:
+    return ceil(-5.0)
+
+@public
+def foq() -> int128:
+    return ceil(-.0000000001)
+
+@public
+def fos() -> int128:
+    return ceil(-5472.9999999999)
+
+@public
+def fot() -> int128:
+    return ceil(-170141183460469231731687303715884105727.0000000001)
+"""
+    c = get_contract_with_gas_estimation(code)
+    assert c.foo() == -11
+    assert c.fop() == -5
+    assert c.foq() == 0
+    assert c.fos() == -5472
+    assert c.fot() == -170141183460469231731687303715884105727
--- tests/parser/functions/test_floor.py
+++ tests/parser/functions/test_floor.py
@@ -11,8 +11,50 @@ def fop() -> int128:
 @public
 def foq() -> int128:
     return floor(170141183460469231731687303715884105723.0000000001)
+
+@public
+def fos() -> int128:
+    return floor(0.0)
+
+@public
+def fot() -> int128:
+    return floor(0.0000000001)
 """
     c = get_contract_with_gas_estimation(code)
     assert c.foo() == 1
     assert c.fop() == 1
     assert c.foq() == 170141183460469231731687303715884105723
+    assert c.fos() == 0
+    assert c.fot() == 0
+
+
+def test_floor_negative(get_contract_with_gas_estimation):
+    code = """
+@public
+def foo() -> int128:
+    x: int128 = -65
+    y: decimal = x / 10
+    return floor(y)
+
+@public
+def fop() -> int128:
+    return floor(-27.0)
+
+@public
+def foq() -> int128:
+    return floor(-9000.0000000001)
+
+@public
+def fos() -> int128:
+    return floor(-0.0000000001)
+
+@public
+def fot() -> int128:
+    return floor(-170141183460469231731687303715884105727.0000000001)
+"""
+    c = get_contract_with_gas_estimation(code)
+    assert c.foo() == -7
+    assert c.fop() == -27
+    assert c.foq() == -9001
+    assert c.fos() == -1
+    assert c.fot() == -170141183460469231731687303715884105728
