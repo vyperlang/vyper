5e20bc64410d7d180d81e23ad76e2b06f5c6ee6d
Jacques Wagener <jacques@dilectum.co.za>
2018-05-22 13:23:07 +0200

    Allow running vyper straight from folder, for development purposes.

--- vyper/__init__.py
+++ vyper/__init__.py
@@ -1,9 +1,13 @@
 import sys
+import pkg_resources
 
-from pkg_resources import get_distribution
+
 if (sys.version_info.major, sys.version_info.minor) < (3, 6):
     # Can't be tested, as our test harness is using python3.6.
     raise Exception("Requires python3.6+")  # pragma: no cover
 
 
-__version__ = get_distribution('vyper').version
+try:
+	__version__ = pkg_resources.get_distribution('vyper').version
+except pkg_resources.DistributionNotFound:
+	__version__ = 'development'
