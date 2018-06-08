5737a51c1160038960df102e16eb45f7fcd6f91a
Jacques Wagener <jacques@dilectum.co.za>
2018-06-08 18:15:22 +0200

    Bump version: 0.0.4 → 0.1.0-beta.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
     license = f.read()
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-version = '0.0.4'
+version = 'version='0.1.0-beta.1','
 
 setup(
     name='vyper',
