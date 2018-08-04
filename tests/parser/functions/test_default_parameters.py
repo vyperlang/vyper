449c02a87a1b1faafd4032fbbf80b437a4cf2b28
Jacques Wagener <jacques@dilectum.co.za>
2018-08-04 16:46:19 +0200

    Add tests for default parameter setting.

--- tests/parser/functions/test_default_parameters.py
+++ tests/parser/functions/test_default_parameters.py
@@ -41,3 +41,26 @@ def fooBar(a:int128, b: uint256 = 333) -> (int128, uint256):
 
     c = get_contract(code)
     assert c.fooBar(456, 444) == [456, 444]
+    assert c.fooBar(456) == [456, 333]
+
+
+def test_basic_default_param_set_2args(get_contract):
+    code = """
+@public
+def fooBar(a:int128, b: uint256 = 999, c: address = 0x0000000000000000000000000000000000000001) -> (int128, uint256, address):
+    return a, b, c
+    """
+
+    c = get_contract(code)
+    c_default_value = '0x0000000000000000000000000000000000000001'
+    b_default_value = 999
+    addr2 = '0x1000000000000000000000000000000000004321'
+
+    # b default value, c default value
+    assert c.fooBar(123) == [123, b_default_value, c_default_value]
+    # c default_value, b set from param
+    assert c.fooBar(456, 444) == [456, 444, c_default_value]
+    # b default set, c set from param
+    assert c.fooBar(555, addr2) == [555, b_default_value, addr2]
+    # no default values
+    assert c.fooBar(6789, 4567, addr2) == [6789, 4567, addr2]
