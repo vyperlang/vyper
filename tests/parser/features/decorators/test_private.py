e32f4c8f7e1e2132ffdc4d4a7d4c186c00d5941e
Jacques Wagener <jacques@dilectum.co.za>
2018-08-15 11:39:49 +0200

    Add basic private function test.

--- tests/parser/features/decorators/test_private.py
+++ tests/parser/features/decorators/test_private.py
@@ -12,4 +12,21 @@ def returnten() -> int128:
     c = get_contract_with_gas_estimation(private_test_code)
     assert c.returnten() == 10
 
-    print("Passed private function test")
+
+def test_private_with_more_vars(get_contract_with_gas_estimation):
+    private_test_code = """
+@private
+def afunc() -> int128:
+    a: int128 = 1
+    return 444
+
+@public
+def return_it() -> int128:
+    a: int128 = 222
+    b: int128 = 111
+    c: int128 = self.afunc()
+    return a + b + c
+    """
+
+    c = get_contract_with_gas_estimation(private_test_code)
+    assert c.return_it() == 777
