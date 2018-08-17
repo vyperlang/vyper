3c5dca541457440d2288ee795acdced5b12ee73f
Jacques Wagener <jacques@dilectum.co.za>
2018-08-17 15:39:08 +0200

    Add test for nested functions.

--- tests/parser/features/decorators/test_private.py
+++ tests/parser/features/decorators/test_private.py
@@ -28,6 +28,9 @@ def return_it() -> int128:
     a: int128 = 111
     b: int128 = 222
     c: int128 = self.afunc()
+    assert a == 111
+    assert b == 222
+    assert c == 444
     return a + b + c
     """
 
@@ -39,24 +42,27 @@ def test_private_with_more_vars_nested(get_contract_with_gas_estimation):
     private_test_code = """
 @private
 def more() -> int128:
-    a: int128 = 11
-    b: int128 = 100
-    c: int128 = 1
+    a: int128 = 50
+    b: int128 = 50
+    c: int128 = 11
     return a + b + c
 
 @private
 def afunc() -> int128:
-    a: int128 = 1
+    a: int128 = 444
     a += self.more()
-    return 444 + self.more()
+    assert a == 555
+    return  a + self.more()
 
 @public
 def return_it() -> int128:
     a: int128 = 222
     b: int128 = 111
-    c: int128 = self.more()
+    c: int128 = self.afunc()
+    assert a == 222
+    assert b == 111
     return a + b + c
     """
 
     c = get_contract_with_gas_estimation(private_test_code)
-    assert c.return_it() == 444
+    assert c.return_it() == 999
