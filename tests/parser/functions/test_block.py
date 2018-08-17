8ba4aa175dd47038a3bbb75e0c00fd2a0f532dbb
Ben Kaufman <ben.kaufman10@gmail.com>
2018-08-17 13:00:44 +0300

    Fixed blockhash bug and added tests
    Removed ability to use int128 to get blockhash and added tests for getting negative, to far in the past and future blockhash

--- tests/parser/functions/test_block.py
+++ tests/parser/functions/test_block.py
@@ -24,3 +24,31 @@ def previous_blockhash() -> bytes32:
 """
     c = get_contract_with_gas_estimation(block_number_code)
     assert c.prev() == c.previous_blockhash()
+
+    
+def test_negative_blockhash(assert_compile_failed, get_contract_with_gas_estimation):
+    code = """
+@public
+def foo() -> bytes32:
+    return blockhash(-1)
+"""
+    assert_compile_failed(lambda: get_contract_with_gas_estimation(code))
+    
+def test_too_old_blockhash(assert_tx_failed, get_contract_with_gas_estimation, w3):
+    w3.testing.mine(257)
+    code = """
+@public
+def get_50_blockhash() -> bytes32:
+    return blockhash(block.number - 257)
+"""
+    c = get_contract_with_gas_estimation(code)
+    assert_tx_failed(lambda: c.get_50_blockhash())
+    
+def test_non_existing_blockhash(assert_tx_failed, get_contract_with_gas_estimation):
+    code = """
+@public
+def get_future_blockhash() -> bytes32:
+    return blockhash(block.number + 1)
+"""
+    c = get_contract_with_gas_estimation(code)
+    assert_tx_failed(lambda: c.get_future_blockhash())
