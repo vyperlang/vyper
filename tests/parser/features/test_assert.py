01dcca3502b4797573454b54b26f06fd86f09c72
Jacques Wagener <jacques@dilectum.co.za>
2018-09-05 15:19:51 +0200

    Add another test for reason message.

--- tests/parser/features/test_assert.py
+++ tests/parser/features/test_assert.py
@@ -1,3 +1,8 @@
+import pytest
+
+from eth_tester.exceptions import (
+    TransactionFailed
+)
 
 
 def test_assert_refund(w3, get_contract_with_gas_estimation, assert_tx_failed):
@@ -20,10 +25,31 @@ def foo():
 def test_assert_reason(w3, get_contract_with_gas_estimation, assert_tx_failed):
     code = """
 @public
-def test(a: int128):
+def test(a: int128) -> int128:
+    assert a > 1, "larger than one please"
+    return 1 + a
+
+@public
+def test2(a: int128, b: int128) -> int128:
+    c: int128 = 11
     assert a > 1, "a is not large enough"
+    assert b == 1, "b may only be 1"
+    return a + b + c
     """
     c = get_contract_with_gas_estimation(code)
 
-    assert c.test(2) == []
-    assert_tx_failed(lambda: c.test(0))
+    assert c.test(2) == 3
+    with pytest.raises(TransactionFailed) as e_info:
+        c.test(0)
+
+    assert e_info.value.args[0] == b'larger than one please'
+    # a = 0, b = 1
+    with pytest.raises(TransactionFailed) as e_info:
+        c.test2(0, 1)
+    assert e_info.value.args[0] == b'a is not large enough'
+    # a = 1, b = 0
+    with pytest.raises(TransactionFailed) as e_info:
+        c.test2(2, 2)
+    assert e_info.value.args[0] == b'b may only be 1'
+    # return correct value
+    assert c.test2(5, 1) == 17
