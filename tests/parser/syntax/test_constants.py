4ea3710ec5076ee5e6546781ccf421e70124ed35
Jacques Wagener <jacques@dilectum.co.za>
2018-09-24 15:42:34 +0200

    Add check for constants with units.

--- tests/parser/syntax/test_constants.py
+++ tests/parser/syntax/test_constants.py
@@ -2,21 +2,59 @@ import pytest
 from pytest import raises
 
 from vyper import compiler
-from vyper.exceptions import TypeMismatchException
+from vyper.exceptions import (
+    StructureException,
+    TypeMismatchException,
+    VariableDeclarationException,
+)
 
 
 fail_list = [
+    # no value
     """
-VAL: constant(uint256) = -123
+VAL: constant(uint256)
     """,
+    # too many args
+    """
+VAL: constant(uint256, int128) = 12
+    """,
+    # invalid type
+    ("""
+VAL: constant(uint256) = "test"
+    """, TypeMismatchException),
+    # invalid range
+    ("""
+VAL: constant(uint256) = -1
+    """, TypeMismatchException),
+    # reserverd keyword
+    ("""
+wei: constant(uint256) = 1
+    """, VariableDeclarationException),
+    # duplicate constant name
+    ("""
+VAL: constant(uint256) = 11
+VAL: constant(uint256) = 11
+    """, VariableDeclarationException),
+    # bytearray too long.
+    ("""
+VAL: constant(bytes[4]) = "testtest"
+    """, TypeMismatchException),
+    # global with same name
+    ("""
+VAL: constant(bytes[4]) = "t"
+VAL: uint256
+    """, VariableDeclarationException)
 ]
 
 
 @pytest.mark.parametrize('bad_code', fail_list)
 def test_as_wei_fail(bad_code):
-
-    with raises(TypeMismatchException):
-        compiler.compile(bad_code)
+    if isinstance(bad_code, tuple):
+        with raises(bad_code[1]):
+            compiler.compile(bad_code[0])
+    else:
+        with raises(StructureException):
+            compiler.compile(bad_code)
 
 
 valid_list = [
