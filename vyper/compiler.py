90f98f33c9b6dc41f3851397b7fd2db2d4972a54
Jacques Wagener <jacques@dilectum.co.za>
2018-11-16 19:03:20 +0200

    Add new (cleaner), compile interface.

--- vyper/compiler.py
+++ vyper/compiler.py
@@ -1,9 +1,10 @@
 from vyper.parser import parser
 from vyper import compile_lll
 from vyper import optimizer
+from collections import OrderedDict
 
 
-def compile(code, *args, **kwargs):
+def __compile(code, *args, **kwargs):
     lll = optimizer.optimize(parser.parse_tree_to_lll(parser.parse(code), code, runtime_only=kwargs.get('bytecode_runtime', False)))
     asm = compile_lll.compile_to_assembly(lll)
 
@@ -48,3 +49,61 @@ def mk_full_signature(code, *args, **kwargs):
         if func_name in gas_estimates and func_name != '__init__':
             abi[idx]['gas'] = gas_estimates[func_name]
     return abi
+
+
+def get_asm(asm_list):
+    output_string = ''
+    skip_newlines = 0
+    for node in asm_list:
+        if isinstance(node, list):
+            output_string += get_asm(node)
+            continue
+
+        is_push = isinstance(node, str) and node.startswith('PUSH')
+
+        output_string += str(node) + ' '
+        if skip_newlines:
+            skip_newlines -= 1
+        elif is_push:
+            skip_newlines = int(node[4:]) - 1
+        else:
+            output_string += '\n'
+    return output_string
+
+
+def get_source_map(code):
+    asm_list = compile_lll.compile_to_assembly(optimizer.optimize(parser.parse_to_lll(code, runtime_only=True)))
+    c, line_number_map = compile_lll.assembly_to_evm(asm_list)
+    # Sort line_number_map
+    out = OrderedDict()
+    keylist = line_number_map.keys()
+    for k in sorted(keylist):
+        out[k] = line_number_map[k]
+    return out
+
+
+output_formats_map = {
+    'abi': lambda code: mk_full_signature(code),
+    'bytecode': lambda code: '0x' + __compile(code).hex(),
+    'bytecode_runtime': lambda code: '0x' + __compile(code, bytecode_runtime=True).hex(),
+    'ir': lambda code: optimizer.optimize(parser.parse_to_lll(code)),
+    'asm': lambda code: get_asm(compile_lll.compile_to_assembly(optimizer.optimize(parser.parse_to_lll(code)))),
+    'source_map': get_source_map,
+}
+
+
+def compile(codes, output_formats, output_type):
+
+    out = OrderedDict()
+    for contract_name, code in codes.items():
+        for output_format in output_formats:
+            if output_format not in output_formats_map:
+                raise Exception('Unsupported format type %s.' % output_format)
+            out.setdefault(contract_name, {})[output_format] = output_formats_map[output_format](code)
+
+    if output_type == 'list':
+        return [v for v in out.values()]
+    elif output_type == 'dict':
+        return out
+    else:
+        raise Exception('Unknown output_type')
