307bfa634257b9f6ae0de4d2fde04eada133f2a7
Jacob Rockland <jakerockland@gmail.com>
2018-11-28 08:26:53 -0700

    Use equality operator, not identity operator

--- vyper/types/convert.py
+++ vyper/types/convert.py
@@ -31,7 +31,7 @@ from vyper.utils import (
 def to_int128(expr, args, kwargs, context):
     in_node = args[0]
     input_type, _ = get_type(in_node)
-    if input_type is 'bytes32':
+    if input_type == 'bytes32':
         if in_node.typ.is_literal and not SizeLimits.in_bounds('int128', in_node.value):
             raise InvalidLiteralException("Number out of range: {}".format(in_node.value), expr)
         return LLLnode.from_list(
@@ -39,19 +39,19 @@ def to_int128(expr, args, kwargs, context):
                         ['mload', MemoryPositions.MAXNUM]], typ=BaseType('int128', in_node.typ.unit), pos=getpos(expr)
         )
 
-    elif input_type is 'bytes':
+    elif input_type == 'bytes':
         if in_node.typ.maxlen > 32:
             raise InvalidLiteralException("Cannot convert bytes array of max length {} to int128".format(in_node.value), expr)
         return byte_array_to_num(in_node, expr, 'int128')
 
-    elif input_type is 'uint256':
+    elif input_type == 'uint256':
         if in_node.typ.is_literal and not SizeLimits.in_bounds('int128', in_node.value):
             raise InvalidLiteralException("Number out of range: {}".format(in_node.value), expr)
         return LLLnode.from_list(
             ['uclample', in_node, SizeLimits.MAXNUM], typ=BaseType('int128', in_node.typ.unit), pos=getpos(expr)
         )
 
-    elif input_type is 'bool':
+    elif input_type == 'bool':
         return LLLnode.from_list(
             ['clamp', ['mload', MemoryPositions.MINNUM], in_node,
                         ['mload', MemoryPositions.MAXNUM]], typ=BaseType('int128', in_node.typ.unit), pos=getpos(expr)
@@ -79,7 +79,7 @@ def to_uint256(expr, args, kwargs, context):
     elif isinstance(in_node, LLLnode) and input_type in ('bytes32', 'address'):
         return LLLnode(value=in_node.value, args=in_node.args, typ=BaseType('uint256'), pos=getpos(expr))
 
-    elif isinstance(in_node, LLLnode) and input_type is 'bytes':
+    elif isinstance(in_node, LLLnode) and input_type == 'bytes':
         if in_node.typ.maxlen > 32:
             raise InvalidLiteralException("Cannot convert bytes array of max length {} to uint256".format(in_node.value), expr)
         return byte_array_to_num(in_node, expr, 'uint256')
