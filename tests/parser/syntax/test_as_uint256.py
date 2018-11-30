7dd3463d0bce25de46160dd9b6c9e870d203bde1
Jacob Rockland <jakerockland@gmail.com>
2018-11-29 23:33:23 -0700

    Refactored all tests related to current convert() built-in function

--- tests/parser/syntax/test_as_uint256.py
+++ tests/parser/syntax/test_as_uint256.py
@@ -8,7 +8,7 @@ from vyper.exceptions import TypeMismatchException
 fail_list = [
     """
 @public
-def convert2(inp: uint256) -> address:
+def convert2(inp: uint256) -> uint256:
     return convert(inp, bytes32)
     """,
     """
@@ -24,27 +24,3 @@ def test_as_wei_fail(bad_code):
 
     with raises(TypeMismatchException):
         compiler.compile_code(bad_code)
-
-
-valid_list = [
-    """
-@public
-def convert1(inp: bytes32) -> uint256:
-    return convert(inp, uint256)
-    """,
-    """
-@public
-def convert1(inp: bytes32) -> uint256:
-    return convert(inp, uint256)
-    """,
-    """
-@public
-def convert2(inp: uint256) -> bytes32:
-    return convert(inp, bytes32)
-    """
-]
-
-
-@pytest.mark.parametrize('good_code', valid_list)
-def test_as_wei_success(good_code):
-    assert compiler.compile_code(good_code) is not None
