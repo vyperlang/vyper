7018706da16a9be402f28c4d34b8b189c7bfd817
Jacques Wagener <jacques@dilectum.co.za>
2019-02-01 20:06:37 +0200

    Add test for logging string.

--- tests/parser/types/test_string.py
+++ tests/parser/types/test_string.py
@@ -99,3 +99,20 @@ def hithere(name: string[100]) -> string[200]:
     c = get_contract_with_gas_estimation(private_test_code)
     assert c.hithere("bob") == "Hello bob"
     assert c.hithere("alice") == "Hello alice"
+
+
+def test_logging_extended_string(get_contract_with_gas_estimation, get_logs):
+    code = """
+MyLog: event({arg1: int128, arg2: string[64], arg3: int128})
+
+@public
+def foo():
+    log.MyLog(667788, 'hellohellohellohellohellohellohellohellohello', 334455)
+    """
+
+    c = get_contract_with_gas_estimation(code)
+    log = get_logs(c.foo(transact={}), c, 'MyLog')
+
+    assert log[0].args.arg1 == 667788
+    assert log[0].args.arg2 == "hello" * 9
+    assert log[0].args.arg3 == 334455
