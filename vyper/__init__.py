3bfcd9a2be216b65a015ab47f740eb9923c64b90
Charles Cooper <cooper.charles.m@gmail.com>
2019-02-18 16:36:20 -0700

    Handle commit not found

--- vyper/__init__.py
+++ vyper/__init__.py
@@ -9,9 +9,15 @@ if (_sys.version_info.major, _sys.version_info.minor) < (3, 6):
 
 try:
     __version__ = _pkg_resources.get_distribution('vyper').version
-    __version__ += '+commit.'
-    __version__ += _pkg_resources.resource_string('vyper', 'GITVER.txt').decode('utf-8')
 except _pkg_resources.DistributionNotFound:
     __version__ = '0.0.0development'
+
+try:
+    __commit__ = '+commit.'
+    __commit__ += _pkg_resources.resource_string('vyper', 'GITVER.txt').decode('utf-8')
+except FileNotFoundError:
+    __commit__ = '+commit.unknown'
+
+__version__ += __commit__
 
 from vyper.compiler import compile_code, compile_codes  # noqa
