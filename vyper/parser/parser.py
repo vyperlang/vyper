1c09be1b32d568aa6fa22b7cee0e3a4d11734ff4
David Sanders <davesque@gmail.com>
2019-03-13 22:10:44 -0600

    Whitespace

--- vyper/parser/parser.py
+++ vyper/parser/parser.py
@@ -74,11 +74,14 @@ if not hasattr(ast, 'AnnAssign'):
 # Converts code to parse tree
 def parse_to_ast(code):
     class_names, code = pre_parse(code)
+
     if '\x00' in code:
         raise ParserException('No null bytes (\\x00) allowed in the source code.')
+
     o = ast.parse(code)  # python ast
     decorate_ast(o, code, class_names)  # decorated python ast
     o = resolve_negative_literals(o)
+
     return o.body
 
 
