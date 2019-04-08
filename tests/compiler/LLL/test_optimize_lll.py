521bdc1c7a720ce4ec0e2255774523a7113b0c20
Charles Cooper <cooper.charles.m@gmail.com>
2019-04-08 11:42:09 -0700

    Add tests for xor optimization

---
+++ tests/compiler/LLL/test_optimize_lll.py
@@ -0,0 +1,28 @@
+import pytest
+
+from vyper import (
+    compile_lll,
+    optimizer,
+)
+from vyper.parser.parser import (
+    LLLnode,
+)
+from vyper.parser.s_expressions import (
+    parse_s_exp,
+)
+
+optimize_list = [
+    (['ne',1,0], ['ne',1,0]), # noop
+    (['if', ['ne',1,0], 'pass'], ['if', ['xor',1,0], 'pass']),
+    (['assert', ['ne', 1,0]], ['assert', ['xor',1,0]]),
+    (['assert_reason', ['ne',1,0], 0,0], ['assert_reason', ['xor',1,0], 0,0]),
+    (['mstore', 0, ['ne',1,0]], ['mstore',0,['ne',1,0]]), # noop
+]
+
+@pytest.mark.parametrize('lll', optimize_list)
+def test_lll_compile_fail(lll):
+    optimized = optimizer.optimize(LLLnode.from_list(lll[0]))
+    optimized.repr_show_gas = True
+    hand_optimized = LLLnode.from_list(lll[1])
+    hand_optimized.repr_show_gas = True
+    assert optimized == hand_optimized
