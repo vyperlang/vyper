f6605a813c48ecaa8fee09da0ee202cfd184a04a
Jacques Wagener <jacques@dilectum.co.za>
2019-05-24 12:47:44 +0200

    Update assert unreachable gas estimate.

--- vyper/opcodes.py
+++ vyper/opcodes.py
@@ -149,7 +149,7 @@ pseudo_opcodes: Dict[str, List[Optional[int]]] = {
     'CLAMP_NONZERO': [None, 1, 1, 19],
     'ASSERT': [None, 1, 0, 85],
     'ASSERT_REASON': [None, 3, 0, 85],
-    'ASSERT_UNREACHABLE': [None, 1, 0, 85],
+    'ASSERT_UNREACHABLE': [None, 1, 0, 17],
     'PASS': [None, 0, 0, 0],
     'BREAK': [None, 0, 0, 20],
     'CONTINUE': [None, 0, 0, 20],
