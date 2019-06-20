675c0d8e3bbf19fab4eda9719bdf39bf16b9812d
Ben Siraphob <bensiraphob@gmail.com>
2019-06-20 15:41:23 +0700

    Use new method of saving/restoring n locals only if n > 10.

--- vyper/parser/self_call.py
+++ vyper/parser/self_call.py
@@ -92,23 +92,26 @@ def call_self_private(stmt_expr, context, sig):
         push_loop_label = 'save_locals_start' + local_save_ident
         pop_loop_label = 'restore_locals_start' + local_save_ident
 
-        push_local_vars = [
-                ['mstore', i_placeholder, mem_from],
-                ['label', push_loop_label],
-                ['mload', ['mload', i_placeholder]],
-                ['mstore', i_placeholder, ['add', ['mload', i_placeholder], 32]],
-                ['if', ['lt', ['mload', i_placeholder], mem_to],
-                    ['goto', push_loop_label]]
-        ]
-
-        pop_local_vars = [
-            ['mstore', i_placeholder, mem_to - 32],
-            ['label', pop_loop_label],
-            ['mstore', ['mload', i_placeholder], 'pass'],
-            ['mstore', i_placeholder, ['sub', ['mload', i_placeholder], 32]],
-            ['if', ['ge', ['mload', i_placeholder], mem_from],
-                   ['goto', pop_loop_label]]
-        ]
+        if mem_to - mem_from > 320:
+            push_local_vars = [
+                    ['mstore', i_placeholder, mem_from],
+                    ['label', push_loop_label],
+                    ['mload', ['mload', i_placeholder]],
+                    ['mstore', i_placeholder, ['add', ['mload', i_placeholder], 32]],
+                    ['if', ['lt', ['mload', i_placeholder], mem_to],
+                        ['goto', push_loop_label]]
+            ]
+            pop_local_vars = [
+                ['mstore', i_placeholder, mem_to - 32],
+                ['label', pop_loop_label],
+                ['mstore', ['mload', i_placeholder], 'pass'],
+                ['mstore', i_placeholder, ['sub', ['mload', i_placeholder], 32]],
+                ['if', ['ge', ['mload', i_placeholder], mem_from],
+                       ['goto', pop_loop_label]]
+            ]
+        else:
+            push_local_vars = [['mload', pos] for pos in range(mem_from, mem_to, 32)]
+            pop_local_vars = [['mstore', pos, 'pass'] for pos in range(mem_to - 32, mem_from - 32, -32)]
 
     # Push Arguments
     if expr_args:
