d65db2e035a6c5c5667ef1d41f684b2c68a2c9cf
brockelmore <31553173+brockelmore@users.noreply.github.com>
2019-07-08 18:35:06 +0200

    Add support for ListType in external contract call
    Imported ListType from types and added case for get_external_contract_call_output
    
    Note: I don't know the codebase well enough to make sure this makes sense. Just submitting this PR in case it really is this simple. Someone more knowledgeable should make any necessary security checks.

--- vyper/parser/external_call.py
+++ vyper/parser/external_call.py
@@ -17,6 +17,7 @@ from vyper.types import (
     BaseType,
     ByteArrayLike,
     TupleLike,
+    ListType,
     get_size_of_type,
 )
 
@@ -99,6 +100,8 @@ def get_external_contract_call_output(sig, context):
     elif isinstance(sig.output_type, ByteArrayLike):
         returner = [0, output_placeholder + 32]
     elif isinstance(sig.output_type, TupleLike):
+        returner = [0, output_placeholder]
+    elif isinstance(sig.output_type, ListType):
         returner = [0, output_placeholder]
     else:
         raise TypeMismatchException("Invalid output type: %s" % sig.output_type)
