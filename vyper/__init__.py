3cacc8c324dafbded2ba93ef3024c91eb838fe22
Bryant Eisenbach <fubuloubu@gmail.com>
2019-07-16 19:04:34 -0400

    refactor: Added extended version info
    changes how --version flag displays to include hash
    of all files in the vyper package
    
    fixes: #1526

--- vyper/__init__.py
+++ vyper/__init__.py
@@ -1,3 +1,8 @@
+import hashlib as _hashlib
+import os as _os
+from pathlib import (
+    Path as _Path,
+)
 import sys as _sys
 
 import pkg_resources as _pkg_resources
@@ -12,7 +17,25 @@ if (_sys.version_info.major, _sys.version_info.minor) < (3, 6):
     raise Exception("Requires python3.6+")  # pragma: no cover
 
 
+def _get_version_hash(root: _Path) -> str:
+    m = _hashlib.sha1()
+    for dirname, _, file_list in _os.walk(root):
+        for fname in file_list:
+            if fname[-3:] == '.py':
+                with open(_Path(dirname).joinpath(fname), 'rb') as f:
+                    m.update(f.read())
+    return m.digest().hex()
+
+
 try:
     __version__ = _pkg_resources.get_distribution('vyper').version
 except _pkg_resources.DistributionNotFound:
     __version__ = '0.0.0development'
+
+
+# Append first 5 chars of sha1 hash of all files in package directory
+# NOTE: First 5 chars used so as not to confuse with git commit hash
+# NOTE: This should only be used a sanity check, please check git commits
+#       if unsure of version correctness
+extended_version = '{}+sha1.{}'.format(__version__,
+                                       _get_version_hash(_Path(__file__).parent)[:5])
