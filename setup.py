8663ac5f6021a70f939fc22372c74b3b1c63785b
Bryant Eisenbach <fubuloubu@gmail.com>
2019-08-27 15:36:00 -0700

    Bump version: 0.1.0-beta.11 → 0.1.0-beta.12

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ except subprocess.CalledProcessError:
 setup(
     name='vyper',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.1.0-beta.11',
+    version='0.1.0-beta.12',
     description='Vyper Programming Language for Ethereum',
     long_description_markdown_filename='README.md',
     author='Vitalik Buterin',
