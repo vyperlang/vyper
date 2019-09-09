46da0456704807b520ffbe11475cd4c94ef19bce
tintinweb <tintinweb@oststrom.com>
2019-09-09 10:59:33 +0200

    make linter happy

--- vyper/__main__.py
+++ vyper/__main__.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 import sys
+
 from vyper.cli import (
     vyper_compile,
     vyper_lll,
-    vyper_serve
+    vyper_serve,
 )
 
 if __name__ == "__main__":
 
-    allowed_subcommands = {
-        "--vyper-compile": vyper_compile,
-        "--vyper-lll": vyper_lll,
-        "--vyper-serve": vyper_serve
-    }
+    allowed_subcommands = ("--vyper-compile", "--vyper-lll", "--vyper-serve")
 
-    if not len(sys.argv) > 1 or sys.argv[1] not in allowed_subcommands.keys():
+    if not len(sys.argv) > 1 or sys.argv[1] not in allowed_subcommands:
         # default (no args, no switch in first arg): run vyper_compile
         vyper_compile._parse_cli_args()
     else:
         # pop switch and forward args to subcommand
-        allowed_subcommands.get(sys.argv.pop(1), vyper_compile)._parse_cli_args()
+        subcommand = sys.argv.pop(1)
+        if subcommand == "--vyper-serve":
+            vyper_serve._parse_cli_args()
+        elif subcommand == "--vyper-lll":
+            vyper_lll._parse_cli_args()
+        else:
+            vyper_compile._parse_cli_args()
