d3ec947a1b09c82a0803e7ff7278f488b4b9d80a
wschwab <31592931+wschwab@users.noreply.github.com>
2019-09-23 16:08:37 +0200

    Apply suggestions from code review

--- vyper/types/convert.py
+++ vyper/types/convert.py
@@ -344,7 +344,7 @@ def to_bytes32(expr, args, kwargs, context):
     if input_type == 'bytes':
         if _len > 32:
             raise TypeMismatchException(
-                f"Unable to convert bytes[{len}] to bytes32, max length is too "
+                f"Unable to convert bytes[{_len}] to bytes32, max length is too "
                 "large."
             )
 
