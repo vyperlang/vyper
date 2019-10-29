533498f389ff7bf0c989ba03c94f7aa617425e11
Charles Cooper <cooper.charles.m@gmail.com>
2019-10-29 18:57:01 +0100

    Force parse_body to be zerovalent
    Seq will issue a pop after every univalent statement but the last one.
    However, if and for statements should have zerovalent bodies, so this
    forces even the last statement to get a pop if it is univalent.

--- vyper/parser/stmt.py
+++ vyper/parser/stmt.py
@@ -1012,8 +1012,10 @@ def parse_stmt(stmt, context):
 def parse_body(code, context):
     if not isinstance(code, list):
         return parse_stmt(code, context)
-    o = []
+
+    o = ['seq']
     for stmt in code:
         lll = parse_stmt(stmt, context)
         o.append(lll)
-    return LLLnode.from_list(['seq'] + o, pos=getpos(code[0]) if code else None)
+    o.append('pass')  # force zerovalent, even last statement
+    return LLLnode.from_list(o, pos=getpos(code[0]) if code else None)
