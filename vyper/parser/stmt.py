9bc4145f31419ccfaf5bb5cb7dd07aad436732ca
Charles Cooper <cooper.charles.m@gmail.com>
2019-10-30 11:47:33 +0100

    remove redundant seq in if stmt generation

--- vyper/parser/stmt.py
+++ vyper/parser/stmt.py
@@ -334,7 +334,7 @@ class Stmt(object):
         if self.stmt.orelse:
             block_scope_id = id(self.stmt.orelse)
             with self.context.make_blockscope(block_scope_id):
-                add_on = [['seq', parse_body(self.stmt.orelse, self.context)]]
+                add_on = [parse_body(self.stmt.orelse, self.context)]
         else:
             add_on = []
 
@@ -345,7 +345,7 @@ class Stmt(object):
             if not self.is_bool_expr(test_expr):
                 raise TypeMismatchException('Only boolean expressions allowed', self.stmt.test)
             body = ['if', test_expr,
-                    ['seq', parse_body(self.stmt.body, self.context)]] + add_on
+                    parse_body(self.stmt.body, self.context)] + add_on
             o = LLLnode.from_list(
                 body,
                 typ=None, pos=getpos(self.stmt)
