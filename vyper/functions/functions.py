294dff578c2842df5e2083e93c47c3c58fbde610
Charles Cooper <cooper.charles.m@gmail.com>
2019-11-12 13:07:39 +0100

    Implement clear(<type>) syntax
    ```
    xs: uint256[10] = clear(uint256[10]) # initializes xs with zeroes
    
    xs[0] = 1
    xs = clear(uint256[10]) # resets xs to zero
    
    clear(xs) # resets xs to zero
    ```

--- vyper/functions/functions.py
+++ vyper/functions/functions.py
@@ -33,6 +33,7 @@ from vyper.types import (
     ByteArrayLike,
     ByteArrayType,
     ListType,
+    NullType,
     StringType,
     TupleType,
     are_units_compatible,
@@ -1266,12 +1267,11 @@ else:
     )
 
 
-def _clear():
-    raise ParserException(
-        "This function should never be called! `clear()` is currently handled "
-        "differently than other functions as it self modifies its input argument "
-        "statement. Please see `_clear()` in `stmt.py`"
-    )
+def clear(expr, context):
+    if len(expr.args) != 1:
+        raise ParserException('function expects two parameters.', expr)
+    output_type = context.parse_type(expr.args[0], expr.args[0].value.id)
+    return LLLnode(None, typ=NullType(output_type), pos=getpos(expr))
 
 
 dispatch_table = {
@@ -1305,11 +1305,11 @@ dispatch_table = {
     'create_forwarder_to': create_forwarder_to,
     'min': _min,
     'max': _max,
+    'clear': clear,
 }
 
 stmt_dispatch_table = {
     'assert_modifiable': assert_modifiable,
-    'clear': _clear,
     'send': send,
     'selfdestruct': selfdestruct,
     'raw_call': raw_call,
