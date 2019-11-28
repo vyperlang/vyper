74f805cc20ed8673be515f5d0632732817d0db3a
Bryant Eisenbach <fubuloubu@gmail.com>
2019-11-28 17:21:55 -0500

    test: Tests can raise either internal or Python syntax errors
    Note: Sort in pytest decorator instead

--- tests/parser/types/test_identifier_naming.py
+++ tests/parser/types/test_identifier_naming.py
@@ -2,6 +2,7 @@ import pytest
 
 from vyper.exceptions import (
     FunctionDeclarationException,
+    PythonSyntaxException,
     VariableDeclarationException,
 )
 from vyper.functions import (
@@ -15,45 +16,49 @@ from vyper.utils import (
     RESERVED_KEYWORDS,
 )
 
-ALL_RESERVED_KEYWORDS = sorted(
-                                set(BUILTIN_CONSTANTS.keys())
-                                .union(BUILTIN_FUNCTIONS)
-                                .union(RESERVED_KEYWORDS)
-                                .union(ENVIRONMENT_VARIABLES)
-                            )
+ALL_RESERVED_KEYWORDS = (
+                            set(BUILTIN_CONSTANTS.keys())
+                            .union(BUILTIN_FUNCTIONS)
+                            .union(RESERVED_KEYWORDS)
+                            .union(ENVIRONMENT_VARIABLES)
+                        )
 
 
-@pytest.mark.parametrize('constant', ALL_RESERVED_KEYWORDS)
+@pytest.mark.parametrize('constant', sorted(ALL_RESERVED_KEYWORDS))
 def test_reserved_keywords_memory(constant, get_contract, assert_compile_failed):
     code = f"""
 @public
 def test():
     {constant}: int128 = 31337
     """
-    assert_compile_failed(lambda: get_contract(code), VariableDeclarationException)
+    assert_compile_failed(lambda: get_contract(code),
+                          (PythonSyntaxException, VariableDeclarationException))
 
 
-@pytest.mark.parametrize('constant', ALL_RESERVED_KEYWORDS)
+@pytest.mark.parametrize('constant', sorted(ALL_RESERVED_KEYWORDS))
 def test_reserved_keywords_storage(constant, get_contract, assert_compile_failed):
     code = f"{constant}: int128"
-    assert_compile_failed(lambda: get_contract(code), VariableDeclarationException)
+    assert_compile_failed(lambda: get_contract(code),
+                          (PythonSyntaxException, VariableDeclarationException))
 
 
-@pytest.mark.parametrize('constant', ALL_RESERVED_KEYWORDS)
+@pytest.mark.parametrize('constant', sorted(ALL_RESERVED_KEYWORDS))
 def test_reserved_keywords_fn_args(constant, get_contract, assert_compile_failed):
     code = f"""
 @public
 def test({constant}: int128):
     pass
     """
-    assert_compile_failed(lambda: get_contract(code), FunctionDeclarationException)
+    assert_compile_failed(lambda: get_contract(code),
+                          (PythonSyntaxException, FunctionDeclarationException))
 
 
-@pytest.mark.parametrize('constant', ALL_RESERVED_KEYWORDS)
+@pytest.mark.parametrize('constant', sorted(ALL_RESERVED_KEYWORDS))
 def test_reserved_keywords_fns(constant, get_contract, assert_compile_failed):
     code = f"""
 @public
 def {constant}(var: int128):
     pass
     """
-    assert_compile_failed(lambda: get_contract(code), FunctionDeclarationException)
+    assert_compile_failed(lambda: get_contract(code),
+                          (PythonSyntaxException, FunctionDeclarationException))
