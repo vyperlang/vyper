7c65e953c6f8218fc69c881f2bf3aef30267e089
Bryant Eisenbach <fubuloubu@gmail.com>
2019-11-29 10:54:56 -0500

    lint: Not even once

--- vyper/optimization/__init__.py
+++ vyper/optimization/__init__.py
@@ -1,5 +1,8 @@
-from .ast_optimizer import optimize as optimize_ast
-from .lll_optimizer import optimize as optimize_lll
-
+from .ast_optimizer import (
+    optimize as optimize_ast,
+)
+from .lll_optimizer import (
+    optimize as optimize_lll,
+)
 
 __all__ = ['optimize_ast', 'optimize_lll']
