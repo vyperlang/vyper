0537f51677ad49aaf54b53b43c78f4f86d33f29f
Charles Cooper <cooper.charles.m@gmail.com>
2019-12-06 11:55:15 -0800

    Fix overspecificity in get_min_val_for_typ

--- tests/parser/functions/test_unary.py
+++ tests/parser/functions/test_unary.py
@@ -1,3 +1,7 @@
+from decimal import (
+    Decimal,
+)
+
 import pytest
 
 from vyper.exceptions import (
@@ -27,6 +31,19 @@ def negate(a: int128) -> int128:
 def test_unary_sub_int128_pass(get_contract, val):
     code = """@public
 def negate(a: int128) -> int128:
+    return -(a)
+    """
+    c = get_contract(code)
+    assert c.negate(val) == -val
+
+
+decimal_divisor = Decimal('1e10')
+min_decimal = (-2**127 + 1) / decimal_divisor
+max_decimal = (2**127 - 1) / decimal_divisor
+@pytest.mark.parametrize("val", [min_decimal, 0, max_decimal])
+def test_unary_sub_decimal_pass(get_contract, val):
+    code = """@public
+def negate(a: decimal) -> decimal:
     return -(a)
     """
     c = get_contract(code)
