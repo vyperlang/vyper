fe8b410e91fd616355287e710dffe8e93c69cd47
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-12-24 01:19:59 +0400

    bump asttokens version to 2.0.3

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     python_requires='>=3.6',
     py_modules=['vyper'],
     install_requires=[
-        'asttokens==1.1.13',
+        'asttokens==2.0.3',
         'pycryptodome>=3.5.1,<4',
     ],
     setup_requires=[
