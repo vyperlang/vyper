9bb91029de355640e37083297ee85ce53a2b2a3b
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2020-02-12 20:20:12 +0400

    merge NodeTransformer classes

--- vyper/ast/annotation.py
+++ vyper/ast/annotation.py
@@ -18,7 +18,13 @@ class AnnotatingVisitor(python_ast.NodeTransformer):
     _source_code: str
     _class_types: ClassTypes
 
-    def __init__(self, source_code: str, class_types: Optional[ClassTypes] = None):
+    def __init__(
+        self,
+        source_code: str,
+        class_types: Optional[ClassTypes] = None,
+        source_id: int = 0,
+    ):
+        self._source_id = source_id
         self._source_code: str = source_code
         self.counter: int = 0
         if class_types is not None:
@@ -27,12 +33,25 @@ class AnnotatingVisitor(python_ast.NodeTransformer):
             self._class_types = {}
 
     def generic_visit(self, node):
-        # Decorate every node in the AST with the original source code. This is
-        # necessary to facilitate error pretty-printing.
+        # Decorate every node with the original source code to allow pretty-printing errors
         node.source_code = self._source_code
         node.node_id = self.counter
         node.ast_type = node.__class__.__name__
         self.counter += 1
+
+        # Decorate every node with source end offsets
+        start = node.first_token.start if hasattr(node, "first_token") else (None, None)
+        end = node.last_token.end if hasattr(node, "last_token") else (None, None)
+
+        node.lineno = start[0]
+        node.col_offset = start[1]
+        node.end_lineno = end[0]
+        node.end_col_offset = end[1]
+
+        if hasattr(node, "last_token"):
+            start_pos = node.first_token.startpos
+            end_pos = node.last_token.endpos
+            node.src = f"{start_pos}:{end_pos-start_pos}:{self._source_id}"
 
         return super().generic_visit(node)
 
@@ -61,39 +80,17 @@ class AnnotatingVisitor(python_ast.NodeTransformer):
 
         return node
 
-
-class RewriteUnarySubVisitor(python_ast.NodeTransformer):
     def visit_UnaryOp(self, node):
         self.generic_visit(node)
-        if isinstance(node.op, python_ast.USub) and isinstance(node.operand, python_ast.Num):
+        # NOTE: This is done so that decimal literal now sees the negative sign as part of it
+        if isinstance(node.op, python_ast.USub) and isinstance(
+            node.operand, python_ast.Num
+        ):
             node.operand.n = 0 - node.operand.n
-            # NOTE: This is done so that decimal literal now sees the negative sign as part of it
             node.operand.col_offset = node.col_offset
             return node.operand
         else:
             return node
-
-
-class AddSourceOffsetVisitor(python_ast.NodeTransformer):
-
-    def __init__(self, source_id):
-        self._source_id = source_id
-
-    def generic_visit(self, node):
-        start = node.first_token.start if hasattr(node, 'first_token') else (None, None)
-        end = node.last_token.end if hasattr(node, 'last_token') else (None, None)
-
-        node.lineno = start[0]
-        node.col_offset = start[1]
-        node.end_lineno = end[0]
-        node.end_col_offset = end[1]
-
-        if hasattr(node, 'last_token'):
-            start_pos = node.first_token.startpos
-            end_pos = node.last_token.endpos
-            node.src = f"{start_pos}:{end_pos-start_pos}:{self._source_id}"
-
-        return super().generic_visit(node)
 
 
 def annotate_python_ast(
@@ -117,8 +114,6 @@ def annotate_python_ast(
     :param class_types: A mapping of class names to original class types.
     :return: The annotated and optmized AST.
     """
-    AnnotatingVisitor(source_code, class_types).visit(parsed_ast)
-    RewriteUnarySubVisitor().visit(parsed_ast)
 
     asttokens.ASTTokens(source_code, tree=parsed_ast)
-    AddSourceOffsetVisitor(source_id).visit(parsed_ast)
+    AnnotatingVisitor(source_code, class_types, source_id).visit(parsed_ast)
