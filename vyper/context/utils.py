1f0e0c245c5487c8da013b30c78596d54a3f5da7
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2020-02-23 14:03:34 +0400

    add compare_types method

--- vyper/context/utils.py
+++ vyper/context/utils.py
@@ -9,6 +9,7 @@ from vyper import (
 from vyper.exceptions import (
     InvalidLiteralException,
     StructureException,
+    TypeMismatchException,
 )
 
 
@@ -47,3 +48,54 @@ def check_numeric_bounds(type_str: str, node: vy_ast.Num) -> bool:
         raise InvalidLiteralException(f"Value is below lower bound for given type ({lower})", node)
     if value > upper:
         raise InvalidLiteralException(f"Value exceeds upper bound for given type ({upper})", node)
+
+
+def compare_types(left, right, node):
+    """
+    Compares types.
+
+    Types may be given as a single type object, a Constant ast node, or a sequence
+    containing types and/or constants.
+
+    Arguments
+    ---------
+    left : _BaseType | Constant | Sequence
+        The left side of the comparison.
+    right : _BaseType | Constant | Sequence
+        The right side of the comparison.
+    node
+        The node where the comparison is taking place (for source highlights if
+        an exception is raised).
+    """
+    if isinstance(right, (list, tuple)):
+        if not hasattr(left, '__len__'):
+            raise TypeMismatchException(
+                f"Cannot perform array operation against base type: {left}", node
+            )
+        if len(left) != len(right):
+            raise TypeMismatchException(
+                f"Invalid length for operation, expected {len(left)} got {len(right)}", node
+            )
+        for i in range(len(right)):
+            compare_types(left[i], right[i], node)
+        return
+
+    literals = [i for i in (left, right) if isinstance(i, vy_ast.Constant)]
+    types = [i for i in (left, right) if i not in literals]
+
+    if not types:
+        if type(node.left) != type(node.right):  # NOQA: E721
+            raise TypeMismatchException(
+                "Cannot perform operation between "
+                f"{node.left.ast_type} and {node.right.ast_type}",
+                node
+            )
+
+    elif len(types) == 1:
+        types[0].validate_literal(literals[0])
+
+    else:
+        if left != right:
+            raise TypeMismatchException(
+                f"Cannot perform operation between {left} and {right}", node
+            )
