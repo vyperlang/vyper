6e7adeed7899a9a2e0bf56ffbe8e62b6809c4ed0
Ben Hauser <ben@hauser.id>
2020-05-06 22:11:24 +0400

    test: add test cases for keccak256 and sha256 builtins

---
+++ tests/functions/folding/test_keccak_sha.py
@@ -0,0 +1,73 @@
+from hypothesis import (
+    given,
+    settings,
+    strategies as st,
+)
+import pytest
+
+from vyper import (
+    ast as vy_ast,
+    functions as vy_fn,
+)
+
+alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&()*+,-./:;<=>?@[]^_`{|}~'  # NOQA: E501
+
+
+@pytest.mark.fuzzing
+@given(value=st.text(alphabet=alphabet, min_size=0, max_size=100))
+@settings(max_examples=50, deadline=1000)
+@pytest.mark.parametrize('fn_name', ['keccak256', 'sha256'])
+def test_string(get_contract, value, fn_name):
+
+    source = f"""
+@public
+def foo(a: string[100]) -> bytes32:
+    return {fn_name}(a)
+    """
+    contract = get_contract(source)
+
+    vyper_ast = vy_ast.parse_to_ast(f"{fn_name}('''{value}''')")
+    old_node = vyper_ast.body[0].value
+    new_node = vy_fn.DISPATCH_TABLE[fn_name].evaluate(old_node)
+
+    assert contract.foo(value) == new_node.value
+
+
+@pytest.mark.fuzzing
+@given(value=st.binary(min_size=0, max_size=100))
+@settings(max_examples=50, deadline=1000)
+@pytest.mark.parametrize('fn_name', ['keccak256', 'sha256'])
+def test_bytes(get_contract, value, fn_name):
+    source = f"""
+@public
+def foo(a: bytes[100]) -> bytes32:
+    return {fn_name}(a)
+    """
+    contract = get_contract(source)
+
+    vyper_ast = vy_ast.parse_to_ast(f"{fn_name}({value})")
+    old_node = vyper_ast.body[0].value
+    new_node = vy_fn.DISPATCH_TABLE[fn_name].evaluate(old_node)
+
+    assert contract.foo(value) == new_node.value
+
+
+@pytest.mark.fuzzing
+@given(value=st.binary(min_size=1, max_size=100))
+@settings(max_examples=50, deadline=1000)
+@pytest.mark.parametrize('fn_name', ['keccak256', 'sha256'])
+def test_hex(get_contract, value, fn_name):
+    source = f"""
+@public
+def foo(a: bytes[100]) -> bytes32:
+    return {fn_name}(a)
+    """
+    contract = get_contract(source)
+
+    value = f"0x{value.hex()}"
+
+    vyper_ast = vy_ast.parse_to_ast(f"{fn_name}({value})")
+    old_node = vyper_ast.body[0].value
+    new_node = vy_fn.DISPATCH_TABLE[fn_name].evaluate(old_node)
+
+    assert contract.foo(value) == new_node.value
