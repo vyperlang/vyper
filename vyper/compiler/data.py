24b63dd8304b458b634e6f2bc26917d5fd578d57
Ben Hauser <ben@hauser.id>
2020-05-11 14:09:52 +0400

    doc: add docstrings and typing for compiler/data

--- vyper/compiler/data.py
+++ vyper/compiler/data.py
@@ -1,8 +1,8 @@
-import warnings
 from typing import (
     Optional,
     Tuple,
 )
+import warnings
 
 from vyper import (
     ast as vy_ast,
@@ -21,79 +21,187 @@ from vyper.typing import (
 
 
 class CompilerData:
-    def __init__(self, contract_name, source_code, interface_codes, source_id):
+    """
+    Object for fetching and storing compiler data for a Vyper contract.
+
+    This object acts as a wrapper over the pure compiler functions, triggering
+    compilation phases as needed and providing the data for use when generating
+    the final compiler outputs.
+
+    Attributes
+    ----------
+    vyper_ast : vy_ast.Module
+        Top-level Vyper AST node
+    global_ctx : GlobalContext
+        Sorted, contextualized representation of the Vyper AST
+    lll_nodes : LLLnode
+        LLL used to generate deployment bytecode
+    lll_runtime : LLLnode
+        LLL used to generate runtime bytecode
+    assembly : list
+        Assembly instructions for deployment bytecode
+    assembly_runtime : list
+        Assembly instructions for runtime bytecode
+    bytecode : bytes
+        Deployment bytecode
+    bytecode_runtime : bytes
+        Runtime bytecode
+    """
+    def __init__(
+        self,
+        contract_name: str,
+        source_code: str,
+        interface_codes: Optional[InterfaceImports],
+        source_id: int
+    ) -> None:
+        """
+        Initialization method.
+
+        Arguments
+        ---------
+        contract_name : str
+            The name of the contract being compiled.
+        source_code : str
+            Vyper source code.
+        interface_codes: Dict, optional
+            Interfaces that may be imported by the contracts during compilation.
+            * Formatted as as `{'interface name': {'type': "json/vyper", 'code': "interface code"}}`
+            * JSON interfaces are given as lists, vyper interfaces as strings
+        source_id : int
+            ID number used to identify this contract in the source map.
+        """
         self.contract_name = contract_name
         self.source_code = source_code
         self.interface_codes = interface_codes
         self.source_id = source_id
 
     @property
-    def vyper_ast(self):
+    def vyper_ast(self) -> vy_ast.Module:
         if not hasattr(self, "_vyper_ast"):
             self._vyper_ast = generate_ast(self.source_code, self.source_id)
+
         return self._vyper_ast
 
     @property
-    def global_ctx(self):
+    def global_ctx(self) -> GlobalContext:
         if not hasattr(self, "_global_ctx"):
-            self._global_ctx = generate_global_context(self.interface_codes, self.vyper_ast)
+            self._global_ctx = generate_global_context(self.vyper_ast, self.interface_codes)
+
         return self._global_ctx
 
-    def _gen_lll(self):
+    def _gen_lll(self) -> None:
+        # fetch both deployment and runtime LLL
         self._lll_nodes, self._lll_runtime = generate_lll_nodes(
             self.source_code, self.global_ctx
         )
 
     @property
-    def lll_nodes(self):
+    def lll_nodes(self) -> parser.LLLnode:
         if not hasattr(self, "_lll_nodes"):
             self._gen_lll()
         return self._lll_nodes
 
     @property
-    def lll_runtime(self):
+    def lll_runtime(self) -> parser.LLLnode:
         if not hasattr(self, "_lll_runtime"):
             self._gen_lll()
         return self._lll_runtime
 
     @property
-    def assembly(self):
+    def assembly(self) -> list:
         if not hasattr(self, "_assembly"):
             self._assembly = generate_assembly(self.lll_nodes)
         return self._assembly
 
     @property
-    def assembly_runtime(self):
+    def assembly_runtime(self) -> list:
         if not hasattr(self, "_assembly_runtime"):
             self._assembly_runtime = generate_assembly(self.lll_runtime)
         return self._assembly_runtime
 
     @property
-    def bytecode(self):
+    def bytecode(self) -> bytes:
         if not hasattr(self, "_bytecode"):
             self._bytecode = generate_bytecode(self.assembly)
         return self._bytecode
 
     @property
-    def bytecode_runtime(self):
+    def bytecode_runtime(self) -> bytes:
         if not hasattr(self, "_bytecode_runtime"):
             self._bytecode_runtime = generate_bytecode(self.assembly_runtime)
         return self._bytecode_runtime
 
 
 def generate_ast(source_code: str, source_id: int) -> vy_ast.Module:
+    """
+    Generate a Vyper AST from source code.
+
+    This is the first phase of compilation.
+
+    Arguments
+    ---------
+    source_code : str
+        Vyper source code.
+    source_id : int
+        ID number used to identify this contract in the source map.
+
+    Returns
+    -------
+    vy_ast.Module
+        Top-level Vyper AST node
+    """
     return vy_ast.parse_to_ast(source_code, source_id)
 
 
 def generate_global_context(
-    interface_codes: Optional[InterfaceImports], vyper_ast: vy_ast.Module
+    vyper_ast_node: vy_ast.Module, interface_codes: Optional[InterfaceImports],
 ) -> GlobalContext:
-    return GlobalContext.get_global_context(vyper_ast, interface_codes=interface_codes)
+    """
+    Generate a contextualized AST from the Vyper AST.
+
+    This is the second phase of compilation.
+
+    Arguments
+    ---------
+    vyper_ast_node : vy_ast.Module
+        Top-level Vyper AST node
+    interface_codes: Dict, optional
+        Interfaces that may be imported by the contracts.
+
+    Returns
+    -------
+    GlobalContext
+        Sorted, contextualized representation of the Vyper AST
+    """
+    return GlobalContext.get_global_context(vyper_ast_node, interface_codes=interface_codes)
 
 
 def generate_lll_nodes(
     source_code: str, global_ctx: GlobalContext
 ) -> Tuple[parser.LLLnode, parser.LLLnode]:
+    """
+    Generate the intermediate representation (LLL) from the contextualized AST.
+
+    This is the third phase of compilation. It also includes LLL-level
+    optimizations.
+
+    This function returns two values, one for generating deployment bytecode and
+    the other for generating runtime bytecode. The remaining compilation phases
+    may be called with either value, depending on the desired final output.
+
+    Arguments
+    ---------
+    source_code : str
+        Vyper source code.
+    global_ctx : GlobalContext
+        Contextualized Vyper AST
+
+    Returns
+    -------
+    (LLLnode, LLLnode)
+        LLL to generate deployment bytecode
+        LLL to generate runtime bytecode
+    """
     lll_nodes, lll_runtime = parser.parse_tree_to_lll(source_code, global_ctx)
     lll_nodes = optimizer.optimize(lll_nodes)
     lll_runtime = optimizer.optimize(lll_runtime)
@@ -101,6 +209,21 @@ def generate_lll_nodes(
 
 
 def generate_assembly(lll_nodes: parser.LLLnode) -> list:
+    """
+    Generate assembly instructions from LLL.
+
+    This is the fourth phase of compilation.
+
+    Arguments
+    ---------
+    lll_nodes : str
+        Top-level LLL nodes. Can be deployment or runtime LLL.
+
+    Returns
+    -------
+    list
+        List of assembly instructions.
+    """
     assembly = compile_lll.compile_to_assembly(lll_nodes)
     if _find_nested_opcode(assembly, "DEBUG"):
         warnings.warn(
@@ -119,4 +242,19 @@ def _find_nested_opcode(assembly, key):
 
 
 def generate_bytecode(assembly: list) -> bytes:
+    """
+    Generate bytecode fro assembly instructions.
+
+    This is the fifth and final phase of compilation.
+
+    Arguments
+    ---------
+    assembly : list
+        Assembly instructions. Can be deployment or runtime assembly.
+
+    Returns
+    -------
+    bytes
+        Final compiled bytecode.
+    """
     return compile_lll.assembly_to_evm(assembly)[0]
