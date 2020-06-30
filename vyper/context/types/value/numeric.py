fa10fbd28c61e11abe6d5bc8c4e040695dc90aa7
Ben Hauser <ben@hauser.id>
2020-06-30 15:19:11 +0400

    feat: add always-overflows checks for exponentiation

--- vyper/context/types/value/numeric.py
+++ vyper/context/types/value/numeric.py
@@ -5,6 +5,7 @@ from vyper.context.types.abstract import FixedAbstractType, IntegerAbstractType
 from vyper.context.types.bases import (
     BasePrimitive,
     BaseTypeDefinition,
+    CompilerPanic,
     ValueTypeDefinition,
 )
 from vyper.exceptions import InvalidOperation, OverflowException
@@ -16,12 +17,18 @@ class _NumericDefinition(ValueTypeDefinition):
 
     Attributes
     ----------
+    _bits : int
+        Number of bits the value occupies in memory
+    _is_signed : bool
+        Is the value signed?
     _invalid_op : VyperNode, optional
         Vyper ast node, or list of nodes, that is not a valid operator for binary
         operations on this type.
     """
 
     _invalid_op: Optional[Type[vy_ast.VyperNode]] = None
+    _bits: int
+    _is_signed: bool
 
     def validate_numeric_op(
         self, node: Union[vy_ast.UnaryOp, vy_ast.BinOp, vy_ast.AugAssign]
@@ -29,9 +36,62 @@ class _NumericDefinition(ValueTypeDefinition):
         if self._invalid_op and isinstance(node.op, self._invalid_op):
             raise InvalidOperation(f"Cannot perform {node.op.description} on {self}", node)
 
+        if isinstance(node.op, vy_ast.Pow):
+            if isinstance(node, vy_ast.BinOp):
+                left, right = node.left, node.right
+            elif isinstance(node, vy_ast.AugAssign):
+                left, right = node.target, node.value
+            else:
+                raise CompilerPanic(f"Unexpected node type for numeric op: {type(node).__name__}")
+
+            value_bits = self._bits - (1 if self._is_signed else 0)
+
+            # constant folding ensures one of `(left, right)` is never a literal
+            if isinstance(left, vy_ast.Int):
+                if left.value >= 2 ** value_bits:
+                    raise OverflowException(
+                        "Base is too large, calculation will always overflow", left
+                    )
+                elif left.value < -(2 ** value_bits):
+                    raise OverflowException(
+                        "Base is too small, calculation will always underflow", left
+                    )
+
+            elif isinstance(right, vy_ast.Int):
+                if right.value < 0:
+                    raise InvalidOperation("Cannot calculate a negative power", right)
+                if right.value > value_bits:
+                    raise OverflowException(
+                        "Power is too large, calculation will always overflow", right
+                    )
+
     def validate_comparator(self, node: vy_ast.Compare) -> None:
         # all comparators are valid on numeric types
         return
+
+
+class _SignedIntegerDefinition(_NumericDefinition):
+    """
+    Private base class for signed integer definitions.
+    """
+
+    _is_signed = True
+
+    @property
+    def _id(self):
+        return f"int{self._bits}"
+
+
+class _UnsignedIntegerDefinition(_NumericDefinition):
+    """
+    Private base class for unsigned integer definitions.
+    """
+
+    _is_signed = False
+
+    @property
+    def _id(self):
+        return f"uint{self._bits}"
 
 
 class _NumericPrimitive(BasePrimitive):
@@ -53,17 +113,19 @@ class _NumericPrimitive(BasePrimitive):
 # definitions
 
 
-class Int128Definition(IntegerAbstractType, _NumericDefinition):
-    _id = "int128"
+class Int128Definition(IntegerAbstractType, _SignedIntegerDefinition):
+    _bits = 128
 
 
-class Uint256Definition(IntegerAbstractType, _NumericDefinition):
-    _id = "uint256"
+class Uint256Definition(IntegerAbstractType, _UnsignedIntegerDefinition):
+    _bits = 256
     _invalid_op = vy_ast.USub
 
 
 class DecimalDefinition(FixedAbstractType, _NumericDefinition):
+    _bits = 168
     _id = "decimal"
+    _is_signed = True
     _invalid_op = vy_ast.Pow
 
 
