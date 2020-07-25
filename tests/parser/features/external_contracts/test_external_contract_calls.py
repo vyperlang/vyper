b8571b8b158a82eb19c339188845a120053919fe
Bryant <fubuloubu@gmail.com>
2020-07-24 22:12:52 -0400

    tests: Use list instead of tuple for return type (doesn't return a tuple)

--- tests/parser/features/external_contracts/test_external_contract_calls.py
+++ tests/parser/features/external_contracts/test_external_contract_calls.py
@@ -825,7 +825,7 @@ def test(addr: address) -> (int128, address):
     c1 = get_contract_with_gas_estimation(contract_1)
     c2 = get_contract_with_gas_estimation(contract_2)
 
-    assert c1.out_literals() == (1, "0x0000000000000000000000000000000000012345")
+    assert c1.out_literals() == [1, "0x0000000000000000000000000000000000012345"]
     assert c2.test(c1.address) == list(c1.out_literals())
 
 
