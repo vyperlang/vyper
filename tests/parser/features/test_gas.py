198cf3691b1eef78fcca610c7a43863428744f46
Ben Hauser <ben@hauser.id>
2020-09-30 03:07:23 +0300

    test: remove test that brings me no joy

--- tests/parser/features/test_gas.py
+++ tests/parser/features/test_gas.py
@@ -1,7 +1,3 @@
-from vyper.parser import parser_utils
-from vyper.parser.parser import parse_to_lll
-
-
 def test_gas_call(get_contract_with_gas_estimation):
     gas_call = """
 @external
@@ -13,19 +9,3 @@ def foo() -> uint256:
 
     assert c.foo(call={"gas": 50000}) < 50000
     assert c.foo(call={"gas": 50000}) > 25000
-
-    print("Passed gas test")
-
-
-def test_gas_estimate_repr():
-    code = """
-x: int128
-
-@external
-def __init__():
-    self.x = 1
-    """
-    parser_utils.LLLnode.repr_show_gas = True
-    out = parse_to_lll(code)
-    assert "35261" in str(out)[:28]
-    parser_utils.LLLnode.repr_show_gas = False
