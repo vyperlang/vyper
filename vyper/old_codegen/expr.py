6204f700fa1f9b2d880ce9f264e521f35d445190
Edward Amor <edward.amor3@gmail.com>
2021-08-10 14:34:09 -0400

    fix: add if branch for msg.data w/ extract32
    New branch copies calldata from 0 to index +32 to memory

--- vyper/old_codegen/expr.py
+++ vyper/old_codegen/expr.py
@@ -395,6 +395,18 @@ class Expr:
                         pos,
                     ]
                     return LLLnode.from_list(node, typ=typ, pos=getpos(self.expr), location="memory")
+                elif parent.get("func.id") == "extract32":
+                    size = parent.args[1].value + 32
+                    typ = ByteArrayType(size)
+                    pos = self.context.new_internal_variable(typ)
+                    node = [
+                        "seq",
+                        ["mstore", pos, size],
+                        ["calldatacopy", pos + 32, 0, size],
+                        pos,
+                    ]
+                    return LLLnode.from_list(node, typ=typ, pos=getpos(self.expr), location="memory")
+
             elif key == "msg.value" and self.context.is_payable:
                 return LLLnode.from_list(
                     ["callvalue"], typ=BaseType("uint256"), pos=getpos(self.expr),
