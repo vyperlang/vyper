d3177d23e280b0ab9d94e3d252ec380447cfac07
Charles Cooper <cooper.charles.m@gmail.com>
2021-09-09 19:48:18 -0700

    lint a test

--- tests/functional/codegen/test_struct_return.py
+++ tests/functional/codegen/test_struct_return.py
@@ -23,8 +23,11 @@ def modify_nested_tuple(_human: Human) -> Human:
     c = get_contract(code)
     addr1 = "0x1234567890123456789012345678901234567890"
     addr2 = "0x1234567890123456789012345678900000000000"
-    #assert c.modify_nested_tuple([addr1, 123], [addr2, 456]) == [[addr1, 124], [addr2, 457]]
-    assert c.modify_nested_tuple({"location": addr1, "animal": {"location": addr2, "fur": "wool"}}) == [(addr1, (addr2, "wool is great"))]
+    # assert c.modify_nested_tuple([addr1, 123], [addr2, 456]) == [[addr1, 124], [addr2, 457]]
+    assert c.modify_nested_tuple(
+        {"location": addr1, "animal": {"location": addr2, "fur": "wool"}}
+    ) == [(addr1, (addr2, "wool is great"))]
+
 
 @pytest.mark.parametrize("string", ["a", "abc", "abcde", "potato"])
 def test_string_inside_tuple(get_contract, string):
