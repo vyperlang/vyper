1c6c60567c798f0e8b4a8874dafd80a8149cfa3d
Charles Cooper <cooper.charles.m@gmail.com>
2021-09-21 17:36:57 +0000

    fix sar emulation for pre-constantinople

--- vyper/old_codegen/arg_clamps.py
+++ vyper/old_codegen/arg_clamps.py
@@ -12,7 +12,11 @@ def _shr(x, bits):
 def _sar(x, bits):
     if version_check(begin="constantinople"):
         return ["sar", bits, x]
-    return ["sdiv", x, ["exp", 2, bits]]
+
+    # emulate for older arches. keep in mind note from EIP 145:
+    # This is not equivalent to PUSH1 2 EXP SDIV, since it rounds
+    # differently. See SDIV(-1, 2) == 0, while SAR(-1, 1) == -1.
+    return ["sdiv", ["add", ["slt", x, 0], x], ["exp", 2, bits]]
 
 
 def address_clamp(lll_node):
