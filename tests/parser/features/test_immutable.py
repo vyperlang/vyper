cd2cb985b6c9332ff291d62eb11aaff74b374d10
Edward Amor <edward.amor3@gmail.com>
2021-10-03 15:26:58 -0400

    test: accessing stored immutable

--- tests/parser/features/test_immutable.py
+++ tests/parser/features/test_immutable.py
@@ -1,3 +1,38 @@
+import pytest
+
+code_list = [
+    (
+        f"""
+VALUE: immutable({typ})
+
+@external
+def __init__(_value: {typ}):
+    VALUE = _value
+
+@view
+@external
+def get_value() -> {typ}:
+    return VALUE
+    """,
+        value,
+    )
+    for typ, value in (
+        ("uint256", 42),
+        ("int256", -(2 ** 200)),
+        ("int128", -(2 ** 126)),
+        ("address", "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"),
+        ("bytes32", b"deadbeef" * 4),
+        ("bool", True),
+    )
+]
+
+
+@pytest.mark.parametrize("code,value", code_list)
+def test_value_is_stored_correctly(code, value, get_contract):
+    c = get_contract(code, value)
+    assert c.get_value() == value
+
+
 def test_simple_usage(get_contract):
     code = """
 VALUE: immutable(uint256)
