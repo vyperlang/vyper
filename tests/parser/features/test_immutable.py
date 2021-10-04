ec13acff8166d907850b61f37b5af195651c8019
Edward Amor <edward.amor3@gmail.com>
2021-10-04 11:59:11 -0400

    chore: modify test parametrization
    Co-authored-by: El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>

--- tests/parser/features/test_immutable.py
+++ tests/parser/features/test_immutable.py
@@ -1,18 +1,18 @@
 import pytest
 
-type_list = (
-    ("uint256", 42),
-    ("int256", -(2 ** 200)),
-    ("int128", -(2 ** 126)),
-    ("address", "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"),
-    ("bytes32", b"deadbeef" * 4),
-    ("bool", True),
-    ("String[10]", "Vyper hiss"),
-    ("Bytes[10]", b"Vyper hiss"),
+@pytest.mark.parametrize(
+    "typ,value",
+    [
+        ("uint256", 42),
+        ("int256", -(2 ** 200)),
+        ("int128", -(2 ** 126)),
+        ("address", "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"),
+        ("bytes32", b"deadbeef" * 4),
+        ("bool", True),
+        ("String[10]", "Vyper hiss"),
+        ("Bytes[10]", b"Vyper hiss"),
+    ],
 )
-
-
-@pytest.mark.parametrize("typ,value", type_list)
 def test_value_storage_retrieval(typ, value, get_contract):
     code = f"""
 VALUE: immutable({typ})
