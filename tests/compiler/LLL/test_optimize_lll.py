e27c93882407c66613cdecb9761c95df5b41b53c
Charles Cooper <cooper.charles.m@gmail.com>
2021-10-19 17:53:28 +0000

    update LLL optimizer tests

--- tests/compiler/LLL/test_optimize_lll.py
+++ tests/compiler/LLL/test_optimize_lll.py
@@ -4,10 +4,11 @@ from vyper.lll import optimizer
 from vyper.old_codegen.parser import LLLnode
 
 optimize_list = [
-    (["ne", 1, 0], ["ne", 1, 0]),  # noop
-    (["if", ["ne", 1, 0], "pass"], ["if", ["xor", 1, 0], "pass"]),
-    (["assert", ["ne", 1, 0]], ["assert", ["xor", 1, 0]]),
-    (["mstore", 0, ["ne", 1, 0]], ["mstore", 0, ["ne", 1, 0]]),  # noop
+    (["eq", 1, 0], ["iszero", 1]),
+    (["eq", 1, 2], ["eq", 1, 2]), # noop
+    (["if", ["eq", 1, 2], "pass"], ["if", ["iszero", ["xor", 1, 2]], "pass"]),
+    (["assert", ["eq", 1, 2]], ["assert", ["iszero", ["xor", 1, 2]]]),
+    (["mstore", 0, ["eq", 1, 2]], ["mstore", 0, ["eq", 1, 2]]),  # noop
 ]
 
 
