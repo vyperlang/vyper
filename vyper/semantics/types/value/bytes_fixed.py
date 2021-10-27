328cb1e6382201a516c87c302a77d9b682de782a
Edward Amor <edward.amor3@gmail.com>
2021-10-27 03:46:20 +0000

    feat: add Bytes4{Definition,Primitive}

--- vyper/semantics/types/value/bytes_fixed.py
+++ vyper/semantics/types/value/bytes_fixed.py
@@ -4,6 +4,32 @@ from vyper.semantics.types.abstract import BytesAbstractType
 from vyper.semantics.types.bases import BasePrimitive, BaseTypeDefinition, ValueTypeDefinition
 
 
+class Bytes4Definition(BytesAbstractType, ValueTypeDefinition):
+
+    # included for compatibility with bytes array methods
+    _id = "bytes4"
+    length = 4
+    _length = 4
+    _min_length = 4
+
+
+class Bytes4Primitive(BasePrimitive):
+
+    _as_array = True
+    _type = Bytes4Definition
+    _id = "bytes4"
+    _valid_literal = (vy_ast.Bytes, vy_ast.Hex)
+
+    @classmethod
+    def from_literal(cls, node: vy_ast.Constant) -> BaseTypeDefinition:
+        obj = super().from_literal(node)
+        if isinstance(node, vy_ast.Bytes) and len(node.value.hex()) != 8:
+            raise InvalidLiteral("Invalid literal for type bytes4", node)
+        if isinstance(node, vy_ast.Hex) and len(node.value) != 10:
+            raise InvalidLiteral("Invalid literal for type bytes4", node)
+        return obj
+
+
 class Bytes32Definition(BytesAbstractType, ValueTypeDefinition):
 
     # included for compatibility with bytes array methods
