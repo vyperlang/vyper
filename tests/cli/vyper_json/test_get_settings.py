59ccdebc5a31c6c1117e8be2447ab60a1f990993
Charles Cooper <cooper.charles.m@gmail.com>
2021-11-06 11:14:29 -0700

    add optimize flag to JSON input settings
    also, rename get_input_dict_settings to `get_evm_version` which is what
    it does

--- tests/cli/vyper_json/test_get_settings.py
+++ tests/cli/vyper_json/test_get_settings.py
@@ -2,27 +2,26 @@
 
 import pytest
 
-from vyper.cli.vyper_json import get_input_dict_settings
+from vyper.cli.vyper_json import get_evm_version
 from vyper.evm.opcodes import DEFAULT_EVM_VERSION
 from vyper.exceptions import JSONError
 
 
 def test_unknown_evm():
     with pytest.raises(JSONError):
-        get_input_dict_settings({"settings": {"evmVersion": "foo"}})
+        get_evm_version({"settings": {"evmVersion": "foo"}})
 
 
 @pytest.mark.parametrize("evm_version", ["homestead", "tangerineWhistle", "spuriousDragon"])
 def test_early_evm(evm_version):
     with pytest.raises(JSONError):
-        get_input_dict_settings({"settings": {"evmVersion": evm_version}})
+        get_evm_version({"settings": {"evmVersion": evm_version}})
 
 
 @pytest.mark.parametrize("evm_version", ["byzantium", "constantinople", "petersburg"])
 def test_valid_evm(evm_version):
-    settings = get_input_dict_settings({"settings": {"evmVersion": evm_version}})
-    assert settings == {"evm_version": evm_version}
+    assert evm_version == get_evm_version({"settings": {"evmVersion": evm_version}})
 
 
 def test_default_evm():
-    get_input_dict_settings({}) == {"evm_version": DEFAULT_EVM_VERSION}
+    assert get_evm_version({}) == DEFAULT_EVM_VERSION
