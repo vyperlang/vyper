156d97c77afca16ece7d6da46c7f01d67d86b0b5
Charles Cooper <cooper.charles.m@gmail.com>
2021-11-29 13:25:14 -0800

    drop use of importlib
    pyinstaller does not emit the package version correctly, it's ok we can
    just use version.py directly

--- vyper/__init__.py
+++ vyper/__init__.py
@@ -1,15 +1,2 @@
 from vyper.compiler import compile_code, compile_codes  # noqa: F401
-
-
-try:
-    from importlib.metadata import PackageNotFoundError  # type: ignore
-    from importlib.metadata import version as _version  # type: ignore
-except ModuleNotFoundError:
-    from importlib_metadata import PackageNotFoundError  # type: ignore
-    from importlib_metadata import version as _version  # type: ignore
-
-try:
-    __version__ = _version(__name__)
-except PackageNotFoundError:
-    # package is not installed
-    __version__ = "<unknown>"
+from vyper.version import version as __version__
