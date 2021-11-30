26c957d4b971e696545c8a47acf278cef3f99c05
Charles Cooper <cooper.charles.m@gmail.com>
2021-11-30 15:32:03 -0800

    fix a bug in usage of scm-setup
    The existing way doesn't work if setup.py hasn't been run yet. This
    way works both with pyinstaller and regular setup.py.

--- vyper/__init__.py
+++ vyper/__init__.py
@@ -1,2 +1,14 @@
 from vyper.compiler import compile_code, compile_codes  # noqa: F401
-from vyper.version import version as __version__
+
+
+try:
+    from importlib.metadata import PackageNotFoundError  # type: ignore
+    from importlib.metadata import version as _version  # type: ignore
+except ModuleNotFoundError:
+    from importlib_metadata import PackageNotFoundError  # type: ignore
+    from importlib_metadata import version as _version  # type: ignore
+
+try:
+    __version__ = _version(__name__)
+except PackageNotFoundError:
+    from vyper.version import version as __version__
