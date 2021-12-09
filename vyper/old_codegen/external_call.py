c060afba70bbc7facabfb5fc3efaa67d59222d50
Charles Cooper <cooper.charles.m@gmail.com>
2021-12-09 13:20:41 -0800

    feat: implement skip_contract_check kwarg (#2551)
    Add skip_contract_check as a kwarg to external contract calls
    which tells codegen to skip extcodesize/returndatasize checks

--- vyper/old_codegen/external_call.py
+++ vyper/old_codegen/external_call.py
@@ -67,7 +67,7 @@ def _returndata_encoding(contract_sig):
     return Encoding.ABI
 
 
-def _unpack_returndata(buf, contract_sig, context, pos):
+def _unpack_returndata(buf, contract_sig, skip_contract_check, context, pos):
     return_t = contract_sig.return_type
     if return_t is None:
         return ["pass"], 0, 0
@@ -91,7 +91,8 @@ def _unpack_returndata(buf, contract_sig, context, pos):
     ret = []
     # runtime: min_return_size <= returndatasize
     # TODO move the -1 optimization to LLL optimizer
-    ret += [["assert", ["gt", "returndatasize", min_return_size - 1]]]
+    if not skip_contract_check:
+        ret += [["assert", ["gt", "returndatasize", min_return_size - 1]]]
 
     # add as the last LLLnode a pointer to the return data structure
 
@@ -113,13 +114,22 @@ def _unpack_returndata(buf, contract_sig, context, pos):
 
 
 def _external_call_helper(
-    contract_address, contract_sig, args_lll, context, pos=None, value=None, gas=None
+    contract_address,
+    contract_sig,
+    args_lll,
+    context,
+    pos=None,
+    value=None,
+    gas=None,
+    skip_contract_check=None,
 ):
 
     if value is None:
         value = 0
     if gas is None:
         gas = "gas"
+    if skip_contract_check is None:
+        skip_contract_check = False
 
     # sanity check
     assert len(contract_sig.base_args) <= len(args_lll) <= len(contract_sig.args)
@@ -136,11 +146,13 @@ def _external_call_helper(
 
     buf, arg_packer, args_ofst, args_len = _pack_arguments(contract_sig, args_lll, context, pos)
 
-    ret_unpacker, ret_ofst, ret_len = _unpack_returndata(buf, contract_sig, context, pos)
+    ret_unpacker, ret_ofst, ret_len = _unpack_returndata(
+        buf, contract_sig, skip_contract_check, context, pos
+    )
 
     sub += arg_packer
 
-    if contract_sig.return_type is None:
+    if contract_sig.return_type is None and not skip_contract_check:
         # if we do not expect return data, check that a contract exists at the
         # target address. we must perform this check BEFORE the call because
         # the contract might selfdestruct. on the other hand we can omit this
@@ -171,26 +183,30 @@ def _external_call_helper(
     return ret
 
 
-# TODO push me up to expr.py
-def get_gas_and_value(stmt_expr, context):
+def _get_special_kwargs(stmt_expr, context):
     from vyper.old_codegen.expr import Expr  # TODO rethink this circular import
 
-    value, gas = None, None
+    value, gas, skip_contract_check = None, None, None
     for kw in stmt_expr.keywords:
         if kw.arg == "gas":
             gas = Expr.parse_value_expr(kw.value, context)
         elif kw.arg == "value":
             value = Expr.parse_value_expr(kw.value, context)
+        elif kw.arg == "skip_contract_check":
+            skip_contract_check = kw.value.value
+            assert isinstance(skip_contract_check, bool), "type checker missed this"
         else:
             raise TypeCheckFailure("Unexpected keyword argument")
-    return value, gas
+
+    # TODO maybe return a small dataclass to reduce verbosity
+    return value, gas, skip_contract_check
 
 
 def lll_for_external_call(stmt_expr, context):
     from vyper.old_codegen.expr import Expr  # TODO rethink this circular import
 
     pos = getpos(stmt_expr)
-    value, gas = get_gas_and_value(stmt_expr, context)
+    value, gas, skip_contract_check = _get_special_kwargs(stmt_expr, context)
     args_lll = [Expr(x, context).lll_node for x in stmt_expr.args]
 
     if isinstance(stmt_expr.func, vy_ast.Attribute) and isinstance(
@@ -241,6 +257,7 @@ def lll_for_external_call(stmt_expr, context):
         pos,
         value=value,
         gas=gas,
+        skip_contract_check=skip_contract_check,
     )
     ret.annotation = stmt_expr.get("node_source_code")
 
