d5f276e8f05d192698b4328364156759d5f149f0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-01-01 08:06:26 +0900

    refactor: more readable variable names

--- vyper/semantics/validation/module.py
+++ vyper/semantics/validation/module.py
@@ -97,18 +97,21 @@ class ModuleNodeVisitor(VyperNodeVisitorBase):
 
         # get list of internal function calls made by each function
         function_defs = self.ast.get_children(vy_ast.FunctionDef)
-        call_function_names = set(node.name for node in function_defs)
+        function_names = set(node.name for node in function_defs)
         for node in function_defs:
-            self_members[node.name].internal_calls = set(
+            calls_to_self = set(
                 i.func.attr for i in node.get_descendants(vy_ast.Call, {"func.value.id": "self"})
-            ).intersection(call_function_names)
+            )
+            # anything that is not a function call will get semantically checked later
+            calls_to_self = calls_to_self.intersection(function_names)
+            self_members[node.name].internal_calls = calls_to_self
             if node.name in self_members[node.name].internal_calls:
                 self_node = node.get_descendants(
                     vy_ast.Attribute, {"value.id": "self", "attr": node.name}
                 )[0]
                 raise CallViolation(f"Function '{node.name}' calls into itself", self_node)
 
-        for fn_name in sorted(call_function_names):
+        for fn_name in sorted(function_names):
 
             if fn_name not in self_members:
                 # the referenced function does not exist - this is an issue, but we'll report
