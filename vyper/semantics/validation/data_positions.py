9ca5b00ce1c4ca7d4985a3d6efa9b649b964739f
abdullathedruid <abdulla.alkamil@gmail.com>
2022-01-06 01:43:23 +0000

    feat: use storage file for overrides

--- vyper/semantics/validation/data_positions.py
+++ vyper/semantics/validation/data_positions.py
@@ -7,7 +7,9 @@ from vyper.semantics.types.bases import CodeOffset, StorageSlot
 from vyper.typing import StorageLayout
 
 
-def set_data_positions(vyper_module: vy_ast.Module) -> StorageLayout:
+def set_data_positions(
+    vyper_module: vy_ast.Module, storage_layout_overrides: StorageLayout = {}
+) -> StorageLayout:
     """
     Parse the annotated Vyper AST, determine data positions for all variables,
     and annotate the AST nodes with the position data.
@@ -18,7 +20,73 @@ def set_data_positions(vyper_module: vy_ast.Module) -> StorageLayout:
         Top-level Vyper AST node that has already been annotated with type data.
     """
     set_code_offsets(vyper_module)
-    return set_storage_slots(vyper_module)
+    return (
+        set_storage_slots_with_overrides(vyper_module, storage_layout_overrides)
+        if storage_layout_overrides
+        else set_storage_slots(vyper_module)
+    )
+
+
+def set_storage_slots_with_overrides(
+    vyper_module: vy_ast.Module, storage_layout_overrides: StorageLayout
+) -> StorageLayout:
+    """
+    Parse module-level Vyper AST to calculate the layout of storage variables.
+    Returns the layout as a dict of variable name -> variable info
+    """
+    # Allocate storage slots from 0
+    # note storage is word-addressable, not byte-addressable
+
+    ret: Dict[str, Dict] = {}
+    is_slot_occupied: Dict[int, bool] = {}
+
+    for node in vyper_module.get_children(vy_ast.FunctionDef):
+        type_ = node._metadata["type"]
+        if type_.nonreentrant is None:
+            continue
+
+        variable_name = f"nonreentrant.{type_.nonreentrant}"
+
+        if variable_name in storage_layout_overrides:
+            reentrant_slot = storage_layout_overrides[variable_name]["slot"]
+            type_.set_reentrancy_key_position(StorageSlot(reentrant_slot))
+
+            if is_slot_occupied[reentrant_slot]:
+                raise ValueError(
+                    f"Storage collision! Slot {reentrant_slot} has already been reserved"
+                )
+
+            ret[variable_name] = {
+                "type": "nonreentrant lock",
+                "location": "storage",
+                "slot": reentrant_slot,
+            }
+            is_slot_occupied[reentrant_slot] = True
+        else:
+            raise KeyError(
+                f"Could not find storage_slot for {variable_name}. Have you used the correct storage layout file?"
+            )
+
+    for node in vyper_module.get_children(vy_ast.AnnAssign):
+
+        if node.get("annotation.func.id") == "immutable":
+            continue
+
+        type_ = node.target._metadata["type"]
+
+        if node.target.id in storage_layout_overrides:
+            var_slot = storage_layout_overrides[node.target.id]["slot"]
+            type_.set_position(StorageSlot(var_slot))
+
+            # this could have better typing but leave it untyped until
+            # we understand the use case better
+            ret[node.target.id] = {"type": str(type_), "location": "storage", "slot": var_slot}
+        else:
+            raise KeyError(
+                f"Could not find storage_slot for {node.target.id}. Have you used the correct storage layout file?"
+            )
+
+    return ret
 
 
 def set_storage_slots(vyper_module: vy_ast.Module) -> StorageLayout:
