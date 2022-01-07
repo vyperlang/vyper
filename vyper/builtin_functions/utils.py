1be3b36b5f44affdfb2f4f522d8cb43d9f1b3124
Charles Cooper <cooper.charles.m@gmail.com>
2022-01-06 21:52:08 -0500

    change Context vars to be optional

--- vyper/builtin_functions/utils.py
+++ vyper/builtin_functions/utils.py
@@ -7,7 +7,7 @@ from vyper.codegen.stmt import parse_body
 def generate_inline_function(code, variables, memory_allocator):
     ast_code = parse_to_ast(code)
     new_context = Context(
-        vars=variables, global_ctx=GlobalContext(), memory_allocator=memory_allocator
+        vars_=variables, global_ctx=GlobalContext(), memory_allocator=memory_allocator
     )
     generated_lll = parse_body(ast_code.body, new_context)
     return new_context, generated_lll
