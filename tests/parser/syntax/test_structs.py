5293954079561f48c41dd8a853b0659745d8d1e6
Gary Tse <tse.rong.gary@gmail.com>
2022-03-19 08:48:32 +0800

    add test for constant struct

--- tests/parser/syntax/test_structs.py
+++ tests/parser/syntax/test_structs.py
@@ -549,6 +549,25 @@ struct C:
 def get_y() -> int128:
     return C({c: A({a: X({x: 1, y: -1}), b: 777}), d: True}).c.a.y - 10
     """,
+    """
+struct X:
+    x: int128
+    y: int128
+
+struct A:
+    a: X
+    b: uint256
+
+struct C:
+    c: A
+    d: bool
+
+FOO: constant(C) = C({c: A({a: X({x: 1, y: -1}), b: 777}), d: True})
+
+@external
+def get_y() -> int128:
+    return FOO.c.a.y - 10
+    """,
 ]
 
 
