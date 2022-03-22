3d28fab132d19acdc1f1eb291566750d3ed973de
Gary Tse <tse.rong.gary@gmail.com>
2022-03-22 13:48:46 +0800

    refactor parametrization

--- tests/parser/functions/test_convert.py
+++ tests/parser/functions/test_convert.py
@@ -2,103 +2,183 @@ from decimal import Decimal
 
 import pytest
 
+from vyper.codegen.types import BYTES_M_TYPES, SIGNED_INTEGER_TYPES, UNSIGNED_INTEGER_TYPES
 from vyper.exceptions import InvalidType, TypeMismatch
 from vyper.utils import checksum_encode
 
 
-@pytest.fixture
-def input_values(request):
-    if request.param["in_type"] == "decimal":
-        request.param["contract_call_in_value"] = Decimal(request.param["in_value"])
-    else:
-        request.param["contract_call_in_value"] = request.param["in_value"]
-    # request.param["contract_call_in_value"] = request.param["in_value"]
-    return request.param
+def _generate_test_cases_for_type(type_, bits=None):
+    """
+    Helper function to generate the test cases for a specific type.
+    """
+    if type_ == "uint":
+        return [0, 1, 2 ** bits - 2, 2 ** bits - 1]
+    elif type_ == "int":
+        return [
+            0,
+            1,
+            2 ** (bits - 1) - 2,
+            2 ** (bits - 1) - 1,
+            -(2 ** (bits - 1)),
+            -(2 ** (bits - 1) - 1),
+        ]
+    elif type_ == "decimal":
+        return [
+            "0.0",
+            "0.0000000001",
+            "0.9999999999",
+            "1.0",
+            "170141183460469231731687303715884105726.9999999999",  # 2 ** 127 - 1.0000000001
+            "-0.0000000001",
+            "-0.9999999999",
+            "-1.0",
+            "-170141183460469231731687303715884105727.9999999999",  # - (2 ** 127 - 0.0000000001)
+        ]
+    elif type_ == "address":
+        return [
+            "0x0000000000000000000000000000000000000000",
+            "0xF5D4020dCA6a62bB1efFcC9212AAF3c9819E30D7",
+            "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF",
+        ]
+    elif type_ == "Bytes":
+        return [
+            b"",
+            b"\x00",
+            b"\x00" * bits,
+            b"\x01",
+            b"\x00\x01",
+            b"\xff" * (bits - 1) + b"\xfe",
+            b"\xff" * bits,
+        ]
+    elif type_ == "bytes":
+        return [
+            "0x" + ("00" * bits),
+            "0x" + ("00" * (bits - 1)) + "01",
+            checksum_encode("0x" + ("FF" * bits)) if bits == 20 else "0x" + ("FF" * bits),
+        ]
 
 
-test_address = "0xF5D4020dCA6a62bB1efFcC9212AAF3c9819E30D7"
+def _generate_input_values_dict(in_type, out_type, cases, out_values):
+    """
+    Helper function to generate the test values for a specific input type and output type,
+    and to modify the output values based on the input type and output type where necessary.
+    """
+    res = []
+    for c, ov in zip(cases, out_values):
+
+        if (
+            in_type.startswith("bytes")
+            and out_type == "address"
+            and ov == "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF"
+        ):
+            bits = int(in_type[5:])
+            ov = checksum_encode("0x" + "00" * (20 - bits) + "ff" * bits)
+
+        if (
+            in_type.startswith("uint")
+            and out_type == "address"
+            and ov == "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF"
+        ):
+            bits = int(in_type[4:])
+            ov = checksum_encode("0x" + hex(c)[2:].rjust(40, "0"))
+
+        res.append(
+            {
+                "in_type": in_type,
+                "out_type": out_type,
+                "in_value": c,
+                "out_value": ov,
+            }
+        )
+    return res
+
+
+def generate_test_convert_values(in_type, out_type, out_values):
+    """
+    Helper function to generate the test values for a generic input type.
+    """
+    result = []
+    if in_type == "uint":
+
+        unsigned_integer_types = (
+            [u for u in UNSIGNED_INTEGER_TYPES if int(u[4:]) < 160]
+            if out_type == "address"
+            else UNSIGNED_INTEGER_TYPES
+        )
+        for t in unsigned_integer_types:
+            bits = int(t[4:])
+            cases = _generate_test_cases_for_type(in_type, bits)
+            result += _generate_input_values_dict(t, out_type, cases, out_values)
+
+    elif in_type == "int":
+        for t in SIGNED_INTEGER_TYPES:
+            bits = int(t[3:])
+            cases = _generate_test_cases_for_type(in_type, bits)
+            result += _generate_input_values_dict(t, out_type, cases, out_values)
+
+    elif in_type[:5] == "bytes":
+        bytes_types = (
+            [b for b in BYTES_M_TYPES if int(b[5:]) < 20]
+            if out_type == "address"
+            else BYTES_M_TYPES
+        )
+        for t in bytes_types:
+            bits = int(t[5:])
+            cases = _generate_test_cases_for_type("bytes", bits)
+            result += _generate_input_values_dict(t, out_type, cases, out_values)
+
+    elif in_type[:5] == "Bytes":
+        bits = int(in_type[6:-1])
+        print(bits)
+        cases = _generate_test_cases_for_type("Bytes", bits)
+        result += _generate_input_values_dict(in_type, out_type, cases, out_values)
+
+    elif in_type in ["decimal", "address"]:
+        cases = _generate_test_cases_for_type(in_type)
+        result += _generate_input_values_dict(in_type, out_type, cases, out_values)
+
+    return sorted(result, key=lambda d: d["in_type"])
 
 
 @pytest.mark.parametrize(
     "input_values",
-    [
-        {
-            "in_type": "bytes32",
-            "out_type": "address",
-            "in_value": int(test_address, 16).to_bytes(20, "big").rjust(32, b"\00"),
-            "out_value": test_address,
-        },  # Bug with state variables,
-        {
-            "in_type": "uint256",
-            "out_type": "address",
-            "in_value": int(test_address, 16),
-            "out_value": test_address,
-        },
-        {"in_type": "decimal", "out_type": "bool", "in_value": 0.0, "out_value": False},
-        {"in_type": "decimal", "out_type": "bool", "in_value": -0.1, "out_value": True},
-        {"in_type": "decimal", "out_type": "bool", "in_value": 100.0, "out_value": True},
-        {"in_type": "uint8", "out_type": "bool", "in_value": 0, "out_value": False},
-        {"in_type": "uint8", "out_type": "bool", "in_value": 1, "out_value": True},
-        {"in_type": "uint8", "out_type": "bool", "in_value": 2 ** 8 - 1, "out_value": True},
-        {"in_type": "int128", "out_type": "bool", "in_value": 0, "out_value": False},
-        {"in_type": "int128", "out_type": "bool", "in_value": 1, "out_value": True},
-        {"in_type": "int128", "out_type": "bool", "in_value": 2 ** 127 - 1, "out_value": True},
-        {"in_type": "int128", "out_type": "bool", "in_value": -1, "out_value": True},
-        {"in_type": "int128", "out_type": "bool", "in_value": -(2 ** 127), "out_value": True},
-        {"in_type": "uint256", "out_type": "bool", "in_value": 0, "out_value": False},
-        {"in_type": "uint256", "out_type": "bool", "in_value": 1, "out_value": True},
-        {"in_type": "uint256", "out_type": "bool", "in_value": 2 ** 256 - 1, "out_value": True},
-        {"in_type": "int256", "out_type": "bool", "in_value": 0, "out_value": False},
-        {"in_type": "int256", "out_type": "bool", "in_value": 1, "out_value": True},
-        {"in_type": "int256", "out_type": "bool", "in_value": 2 ** 255 - 1, "out_value": True},
-        {"in_type": "int256", "out_type": "bool", "in_value": -1, "out_value": True},
-        {"in_type": "int256", "out_type": "bool", "in_value": -(2 ** 255), "out_value": True},
-        {
-            "in_type": "bytes32",
-            "out_type": "bool",
-            "in_value": "0x0000000000000000000000000000000000000000000000000000000000000000",
-            "out_value": False,
-        },
-        {
-            "in_type": "bytes32",
-            "out_type": "bool",
-            "in_value": "0x000000000FFF0000000000000000000000000000FF0000000000000000000FFF",
-            "out_value": True,
-        },
-        {"in_type": "Bytes[32]", "out_type": "bool", "in_value": b"", "out_value": False},
-        {"in_type": "Bytes[32]", "out_type": "bool", "in_value": b"\x00", "out_value": False},
-        {"in_type": "Bytes[32]", "out_type": "bool", "in_value": b"\x00" * 32, "out_value": False},
-        {"in_type": "Bytes[32]", "out_type": "bool", "in_value": b"\x01", "out_value": True},
-        {"in_type": "Bytes[32]", "out_type": "bool", "in_value": b"\x00\x01", "out_value": True},
-        {
-            "in_type": "Bytes[32]",
-            "out_type": "bool",
-            "in_value": b"\x01\x00\x00\x00\x01",
-            "out_value": True,
-        },
-        {"in_type": "Bytes[32]", "out_type": "bool", "in_value": b"\xff" * 32, "out_value": True},
-        {
-            "in_type": "Bytes[32]",
-            "out_type": "bool",
-            "in_value": b"\x00\x00\x00\x00\x00",
-            "out_value": False,
-        },
-        {
-            "in_type": "Bytes[32]",
-            "out_type": "bool",
-            "in_value": b"\x00\x07\x5B\xCD\x15",
-            "out_value": True,
-        },
-        {
-            "in_type": "address",
-            "out_type": "bool",
-            "in_value": "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF",
-            "out_value": True,
-        },
-    ],
-    indirect=True,
+    generate_test_convert_values("uint", "bool", [False, True, True, True])
+    + generate_test_convert_values("int", "bool", [False, True, True, True, True, True])
+    + generate_test_convert_values(
+        "decimal", "bool", [False, True, True, True, True, True, True, True, True]
+    )
+    + generate_test_convert_values("address", "bool", [False, True, True])
+    + generate_test_convert_values(
+        "Bytes[32]", "bool", [False, False, False, True, True, True, True]
+    )
+    + generate_test_convert_values("bytes", "bool", [False, True, True])
+    + generate_test_convert_values(
+        "uint",
+        "address",
+        [
+            "0x0000000000000000000000000000000000000000",
+            "0x0000000000000000000000000000000000000001",
+            "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF",  # Placeholder value
+            "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF",  # Placeholder value
+        ],
+    )
+    + generate_test_convert_values(
+        "bytes",
+        "address",
+        [
+            "0x0000000000000000000000000000000000000000",
+            "0x0000000000000000000000000000000000000001",
+            "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF",  # Placeholder value
+        ],
+    ),
 )
-def test_convert(get_contract_with_gas_estimation, input_values):
+def test_convert_2(get_contract_with_gas_estimation, input_values):
+
+    if (
+        input_values["out_type"] == "address"
+        and input_values["out_value"] == "0x0000000000000000000000000000000000000000"
+    ):
+        input_values["out_value"] = None
 
     contract_1 = f"""
 @external
@@ -116,9 +196,10 @@ def test_input_convert(x: {input_values["in_type"]}) -> {input_values["out_type"
         """
 
     c2 = get_contract_with_gas_estimation(contract_2)
-    assert (
-        c2.test_input_convert(input_values["contract_call_in_value"]) == input_values["out_value"]
-    )
+    if input_values["in_type"] == "decimal":
+        assert c2.test_input_convert(Decimal(input_values["in_value"])) == input_values["out_value"]
+    else:
+        assert c2.test_input_convert(input_values["in_value"]) == input_values["out_value"]
 
     contract_3 = f"""
 bar: {input_values["in_type"]}
