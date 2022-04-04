2380fedf3acfa737ac49fdeedbff0e0c921d69d4
Charles Cooper <cooper.charles.m@gmail.com>
2022-04-04 22:36:26 +0000

    check duplicate keys earlier in json parsing
    this is stronger than the existing behavior and also throws on keys
    which are equal even before normalizing the path (e.g., "foo.vy" and
    "foo.vy").

--- vyper/cli/vyper_json.py
+++ vyper/cli/vyper_json.py
@@ -5,7 +5,7 @@ import json
 import sys
 import warnings
 from pathlib import Path
-from typing import Callable, Dict, Tuple, Union
+from typing import Callable, Dict, Tuple, Union, List, Hashable, Any
 
 import vyper
 from vyper.cli.utils import extract_file_interface_imports, get_interface_file_path
@@ -434,6 +434,18 @@ def format_to_output_dict(compiler_data: Dict) -> Dict:
     return output_dict
 
 
+# https://stackoverflow.com/a/49518779
+def _raise_on_duplicate_keys(ordered_pairs: List[Tuple[Hashable, Any]]) -> Dict:
+    """Raise ValueError if a duplicate key exists in provided ordered list of pairs, otherwise return a dict."""
+    dict_out = {}
+    for key, val in ordered_pairs:
+        if key in dict_out:
+            raise JSONError(f"Duplicate key: {key}")
+        else:
+            dict_out[key] = val
+    return dict_out
+
+
 def compile_json(
     input_json: Union[Dict, str],
     exc_handler: Callable = exc_handler_raises,
@@ -443,7 +455,7 @@ def compile_json(
     try:
         if isinstance(input_json, str):
             try:
-                input_dict: Dict = json.loads(input_json)
+                input_dict: Dict = json.loads(input_json, object_pairs_hook=_raise_on_duplicate_keys)
             except json.decoder.JSONDecodeError as exc:
                 new_exc = JSONError(str(exc), exc.lineno, exc.colno)
                 return exc_handler(json_path, new_exc, "json")
