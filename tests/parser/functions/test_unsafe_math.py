063d2347fb2a23d5849c13e96abac4a9ab65ee9d
Gary Tse <8017125+tserg@users.noreply.github.com>
2022-05-05 18:33:05 +0800

    merge unsafe math tests

--- tests/parser/functions/test_unsafe_math.py
+++ tests/parser/functions/test_unsafe_math.py
@@ -15,16 +15,23 @@ integer_types = sorted(list(INTEGER_TYPES))
 @pytest.mark.parametrize("op", ["add", "sub", "mul", "div"])
 @pytest.mark.fuzzing
 def test_unsafe_op_int(get_contract, typ, op):
-    code = f"""
+    contract_1 = f"""
 @external
 def foo(x: {typ}, y: {typ}) -> {typ}:
     return unsafe_{op}(x, y)
     """
+
+    contract_2 = """
+@external
+def foo(x: {}) -> {}:
+    return unsafe_{}(x, {})
+    """
+
     fns = {"add": operator.add, "sub": operator.sub, "mul": operator.mul, "div": evm_div}
     fn = fns[op]
 
     int_info = parse_integer_typeinfo(typ)
-    c = get_contract(code)
+    c1 = get_contract(contract_1)
 
     lo, hi = int_bounds(int_info.is_signed, int_info.bits)
     # (roughly 8k cases total generated)
@@ -43,7 +50,11 @@ def foo(x: {typ}, y: {typ}) -> {typ}:
         for (x, y) in itertools.product(xs, ys):
             expected = unsigned_to_signed(fn(x, y) % mod_bound, int_info.bits)
 
-            assert c.foo(x, y) == expected
+            assert c1.foo(x, y) == expected
+
+            c2 = get_contract(contract_2.format(typ, typ, op, y))
+            assert c2.foo(x) == expected
+
     else:
         # 0x80 has some weird properties, like
         # it's a fixed point of multiplication by 0xFF
@@ -51,46 +62,7 @@ def foo(x: {typ}, y: {typ}) -> {typ}:
         xs += [0, 1, hi - 1, hi, fixed_pt]
         ys += [0, 1, hi - 1, hi, fixed_pt]
         for (x, y) in itertools.product(xs, ys):
-            assert c.foo(x, y) == fn(x, y) % mod_bound
+            assert c1.foo(x, y) == fn(x, y) % mod_bound
 
-
-@pytest.mark.parametrize("typ", integer_types)
-@pytest.mark.parametrize("literal", [-2, -1, 1, 2])
-@pytest.mark.parametrize("op", ["add", "sub", "mul", "div"])
-@pytest.mark.fuzzing
-def test_unsafe_op_literal(get_contract, typ, literal, op):
-    code = f"""
-@external
-def foo(x: {typ}) -> {typ}:
-    return unsafe_{op}(x, {literal})
-    """
-    fns = {"add": operator.add, "sub": operator.sub, "mul": operator.mul, "div": evm_div}
-    fn = fns[op]
-
-    int_info = parse_integer_typeinfo(typ)
-    if not (not int_info.is_signed and literal < 0):
-        c = get_contract(code)
-
-        lo, hi = int_bounds(int_info.is_signed, int_info.bits)
-        # (roughly 8k cases total generated)
-        # TODO refactor to use fixtures
-        NUM_CASES = 15
-        xs = [random.randrange(lo, hi) for _ in range(NUM_CASES)]
-
-        mod_bound = 2 ** int_info.bits
-
-        # poor man's fuzzing - hypothesis doesn't make it easy
-        # with the parametrized strategy
-        if int_info.is_signed:
-            xs += [lo, lo + 1, -1, 0, 1, hi - 1, hi]
-            for x in xs:
-                expected = unsigned_to_signed(fn(x, literal) % mod_bound, int_info.bits)
-
-                assert c.foo(x) == expected
-        else:
-            # 0x80 has some weird properties, like
-            # it's a fixed point of multiplication by 0xFF
-            fixed_pt = 2 ** (int_info.bits - 1)
-            xs += [0, 1, hi - 1, hi, fixed_pt]
-            for x in xs:
-                assert c.foo(x) == fn(x, literal) % mod_bound
+            c2 = get_contract(contract_2.format(typ, typ, op, y))
+            assert c2.foo(x) == fn(x, y) % mod_bound
