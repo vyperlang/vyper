0f99ba029150d78a634484611543660cf933558d
Charles Cooper <cooper.charles.m@gmail.com>
2022-05-25 11:25:15 +0200

    allow external pure calls

--- vyper/semantics/validation/local.py
+++ vyper/semantics/validation/local.py
@@ -479,9 +479,9 @@ class FunctionNodeVisitor(VyperNodeVisitorBase):
                     node,
                 )
 
-            # can call internal, pure functions from pure functions
-            if self.func.mutability == StateMutability.PURE and (
-                fn_type.is_external or fn_type.mutability != StateMutability.PURE
+            if (
+                self.func.mutability == StateMutability.PURE
+                and fn_type.mutability != StateMutability.PURE
             ):
                 raise StateAccessViolation(
                     "Cannot call non-pure function from a pure function", node
