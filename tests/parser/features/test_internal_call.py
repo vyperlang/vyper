vyperlang/vyper

commit efe1dbef873af6995833d1220a3d56978dfb9de0

feat: more arithmetic optimizations (#2647)
this is a small rewrite of the IR optimizer. it changes the structure of
the binop optimizations so that it is easier to add more optimizations.
it also refactors the `clamp` optimizations to be in terms of an
`assert` statement, so that the clamp conditions can be optimized using
the binop optimizer code.

Co-authored-by: El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>

vyper/codegen/ir_node.py
@@ -68,7 +68,6 @@ class IRnode:
         annotation: Optional[str] = None,
         mutable: bool = True,
         add_gas_estimate: int = 0,
-        valency: Optional[int] = None,
         encoding: Encoding = Encoding.VYPER,
     ):
         if args is None:
@@ -99,6 +98,10 @@ class IRnode:
         # Numbers
         if isinstance(self.value, int):
             _check(len(self.args) == 0, "int can't have arguments")
+
+            # integers must be in the range (MIN_INT256, MAX_UINT256)
+            _check(-(2 ** 255) <= self.value < 2 ** 256, "out of range")
+
             self.valency = 1
             self._gas = 5
         elif isinstance(self.value, str):
@@ -252,9 +255,6 @@ class IRnode:
         else:
             raise CompilerPanic(f"Invalid value for IR AST node: {self.value}")
         assert isinstance(self.args, list)
-
-        if valency is not None:
-            self.valency = valency
 
     # TODO would be nice to rename to `gas_estimate` or `gas_bound`
     @property
@@ -449,7 +449,6 @@ class IRnode:
         annotation: Optional[str] = None,
         mutable: bool = True,
         add_gas_estimate: int = 0,
-        valency: Optional[int] = None,
         encoding: Encoding = Encoding.VYPER,
     ) -> "IRnode":
         if isinstance(typ, str):
@@ -477,7 +476,6 @@ class IRnode:
                 annotation=annotation,
                 mutable=mutable,
                 add_gas_estimate=add_gas_estimate,
-                valency=valency,
                 encoding=encoding,
             )
         else:
@@ -490,6 +488,5 @@ class IRnode:
                 mutable=mutable,
                 source_pos=source_pos,
                 add_gas_estimate=add_gas_estimate,
-                valency=valency,
                 encoding=encoding,
             )

tests/compiler/ir/test_optimize_ir.py
@@ -1,0 +1,147 @@
+import pytest
+
+from vyper.codegen.ir_node import IRnode
+from vyper.exceptions import StaticAssertionException
+from vyper.ir import optimizer
+
+optimize_list = [
+    (["eq", 1, 2], [0]),
+    (["lt", 1, 2], [1]),
+    (["eq", "x", 0], ["iszero", "x"]),
+    # branch pruner
+    (["if", ["eq", 1, 2], "pass"], ["seq"]),
+    (["if", ["eq", 1, 1], 3, 4], [3]),
+    (["if", ["eq", 1, 2], 3, 4], [4]),
+    (["seq", ["assert", ["lt", 1, 2]]], ["seq"]),
+    (["seq", ["assert", ["lt", 1, 2]], 2], [2]),
+    # condition rewriter
+    (["if", ["eq", "x", "y"], "pass"], ["if", ["iszero", ["sub", "x", "y"]], "pass"]),
+    (["if", "cond", 1, 0], ["if", ["iszero", "cond"], 0, 1]),
+    (["assert", ["eq", "x", "y"]], ["assert", ["iszero", ["sub", "x", "y"]]]),
+    # nesting
+    (["mstore", 0, ["eq", 1, 2]], ["mstore", 0, 0]),
+    # conditions
+    (["ge", "x", 0], [1]),  # x >= 0 == True
+    (["iszero", ["gt", "x", 2 ** 256 - 1]], [1]),  # x >= MAX_UINT256 == False
+    (["iszero", ["sgt", "x", 2 ** 255 - 1]], [1]),  # signed x >= MAX_INT256 == False
+    (["le", "x", 0], ["iszero", "x"]),
+    (["le", 0, "x"], [1]),
+    (["lt", "x", 0], [0]),
+    (["lt", 0, "x"], ["iszero", ["iszero", "x"]]),
+    (["gt", 5, "x"], ["lt", "x", 5]),
+    (["ge", 5, "x"], ["le", "x", 5]),
+    (["lt", 5, "x"], ["gt", "x", 5]),
+    (["le", 5, "x"], ["ge", "x", 5]),
+    (["sgt", 5, "x"], ["slt", "x", 5]),
+    (["sge", 5, "x"], ["sle", "x", 5]),
+    (["slt", 5, "x"], ["sgt", "x", 5]),
+    (["sle", 5, "x"], ["sge", "x", 5]),
+    (["slt", "x", -(2 ** 255)], ["slt", "x", -(2 ** 255)]),  # unimplemented
+    # tricky conditions
+    (["sgt", 2 ** 256 - 1, 0], [0]),  # -1 > 0
+    (["gt", 2 ** 256 - 1, 0], [1]),  # -1 > 0
+    (["gt", 2 ** 255, 0], [1]),  # 0x80 > 0
+    (["sgt", 2 ** 255, 0], [0]),  # 0x80 > 0
+    (["sgt", 2 ** 255, 2 ** 255 - 1], [0]),  # 0x80 > 0x81
+    (["gt", -(2 ** 255), 2 ** 255 - 1], [1]),  # 0x80 > 0x81
+    (["slt", 2 ** 255, 2 ** 255 - 1], [1]),  # 0x80 < 0x7f
+    (["lt", -(2 ** 255), 2 ** 255 - 1], [0]),  # 0x80 < 0x7f
+    (["sle", -1, 2 ** 256 - 1], [1]),  # -1 <= -1
+    (["sge", -(2 ** 255), 2 ** 255], [1]),  # 0x80 >= 0x80
+    (["sgt", -(2 ** 255), 2 ** 255], [0]),  # 0x80 > 0x80
+    (["slt", 2 ** 255, -(2 ** 255)], [0]),  # 0x80 < 0x80
+    # arithmetic
+    (["add", "x", 0], ["x"]),
+    (["add", 0, "x"], ["x"]),
+    (["sub", "x", 0], ["x"]),
+    (["sub", "x", "x"], [0]),
+    (["sub", ["sload", 0], ["sload", 0]], ["sub", ["sload", 0], ["sload", 0]]),  # no-op
+    (["sub", ["callvalue"], ["callvalue"]], ["sub", ["callvalue"], ["callvalue"]]),  # no-op
+    (["mul", "x", 1], ["x"]),
+    (["div", "x", 1], ["x"]),
+    (["sdiv", "x", 1], ["x"]),
+    (["mod", "x", 1], [0]),
+    (["smod", "x", 1], [0]),
+    (["mul", "x", -1], ["sub", 0, "x"]),
+    (["sdiv", "x", -1], ["sub", 0, "x"]),
+    (["mul", "x", 0], [0]),
+    (["div", "x", 0], [0]),
+    (["sdiv", "x", 0], [0]),
+    (["mod", "x", 0], [0]),
+    (["smod", "x", 0], [0]),
+    (["mul", "x", 32], ["shl", 5, "x"]),
+    (["div", "x", 64], ["shr", 6, "x"]),
+    (["mod", "x", 128], ["and", "x", 127]),
+    (["sdiv", "x", 64], ["sdiv", "x", 64]),  # no-op
+    (["smod", "x", 64], ["smod", "x", 64]),  # no-op
+    # bitwise ops
+    (["shr", 0, "x"], ["x"]),
+    (["sar", 0, "x"], ["x"]),
+    (["shl", 0, "x"], ["x"]),
+    (["and", 1, 2], [0]),
+    (["or", 1, 2], [3]),
+    (["xor", 1, 2], [3]),
+    (["xor", 3, 2], [1]),
+    (["and", 0, "x"], [0]),
+    (["and", "x", 0], [0]),
+    (["or", "x", 0], ["x"]),
+    (["or", 0, "x"], ["x"]),
+    (["xor", "x", 0], ["x"]),
+    (["xor", "x", 1], ["xor", "x", 1]),  # no-op
+    (["and", "x", 1], ["and", "x", 1]),  # no-op
+    (["or", "x", 1], ["or", "x", 1]),  # no-op
+    (["xor", 0, "x"], ["x"]),
+    (["iszero", ["or", "x", 1]], [0]),
+    (["iszero", ["or", 2, "x"]], [0]),
+    # nested optimizations
+    (["eq", 0, ["sub", 1, 1]], [1]),
+    (["eq", 0, ["add", 2 ** 255, 2 ** 255]], [1]),  # test compile-time wrapping
+    (["eq", 0, ["add", 2 ** 255, -(2 ** 255)]], [1]),  # test compile-time wrapping
+    (["eq", -1, ["add", 0, -1]], [1]),  # test compile-time wrapping
+    (["eq", -1, ["add", 2 ** 255, 2 ** 255 - 1]], [1]),  # test compile-time wrapping
+    (["eq", -1, ["add", -(2 ** 255), 2 ** 255 - 1]], [1]),  # test compile-time wrapping
+    (["eq", -2, ["add", 2 ** 256 - 1, 2 ** 256 - 1]], [1]),  # test compile-time wrapping
+]
+
+
+@pytest.mark.parametrize("ir", optimize_list)
+def test_ir_optimizer(ir):
+    optimized = optimizer.optimize(IRnode.from_list(ir[0]))
+    optimized.repr_show_gas = True
+    hand_optimized = IRnode.from_list(ir[1])
+    hand_optimized.repr_show_gas = True
+    assert optimized == hand_optimized
+
+
+static_assertions_list = [
+    ["assert", ["eq", 2, 1]],
+    ["assert", ["ne", 1, 1]],
+    ["assert", ["sub", 1, 1]],
+    ["assert", ["lt", 2, 1]],
+    ["assert", ["lt", 1, 1]],
+    ["assert", ["lt", "x", 0]],  # +x < 0
+    ["assert", ["le", 1, 0]],
+    ["assert", ["le", 2 ** 256 - 1, 0]],
+    ["assert", ["gt", 1, 2]],
+    ["assert", ["gt", 1, 1]],
+    ["assert", ["gt", 0, 2 ** 256 - 1]],
+    ["assert", ["gt", "x", 2 ** 256 - 1]],
+    ["assert", ["ge", 1, 2]],
+    ["assert", ["ge", 1, 2]],
+    ["assert", ["slt", 2, 1]],
+    ["assert", ["slt", 1, 1]],
+    ["assert", ["slt", 0, 2 ** 256 - 1]],  # 0 < -1
+    ["assert", ["slt", -(2 ** 255), 2 ** 255]],  # 0x80 < 0x80
+    ["assert", ["sle", 0, 2 ** 255]],  # 0 < 0x80
+    ["assert", ["sgt", 1, 2]],
+    ["assert", ["sgt", 1, 1]],
+    ["assert", ["sgt", 2 ** 256 - 1, 0]],  # -1 > 0
+    ["assert", ["sgt", 2 ** 255, -(2 ** 255)]],  # 0x80 > 0x80
+    ["assert", ["sge", 2 ** 255, 0]],  # 0x80 > 0
+]
+
+
+@pytest.mark.parametrize("ir", static_assertions_list)
+def test_static_assertions(ir, assert_compile_failed):
+    ir = IRnode.from_list(ir)
+    assert_compile_failed(lambda: optimizer.optimize(ir), StaticAssertionException)

examples/auctions/blind_auction.vy
@@ -127,9 +127,7 @@ def reveal(_numBids: int128, _values: uint256[128], _fakes: bool[128], _secrets:
 
         # Bid was not actually revealed
         # Do not refund deposit
-        if (blindedBid != bidToCheck.blindedBid):
-            assert 1 == 0
-            continue
+        assert blindedBid == bidToCheck.blindedBid
 
         # Add deposit to refund if bid was indeed revealed
         refund += bidToCheck.deposit

tests/compiler/LLL/test_optimize_lll.py
@@ -1,21 +1,0 @@
-import pytest
-
-from vyper.codegen.ir_node import IRnode
-from vyper.ir import optimizer
-
-optimize_list = [
-    (["eq", 1, 0], ["iszero", 1]),
-    (["eq", 1, 2], ["eq", 1, 2]),  # noop
-    (["if", ["eq", 1, 2], "pass"], ["if", ["iszero", ["xor", 1, 2]], "pass"]),
-    (["assert", ["eq", 1, 2]], ["assert", ["iszero", ["xor", 1, 2]]]),
-    (["mstore", 0, ["eq", 1, 2]], ["mstore", 0, ["eq", 1, 2]]),  # noop
-]
-
-
-@pytest.mark.parametrize("ir", optimize_list)
-def test_ir_compile_fail(ir):
-    optimized = optimizer.optimize(IRnode.from_list(ir[0]))
-    optimized.repr_show_gas = True
-    hand_optimized = IRnode.from_list(ir[1])
-    hand_optimized.repr_show_gas = True
-    assert optimized == hand_optimized

tests/compiler/ir/test_compile_ir.py
@@ -23,10 +23,10 @@ def test_ir_compile_fail(bad_ir, get_contract_from_ir, assert_compile_failed):
 
 valid_list = [
     ["pass"],
-    ["clamplt", ["mload", 0], 300],
-    ["clampgt", ["mload", 0], -1],
-    ["uclampgt", 1, ["mload", 0]],
-    ["uclampge", ["mload", 0], 0],
+    ["assert", ["slt", ["mload", 0], 300]],
+    ["assert", ["sgt", ["mload", 0], -1]],
+    ["assert", ["gt", 1, ["mload", 0]]],
+    ["assert", ["ge", ["mload", 0], 0]],
 ]
 
 

commit f5051e1ba5e1155040d3930628e5a98e897568a0

fix: optimizer regression (#2868)
the optimizer passes introduced in efe1dbef873 were too aggressive, and
would optimize out expressions that had side effects.

vyper/codegen/ir_node.py
@@ -268,7 +268,7 @@ class IRnode:
     def is_complex_ir(self):
         # list of items not to cache. note can add other env variables
         # which do not change, e.g. calldatasize, coinbase, etc.
-        do_not_cache = {"~empty"}
+        do_not_cache = {"~empty", "calldatasize"}
         return (
             isinstance(self.value, str)
             and (self.value.lower() in VALID_IR_MACROS or self.value.upper() in get_ir_opcodes())

vyper/ir/optimizer.py
@@ -39,6 +39,12 @@ def _is_int(node: IRnode) -> bool:
     return isinstance(node.value, int)
 
 
+def _deep_contains(node_or_list, node):
+    if isinstance(node_or_list, list):
+        return any(_deep_contains(t, node) for t in node_or_list)
+    return node is node_or_list
+
+
 arith = {
     "add": (operator.add, "+", UNSIGNED),
     "sub": (operator.sub, "-", UNSIGNED),
@@ -109,7 +115,7 @@ def _optimize_arith(binop, args, ann, parent_op):
         left, right = _int(args[0]), _int(args[1])
         new_val = fn(left, right)
         new_val = _wrap(new_val)
-        return False, new_val, [], new_ann
+        return new_val, [], new_ann
 
     new_val = None
     new_args = None
@@ -278,10 +284,16 @@ def _optimize_arith(binop, args, ann, parent_op):
         new_val = "iszero"
         new_args = [["iszero", args[0]]]
 
-    if new_val is None:
-        return False, binop, args, ann
+    rollback = (
+        new_val is None
+        or (args[0].is_complex_ir and not _deep_contains(new_args, args[0]))
+        or (args[1].is_complex_ir and not _deep_contains(new_args, args[1]))
+    )
 
-    return True, new_val, new_args, new_ann
+    if rollback:
+        return None
+
+    return new_val, new_args, new_ann
 
 
 def optimize(node: IRnode, parent: Optional[IRnode] = None) -> IRnode:
@@ -293,6 +305,16 @@ def optimize(node: IRnode, parent: Optional[IRnode] = None) -> IRnode:
     source_pos = node.source_pos
     annotation = node.annotation
     add_gas_estimate = node.add_gas_estimate
+
+    def finalize(ir_builder):
+        return IRnode.from_list(
+            ir_builder,
+            typ=typ,
+            location=location,
+            source_pos=source_pos,
+            annotation=annotation,
+            add_gas_estimate=add_gas_estimate,
+        )
 
     optimize_more = False
 
@@ -308,7 +330,11 @@ def optimize(node: IRnode, parent: Optional[IRnode] = None) -> IRnode:
 
     elif value in arith:
         parent_op = parent.value if parent is not None else None
-        optimize_more, value, argz, annotation = _optimize_arith(value, argz, annotation, parent_op)
+
+        res = _optimize_arith(value, argz, annotation, parent_op)
+        if res is not None:
+            optimize_more = True
+            value, argz, annotation = res
 
     ###
     # BITWISE OPS
@@ -369,18 +395,12 @@ def optimize(node: IRnode, parent: Optional[IRnode] = None) -> IRnode:
             argz = []
 
     # NOTE: this is really slow (compile-time).
-    # maybe should optimize the tree in-place
-    ret = IRnode.from_list(
-        [value, *argz],
-        typ=typ,
-        location=location,
-        source_pos=source_pos,
-        annotation=annotation,
-        add_gas_estimate=add_gas_estimate,
-    )
+    # ideal would be to optimize the tree in-place
+    ret = finalize([value, *argz])
 
     if optimize_more:
         ret = optimize(ret, parent=parent)
+
     return ret
 
 

tests/compiler/ir/test_optimize_ir.py
@@ -8,6 +8,7 @@ optimize_list = [
     (["eq", 1, 2], [0]),
     (["lt", 1, 2], [1]),
     (["eq", "x", 0], ["iszero", "x"]),
+    (["eq", ["sload", 0], 0], ["iszero", ["sload", 0]]),
     # branch pruner
     (["if", ["eq", 1, 2], "pass"], ["seq"]),
     (["if", ["eq", 1, 1], 3, 4], [3]),
@@ -22,20 +23,22 @@ optimize_list = [
     (["mstore", 0, ["eq", 1, 2]], ["mstore", 0, 0]),
     # conditions
     (["ge", "x", 0], [1]),  # x >= 0 == True
+    (["ge", ["sload", 0], 0], None),  # no-op
     (["iszero", ["gt", "x", 2 ** 256 - 1]], [1]),  # x >= MAX_UINT256 == False
     (["iszero", ["sgt", "x", 2 ** 255 - 1]], [1]),  # signed x >= MAX_INT256 == False
     (["le", "x", 0], ["iszero", "x"]),
     (["le", 0, "x"], [1]),
+    (["le", 0, ["sload", 0]], None),  # no-op
     (["lt", "x", 0], [0]),
     (["lt", 0, "x"], ["iszero", ["iszero", "x"]]),
-    (["gt", 5, "x"], ["lt", "x", 5]),
-    (["ge", 5, "x"], ["le", "x", 5]),
-    (["lt", 5, "x"], ["gt", "x", 5]),
-    (["le", 5, "x"], ["ge", "x", 5]),
-    (["sgt", 5, "x"], ["slt", "x", 5]),
-    (["sge", 5, "x"], ["sle", "x", 5]),
-    (["slt", 5, "x"], ["sgt", "x", 5]),
-    (["sle", 5, "x"], ["sge", "x", 5]),
+    (["gt", 5, "x"], None),
+    (["ge", 5, "x"], None),
+    (["lt", 5, "x"], None),
+    (["le", 5, "x"], None),
+    (["sgt", 5, "x"], None),
+    (["sge", 5, "x"], None),
+    (["slt", 5, "x"], None),
+    (["sle", 5, "x"], None),
     (["slt", "x", -(2 ** 255)], ["slt", "x", -(2 ** 255)]),  # unimplemented
     # tricky conditions
     (["sgt", 2 ** 256 - 1, 0], [0]),  # -1 > 0
@@ -55,29 +58,37 @@ optimize_list = [
     (["add", 0, "x"], ["x"]),
     (["sub", "x", 0], ["x"]),
     (["sub", "x", "x"], [0]),
-    (["sub", ["sload", 0], ["sload", 0]], ["sub", ["sload", 0], ["sload", 0]]),  # no-op
-    (["sub", ["callvalue"], ["callvalue"]], ["sub", ["callvalue"], ["callvalue"]]),  # no-op
+    (["sub", ["sload", 0], ["sload", 0]], None),
+    (["sub", ["callvalue"], ["callvalue"]], None),
     (["mul", "x", 1], ["x"]),
     (["div", "x", 1], ["x"]),
     (["sdiv", "x", 1], ["x"]),
     (["mod", "x", 1], [0]),
+    (["mod", ["sload", 0], 1], None),
     (["smod", "x", 1], [0]),
     (["mul", "x", -1], ["sub", 0, "x"]),
     (["sdiv", "x", -1], ["sub", 0, "x"]),
     (["mul", "x", 0], [0]),
+    (["mul", ["sload", 0], 0], None),
     (["div", "x", 0], [0]),
+    (["div", ["sload", 0], 0], None),
     (["sdiv", "x", 0], [0]),
+    (["sdiv", ["sload", 0], 0], None),
     (["mod", "x", 0], [0]),
+    (["mod", ["sload", 0], 0], None),
     (["smod", "x", 0], [0]),
     (["mul", "x", 32], ["shl", 5, "x"]),
     (["div", "x", 64], ["shr", 6, "x"]),
     (["mod", "x", 128], ["and", "x", 127]),
-    (["sdiv", "x", 64], ["sdiv", "x", 64]),  # no-op
-    (["smod", "x", 64], ["smod", "x", 64]),  # no-op
+    (["sdiv", "x", 64], None),
+    (["smod", "x", 64], None),
     # bitwise ops
     (["shr", 0, "x"], ["x"]),
     (["sar", 0, "x"], ["x"]),
     (["shl", 0, "x"], ["x"]),
+    (["shr", 256, "x"], None),
+    (["sar", 256, "x"], None),
+    (["shl", 256, "x"], None),
     (["and", 1, 2], [0]),
     (["or", 1, 2], [3]),
     (["xor", 1, 2], [3]),
@@ -87,12 +98,13 @@ optimize_list = [
     (["or", "x", 0], ["x"]),
     (["or", 0, "x"], ["x"]),
     (["xor", "x", 0], ["x"]),
-    (["xor", "x", 1], ["xor", "x", 1]),  # no-op
-    (["and", "x", 1], ["and", "x", 1]),  # no-op
-    (["or", "x", 1], ["or", "x", 1]),  # no-op
+    (["xor", "x", 1], None),
+    (["and", "x", 1], None),
+    (["or", "x", 1], None),
     (["xor", 0, "x"], ["x"]),
     (["iszero", ["or", "x", 1]], [0]),
     (["iszero", ["or", 2, "x"]], [0]),
+    (["iszero", ["or", 1, ["sload", 0]]], None),
     # nested optimizations
     (["eq", 0, ["sub", 1, 1]], [1]),
     (["eq", 0, ["add", 2 ** 255, 2 ** 255]], [1]),  # test compile-time wrapping
@@ -108,9 +120,13 @@ optimize_list = [
 def test_ir_optimizer(ir):
     optimized = optimizer.optimize(IRnode.from_list(ir[0]))
     optimized.repr_show_gas = True
-    hand_optimized = IRnode.from_list(ir[1])
-    hand_optimized.repr_show_gas = True
-    assert optimized == hand_optimized
+    if ir[1] is None:
+        # no-op, assert optimizer does nothing
+        expected = IRnode.from_list(ir[0])
+    else:
+        expected = IRnode.from_list(ir[1])
+    expected.repr_show_gas = True
+    assert optimized == expected
 
 
 static_assertions_list = [

tests/parser/features/test_internal_call.py
@@ -51,6 +51,24 @@ def return_hash_of_rzpadded_cow() -> bytes32:
     print("Passed single fixed-size argument self-call test")
 
 
+# test that side-effecting self calls do not get optimized out
+def test_selfcall_optimizer(get_contract):
+    code = """
+counter: uint256
+
+@internal
+def increment_counter() -> uint256:
+    self.counter += 1
+    return self.counter
+@external
+def foo() -> (uint256, uint256):
+    x: uint256 = unsafe_mul(self.increment_counter(), 0)
+    return x, self.counter
+    """
+    c = get_contract(code)
+    assert c.foo() == [0, 1]
+
+
 def test_selfcall_code_3(get_contract_with_gas_estimation, keccak):
     selfcall_code_3 = """
 @internal