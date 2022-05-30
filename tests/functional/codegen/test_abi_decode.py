0072648e06e0a521cfa327febce354af78e1e0fe
tserg <8017125+tserg@users.noreply.github.com>
2022-05-30 11:19:26 +0800

    test: add test case

---
+++ tests/functional/codegen/test_abi_decode.py
@@ -0,0 +1,15 @@
+def test_abi_decode(get_contract, abi_encode):
+    contract = """
+@external
+def abi_decode(x: Bytes[64]) -> (uint256, uint256):
+    a: uint256 = 0
+    b: uint256 = 0
+    a, b = _abi_decode(x, types=[uint256, uint256])
+    return a, b
+    """
+
+    c = get_contract(contract)
+
+    arg1 = 123
+    arg2 = 456
+    assert c.abi_decode(abi_encode("(uint256,uint256)", (arg1, arg2))) == (123, 456)
