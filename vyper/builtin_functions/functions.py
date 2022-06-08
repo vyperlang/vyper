43081fb52a40345eb291dee000b31faa11da10a8
tserg <8017125+tserg@users.noreply.github.com>
2022-06-08 09:58:18 +0800

    Apply suggestions from code review
    Co-authored-by: Charles Cooper <cooper.charles.m@gmail.com>

--- vyper/builtin_functions/functions.py
+++ vyper/builtin_functions/functions.py
@@ -2194,11 +2194,12 @@ class ABIDecode(_SimpleBuiltinFunction):
         # Get the size of data
         input_max_len = data.typ.maxlen
 
+        assert abi_min_size <= abi_size_bound, "bad abi type"
         if not abi_min_size <= input_max_len <= abi_size_bound:
             raise StructureException(
                 (
                     "Mismatch between size of input and size of decoded types. "
-                    f"Expected input to be between {abi_min_size} and {abi_size_bound} "
+                    f"length of ABI-encoded {wrapped_typ} is between {abi_min_size} and {abi_size_bound} "
                     f"but input is {input_max_len}."
                 ),
                 expr.args[0],
