fd645b7ce4a4ecf47cde5eb60f3bdcdd6571b6a0
tserg <8017125+tserg@users.noreply.github.com>
2022-06-10 17:05:26 +0800

    fix decimal folding tests

--- tests/ast/nodes/test_evaluate_binop_decimal.py
+++ tests/ast/nodes/test_evaluate_binop_decimal.py
@@ -6,6 +6,7 @@ from hypothesis import strategies as st
 
 from vyper import ast as vy_ast
 from vyper.exceptions import OverflowException, TypeMismatch, ZeroDivisionException
+from vyper.semantics import validate_semantics
 
 st_decimals = st.decimals(
     min_value=-(2 ** 32), max_value=2 ** 32, allow_nan=False, allow_infinity=False, places=10
@@ -28,9 +29,16 @@ def foo(a: decimal, b: decimal) -> decimal:
     """
     contract = get_contract(source)
 
-    vyper_ast = vy_ast.parse_to_ast(f"{left} {op} {right}")
-    old_node = vyper_ast.body[0].value
+    expected_code = f"""
+@external
+def foo() -> decimal:
+    return {left} {op} {right}
+    """
+
     try:
+        vyper_ast = vy_ast.parse_to_ast(expected_code)
+        validate_semantics(vyper_ast, None)
+        old_node = vyper_ast.body[0].body[0].value
         new_node = old_node.evaluate()
         is_valid = True
     except ZeroDivisionException:
@@ -71,10 +79,17 @@ def foo({input_value}) -> decimal:
 
     literal_op = " ".join(f"{a} {b}" for a, b in zip(values, ops))
     literal_op = literal_op.rsplit(maxsplit=1)[0]
-    vyper_ast = vy_ast.parse_to_ast(literal_op)
+    expected = f"""
+@external
+def foo() -> decimal:
+    return {literal_op}
+    """
+
     try:
+        vyper_ast = vy_ast.parse_to_ast(expected)
+        validate_semantics(vyper_ast, None)
         vy_ast.folding.replace_literal_ops(vyper_ast)
-        expected = vyper_ast.body[0].value.value
+        expected = vyper_ast.body[0].body[0].value.value
         is_valid = -(2 ** 127) <= expected < 2 ** 127
     except (OverflowException, ZeroDivisionException):
         # for overflow or division/modulus by 0, expect the contract call to revert
