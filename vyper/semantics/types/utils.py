2ee7f57e80a705c4d7f0dba9d9ad9cb4a1bd0a90
tserg <8017125+tserg@users.noreply.github.com>
2022-06-11 11:15:08 +0800

    fix lint

--- vyper/semantics/types/utils.py
+++ vyper/semantics/types/utils.py
@@ -12,7 +12,7 @@ from vyper.exceptions import (
 )
 from vyper.semantics.namespace import get_namespace
 from vyper.semantics.types.bases import BaseTypeDefinition, DataLocation
-from vyper.semantics.types.indexable.sequence import ArrayDefinition
+from vyper.semantics.types.indexable.sequence import ArrayDefinition, TupleDefinition
 from vyper.semantics.validation.levenshtein_utils import get_levenshtein_error_suggestions
 from vyper.semantics.validation.utils import get_exact_type_from_node, get_index_value
 
