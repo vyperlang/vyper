e8a50ab58294fce245cfea13d951735831564a5f
Charles Cooper <cooper.charles.m@gmail.com>
2022-06-16 19:32:28 -0400

    fix a comment

--- vyper/codegen/module.py
+++ vyper/codegen/module.py
@@ -228,7 +228,7 @@ def generate_ir_for_module(global_ctx: GlobalContext) -> Tuple[IRnode, IRnode, F
         deploy_code.append(["deploy", 0, runtime, 0])
 
     if init_function and init_function._metadata["type"].is_factory:
-        # inception
+        # initception
         runtime = deploy_code
         deploy_code = ["deploy", 0, deploy_code, 0]
 
