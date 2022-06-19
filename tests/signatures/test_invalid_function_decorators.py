6ae0eedbc3aa63cd3168c84acc0fab7adafc3307
Charles Cooper <cooper.charles.m@gmail.com>
2022-06-19 16:07:18 -0400

    feat: allow "nonreentrant" on view functions (#2921)
    semantics: on view functions, the sstores are not issued, but the
    `sload` check is.

--- tests/signatures/test_invalid_function_decorators.py
+++ tests/signatures/test_invalid_function_decorators.py
@@ -6,7 +6,7 @@ from vyper.exceptions import StructureException
 FAILING_CONTRACTS = [
     """
 @external
-@view
+@pure
 @nonreentrant('lock')
 def nonreentrant_foo() -> uint256:
     return 1
--- vyper/codegen/function_definitions/utils.py
+++ vyper/codegen/function_definitions/utils.py
@@ -1,8 +1,16 @@
+from vyper.semantics.types.function import StateMutability
+
+
 def get_nonreentrant_lock(func_type):
     if not func_type.nonreentrant:
         return ["pass"], ["pass"]
 
     nkey = func_type.reentrancy_key_position.position
-    nonreentrant_pre = [["seq", ["assert", ["iszero", ["sload", nkey]]], ["sstore", nkey, 1]]]
-    nonreentrant_post = [["sstore", nkey, 0]]
-    return nonreentrant_pre, nonreentrant_post
+
+    if func_type.mutability == StateMutability.VIEW:
+        return [["assert", ["iszero", ["sload", nkey]]]], [["seq"]]
+
+    else:
+        nonreentrant_pre = [["seq", ["assert", ["iszero", ["sload", nkey]]], ["sstore", nkey, 1]]]
+        nonreentrant_post = [["sstore", nkey, 0]]
+        return nonreentrant_pre, nonreentrant_post
