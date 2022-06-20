9a363dc3dd3eef466412c19a0e17e047db30b04e
Charles Cooper <cooper.charles.m@gmail.com>
2022-06-20 01:08:29 -0400

    optimize runtime values for nonreentrant slots (#2923)
    * update runtime values for nonreentrant slots
    
    after berlin, it is cheaper to use a nonzero slot. this brings the total
    cost of a nonreentrancy lock down from 4400 gas to 2300 gas.
    
    * fiddle with the nonreentrant values

--- vyper/codegen/function_definitions/utils.py
+++ vyper/codegen/function_definitions/utils.py
@@ -1,3 +1,4 @@
+from vyper.evm.opcodes import version_check
 from vyper.semantics.types.function import StateMutability
 
 
@@ -7,10 +8,20 @@ def get_nonreentrant_lock(func_type):
 
     nkey = func_type.reentrancy_key_position.position
 
+    if version_check(begin="berlin"):
+        # any nonzero values would work here (see pricing as of net gas
+        # metering); these values are chosen so that downgrading to the
+        # 0,1 scheme (if it is somehow necessary) is safe.
+        final_value, temp_value = 3, 2
+    else:
+        final_value, temp_value = 0, 1
+
+    check_notset = ["assert", ["ne", temp_value, ["sload", nkey]]]
+
     if func_type.mutability == StateMutability.VIEW:
-        return [["assert", ["iszero", ["sload", nkey]]]], [["seq"]]
+        return [check_notset], [["seq"]]
 
     else:
-        nonreentrant_pre = [["seq", ["assert", ["iszero", ["sload", nkey]]], ["sstore", nkey, 1]]]
-        nonreentrant_post = [["sstore", nkey, 0]]
-        return nonreentrant_pre, nonreentrant_post
+        pre = ["seq", check_notset, ["sstore", nkey, temp_value]]
+        post = ["sstore", nkey, final_value]
+        return [pre], [post]
