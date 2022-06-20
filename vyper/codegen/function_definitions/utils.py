ed1b7fe3dc2ce5a47d8dea1ea19ac2f8cf189398
Charles Cooper <cooper.charles.m@gmail.com>
2022-06-19 20:53:16 -0400

    update runtime values for nonreentrant slots
    after berlin, it is cheaper to use a nonzero slot. this brings the total
    cost of a nonreentrancy lock down from 4400 gas to 2300 gas.

--- vyper/codegen/function_definitions/utils.py
+++ vyper/codegen/function_definitions/utils.py
@@ -1,3 +1,4 @@
+from vyper.evm.opcodes import version_check
 from vyper.semantics.types.function import StateMutability
 
 
@@ -7,10 +8,17 @@ def get_nonreentrant_lock(func_type):
 
     nkey = func_type.reentrancy_key_position.position
 
+    if version_check(begin="berlin"):
+        final_value, temp_value = 1, 2
+    else:
+        final_value, temp_value = 0, 1
+
+    check_notset = ["assert", ["ne", temp_value, ["sload", nkey]]]
+
     if func_type.mutability == StateMutability.VIEW:
-        return [["assert", ["iszero", ["sload", nkey]]]], [["seq"]]
+        return [check_notset], [["seq"]]
 
     else:
-        nonreentrant_pre = [["seq", ["assert", ["iszero", ["sload", nkey]]], ["sstore", nkey, 1]]]
-        nonreentrant_post = [["sstore", nkey, 0]]
-        return nonreentrant_pre, nonreentrant_post
+        pre = ["seq", check_notset, ["sstore", nkey, temp_value]]
+        post = ["sstore", nkey, final_value]
+        return [pre], [post]
