aa71c4b9c62e23d21aa054ea70b801880be19c2d
tserg <8017125+tserg@users.noreply.github.com>
2022-06-23 16:42:31 +0800

    add passing tests

---
+++ tests/parser/functions/test_minmax_value.py
@@ -0,0 +1,46 @@
+import pytest
+
+from vyper.codegen.types import (
+    DECIMAL_TYPES,
+    INTEGER_TYPES,
+    parse_decimal_info,
+    parse_integer_typeinfo,
+)
+from vyper.exceptions import InvalidType
+from vyper.utils import int_bounds
+
+
+@pytest.mark.parametrize("typ", sorted(INTEGER_TYPES))
+@pytest.mark.parametrize("op", ("min_value", "max_value"))
+def test_minmax_value_int(get_contract, op, typ):
+    code = f"""
+@external
+def foo() -> {typ}:
+    return {op}({typ})
+    """
+    c = get_contract(code)
+
+    typ_info = parse_integer_typeinfo(typ)
+    (lo, hi) = int_bounds(typ_info.is_signed, typ_info.bits)
+    if op == "min_value":
+        assert c.foo() == lo
+    elif op == "max_value":
+        assert c.foo() == hi
+
+
+@pytest.mark.parametrize("typ", sorted(DECIMAL_TYPES))
+@pytest.mark.parametrize("op", ("min_value", "max_value"))
+def test_minmax_value_decimal(get_contract, op, typ):
+    code = f"""
+@external
+def foo() -> {typ}:
+    return {op}({typ})
+    """
+    c = get_contract(code)
+
+    typ_info = parse_decimal_info(typ)
+    (lo, hi) = typ_info.decimal_bounds
+    if op == "min_value":
+        assert c.foo() == lo
+    elif op == "max_value":
+        assert c.foo() == hi
