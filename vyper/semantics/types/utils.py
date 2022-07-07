c38f8c0141bb4ed87e5d9beaba8e0aac60d685e7
tserg <8017125+tserg@users.noreply.github.com>
2022-07-07 18:46:14 +0800

    refactor check_kwargable and check_constant'

--- vyper/semantics/types/utils.py
+++ vyper/semantics/types/utils.py
@@ -235,28 +235,10 @@ def check_constant(node: vy_ast.VyperNode, vyper_module: vy_ast.Module = None) -
         return True
     if isinstance(node, (vy_ast.Tuple, vy_ast.List)):
         return all(check_constant(item) for item in node.elements)
-    if isinstance(node, vy_ast.Call):
-        # Literal structs
-        args = node.args
-        if len(args) == 1 and isinstance(args[0], vy_ast.Dict):
-            return all(check_constant(v) for v in args[0].values)
-
-        # Foldable builtin functions
-        node_type = get_exact_type_from_node(node.func)
-        if hasattr(node_type, "evaluate"):
-            try:
-                node_type.evaluate(node)
-                return True
-            except UnfoldableNode:
-                return False
-
-    # Foldable literal ops
-    if isinstance(node, (vy_ast.BoolOp, vy_ast.BinOp, vy_ast.UnaryOp, vy_ast.Compare)):
-        try:
-            node.validate_foldable()  # type: ignore
-            return True
-        except UnfoldableNode:
-            return False
+    from vyper.ast.utils import get_constant_value
+    val = get_constant_value(node)
+    if val is not None:
+        return True
 
     value_type = get_exact_type_from_node(node)
     return getattr(value_type, "is_constant", False)
@@ -270,24 +252,11 @@ def check_kwargable(node: vy_ast.VyperNode) -> bool:
         return True
     if isinstance(node, (vy_ast.Tuple, vy_ast.List)):
         return all(check_kwargable(item) for item in node.elements)
-    if isinstance(node, vy_ast.Call):
-        args = node.args
-        if len(args) == 1 and isinstance(args[0], vy_ast.Dict):
-            return all(check_kwargable(v) for v in args[0].values)
 
-        node_type = get_exact_type_from_node(node.func)
-        if hasattr(node_type, "evaluate"):
-            try:
-                node_type.evaluate(node)
-                return True
-            except UnfoldableNode:
-                return False
-    if isinstance(node, (vy_ast.BoolOp, vy_ast.BinOp, vy_ast.UnaryOp, vy_ast.Compare)):
-        try:
-            node.validate_foldable()  # type: ignore
-            return True
-        except UnfoldableNode:
-            return False
+    from vyper.ast.utils import get_constant_value
+    val = get_constant_value(node)
+    if val is not None:
+        return True
 
     value_type = get_exact_type_from_node(node)
     return getattr(value_type, "not_assignable", False)
