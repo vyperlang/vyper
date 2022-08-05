vyperlang/vyper

commit be2c59a604070c75212dca90beb4c33fed908c8b

perf: remove compiler hotspots (#2981)
the changed lines triggered re-traversals of the entire AST, resulting
in O(n^2) build times. for non-trivial contracts near the 24KB limit,
this resulted in near-minute long build times.

this commit also adds timer utility functions so that the profiling can
be easily done again.

vyper/ast/expansion.py
@@ -37,7 +37,7 @@ def generate_public_variable_getters(vyper_module: vy_ast.Module) -> None:
 
         # use the annotation node as a base to build the input args and return type
         # starting with `args[0]` to remove the surrounding `public()` call`
-        annotation = copy.deepcopy(node.annotation.args[0])
+        annotation = copy.copy(node.annotation.args[0])
 
         # the base return statement is an `Attribute` node, e.g. `self.<var_name>`
         # for each input type we wrap it in a `Subscript` to access a specific member
@@ -67,10 +67,6 @@ def generate_public_variable_getters(vyper_module: vy_ast.Module) -> None:
 
         # after iterating the input types, the remaining annotation node is our return type
         return_node = annotation
-        if isinstance(return_node, vy_ast.Name) and return_node.id != return_type._id:
-            # special case when the return type is an interface
-            # TODO allow interfaces as return types and remove this
-            return_node.id = return_type._id
 
         # join everything together as a new `FunctionDef` node, annotate it
         # with the type, and append it to the existing `Module` node
@@ -83,6 +79,7 @@ def generate_public_variable_getters(vyper_module: vy_ast.Module) -> None:
             returns=return_node,
         )
         expanded._metadata["type"] = func_type
+        return_node.set_parent(expanded)
         vyper_module.add_to_body(expanded)
 
 

tests/ast/nodes/test_replace_in_tree.py
@@ -58,16 +58,6 @@ def test_parents_children():
     assert new_node in test_tree.get_descendants()
 
 
-def test_node_does_not_exist():
-    test_tree = vy_ast.parse_to_ast("foo = 42")
-    old_node = test_tree.body[0].target
-
-    new_node = vy_ast.parse_to_ast("42").body[0].value
-
-    with pytest.raises(CompilerPanic):
-        test_tree.replace_in_tree(new_node, old_node)
-
-
 def test_cannot_replace_twice():
     test_tree = vy_ast.parse_to_ast("foo = 42")
     old_node = test_tree.body[0].target

vyper/ast/folding.py
@@ -1,4 +1,3 @@
-import copy
 from typing import Union
 
 from vyper.ast import nodes as vy_ast
@@ -286,8 +285,7 @@ def replace_constant(
                 continue
 
         try:
-            # Codegen may mutate AST (particularly structs).
-            replacement_node = copy.deepcopy(replacement_node)
+            # note: _replace creates a copy of the replacement_node
             new_node = _replace(node, replacement_node, type_=type_)
         except UnfoldableNode:
             if raise_on_error:

vyper/ast/nodes.py
@@ -248,8 +248,7 @@ class VyperNode:
         **kwargs : dict
             Dictionary of fields to be included within the node.
         """
-        self._parent = parent
-        self._depth = getattr(parent, "_depth", -1) + 1
+        self.set_parent(parent)
         self._children: set = set()
         self._metadata: dict = {}
 
@@ -284,6 +283,11 @@ class VyperNode:
         # add to children of parent last to ensure an accurate hash is generated
         if parent is not None:
             parent._children.add(self)
+
+    # set parent, can be useful when inserting copied nodes into the AST
+    def set_parent(self, parent: "VyperNode"):
+        self._parent = parent
+        self._depth = getattr(parent, "_depth", -1) + 1
 
     @classmethod
     def from_node(cls, node: "VyperNode", **kwargs) -> "VyperNode":
@@ -588,8 +592,7 @@ class Module(TopLevel):
         Parameters
         ----------
         old_node : VyperNode
-            Node object to be replaced. If the node does not currently exist
-            within the AST, a `CompilerPanic` is raised.
+            Node object to be replaced.
         new_node : VyperNode
             Node object to replace new_node.
 
@@ -598,8 +601,6 @@ class Module(TopLevel):
         None
         """
         parent = old_node._parent
-        if old_node not in self.get_descendants(type(old_node)):
-            raise CompilerPanic("Node to be replaced does not exist within the tree")
 
         if old_node not in parent._children:
             raise CompilerPanic("Node to be replaced does not exist within parent children")

vyper/utils.py
@@ -1,6 +1,9 @@
 import binascii
+import contextlib
 import decimal
+import functools
 import sys
+import time
 import traceback
 import warnings
 from typing import List, Union
@@ -335,6 +338,27 @@ def indent(text: str, indent_chars: Union[str, List[str]] = " ", level: int = 1)
         raise ValueError("Unrecognized indentation characters value")
 
     return "".join(indented_lines)
+
+
+def timeit(func):
+    @functools.wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f"Function {func.__name__} Took {total_time:.4f} seconds")
+        return result
+
+    return timeit_wrapper
+
+
+@contextlib.contextmanager
+def timer(msg):
+    t0 = time.time()
+    yield
+    t1 = time.time()
+    print(f"{msg} took {t1 - t0}s")
 
 
 def annotate_source_code(

commit c0a68f5c6717d61c689e284dd45bf5a3582ec1fe

fix: public DynArray getters
DynArray getter AST expansion had a regression in be2c59a604. the
removed lines 70-73 modified the return node ID, which incidentally
patched the fact that the type unpacking logic in the previous lines
didn't account for DynArrays properly. this commit handles the DynArray
case properly

vyper/ast/expansion.py
@@ -53,6 +53,9 @@ def generate_public_variable_getters(vyper_module: vy_ast.Module) -> None:
             if annotation.value.get("id") == "HashMap":  # type: ignore
                 # for a HashMap, split the key/value types and use the key type as the next arg
                 arg, annotation = annotation.slice.value.elements  # type: ignore
+            elif annotation.value.get("id") == "DynArray":
+                arg = vy_ast.Name(id=type_._id)
+                annotation = annotation.slice.value.elements[0]
             else:
                 # for other types, build an input arg node from the expected type
                 # and remove the outer `Subscript` from the annotation
@@ -66,7 +69,7 @@ def generate_public_variable_getters(vyper_module: vy_ast.Module) -> None:
             )
 
         # after iterating the input types, the remaining annotation node is our return type
-        return_node = annotation
+        return_node = copy.copy(annotation)
 
         # join everything together as a new `FunctionDef` node, annotate it
         # with the type, and append it to the existing `Module` node