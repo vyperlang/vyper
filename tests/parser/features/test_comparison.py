vyperlang/vyper

commit 2db84a90cdff9da08ed937d13763ddc1c93f2660

unroll `in` and `not in` expressions (#3017)
for list literals, the following
```
x in [a, b, c]
```
can be unrolled to
```
x == a or x == b or x == c
```

likewise,
```
x not in [a, b, c]
```
can be unrolled to
```
x != a and x != b and x != c
```

this commit slightly refactors the code for logical and and logical or
to make this easier

it also fixes some optimizer heuristics wrt when to insert iszero vs not

vyper/codegen/expr.py
@@ -11,7 +11,6 @@ from vyper.codegen.core import (
     get_dyn_array_count,
     get_element_ptr,
     getpos,
-    make_setter,
     pop_dyn_array,
     unwrap_location,
 )
@@ -399,12 +398,14 @@ class Expr:
                 "`in` not allowed for arrays of non-base types, tracked in issue #2637", self.expr
             )
 
+        left = unwrap_location(left)
+
         if isinstance(self.expr.op, vy_ast.In):
             found, not_found = 1, 0
         elif isinstance(self.expr.op, vy_ast.NotIn):
             found, not_found = 0, 1
-        else:
-            return  # pragma: notest
+        else:  # pragma: no cover
+            return
 
         i = IRnode.from_list(self.context.fresh_varname("in_ix"), typ="uint256")
 
@@ -412,18 +413,22 @@ class Expr:
 
         ret = ["seq"]
 
-        left = unwrap_location(left)
         with left.cache_when_complex("needle") as (b1, left), right.cache_when_complex(
             "haystack"
         ) as (b2, right):
+            # unroll the loop for compile-time list literals
             if right.value == "multi":
-                # Copy literal to memory to be compared.
-                tmp_list = IRnode.from_list(
-                    self.context.new_internal_variable(right.typ), typ=right.typ, location=MEMORY
-                )
-                ret.append(make_setter(tmp_list, right))
+                # empty list literals should be rejected at typechecking time
+                assert len(right.args) > 0
+                if isinstance(self.expr.op, vy_ast.In):
+                    checks = [["eq", left, val] for val in right.args]
+                    return b1.resolve(b2.resolve(Expr._logical_or(checks)))
+                if isinstance(self.expr.op, vy_ast.NotIn):
+                    checks = [["ne", left, val] for val in right.args]
+                    return b1.resolve(b2.resolve(Expr._logical_and(checks)))
 
-                right = tmp_list
+            # general case: loop over the list and check each element
+            # for equality
 
             # location of i'th item from list
             ith_element_ptr = get_element_ptr(right, i, array_bounds_check=False)
@@ -524,41 +529,54 @@ class Expr:
         return IRnode.from_list([op, left, right], typ="bool")
 
     def parse_BoolOp(self):
+        values = []
         for value in self.expr.values:
             # Check for boolean operations with non-boolean inputs
-            _expr = Expr.parse_value_expr(value, self.context)
-            if not is_base_type(_expr.typ, "bool"):
-                return
+            ir_val = Expr.parse_value_expr(value, self.context)
+            assert is_base_type(ir_val.typ, "bool")
+            values.append(ir_val)
 
-        def _build_if_ir(condition, true, false):
-            # generate a basic if statement in IR
-            o = ["if", condition, true, false]
-            return o
+        assert len(values) >= 2, "bad BoolOp"
 
         if isinstance(self.expr.op, vy_ast.And):
-            # create the initial `x and y` from the final two values
-            ir_node = _build_if_ir(
-                Expr.parse_value_expr(self.expr.values[-2], self.context),
-                Expr.parse_value_expr(self.expr.values[-1], self.context),
-                [0],
-            )
-            # iterate backward through the remaining values
-            for node in self.expr.values[-3::-1]:
-                ir_node = _build_if_ir(Expr.parse_value_expr(node, self.context), ir_node, [0])
+            return Expr._logical_and(values)
 
-        elif isinstance(self.expr.op, vy_ast.Or):
-            # create the initial `x or y` from the final two values
-            ir_node = _build_if_ir(
-                Expr.parse_value_expr(self.expr.values[-2], self.context),
-                [1],
-                Expr.parse_value_expr(self.expr.values[-1], self.context),
-            )
+        if isinstance(self.expr.op, vy_ast.Or):
+            return Expr._logical_or(values)
 
-            # iterate backward through the remaining values
-            for node in self.expr.values[-3::-1]:
-                ir_node = _build_if_ir(Expr.parse_value_expr(node, self.context), 1, ir_node)
-        else:
-            raise TypeCheckFailure(f"Unexpected boolean operator: {type(self.expr.op).__name__}")
+        raise TypeCheckFailure(f"Unexpected boolop: {self.expr.op}")  # pragma: notest
+
+    @staticmethod
+    def _logical_and(values):
+        # return the logical and of a list of IRnodes
+
+        # create a nested if statement starting from the
+        # innermost node. note this also serves as the base case
+        # (`_logical_and([x]) == x`)
+        ir_node = values[-1]
+
+        # iterate backward through the remaining values,
+        # nesting further at each step
+        for val in values[-2::-1]:
+            # `x and y` => `if x { then y } { else 0 }`
+            ir_node = ["if", val, ir_node, 0]
+
+        return IRnode.from_list(ir_node, typ="bool")
+
+    @staticmethod
+    def _logical_or(values):
+        # return the logical or of a list of IRnodes
+
+        # create a nested if statement starting from the
+        # innermost node. note this also serves as the base case
+        # (`_logical_or([x]) == x`)
+        ir_node = values[-1]
+
+        # iterate backward through the remaining values,
+        # nesting further at each step
+        for val in values[-2::-1]:
+            # `x or y` => `if x { then 1 } { else y }`
+            ir_node = ["if", val, 1, ir_node]
 
         return IRnode.from_list(ir_node, typ="bool")
 

tests/ast/nodes/test_evaluate_compare.py
@@ -55,6 +55,11 @@ def test_compare_in(left, right, get_contract):
 def foo(a: int128, b: int128[{len(right)}]) -> bool:
     c: int128[{len(right)}] = b
     return a in c
+
+@external
+def bar(a: int128) -> bool:
+    # note: codegen unrolls to `a == right[0] or a == right[1] ...`
+    return a in {right}
     """
     contract = get_contract(source)
 
@@ -62,7 +67,12 @@ def foo(a: int128, b: int128[{len(right)}]) -> bool:
     old_node = vyper_ast.body[0].value
     new_node = old_node.evaluate()
 
+    # check runtime == fully folded
     assert contract.foo(left, right) == new_node.value
+    # check unrolled runtime == fully folded
+    assert contract.bar(left) == new_node.value
+    # check folding matches python semantics
+    assert (left in right) == new_node.value
 
 
 @pytest.mark.fuzzing
@@ -74,6 +84,11 @@ def test_compare_not_in(left, right, get_contract):
 def foo(a: int128, b: int128[{len(right)}]) -> bool:
     c: int128[{len(right)}] = b
     return a not in c
+
+@external
+def bar(a: int128) -> bool:
+    # note: codegen unrolls to `a != right[0] and a != right[1] ...`
+    return a not in {right}
     """
     contract = get_contract(source)
 
@@ -81,7 +96,12 @@ def foo(a: int128, b: int128[{len(right)}]) -> bool:
     old_node = vyper_ast.body[0].value
     new_node = old_node.evaluate()
 
+    # check runtime == fully folded
     assert contract.foo(left, right) == new_node.value
+    # check unrolled runtime == fully folded
+    assert contract.bar(left) == new_node.value
+    # check folding matches python semantics
+    assert (left not in right) == new_node.value
 
 
 @pytest.mark.parametrize("op", ["==", "!=", "<", "<=", ">=", ">"])

tests/compiler/ir/test_optimize_ir.py
@@ -21,7 +21,14 @@ optimize_list = [
     # condition rewriter
     (["if", ["eq", "x", "y"], "pass"], ["if", ["iszero", ["xor", "x", "y"]], "pass"]),
     (["if", "cond", 1, 0], ["if", ["iszero", "cond"], 0, 1]),
+    (["if", ["ne", "x", 1], [1]], None),
+    (
+        # TODO: this is perf issue (codegen should usually generate `if (ne x y)` though)
+        ["if", ["iszero", ["eq", "x", "y"]], [1]],
+        ["if", ["iszero", ["iszero", ["xor", "x", "y"]]], 1],
+    ),
     (["assert", ["eq", "x", "y"]], ["assert", ["iszero", ["xor", "x", "y"]]]),
+    (["assert", ["ne", "x", "y"]], None),
     # nesting
     (["mstore", 0, ["eq", 1, 2]], ["mstore", 0, 0]),
     # conditions

tests/parser/types/test_dynamic_array.py
@@ -418,6 +418,27 @@ def check(a: {type}) -> bool:
     assert c.check(false_value) is False
 
 
+@pytest.mark.parametrize("type_", ("uint256", "bytes32", "address"))
+def test_member_in_empty_list(get_contract_with_gas_estimation, type_):
+    code = f"""
+@external
+def check_in(s: uint128) -> bool:
+    a: {type_} = convert(s, {type_})
+    x: DynArray[{type_}, 2] = []
+    return a in x
+
+@external
+def check_not_in(s: uint128) -> bool:
+    a: {type_} = convert(s, {type_})
+    x: DynArray[{type_}, 2] = []
+    return a not in x
+    """
+    c = get_contract_with_gas_estimation(code)
+    for s in (0, 1, 2, 3):
+        assert c.check_in(s) is False
+        assert c.check_not_in(s) is True
+
+
 @pytest.mark.parametrize(
     "type,values,false_values",
     [
@@ -1598,6 +1619,26 @@ def ix(i: uint256) -> decimal:
         assert c.ix(i) == p
     # assert oob
     assert_tx_failed(lambda: c.ix(len(some_good_primes) + 1))
+
+
+# CMC 2022-08-04 these are blocked due to typechecker bug; leaving as
+# negative tests so we know if/when the typechecker is fixed.
+# (don't consider it a high priority to fix since membership in
+# in empty list literal seems like something we should plausibly
+# reject at compile-time anyway)
+def test_empty_list_membership_fail(get_contract, assert_compile_failed):
+    code = """
+@external
+def foo(x: uint256) -> bool:
+    return x in []
+    """
+    assert_compile_failed(lambda: get_contract(code))
+    code = """
+@external
+def foo(x: uint256) -> bool:
+    return x not in []
+    """
+    assert_compile_failed(lambda: get_contract(code))
 
 
 # TODO test loops

vyper/ast/nodes.py
@@ -1108,6 +1108,8 @@ class Compare(VyperNode):
         if not isinstance(left, Constant):
             raise UnfoldableNode("Node contains invalid field(s) for evaluation")
 
+        # CMC 2022-08-04 we could probably remove these evaluation rules as they
+        # are taken care of in the IR optimizer now.
         if isinstance(self.op, (In, NotIn)):
             if not isinstance(right, List):
                 raise UnfoldableNode("Node contains invalid field(s) for evaluation")

vyper/ir/optimizer.py
@@ -376,8 +376,10 @@ def _optimize_binop(binop, args, ann, parent_op):
             # note that (xor (-1) x) has its own rule
             return finalize("iszero", [["xor", args[0], args[1]]])
 
-        if binop == "ne":
-            # trigger other optimizations
+        if binop == "ne" and parent_op == "iszero":
+            # for iszero, trigger other optimizations
+            # (for `if` and `assert`, `ne` will generate two ISZEROs
+            # which will get optimized out during assembly)
             return finalize("iszero", [["eq", *args]])
 
         # TODO can we do this?
@@ -519,7 +521,7 @@ def _optimize(node: IRnode, parent: Optional[IRnode]) -> Tuple[bool, IRnode]:
                 # return the first branch
                 return finalize("seq", [argz[1]])
 
-        elif len(argz) == 3 and argz[0].value != "iszero":
+        elif len(argz) == 3 and argz[0].value not in ("iszero", "ne"):
             # if(x) compiles to jumpi(_, iszero(x))
             # there is an asm optimization for the sequence ISZERO ISZERO..JUMPI
             # so we swap the branches here to activate that optimization.

commit ec3e5880bb8e221fed8f32723eb7cc27c9d6ba10

fix: `in` expr when list members are vars (#3035)
fix a regression introduced in 2db84a90cd. when the members of a list
literal are not themselves literals, they need to be dereferenced.

vyper/codegen/expr.py
@@ -420,12 +420,14 @@ class Expr:
             if right.value == "multi":
                 # empty list literals should be rejected at typechecking time
                 assert len(right.args) > 0
+                args = [unwrap_location(val) for val in right.args]
                 if isinstance(self.expr.op, vy_ast.In):
-                    checks = [["eq", left, val] for val in right.args]
+                    checks = [["eq", left, val] for val in args]
                     return b1.resolve(b2.resolve(Expr._logical_or(checks)))
                 if isinstance(self.expr.op, vy_ast.NotIn):
-                    checks = [["ne", left, val] for val in right.args]
+                    checks = [["ne", left, val] for val in args]
                     return b1.resolve(b2.resolve(Expr._logical_and(checks)))
+                return  # fail
 
             # general case: loop over the list and check each element
             # for equality

tests/parser/features/test_comparison.py
@@ -1,0 +1,48 @@
+# test syntactic comparisons
+# most tests under tests/ast/nodes/test_evaluate_compare.py
+import pytest
+
+
+def test_3034_verbatim(get_contract):
+    # test issue #3034 exactly
+    code = """
+@view
+@external
+def showError():
+    adr1: address = 0xFbEEa1C75E4c4465CB2FCCc9c6d6afe984558E20
+    adr2: address = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
+    adr3: address = 0xFbEEa1C75E4c4465CB2FCCc9c6d6afe984558E20
+    assert adr1 in [adr2,adr3], "error in comparison with in statement!"
+    """
+    c = get_contract(code)
+    c.showError()
+
+
+@pytest.mark.parametrize("invert", (True, False))
+def test_in_list(get_contract, invert):
+    # test slightly more complicated variations of #3034
+    INVERT = "not" if invert else ""
+    code = f"""
+SOME_ADDRESS: constant(address) = 0x22cb70ba2EC32347D9e32740fc14b2f3d038Ce8E
+@view
+@external
+def test_in(addr: address) -> bool:
+    x: address = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
+    y: address = 0xFbEEa1C75E4c4465CB2FCCc9c6d6afe984558E20
+    # in list which
+    return addr {INVERT} in [x, y, SOME_ADDRESS]
+    """
+    c = get_contract(code)
+    should_in = [
+        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        "0xFbEEa1C75E4c4465CB2FCCc9c6d6afe984558E20",
+        "0x22cb70ba2EC32347D9e32740fc14b2f3d038Ce8E",
+    ]
+    should_not_in = [
+        "0x" + "00" * 20,
+        "0xfBeeA1C75E4C4465CB2fccC9C6d6AFe984558e21",  # y but last bit flipped
+    ]
+    for t in should_in:
+        assert c.test_in(t) is (True if not invert else False)
+    for t in should_not_in:
+        assert c.test_in(t) is (True if invert else False)