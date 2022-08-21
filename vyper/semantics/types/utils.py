3b04db54c61e35de8f0e4b92edb4e1a8ca007668
Charles Cooper <cooper.charles.m@gmail.com>
2022-08-21 10:27:58 -0700

    remove dead code

--- vyper/semantics/types/utils.py
+++ vyper/semantics/types/utils.py
@@ -17,56 +17,6 @@ from vyper.semantics.analysis.levenshtein_utils import get_levenshtein_error_sug
 from vyper.semantics.analysis.utils import get_index_value
 
 
-class StringEnum(enum.Enum):
-    @staticmethod
-    def auto():
-        return enum.auto()
-
-    # Must be first, or else won't work, specifies what .value is
-    def _generate_next_value_(name, start, count, last_values):
-        return name.lower()
-
-    # Override ValueError with our own internal exception
-    @classmethod
-    def _missing_(cls, value):
-        raise VyperInternalException(f"{value} is not a valid {cls.__name__}")
-
-    @classmethod
-    def is_valid_value(cls, value: str) -> bool:
-        return value in set(o.value for o in cls)
-
-    @classmethod
-    def options(cls) -> List["StringEnum"]:
-        return list(cls)
-
-    @classmethod
-    def values(cls) -> List[str]:
-        return [v.value for v in cls.options()]
-
-    # Comparison operations
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, self.__class__):
-            raise CompilerPanic("Can only compare like types.")
-        return self is other
-
-    # Python normally does __ne__(other) ==> not self.__eq__(other)
-
-    def __lt__(self, other: object) -> bool:
-        if not isinstance(other, self.__class__):
-            raise CompilerPanic("Can only compare like types.")
-        options = self.__class__.options()
-        return options.index(self) < options.index(other)  # type: ignore
-
-    def __le__(self, other: object) -> bool:
-        return self.__eq__(other) or self.__lt__(other)
-
-    def __gt__(self, other: object) -> bool:
-        return not self.__le__(other)
-
-    def __ge__(self, other: object) -> bool:
-        return self.__eq__(other) or self.__gt__(other)
-
-
 def type_from_abi(abi_type: Dict) -> VyperType:
     """
     Return a type object from an ABI type definition.
@@ -151,7 +101,7 @@ def type_from_annotation(node: vy_ast.VyperNode) -> VyperType:
         and node.value.get("id") != "DynArray"
     ):
         # TODO: handle `is_immutable` for arrays
-        # if type can be an array and node is a subscript, create an `ArrayDefinition`
+        # if type can be an array and node is a subscript, create an `SArrayT`
         length = get_index_value(node.slice)
         value_type = type_from_annotation(node.value)
         return SArrayT(value_type, length)
