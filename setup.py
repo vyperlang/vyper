d85cf2aebd9fe7919d895a3b6e3cf5c46f04b3ef
Charles Cooper <cooper.charles.m@gmail.com>
2022-08-23 12:48:47 -0700

    chore: drop python 3.7 support (#3071)
    drop tests for 3.7 since isqrt does not exist in 3.8. note that vyper can
    still be installed on 3.7 but we no longer officially test it.

--- setup.py
+++ setup.py
@@ -113,7 +113,6 @@ setup(
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
