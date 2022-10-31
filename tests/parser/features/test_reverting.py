dfe55e7dcb7b936bf4542a4bb70413fcbe13744f
z80 <z80@ophy.xyz>
2022-10-31 17:39:26 -0400

    add test

---
+++ tests/parser/features/test_reverting.py
@@ -0,0 +1,26 @@
+from decimal import Decimal
+
+import eth_abi
+import pytest
+
+from eth_tester.exceptions import TransactionFailed
+
+from vyper.utils import keccak256
+
+pytestmark = pytest.mark.usefixtures("memory_mocker")
+
+
+def test_revert_reason(w3, assert_tx_failed, tester, keccak, get_contract_with_gas_estimation):
+    reverty_code = """
+@external
+def foo():
+    val: uint256 = 5
+    data: Bytes[100] = _abi_encode(val, method_id=method_id("NoFives(uint256)"))
+    raw_revert(data)
+    """
+
+    assert_tx_failed(
+        lambda: get_contract_with_gas_estimation(reverty_code).foo(transact={}),
+        TransactionFailed,
+        exc_text="execution reverted: b'.\\x7f\\xb9\\x1f\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x05'",
+    )
