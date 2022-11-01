0bfe601a6aee5b2c4bcee77a013855ef426b93de
z80 <z80@ophy.xyz>
2022-10-31 23:40:49 -0400

    update test

--- tests/parser/features/test_reverting.py
+++ tests/parser/features/test_reverting.py
@@ -1,10 +1,47 @@
+import re
+
 import pytest
+from eth_abi import encode
 from eth_tester.exceptions import TransactionFailed
+
+from vyper.utils import keccak256
 
 pytestmark = pytest.mark.usefixtures("memory_mocker")
 
 
+def build_revert_bytestring(errorDecl, *data):
+    # revert bytes should be 4-byte selector (from keccak of error definition)
+    # followed by abi-encoded data
+    error_selector = keccak256(bytes(errorDecl, "utf-8"))[:4]
+    match = re.search(r"\((.*?)\)", errorDecl)  # .group(1)
+    if match and match.group(1) != "":
+        arg_types = match.group(1)
+        encoded_data = encode(arg_types.split(","), data)
+        return b"".join([error_selector, encoded_data])
+    else:
+        return error_selector
+
+
 def test_revert_reason(w3, assert_tx_failed, tester, keccak, get_contract_with_gas_estimation):
+    reverty_code = """
+@external
+def foo():
+    data: Bytes[4] = method_id("NoFives()")
+    raw_revert(data)
+    """
+
+    revert_bytes = build_revert_bytestring("NoFives()")
+
+    assert_tx_failed(
+        lambda: get_contract_with_gas_estimation(reverty_code).foo(transact={}),
+        TransactionFailed,
+        exc_text=f"execution reverted: {revert_bytes}",
+    )
+
+
+def test_revert_reason_typed(
+    w3, assert_tx_failed, tester, keccak, get_contract_with_gas_estimation
+):
     reverty_code = """
 @external
 def foo():
@@ -13,14 +50,10 @@ def foo():
     raw_revert(data)
     """
 
+    revert_bytes = build_revert_bytestring("NoFives(uint256)", 5)
+
     assert_tx_failed(
         lambda: get_contract_with_gas_estimation(reverty_code).foo(transact={}),
         TransactionFailed,
-        exc_text=(
-            "execution reverted: "
-            "b'.\\x7f\\xb9\\x1f"
-            "\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00"
-            "\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00"
-            "\\x00\\x00\\x00\\x05'"
-        ),
+        exc_text=f"execution reverted: {revert_bytes}",
     )
