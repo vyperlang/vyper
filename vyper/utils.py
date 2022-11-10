bc26f5bec1225d822488f4a531b661b5f4d1281d
z80 <z80@ophy.xyz>
2022-11-10 12:00:10 -0500

    reduce code duplication

--- vyper/utils.py
+++ vyper/utils.py
@@ -107,7 +107,8 @@ def vyper_warn(msg, prefix="Warning: ", file_=sys.stderr):
 # converts a signature like Func(bool,uint256,address) to its 4 byte method ID
 # TODO replace manual calculations in codebase with this
 def method_id_int(method_sig: str) -> int:
-    return fourbytes_to_int(keccak256(bytes(method_sig, "utf-8"))[:4])
+    method_id_bytes = method_id(method_sig)
+    return fourbytes_to_int(method_id_bytes)
 
 
 def method_id(method_str: str) -> bytes:
