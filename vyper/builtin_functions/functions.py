f6b6486a9ccc3490837eb4e4ab7c2ed8a1b7bed8
emc415 <theemc3@gmail.com>
2022-11-25 22:51:46 +0000

    make send() gas stipend configurable

--- vyper/builtin_functions/functions.py
+++ vyper/builtin_functions/functions.py
@@ -1264,14 +1264,18 @@ class Send(BuiltinFunction):
 
     _id = "send"
     _inputs = [("to", AddressDefinition()), ("value", Uint256Definition())]
+    _kwargs = {"gas": KwargSettings(Uint256Definition(), "gas")}
     _return_type = None
 
     @process_inputs
     def build_IR(self, expr, args, kwargs, context):
         to, value = args
+        gas = kwargs["gas"]
         context.check_is_not_constant("send ether", expr)
-        return IRnode.from_list(["assert", ["call", 0, to, value, 0, 0, 0, 0]])
-
+        if isinstance(gas, str):
+            return IRnode.from_list(["assert", ["call", 0, to, value, 0, 0, 0, 0]])
+        else:
+            return IRnode.from_list(["assert", ["call", gas, to, value, 0, 0, 0, 0]])
 
 class SelfDestruct(BuiltinFunction):
 
