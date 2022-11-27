20f631d872552c832812d669495e3387da60e1eb
Charles Cooper <cooper.charles.m@gmail.com>
2022-11-27 08:59:17 -0800

    clean up an import

--- vyper/builtins/_signatures.py
+++ vyper/builtins/_signatures.py
@@ -8,8 +8,7 @@ from vyper.codegen.ir_node import IRnode
 from vyper.codegen.types.convert import new_type_to_old_type
 from vyper.exceptions import CompilerPanic, TypeMismatch
 from vyper.semantics.analysis.utils import get_exact_type_from_node, validate_expected_type
-from vyper.semantics.types import VyperType
-from vyper.semantics.types.base import TYPE_T, KwargSettings
+from vyper.semantics.types import VyperType, TYPE_T, KwargSettings
 from vyper.semantics.types.utils import type_from_annotation
 
 
