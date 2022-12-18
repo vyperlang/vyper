8ba12a33154b370b8c08b48b486f7fa719b509b9
tserg <8017125+tserg@users.noreply.github.com>
2022-12-18 16:45:54 +0800

    skip propagation if one type and node is compare

--- vyper/ast/folding.py
+++ vyper/ast/folding.py
@@ -72,14 +72,16 @@ def replace_literal_ops(vyper_module: vy_ast.Module) -> int:
                 ]
                 # if there are two propagated types, check for type mismatch;
                 # otherwise, if there is one or more propagated types, set
-                # folded node to that type for typechecking downstream
+                # folded node to that type for typechecking downstream if the node
+                # is vy_ast.BinOp - there is no need to propagate if the node is
+                # vy_ast.Compare because it is of boolean type
                 if len(propagated_types) == 2 and propagated_types[0] != propagated_types[1]:
                     raise TypeMismatch(
                         f"Unable to perform {node.op.description} on "
                         f"{propagated_types[0]} and {propagated_types[1]}",
                         node,
                     )
-                elif len(propagated_types) >= 1:
+                elif isinstance(node, vy_ast.BinOp) and len(propagated_types) >= 1:
                     typ = propagated_types.pop()
             elif isinstance(node, vy_ast.UnaryOp):
                 typ = node.operand._metadata.get("type")  # type: ignore
