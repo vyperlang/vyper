1cf6f2fe768d4c7aa6b30c4b6ac211eee5f62e68
emc415 <theemc3@gmail.com>
2022-12-26 20:03:43 +0000

    moved raise UNREACHABLE handling to _assert_reason so that all UNREACHABLE is handled in one block

--- vyper/codegen/stmt.py
+++ vyper/codegen/stmt.py
@@ -163,6 +163,8 @@ class Stmt:
             return IRnode.from_list(
                 ["assert_unreachable", test_expr], error_msg="assert unreachable"
             )
+        elif isinstance(msg, vy_ast.Name) and msg.id == "raise UNREACHABLE":
+            return IRnode.from_list(["invalid"], error_msg="raise unreachable")
 
         # set constant so that revert reason str is well behaved
         try:
@@ -221,9 +223,8 @@ class Stmt:
     def parse_Raise(self):
         if self.stmt.exc:
             if isinstance(self.stmt.exc, vy_ast.Name) and self.stmt.exc.id == "UNREACHABLE":
-                return IRnode.from_list(["invalid"], error_msg="raise unreachable")
-            else:
-                return self._assert_reason(None, self.stmt.exc)
+                self.stmt.exc.id = "raise UNREACHABLE"
+            return self._assert_reason(None, self.stmt.exc)
         else:
             return IRnode.from_list(["revert", 0, 0], error_msg="user raise")
 
