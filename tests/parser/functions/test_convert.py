423eb3a7cfb8c22eeb45942f7060e861991d2d8e
Charles Cooper <cooper.charles.m@gmail.com>
2022-12-28 12:14:54 -0800

    remove stray comment

--- tests/parser/functions/test_convert.py
+++ tests/parser/functions/test_convert.py
@@ -311,7 +311,6 @@ def _py_convert(val, i_typ, o_typ):
     if _padding_direction(i_typ) != _padding_direction(o_typ):
         # subtle! the padding conversion follows the bytes argument
         if isinstance(i_typ, (BytesM_T, BytesT)):
-            # n = i_typ.type_bytes
             n = bytes_of_type(i_typ)
             padding_byte = None
         else:
