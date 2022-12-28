d6fcd712f1ec34e3074f34eb97961578b594721b
Charles Cooper <cooper.charles.m@gmail.com>
2022-12-28 12:26:14 -0800

    fix min/max_value

--- tests/parser/functions/test_minmax_value.py
+++ tests/parser/functions/test_minmax_value.py
@@ -5,9 +5,9 @@ from vyper.semantics.types import DecimalT, IntegerT
 from vyper.semantics.types.shortcuts import INT256_T, UINT256_T
 
 
-@pytest.mark.parametrize("typ", sorted(IntegerT.all()))
+@pytest.mark.parametrize("typ", sorted(IntegerT.all() + (DecimalT(),)))
 @pytest.mark.parametrize("op", ("min_value", "max_value"))
-def test_minmax_value_int(get_contract, op, typ):
+def test_minmax_value(get_contract, op, typ):
     code = f"""
 @external
 def foo() -> {typ}:
@@ -15,7 +15,7 @@ def foo() -> {typ}:
     """
     c = get_contract(code)
 
-    lo, hi = typ.int_bounds
+    lo, hi = typ.ast_bounds
     if op == "min_value":
         assert c.foo() == lo
     elif op == "max_value":
@@ -45,23 +45,6 @@ def foo():
         assert_compile_failed(lambda: get_contract(lower), OverflowException)
     else:
         assert_compile_failed(lambda: get_contract(lower), InvalidType)
-
-
-@pytest.mark.parametrize("typ", [DecimalT()])
-@pytest.mark.parametrize("op", ("min_value", "max_value"))
-def test_minmax_value_decimal(get_contract, op, typ):
-    code = f"""
-@external
-def foo() -> {typ}:
-    return {op}({typ})
-    """
-    c = get_contract(code)
-
-    (lo, hi) = typ.decimal_bounds
-    if op == "min_value":
-        assert c.foo() == lo
-    elif op == "max_value":
-        assert c.foo() == hi
 
 
 @pytest.mark.parametrize("typ", [DecimalT()])
