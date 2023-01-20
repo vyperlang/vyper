deb3378e87145339e2a717f59079dc7752a25b4a
tserg <8017125+tserg@users.noreply.github.com>
2023-01-21 00:00:11 +0800

    fix: bitwise not constant folding (#3222)

--- vyper/ast/nodes.py
+++ vyper/ast/nodes.py
@@ -937,7 +937,9 @@ class Invert(Operator):
     __slots__ = ()
     _description = "bitwise not"
     _pretty = "~"
-    _op = operator.inv
+
+    def _op(self, value):
+        return (2 ** 256 - 1) ^ value
 
 
 class BinOp(ExprNode):
