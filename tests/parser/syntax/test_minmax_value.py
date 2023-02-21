25a75f60e3be94b4cf51f70143fb686c12630fc1
tserg <8017125+tserg@users.noreply.github.com>
2023-02-21 10:52:05 +0800

    add test

--- tests/parser/syntax/test_minmax_value.py
+++ tests/parser/syntax/test_minmax_value.py
@@ -1,22 +1,36 @@
 import pytest
 
-from vyper.exceptions import InvalidType
+from vyper.exceptions import InvalidType, TypeMismatch
 
 fail_list = [
-    """
+    (
+        """
 @external
 def foo():
     a: address = min_value(address)
     """,
-    """
+        InvalidType,
+    ),
+    (
+        """
 @external
 def foo():
     a: address = max_value(address)
     """,
+        InvalidType,
+    ),
+    (
+        """
+@external
+def foo():
+    a: int16 = min(min_value(int16), max_value(int8))
+    """,
+        TypeMismatch,
+    ),
 ]
 
 
-@pytest.mark.parametrize("bad_code", fail_list)
-def test_block_fail(assert_compile_failed, get_contract_with_gas_estimation, bad_code):
+@pytest.mark.parametrize("bad_code,exc", fail_list)
+def test_block_fail(assert_compile_failed, get_contract_with_gas_estimation, bad_code, exc):
 
-    assert_compile_failed(lambda: get_contract_with_gas_estimation(bad_code), InvalidType)
+    assert_compile_failed(lambda: get_contract_with_gas_estimation(bad_code), exc)
