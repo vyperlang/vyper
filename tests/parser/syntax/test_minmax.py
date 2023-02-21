b877e6a1b427dcf41c888e78b54b668ac03d08d5
tserg <8017125+tserg@users.noreply.github.com>
2023-02-21 12:14:03 +0800

    add tests

--- tests/parser/syntax/test_minmax.py
+++ tests/parser/syntax/test_minmax.py
@@ -1,6 +1,6 @@
 import pytest
 
-from vyper.exceptions import InvalidType
+from vyper.exceptions import InvalidType, TypeMismatch
 
 fail_list = [
     (
@@ -10,7 +10,23 @@ def foo():
     y: int128 = min(7, 0x1234567890123456789012345678901234567890)
     """,
         InvalidType,
-    )
+    ),
+    (
+        """
+@external
+def foo():
+    y: int256 = min(-1, max_value(int256) + 1)
+    """,
+        TypeMismatch,
+    ),
+    (
+        """
+@external
+def foo():
+    y: int256 = min(-1, 57896044618658097711785492504343953926634992332820282019728792003956564819968)
+    """,
+        TypeMismatch,
+    ),
 ]
 
 
