a096ac33fca8e2a989b2d7caf616ad686b1615ed
tserg <8017125+tserg@users.noreply.github.com>
2023-04-05 07:41:07 +0800

    check for breakpoint opcode

--- vyper/ir/compile_ir.py
+++ vyper/ir/compile_ir.py
@@ -755,7 +755,7 @@ def note_line_num(line_number_map, item, pos):
 
 def note_breakpoint(line_number_map, item, pos):
     # Record line number attached to pos.
-    if item == "DEBUG":
+    if item in ("BREAKPOINT", "DEBUG"):
         # Is PC debugger, create PC breakpoint.
         if item.pc_debugger:
             line_number_map["pc_breakpoints"].add(pos)
