vyperlang/vyper

commit 046ea166d93b8351da7c5d74115893eea2f60d76

refactor: front-end type system (#2974)
This commit refactors the front-end type system.

Currently, there are essentially two classes types for each vyper type.
"Primitives" represented raw types, and "Definitions" represent
the instantiation of types attached to each expression in a program.
"Definitions" additionally carry around annotation information related
to the expressions they tag, such as mutability and location info. This
system also handled dispatching into the correct routines for cases
where a raw type (e.g. "MyStruct") is used in expr land (vs an instance
of a type, e.g.  "my_struct"). This is important because certain types
are callable or attributable - specifically, constructors and enum
members (ex. `MyStruct()` and `MyEnum.FOO`).

This commit reworks the system by factoring the annotation info into
VyperType and ExprInfo. "Primitives" in the old system can be thought of
as VyperTypes, and "Definitions" in the old system are most similar to
the new ExprInfo. To handle cases where types can live in expr land,
this commit also clarifies that usage by promoting raw types into the
special, internal TYPE_T type (which can be thought of as "the type of
a type"). This commit also simplifies the inheritance structure of vyper
types.

Some other miscellaneous things in this commit:
- AST class hierarchy gets more refined - addition of ExprNode
- rename of `vyper/semantics/validation/` -> `vyper/semantics/analysis/`
- type system directory structure simplified
- merge of `builtin_functions/` and `builtin_interfaces/` to `builtins/`

Co-authored-by: z80 <z80dev@mlnl.finance>

vyper/semantics/environment.py
@@ -1,35 +1,49 @@
 from typing import Dict
 
-# TODO consolidate some of these imports
-from vyper.semantics.types.user.struct import StructDefinition
-from vyper.semantics.types.value.address import AddressDefinition
-from vyper.semantics.types.value.array_value import BytesArrayDefinition
-from vyper.semantics.types.value.bytes_fixed import Bytes32Definition
-from vyper.semantics.types.value.numeric import Uint256Definition  # type: ignore
-
-CONSTANT_ENVIRONMENT_VARS: Dict[str, Dict[str, type]] = {
-    "block": {
-        "coinbase": AddressDefinition,
-        "difficulty": Uint256Definition,
-        "prevrandao": Uint256Definition,
-        "number": Uint256Definition,
-        "gaslimit": Uint256Definition,
-        "basefee": Uint256Definition,
-        "prevhash": Bytes32Definition,
-        "timestamp": Uint256Definition,
-    },
-    "chain": {"id": Uint256Definition},
-    "msg": {
-        "data": BytesArrayDefinition,
-        "gas": Uint256Definition,
-        "sender": AddressDefinition,
-        "value": Uint256Definition,
-    },
-    "tx": {"origin": AddressDefinition, "gasprice": Uint256Definition},
-}
+from vyper.semantics.analysis.base import VarInfo
+from vyper.semantics.types import AddressT, BytesT, VyperType
+from vyper.semantics.types.shortcuts import BYTES32_T, UINT256_T
 
 
-MUTABLE_ENVIRONMENT_VARS: Dict[str, type] = {"self": AddressDefinition}
+# common properties for environment variables
+class _EnvType(VyperType):
+    def __eq__(self, other):
+        return self is other
+
+    def __hash__(self):
+        return hash(id(self))
+
+
+class _Block(_EnvType):
+    _id = "block"
+    _type_members = {
+        "coinbase": AddressT(),
+        "difficulty": UINT256_T,
+        "prevrandao": UINT256_T,
+        "number": UINT256_T,
+        "gaslimit": UINT256_T,
+        "basefee": UINT256_T,
+        "prevhash": BYTES32_T,
+        "timestamp": UINT256_T,
+    }
+
+
+class _Chain(_EnvType):
+    _id = "chain"
+    _type_members = {"id": UINT256_T}
+
+
+class _Msg(_EnvType):
+    _id = "msg"
+    _type_members = {"data": BytesT(), "gas": UINT256_T, "sender": AddressT(), "value": UINT256_T}
+
+
+class _Tx(_EnvType):
+    _id = "tx"
+    _type_members = {"origin": AddressT(), "gasprice": UINT256_T}
+
+
+CONSTANT_ENVIRONMENT_VARS = {t._id: t for t in (_Block(), _Chain(), _Tx(), _Msg())}
 
 
 def get_constant_vars() -> Dict:
@@ -37,11 +51,18 @@ def get_constant_vars() -> Dict:
     Get a dictionary of constant environment variables.
     """
     result = {}
-    for name, members in CONSTANT_ENVIRONMENT_VARS.items():
-        members = {k: v(is_constant=True) for k, v in members.items()}
-        result[name] = StructDefinition(name, members, is_constant=True)
+    for k, v in CONSTANT_ENVIRONMENT_VARS.items():
+        result[k] = VarInfo(v, is_constant=True)
 
     return result
+
+
+# Not sure this is necessary, but add an ad-hoc type for `self` for clarity
+class _SelfT(AddressT):
+    pass
+
+
+MUTABLE_ENVIRONMENT_VARS: Dict[str, type] = {"self": _SelfT}
 
 
 def get_mutable_vars() -> Dict:
@@ -49,4 +70,4 @@ def get_mutable_vars() -> Dict:
     Get a dictionary of mutable environment variables (those that are
     modified during the course of contract execution, such as `self`).
     """
-    return {name: type_(is_constant=True) for name, type_ in MUTABLE_ENVIRONMENT_VARS.items()}
+    return {name: VarInfo(type_()) for name, type_ in MUTABLE_ENVIRONMENT_VARS.items()}

commit 54395b2a980182fb56445bbd84a06ce80233dc3c

fix: SelfT does not compare with address (#3340)
this was a regression presumably introduced in 046ea166d9

vyper/semantics/environment.py
@@ -57,12 +57,7 @@ def get_constant_vars() -> Dict:
     return result
 
 
-# Not sure this is necessary, but add an ad-hoc type for `self` for clarity
-class _SelfT(AddressT):
-    pass
-
-
-MUTABLE_ENVIRONMENT_VARS: Dict[str, type] = {"self": _SelfT}
+MUTABLE_ENVIRONMENT_VARS: Dict[str, type] = {"self": AddressT}
 
 
 def get_mutable_vars() -> Dict: