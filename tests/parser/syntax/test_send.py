6436bf2578d4c067114ae4261ae3c0c3947111a1
tserg <8017125+tserg@users.noreply.github.com>
2023-04-07 16:28:32 +0800

    add deleted test

--- tests/parser/syntax/test_send.py
+++ tests/parser/syntax/test_send.py
@@ -143,6 +143,16 @@ def foo():
     send(0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe, 5, gas=5000)
     """,
     """
+    # Test custom send method
+@internal
+def send(a: address, w: uint256):
+    send(0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe, 1)
+@external
+@payable
+def foo():
+    self.send(msg.sender, msg.value)
+    """,
+    """
 x: uint256
 
 #Test send gas stipend
