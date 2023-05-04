vyperlang/vyper

commit 66930fdfc396a883049c512290260818aa69ab9a

fix: typechecker hotspot (#3318)
`get_possible_types_from_node` had blowup for deeply nested expressions.
this commit caches intermediate results using dynamic programming to
prune the calculation time.

note that caching has a funny interaction with the type inference
algorithm for loop iterators (i.e., try all different types for the loop
iterator, and pick the first one which does not throw an exception) -
since the loop iterator inference algorithm depends on optimistically
trying different types, some cached values can be invalid in the case
that they are generated inside of the loop iterator inference algorithm,
because the exception is caught and handled, but the state changes
(i.e., changes to the cache) are not rolled back. to solve this, this
commit introduces a simple commit/rollback mechanism to deal with state
changes in the type checker. it remains to be seen whether this
mechanism will be more generally useful in the future for other
potentially stateful type-checking operations, or if a better long-term
solution is to simply redesign how the type checker infers loop
iterators.

vyper/semantics/analysis/__init__.py
@@ -2,6 +2,7 @@ from .. import types  # break a dependency cycle.
 from ..namespace import get_namespace
 from .local import validate_functions
 from .module import add_module_namespace
+from .utils import _ExprAnalyser
 
 
 def validate_semantics(vyper_ast, interface_codes):
@@ -11,3 +12,6 @@ def validate_semantics(vyper_ast, interface_codes):
     with namespace.enter_scope():
         add_module_namespace(vyper_ast, interface_codes)
         validate_functions(vyper_ast)
+
+    # clean up. not sure if this is necessary, but do it for hygiene's sake.
+    _ExprAnalyser._reset_taint()

vyper/semantics/analysis/local.py
@@ -21,6 +21,7 @@ from vyper.semantics.analysis.annotation import StatementAnnotationVisitor
 from vyper.semantics.analysis.base import DataLocation, VarInfo
 from vyper.semantics.analysis.common import VyperNodeVisitorBase
 from vyper.semantics.analysis.utils import (
+    _ExprAnalyser,
     get_common_types,
     get_exact_type_from_node,
     get_expr_info,
@@ -454,12 +455,23 @@ class FunctionNodeVisitor(VyperNodeVisitorBase):
                 try:
                     for n in node.body:
                         self.visit(n)
-                    # type information is applied directly because the scope is
-                    # closed prior to the call to `StatementAnnotationVisitor`
-                    node.target._metadata["type"] = type_
-                    return
                 except (TypeMismatch, InvalidOperation) as exc:
                     for_loop_exceptions.append(exc)
+                    # rollback any changes to the tree
+                    _ExprAnalyser._rollback_taint()
+                else:
+                    # type information is applied directly here because the
+                    # scope is closed prior to the call to
+                    # `StatementAnnotationVisitor`
+                    node.target._metadata["type"] = type_
+
+                    # perf - persist all calculated types
+                    _ExprAnalyser._commit_taint()
+                    # success -- bail out instead of error handling.
+                    return
+
+        # if we have gotten here, there was an error for
+        # every type tried for the iterator
 
         if len(set(str(i) for i in for_loop_exceptions)) == 1:
             # if every attempt at type checking raised the same exception

vyper/semantics/analysis/utils.py
@@ -57,6 +57,13 @@ class _ExprAnalyser:
     class's method resolution order is examined to decide which method to call.
     """
 
+    # this allows for a very simple commit/rollback scheme for metadata
+    # caching. in the case that an exception is thrown and caught during
+    # type checking (currently, only during for loop iterator variable
+    # type inference), we can roll back any state updates due to type
+    # checking.
+    _tainted_nodes: set[tuple[vy_ast.VyperNode, str]] = set()
+
     def __init__(self):
         self.namespace = get_namespace()
 
@@ -123,6 +130,7 @@ class _ExprAnalyser:
 
         if len(types_list) > 1:
             raise StructureException("Ambiguous type", node)
+
         return types_list[0]
 
     def get_possible_types_from_node(self, node, include_type_exprs=False):
@@ -145,20 +153,44 @@ class _ExprAnalyser:
         if "type" in node._metadata:
             return [node._metadata["type"]]
 
-        fn = self._find_fn(node)
-        ret = fn(node)
+        # this method is a perf hotspot, so we cache the result and
+        # try to return it if found.
+        k = f"possible_types_from_node_{include_type_exprs}"
+        if k not in node._metadata:
+            fn = self._find_fn(node)
+            ret = fn(node)
 
-        if not include_type_exprs:
-            invalid = next((i for i in ret if isinstance(i, TYPE_T)), None)
-            if invalid is not None:
-                raise InvalidReference(f"not a variable or literal: '{invalid.typedef}'", node)
+            if not include_type_exprs:
+                invalid = next((i for i in ret if isinstance(i, TYPE_T)), None)
+                if invalid is not None:
+                    raise InvalidReference(f"not a variable or literal: '{invalid.typedef}'", node)
 
-        if all(isinstance(i, IntegerT) for i in ret):
-            # for numeric types, sort according by number of bits descending
-            # this ensures literals are cast with the largest possible type
-            ret.sort(key=lambda k: (k.bits, not k.is_signed), reverse=True)
+            if all(isinstance(i, IntegerT) for i in ret):
+                # for numeric types, sort according by number of bits descending
+                # this ensures literals are cast with the largest possible type
+                ret.sort(key=lambda k: (k.bits, not k.is_signed), reverse=True)
 
-        return ret
+            node._metadata[k] = ret
+            # register with list of tainted nodes, in case the cache
+            # needs to be invalidated in case of a state rollback
+            self._tainted_nodes.add((node, k))
+
+        return node._metadata[k].copy()
+
+    @classmethod
+    def _rollback_taint(cls):
+        for node, k in cls._tainted_nodes:
+            node._metadata.pop(k, None)
+        # taint has been rolled back, no need to track it anymore
+        cls._reset_taint()
+
+    @classmethod
+    def _commit_taint(cls):
+        cls._reset_taint()
+
+    @classmethod
+    def _reset_taint(cls):
+        cls._tainted_nodes.clear()
 
     def _find_fn(self, node):
         # look for a type-check method for each class in the given class mro
@@ -268,7 +300,7 @@ class _ExprAnalyser:
     def types_from_Constant(self, node):
         # literal value (integer, string, etc)
         types_list = []
-        for t in types.get_primitive_types().values():
+        for t in types.PRIMITIVE_TYPES.values():
             try:
                 # clarity and perf note: will be better to construct a
                 # map from node types to valid vyper types
@@ -302,7 +334,7 @@ class _ExprAnalyser:
         if _is_empty_list(node):
             # empty list literal `[]`
             # subtype can be anything
-            types_list = types.get_types()
+            types_list = types.PRIMITIVE_TYPES
             # 1 is minimum possible length for dynarray, assignable to anything
             ret = [DArrayT(t, 1) for t in types_list.values()]
             return ret
@@ -421,7 +453,6 @@ def get_exact_type_from_node(node):
     BaseType
         Type object.
     """
-
     return _ExprAnalyser().get_exact_type_from_node(node, include_type_exprs=True)
 
 
@@ -451,6 +482,7 @@ def get_common_types(*nodes: vy_ast.VyperNode, filter_fn: Callable = None) -> Li
         new_types = _ExprAnalyser().get_possible_types_from_node(item)
 
         common = [i for i in common_types if _is_type_in_list(i, new_types)]
+
         rejected = [i for i in common_types if i not in common]
         common += [i for i in new_types if _is_type_in_list(i, rejected)]
 

vyper/semantics/types/__init__.py
@@ -7,7 +7,7 @@ from .subscriptable import DArrayT, HashMapT, SArrayT, TupleT
 from .user import EnumT, EventT, InterfaceT, StructT
 
 
-def get_primitive_types():
+def _get_primitive_types():
     res = [BoolT(), DecimalT()]
 
     res.extend(IntegerT.all())
@@ -46,8 +46,5 @@ def _get_sequence_types():
     return ret
 
 
-def get_types():
-    result = {}
-    result.update(get_primitive_types())
-
-    return result
+# note: it might be good to make this a frozen dict of some sort
+PRIMITIVE_TYPES = _get_primitive_types()

tests/functional/semantics/test_namespace.py
@@ -3,7 +3,7 @@ import pytest
 from vyper.exceptions import CompilerPanic, NamespaceCollision, UndeclaredDefinition
 from vyper.semantics import environment
 from vyper.semantics.namespace import get_namespace
-from vyper.semantics.types import get_types
+from vyper.semantics.types import PRIMITIVE_TYPES
 
 
 def test_get_namespace():
@@ -22,13 +22,13 @@ def test_builtin_context_manager(namespace):
 
 
 def test_builtin_types(namespace):
-    for key, value in get_types().items():
+    for key, value in PRIMITIVE_TYPES.items():
         assert namespace[key] == value
 
 
 def test_builtin_types_persist_after_clear(namespace):
     namespace.clear()
-    for key, value in get_types().items():
+    for key, value in PRIMITIVE_TYPES.items():
         assert namespace[key] == value
 
 

tests/functional/semantics/types/test_type_from_abi.py
@@ -1,7 +1,7 @@
 import pytest
 
 from vyper.exceptions import UnknownType
-from vyper.semantics.types import SArrayT, get_primitive_types
+from vyper.semantics.types import PRIMITIVE_TYPES, SArrayT
 from vyper.semantics.types.utils import type_from_abi
 
 BASE_TYPES = ["int128", "uint256", "bool", "address", "bytes32"]
@@ -9,7 +9,7 @@ BASE_TYPES = ["int128", "uint256", "bool", "address", "bytes32"]
 
 @pytest.mark.parametrize("type_str", BASE_TYPES)
 def test_base_types(type_str):
-    base_t = get_primitive_types()[type_str]
+    base_t = PRIMITIVE_TYPES[type_str]
     type_t = type_from_abi({"type": type_str})
 
     assert base_t == type_t
@@ -17,7 +17,7 @@ def test_base_types(type_str):
 
 @pytest.mark.parametrize("type_str", BASE_TYPES)
 def test_base_types_as_arrays(type_str):
-    base_t = get_primitive_types()[type_str]
+    base_t = PRIMITIVE_TYPES[type_str]
     type_t = type_from_abi({"type": f"{type_str}[3]"})
 
     assert type_t == SArrayT(base_t, 3)
@@ -25,7 +25,7 @@ def test_base_types_as_arrays(type_str):
 
 @pytest.mark.parametrize("type_str", BASE_TYPES)
 def test_base_types_as_multidimensional_arrays(type_str):
-    base_t = get_primitive_types()[type_str]
+    base_t = PRIMITIVE_TYPES[type_str]
 
     type_t = type_from_abi({"type": f"{type_str}[3][5]"})
 

tests/functional/semantics/types/test_type_from_annotation.py
@@ -6,7 +6,7 @@ from vyper.exceptions import (
     StructureException,
     UndeclaredDefinition,
 )
-from vyper.semantics.types import HashMapT, SArrayT, get_primitive_types
+from vyper.semantics.types import PRIMITIVE_TYPES, HashMapT, SArrayT
 from vyper.semantics.types.utils import type_from_annotation
 
 BASE_TYPES = ["int128", "uint256", "bool", "address", "bytes32"]
@@ -16,7 +16,7 @@ BYTESTRING_TYPES = ["String", "Bytes"]
 @pytest.mark.parametrize("type_str", BASE_TYPES)
 def test_base_types(build_node, type_str):
     node = build_node(type_str)
-    base_t = get_primitive_types()[type_str]
+    base_t = PRIMITIVE_TYPES[type_str]
 
     ann_t = type_from_annotation(node)
 
@@ -26,7 +26,7 @@ def test_base_types(build_node, type_str):
 @pytest.mark.parametrize("type_str", BYTESTRING_TYPES)
 def test_array_value_types(build_node, type_str):
     node = build_node(f"{type_str}[1]")
-    base_t = get_primitive_types()[type_str](1)
+    base_t = PRIMITIVE_TYPES[type_str](1)
 
     ann_t = type_from_annotation(node)
 
@@ -36,7 +36,7 @@ def test_array_value_types(build_node, type_str):
 @pytest.mark.parametrize("type_str", BASE_TYPES)
 def test_base_types_as_arrays(build_node, type_str):
     node = build_node(f"{type_str}[3]")
-    base_t = get_primitive_types()[type_str]
+    base_t = PRIMITIVE_TYPES[type_str]
 
     ann_t = type_from_annotation(node)
 
@@ -54,7 +54,7 @@ def test_array_value_types_as_arrays(build_node, type_str):
 @pytest.mark.parametrize("type_str", BASE_TYPES)
 def test_base_types_as_multidimensional_arrays(build_node, namespace, type_str):
     node = build_node(f"{type_str}[3][5]")
-    base_t = get_primitive_types()[type_str]
+    base_t = PRIMITIVE_TYPES[type_str]
 
     ann_t = type_from_annotation(node)
 
@@ -75,7 +75,7 @@ def test_invalid_index(build_node, idx, type_str):
 @pytest.mark.parametrize("type_str2", BASE_TYPES)
 def test_mapping(build_node, type_str, type_str2):
     node = build_node(f"HashMap[{type_str}, {type_str2}]")
-    types = get_primitive_types()
+    types = PRIMITIVE_TYPES
 
     ann_t = type_from_annotation(node)
 
@@ -89,7 +89,7 @@ def test_mapping(build_node, type_str, type_str2):
 @pytest.mark.parametrize("type_str2", BASE_TYPES)
 def test_multidimensional_mapping(build_node, type_str, type_str2):
     node = build_node(f"HashMap[{type_str}, HashMap[{type_str}, {type_str2}]]")
-    types = get_primitive_types()
+    types = PRIMITIVE_TYPES
 
     ann_t = type_from_annotation(node)
 

vyper/semantics/namespace.py
@@ -28,9 +28,9 @@ class Namespace(dict):
         from vyper.builtins.functions import get_builtin_functions
         from vyper.semantics import environment
         from vyper.semantics.analysis.base import VarInfo
-        from vyper.semantics.types import get_types
+        from vyper.semantics.types import PRIMITIVE_TYPES
 
-        self.update(get_types())
+        self.update(PRIMITIVE_TYPES)
         self.update(environment.get_constant_vars())
         self.update({k: VarInfo(b) for (k, b) in get_builtin_functions().items()})
 

commit 7c3cf61665ddc3abb2432f84868b3b6da2be80dd

fix: rewrite typechecker journal to handle nested commits (#3375)
this commit fixes a bug which was introduced in 66930fdfc. when the
typechecker enters a nested loop, it can typecheck the inner loop
(committing it), and result in invalid state if the outer loop fails to
typecheck. this commit implements a checkpointing system in the
typechecker state committer so that it can handle nested changes. it
also cleans up the data structure used so that there is a single entry
point and users of the data structure do not need to think about
implementation details.

one drawback of this approach is that it *only* handles changes to the
metadata dict. non-idempotent changes to the AST during typechecking
(such as the use case we are using them for - caching) should then be
restricted to changes to the metadata dict so that they can register
automatically with the node metadata journal.

vyper/ast/metadata.py
@@ -1,0 +1,80 @@
+import contextlib
+from typing import Any
+
+from vyper.exceptions import VyperException
+
+
+# a commit/rollback scheme for metadata caching. in the case that an
+# exception is thrown and caught during type checking (currently, only
+# during for loop iterator variable type inference), we can roll back
+# any state updates due to type checking.
+# this is implemented as a stack of changesets, because we need to
+# handle nested rollbacks in the case of nested for loops
+class _NodeMetadataJournal:
+    _NOT_FOUND = object()
+
+    def __init__(self):
+        self._node_updates: list[dict[tuple[int, str, Any], NodeMetadata]] = []
+
+    def register_update(self, metadata, k):
+        prev = metadata.get(k, self._NOT_FOUND)
+        self._node_updates[-1][(id(metadata), k)] = (metadata, prev)
+
+    @contextlib.contextmanager
+    def enter(self):
+        self._node_updates.append({})
+        try:
+            yield
+        except VyperException as e:
+            # note: would be better to only catch typechecker exceptions here.
+            self._rollback_inner()
+            raise e from e
+        else:
+            self._commit_inner()
+
+    def _rollback_inner(self):
+        for (_, k), (metadata, prev) in self._node_updates[-1].items():
+            if prev is self._NOT_FOUND:
+                metadata.pop(k, None)
+            else:
+                metadata[k] = prev
+        self._pop_inner()
+
+    def _commit_inner(self):
+        inner = self._pop_inner()
+
+        if len(self._node_updates) == 0:
+            return
+
+        outer = self._node_updates[-1]
+
+        # register with previous frame in case inner gets commited
+        # but outer needs to be rolled back
+        for (_, k), (metadata, prev) in inner.items():
+            if (id(metadata), k) not in outer:
+                outer[(id(metadata), k)] = (metadata, prev)
+
+    def _pop_inner(self):
+        return self._node_updates.pop()
+
+
+class NodeMetadata(dict):
+    """
+    A data structure which allows for journaling.
+    """
+
+    _JOURNAL: _NodeMetadataJournal = _NodeMetadataJournal()
+
+    def __setitem__(self, k, v):
+        # if we are in a context where we need to journal, add
+        # this to the changeset.
+        if len(self._JOURNAL._node_updates) != 0:
+            self._JOURNAL.register_update(self, k)
+
+        super().__setitem__(k, v)
+
+    @classmethod
+    @contextlib.contextmanager
+    def enter_typechecker_speculation(cls):
+        with cls._JOURNAL.enter():
+            yield

vyper/ast/nodes.py
@@ -5,6 +5,7 @@ import operator
 import sys
 from typing import Any, Optional, Union
 
+from vyper.ast.metadata import NodeMetadata
 from vyper.compiler.settings import VYPER_ERROR_CONTEXT_LINES, VYPER_ERROR_LINE_NUMBERS
 from vyper.exceptions import (
     ArgumentException,
@@ -254,7 +255,7 @@ class VyperNode:
         """
         self.set_parent(parent)
         self._children: set = set()
-        self._metadata: dict = {}
+        self._metadata: NodeMetadata = NodeMetadata()
 
         for field_name in NODE_SRC_ATTRIBUTES:
             # when a source offset is not available, use the parent's source offset

vyper/semantics/analysis/__init__.py
@@ -12,6 +12,3 @@ def validate_semantics(vyper_ast, interface_codes):
     with namespace.enter_scope():
         add_module_namespace(vyper_ast, interface_codes)
         validate_functions(vyper_ast)
-
-    # clean up. not sure if this is necessary, but do it for hygiene's sake.
-    _ExprAnalyser._reset_taint()

vyper/semantics/analysis/local.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from vyper import ast as vy_ast
+from vyper.ast.metadata import NodeMetadata
 from vyper.ast.validation import validate_call_args
 from vyper.exceptions import (
     ExceptionList,
@@ -21,7 +22,6 @@ from vyper.semantics.analysis.annotation import StatementAnnotationVisitor
 from vyper.semantics.analysis.base import DataLocation, VarInfo
 from vyper.semantics.analysis.common import VyperNodeVisitorBase
 from vyper.semantics.analysis.utils import (
-    _ExprAnalyser,
     get_common_types,
     get_exact_type_from_node,
     get_expr_info,
@@ -453,20 +453,17 @@ class FunctionNodeVisitor(VyperNodeVisitorBase):
                     raise exc.with_annotation(node) from None
 
                 try:
-                    for n in node.body:
-                        self.visit(n)
+                    with NodeMetadata.enter_typechecker_speculation():
+                        for n in node.body:
+                            self.visit(n)
                 except (TypeMismatch, InvalidOperation) as exc:
                     for_loop_exceptions.append(exc)
-                    # rollback any changes to the tree
-                    _ExprAnalyser._rollback_taint()
                 else:
                     # type information is applied directly here because the
                     # scope is closed prior to the call to
                     # `StatementAnnotationVisitor`
                     node.target._metadata["type"] = type_
 
-                    # perf - persist all calculated types
-                    _ExprAnalyser._commit_taint()
                     # success -- bail out instead of error handling.
                     return
 

vyper/semantics/analysis/utils.py
@@ -57,13 +57,6 @@ class _ExprAnalyser:
     class's method resolution order is examined to decide which method to call.
     """
 
-    # this allows for a very simple commit/rollback scheme for metadata
-    # caching. in the case that an exception is thrown and caught during
-    # type checking (currently, only during for loop iterator variable
-    # type inference), we can roll back any state updates due to type
-    # checking.
-    _tainted_nodes: set[tuple[vy_ast.VyperNode, str]] = set()
-
     def __init__(self):
         self.namespace = get_namespace()
 
@@ -171,26 +164,8 @@ class _ExprAnalyser:
                 ret.sort(key=lambda k: (k.bits, not k.is_signed), reverse=True)
 
             node._metadata[k] = ret
-            # register with list of tainted nodes, in case the cache
-            # needs to be invalidated in case of a state rollback
-            self._tainted_nodes.add((node, k))
 
         return node._metadata[k].copy()
-
-    @classmethod
-    def _rollback_taint(cls):
-        for node, k in cls._tainted_nodes:
-            node._metadata.pop(k, None)
-        # taint has been rolled back, no need to track it anymore
-        cls._reset_taint()
-
-    @classmethod
-    def _commit_taint(cls):
-        cls._reset_taint()
-
-    @classmethod
-    def _reset_taint(cls):
-        cls._tainted_nodes.clear()
 
     def _find_fn(self, node):
         # look for a type-check method for each class in the given class mro

tests/functional/semantics/analysis/test_for_loop.py
@@ -1,7 +1,7 @@
 import pytest
 
 from vyper.ast import parse_to_ast
-from vyper.exceptions import ImmutableViolation
+from vyper.exceptions import ImmutableViolation, TypeMismatch
 from vyper.semantics.analysis import validate_semantics
 
 
@@ -98,4 +98,45 @@ def baz():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(ImmutableViolation):
+        validate_semantics(vyper_module, {})
+
+
+iterator_inference_codes = [
+    """
+@external
+def main():
+    for j in range(3):
+        x: uint256 = j
+        y: uint16 = j
+    """,  # issue 3212
+    """
+@external
+def foo():
+    for i in [1]:
+        a:uint256 = i
+        b:uint16 = i
+    """,  # issue 3374
+    """
+@external
+def foo():
+    for i in [1]:
+        for j in [1]:
+            a:uint256 = i
+        b:uint16 = i
+    """,  # issue 3374
+    """
+@external
+def foo():
+    for i in [1,2,3]:
+        for j in [1,2,3]:
+            b:uint256 = j + i
+        c:uint16 = i
+    """,  # issue 3374
+]
+
+
+@pytest.mark.parametrize("code", iterator_inference_codes)
+def test_iterator_type_inference_checker(namespace, code):
+    vyper_module = parse_to_ast(code)
+    with pytest.raises(TypeMismatch):
         validate_semantics(vyper_module, {})