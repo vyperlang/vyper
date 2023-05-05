13a919997979eb92a417a20dfc0c93ffa50bf890
Charles Cooper <cooper.charles.m@gmail.com>
2023-05-04 18:21:38 -0700

    add test

---
+++ tests/parser/features/test_string_map_keys.py
@@ -0,0 +1,12 @@
+def test_string_map_keys(get_contract):
+    code = """
+f:HashMap[String[1], bool]
+@external
+def test() -> bool:
+    a:String[1] = "a"
+    b:String[1] = "b"
+    self.f[a] = True
+    return self.f[b]  # should return False
+    """
+    c = get_contract(code)
+    assert c.test() is False
