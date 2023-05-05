vyperlang/vyper

commit 3abe588e05be6f6cbfac283739d797ef6c485756

refactor: merge front-end and codegen type systems (#3182)
for historical reasons, vyper models its type system in two places, in
`vyper/semantics/types` and `vyper/codegen/types`. this merges the two
systems (with plenty of pair programming help from @z80dev!)          

this commit required a couple kludges in order to get machinery from the
new type system into codegen regions of the code, but those should get
refactored out with follow-up work on `GlobalContext` and
`FunctionSignature`.

also:

- rename `ModuleNodeVisitor` to `ModuleAnalyzer`
- move the `VariableRecord` class to `vyper/codegen/context.py`
- simplify `GlobalContext.parse_type` and a lot of associated machinery

Co-authored-by: z80 <z80@ophy.xyz>

vyper/codegen/events.py
@@ -1,9 +1,9 @@
 from vyper.codegen.abi_encoder import abi_encode
-from vyper.codegen.core import ir_tuple_from_args, unwrap_location
+from vyper.codegen.core import get_type_for_exact_size, ir_tuple_from_args, unwrap_location
 from vyper.codegen.ir_node import IRnode
 from vyper.codegen.keccak256_helper import keccak256_helper
-from vyper.codegen.types.types import BaseType, ByteArrayLike, get_type_for_exact_size
 from vyper.exceptions import TypeMismatch
+from vyper.semantics.types.bytestrings import _BytestringT
 
 
 # docs.soliditylang.org/en/v0.8.6/abi-spec.html#indexed-event-encoding
@@ -11,10 +11,10 @@ def _encode_log_topics(expr, event_id, arg_nodes, context):
     topics = [event_id]
 
     for arg in arg_nodes:
-        if isinstance(arg.typ, BaseType):
+        if arg.typ._is_prim_word:
             value = unwrap_location(arg)
 
-        elif isinstance(arg.typ, ByteArrayLike):
+        elif isinstance(arg.typ, _BytestringT):
             value = keccak256_helper(expr, arg, context=context)
         else:
             # TODO block at higher level

vyper/codegen/keccak256_helper.py
@@ -2,13 +2,14 @@ from math import ceil
 
 from vyper.codegen.core import bytes_data_ptr, ensure_in_memory, get_bytearray_length
 from vyper.codegen.ir_node import IRnode
-from vyper.codegen.types import BaseType, ByteArrayLike, is_base_type
 from vyper.exceptions import CompilerPanic
+from vyper.semantics.types.bytestrings import _BytestringT
+from vyper.semantics.types.shortcuts import BYTES32_T
 from vyper.utils import SHA3_BASE, SHA3_PER_WORD, MemoryPositions, bytes_to_int, keccak256
 
 
 def _check_byteslike(typ, _expr):
-    if not isinstance(typ, ByteArrayLike) and not is_base_type(typ, "bytes32"):
+    if not isinstance(typ, _BytestringT) and typ != BYTES32_T:
         # NOTE this may be checked at a higher level, but just be safe
         raise CompilerPanic("keccak256 only accepts bytes-like objects")
 
@@ -23,17 +24,18 @@ def keccak256_helper(expr, to_hash, context):
     # Can hash literals
     # TODO this is dead code.
     if isinstance(to_hash, bytes):
-        return IRnode.from_list(bytes_to_int(keccak256(to_hash)), typ=BaseType("bytes32"))
+        return IRnode.from_list(bytes_to_int(keccak256(to_hash)), typ=BYTES32_T)
 
     # Can hash bytes32 objects
-    if is_base_type(to_hash.typ, "bytes32"):
+    # TODO: Want to generalize to all bytes_M
+    if to_hash.typ == BYTES32_T:
         return IRnode.from_list(
             [
                 "seq",
                 ["mstore", MemoryPositions.FREE_VAR_SPACE, to_hash],
                 ["sha3", MemoryPositions.FREE_VAR_SPACE, 32],
             ],
-            typ=BaseType("bytes32"),
+            typ=BYTES32_T,
             add_gas_estimate=_gas_bound(1),
         )
 
@@ -45,7 +47,7 @@ def keccak256_helper(expr, to_hash, context):
         return b1.resolve(
             IRnode.from_list(
                 ["sha3", data, len_],
-                typ="bytes32",
+                typ=BYTES32_T,
                 annotation="keccak256",
                 add_gas_estimate=_gas_bound(ceil(to_hash.typ.maxlen / 32)),
             )

tests/fixtures/memorymock.py
@@ -1,7 +1,7 @@
 import pytest
 
 from vyper.codegen.context import Context
-from vyper.codegen.types import BaseType
+from vyper.codegen.core import get_type_for_exact_size
 
 
 class ContextMock(Context):
@@ -13,7 +13,9 @@ class ContextMock(Context):
     def internal_memory_scope(self):
         if not self._mock_vars:
             for i in range(20):
-                self._new_variable(f"#mock{i}", BaseType(self._size), self._size, bool(i % 2))
+                self._new_variable(
+                    f"#mock{i}", get_type_for_exact_size(self._size), self._size, bool(i % 2)
+                )
             self._mock_vars = True
         return super().internal_memory_scope()
 

tests/functional/semantics/analysis/test_cyclic_function_calls.py
@@ -3,7 +3,7 @@ import pytest
 from vyper.ast import parse_to_ast
 from vyper.exceptions import CallViolation, StructureException
 from vyper.semantics.analysis import validate_semantics
-from vyper.semantics.analysis.module import ModuleNodeVisitor
+from vyper.semantics.analysis.module import ModuleAnalyzer
 
 
 def test_cyclic_function_call(namespace):
@@ -19,7 +19,7 @@ def bar():
     vyper_module = parse_to_ast(code)
     with namespace.enter_scope():
         with pytest.raises(CallViolation):
-            ModuleNodeVisitor(vyper_module, {}, namespace)
+            ModuleAnalyzer(vyper_module, {}, namespace)
 
 
 def test_multi_cyclic_function_call(namespace):
@@ -43,7 +43,7 @@ def potato():
     vyper_module = parse_to_ast(code)
     with namespace.enter_scope():
         with pytest.raises(CallViolation):
-            ModuleNodeVisitor(vyper_module, {}, namespace)
+            ModuleAnalyzer(vyper_module, {}, namespace)
 
 
 def test_global_ann_assign_callable_no_crash():

tests/functional/semantics/analysis/test_potential_types.py
@@ -200,7 +200,7 @@ def test_tuple(build_node, namespace):
     namespace["bar"] = VarInfo(AddressT())
     types_list = get_possible_types_from_node(node)
 
-    assert types_list[0].value_type == [namespace["foo"].typ, namespace["bar"].typ]
+    assert types_list[0].member_types == [namespace["foo"].typ, namespace["bar"].typ]
 
 
 def test_tuple_subscript(build_node, namespace):

tests/parser/functions/test_convert.py
@@ -2,25 +2,17 @@ import enum
 import itertools
 
 # import random
-from dataclasses import dataclass
 from decimal import Decimal
-from typing import Any
 
 import eth_abi.exceptions
 import pytest
 from eth_abi import decode_single, encode_single
 
-from vyper.codegen.types import (
-    BASE_TYPES,
-    INTEGER_TYPES,
-    parse_bytes_m_info,
-    parse_decimal_info,
-    parse_integer_typeinfo,
-)
 from vyper.exceptions import InvalidLiteral, InvalidType, TypeMismatch
+from vyper.semantics.types import AddressT, BoolT, BytesM_T, BytesT, DecimalT, IntegerT, StringT
+from vyper.semantics.types.shortcuts import BYTES20_T, BYTES32_T, UINT, UINT160_T, UINT256_T
 from vyper.utils import (
     DECIMAL_DIVISOR,
-    SizeLimits,
     checksum_encode,
     int_bounds,
     is_checksum_encoded,
@@ -28,7 +20,9 @@ from vyper.utils import (
     unsigned_to_signed,
 )
 
-TEST_TYPES = BASE_TYPES | {"Bytes[32]"}
+BASE_TYPES = set(IntegerT.all()) | set(BytesM_T.all()) | {DecimalT(), AddressT(), BoolT()}
+
+TEST_TYPES = BASE_TYPES | {BytesT(32)}
 
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
@@ -36,24 +30,25 @@ ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 DECIMAL_EPSILON = Decimal(1) / DECIMAL_DIVISOR
 
 
-@dataclass
-class TestType:
-    """
-    Simple class to model Vyper types.
-    """
+def _bits_of_type(typ):
+    if isinstance(typ, (IntegerT, DecimalT)):
+        return typ.bits
+    if isinstance(typ, BoolT):
+        return 8
+    if isinstance(typ, AddressT):
+        return 160
+    if isinstance(typ, BytesM_T):
+        return typ.m_bits
+    if isinstance(typ, BytesT):
+        return typ.length * 8
 
-    type_name: str
-    type_bytes: int  # number of nonzero bytes this type can take
-    type_class: str  # e.g. int, bytes, String, decimal
-    info: Any  # e.g. DecimalInfo
+    raise Exception(f"Unknown type {typ}")
 
-    @property
-    def abi_type(self):
-        if self.type_name == "decimal":
-            return "fixed168x10"
-        if self.type_class in ("Bytes", "String"):
-            return self.type_class.lower()
-        return self.type_name
+
+def bytes_of_type(typ):
+    ret = _bits_of_type(typ)
+    assert ret % 8 == 0
+    return ret // 8
 
 
 class _OutOfBounds(Exception):
@@ -64,32 +59,6 @@ class _OutOfBounds(Exception):
     pass
 
 
-def _parse_type(typename):
-    if typename.startswith(("uint", "int")):
-        info = parse_integer_typeinfo(typename)
-        assert info.bits % 8 == 0
-        return TestType(typename, info.bits // 8, "int", info)
-    elif typename == "decimal":
-        info = parse_decimal_info(typename)
-        assert info.bits % 8 == 0
-        return TestType(typename, info.bits // 8, "decimal", info)
-    elif typename.startswith("bytes"):
-        info = parse_bytes_m_info(typename)
-        return TestType(typename, info.m, "bytes", info)
-    elif typename.startswith("Bytes"):
-        assert typename == "Bytes[32]"  # TODO test others
-        return TestType(typename, 32, "Bytes", None)
-    elif typename.startswith("String"):
-        assert typename == "String[32]"  # TODO test others
-        return TestType(typename, 32, "String", None)
-    elif typename == "address":
-        return TestType(typename, 20, "address", None)
-    elif typename == "bool":
-        return TestType(typename, 1, "bool", None)
-
-    raise AssertionError(f"no info {typename}")
-
-
 def can_convert(i_typ, o_typ):
     """
     Checks whether conversion from one type to another is valid.
@@ -97,44 +66,41 @@ def can_convert(i_typ, o_typ):
     if i_typ == o_typ:
         return False
 
-    i_detail = _parse_type(i_typ)
-    o_detail = _parse_type(o_typ)
-
-    if o_typ == "bool":
+    if isinstance(o_typ, BoolT):
         return True
-    if i_typ == "bool":
-        return o_typ not in {"address"}
+    if isinstance(i_typ, BoolT):
+        return not isinstance(o_typ, AddressT)
 
-    if i_detail.type_class == "int":
-        if o_detail.type_class == "bytes":
-            return i_detail.type_bytes <= o_detail.type_bytes
+    if isinstance(i_typ, IntegerT):
+        if isinstance(o_typ, BytesM_T):
+            return bytes_of_type(i_typ) <= bytes_of_type(o_typ)
 
-        ret = o_detail.type_class in ("int", "decimal", "bytes", "Bytes")
-        if not i_detail.info.is_signed:
-            ret |= o_typ == "address"
+        ret = isinstance(o_typ, (IntegerT, DecimalT, BytesM_T, BytesT))
+        if not i_typ.is_signed:
+            ret |= isinstance(o_typ, AddressT)
         return ret
 
-    elif i_detail.type_class == "bytes":
-        if o_detail.type_class == "Bytes":
+    if isinstance(i_typ, BytesM_T):
+        if isinstance(o_typ, BytesT):
             # bytesN must be of equal or smaller size to the input
-            return i_detail.type_bytes <= o_detail.type_bytes
+            return bytes_of_type(i_typ) <= bytes_of_type(o_typ)
 
-        return o_detail.type_class in ("decimal", "bytes", "int", "address")
+        return isinstance(o_typ, (DecimalT, BytesM_T, IntegerT, AddressT))
 
-    elif i_detail.type_class == "Bytes":
-        return o_detail.type_class in ("int", "decimal", "address")
+    if isinstance(i_typ, BytesT):
+        return isinstance(o_typ, (IntegerT, DecimalT, AddressT))
 
-    elif i_typ == "decimal":
-        if o_detail.type_class == "bytes":
-            return i_detail.type_bytes <= o_detail.type_bytes
+    if isinstance(i_typ, DecimalT):
+        if isinstance(o_typ, BytesM_T):
+            return bytes_of_type(i_typ) <= bytes_of_type(o_typ)
 
-        return o_detail.type_class in ("int", "bool")
+        return isinstance(o_typ, (IntegerT, BoolT))
 
-    elif i_typ == "address":
-        if o_detail.type_class == "bytes":
-            return i_detail.type_bytes <= o_detail.type_bytes
-        elif o_detail.type_class == "int":
-            return not o_detail.info.is_signed
+    if isinstance(i_typ, AddressT):
+        if isinstance(o_typ, BytesM_T):
+            return bytes_of_type(i_typ) <= bytes_of_type(o_typ)
+        if isinstance(o_typ, IntegerT):
+            return not o_typ.is_signed
         return False
 
     raise AssertionError(f"unreachable {i_typ} {o_typ}")
@@ -145,9 +111,7 @@ def uniq(xs):
 
 
 def _cases_for_int(typ):
-    info = parse_integer_typeinfo(typ)
-
-    lo, hi = info.bounds
+    lo, hi = typ.ast_bounds
 
     ret = [lo - 1, lo, lo + 1, -1, 0, 1, hi - 1, hi, hi + 1]
 
@@ -159,9 +123,7 @@ def _cases_for_int(typ):
 
 
 def _cases_for_decimal(typ):
-    info = parse_decimal_info(typ)
-
-    lo, hi = info.decimal_bounds
+    lo, hi = typ.ast_bounds
 
     ret = [Decimal(i) for i in [-1, 0, 1]]
     ret.extend([lo - 1, lo, lo + 1, hi - 1, hi, hi + 1])
@@ -181,8 +143,8 @@ def _cases_for_decimal(typ):
 
 
 def _cases_for_address(_typ):
-    cases = _filter_cases(_cases_for_int("uint160"), "uint160")
-    return [_py_convert(c, "uint160", "address") for c in cases]
+    cases = _filter_cases(_cases_for_int(UINT160_T), UINT160_T)
+    return [_py_convert(c, UINT160_T, AddressT()) for c in cases]
 
 
 def _cases_for_bool(_typ):
@@ -190,10 +152,8 @@ def _cases_for_bool(_typ):
 
 
 def _cases_for_bytes(typ):
-    detail = _parse_type(typ)
-    m_bits = detail.info.m_bits
     # reuse the cases for the equivalent int type
-    equiv_int_type = f"uint{m_bits}"
+    equiv_int_type = UINT(typ.m_bits)
     cases = _filter_cases(_cases_for_int(equiv_int_type), equiv_int_type)
     return [_py_convert(c, equiv_int_type, typ) for c in cases]
 
@@ -202,24 +162,23 @@ def _cases_for_Bytes(typ):
     ret = []
     # would not need this if we tested all Bytes[1]...Bytes[32] types.
     for i in range(32):
-        ret.extend(_cases_for_bytes(f"bytes{i+1}"))
+        ret.extend(_cases_for_bytes(BytesM_T(i + 1)))
     return uniq(ret)
 
 
 # generate all cases of interest for a type, potentially including invalid cases
 def interesting_cases_for_type(typ):
-    detail = _parse_type(typ)
-    if detail.type_class == "int":
+    if isinstance(typ, IntegerT):
         return _cases_for_int(typ)
-    if detail.type_class == "decimal":
+    if isinstance(typ, DecimalT):
         return _cases_for_decimal(typ)
-    if detail.type_class == "bytes":
+    if isinstance(typ, BytesM_T):
         return _cases_for_bytes(typ)
-    if detail.type_class == "Bytes":
+    if isinstance(typ, BytesT):
         return _cases_for_Bytes(typ)
-    if detail.type_class == "bool":
+    if isinstance(typ, BoolT):
         return _cases_for_bool(typ)
-    if detail.type_class == "address":
+    if isinstance(typ, AddressT):
         return _cases_for_address(typ)
 
 
@@ -241,8 +200,7 @@ class _PadDirection(enum.Enum):
 
 
 def _padding_direction(typ):
-    detail = _parse_type(typ)
-    if detail.type_class in ("bytes", "String", "Bytes"):
+    if isinstance(typ, (BytesM_T, StringT, BytesT)):
         return _PadDirection.Right
     return _PadDirection.Left
 
@@ -275,17 +233,15 @@ def _padconvert(val_bits, direction, n, padding_byte=None):
 
 def _from_bits(val_bits, o_typ):
     # o_typ: the type to convert to
-    detail = _parse_type(o_typ)
     try:
-        return decode_single(detail.abi_type, val_bits)
+        return decode_single(o_typ.abi_type.selector_name(), val_bits)
     except eth_abi.exceptions.NonEmptyPaddingBytes:
         raise _OutOfBounds() from None
 
 
 def _to_bits(val, i_typ):
     # i_typ: the type to convert from
-    detail = _parse_type(i_typ)
-    return encode_single(detail.abi_type, val)
+    return encode_single(i_typ.abi_type.selector_name(), val)
 
 
 def _signextend(val_bytes, bits):
@@ -296,19 +252,26 @@ def _signextend(val_bytes, bits):
     return (as_sint % 2 ** 256).to_bytes(32, byteorder="big")
 
 
+def _convert_int_to_int(val, o_typ):
+    lo, hi = o_typ.int_bounds
+    if not lo <= val <= hi:
+        return None
+    return val
+
+
 def _convert_decimal_to_int(val, o_typ):
     # note special behavior for decimal: catch OOB before truncation.
-    if not SizeLimits.in_bounds(o_typ, val):
+    lo, hi = o_typ.int_bounds
+    if not lo <= val <= hi:
         return None
 
     return round_towards_zero(val)
 
 
 def _convert_int_to_decimal(val, o_typ):
-    detail = _parse_type(o_typ)
     ret = Decimal(val)
-    # note: SizeLimits.in_bounds is for the EVM int value, not the python value
-    lo, hi = detail.info.decimal_bounds
+    lo, hi = o_typ.ast_bounds
+
     if not lo <= ret <= hi:
         return None
 
@@ -320,49 +283,45 @@ def _py_convert(val, i_typ, o_typ):
     Perform conversion on the Python representation of a Vyper value.
     Returns None if the conversion is invalid (i.e., would revert in Vyper)
     """
-    i_detail = _parse_type(i_typ)
-    o_detail = _parse_type(o_typ)
 
-    if i_detail.type_class == "int" and o_detail.type_class == "int":
-        if not SizeLimits.in_bounds(o_typ, val):
-            return None
-        return val
+    if isinstance(i_typ, IntegerT) and isinstance(o_typ, IntegerT):
+        return _convert_int_to_int(val, o_typ)
 
-    if i_typ == "decimal" and o_typ in INTEGER_TYPES:
+    if isinstance(i_typ, DecimalT) and isinstance(o_typ, IntegerT):
         return _convert_decimal_to_int(val, o_typ)
 
-    if i_detail.type_class in ("bool", "int") and o_typ == "decimal":
+    if isinstance(i_typ, (BoolT, IntegerT)) and isinstance(o_typ, DecimalT):
         # Note: Decimal(True) == Decimal("1")
         return _convert_int_to_decimal(val, o_typ)
 
     val_bits = _to_bits(val, i_typ)
 
-    if i_detail.type_class in ("Bytes", "String"):
+    if isinstance(i_typ, (BytesT, StringT)):
         val_bits = val_bits[32:]
 
     if _padding_direction(i_typ) != _padding_direction(o_typ):
         # subtle! the padding conversion follows the bytes argument
-        if i_detail.type_class in ("bytes", "Bytes"):
-            n = i_detail.type_bytes
+        if isinstance(i_typ, (BytesM_T, BytesT)):
+            n = bytes_of_type(i_typ)
             padding_byte = None
         else:
             # output type is bytes
-            n = o_detail.type_bytes
+            n = bytes_of_type(o_typ)
             padding_byte = b"\x00"
 
         val_bits = _padconvert(val_bits, _padding_direction(o_typ), n, padding_byte)
 
-    if getattr(o_detail.info, "is_signed", False) and i_detail.type_class == "bytes":
-        n_bits = i_detail.type_bytes * 8
+    if getattr(o_typ, "is_signed", False) and isinstance(i_typ, BytesM_T):
+        n_bits = _bits_of_type(i_typ)
         val_bits = _signextend(val_bits, n_bits)
 
     try:
-        if o_typ == "bool":
-            return _from_bits(val_bits, "uint256") != 0
+        if isinstance(o_typ, BoolT):
+            return _from_bits(val_bits, UINT256_T) != 0
 
         ret = _from_bits(val_bits, o_typ)
 
-        if o_typ == "address":
+        if isinstance(o_typ, AddressT):
             return checksum_encode(ret)
         return ret
 
@@ -437,10 +396,9 @@ def generate_reverting_cases():
 
 
 def _vyper_literal(val, typ):
-    detail = _parse_type(typ)
-    if detail.type_class == "bytes":
+    if isinstance(typ, BytesM_T):
         return "0x" + val.hex()
-    if detail.type_class == "decimal":
+    if isinstance(typ, DecimalT):
         tmp = val
         val = val.quantize(DECIMAL_EPSILON)
         assert tmp == val
@@ -454,7 +412,7 @@ def test_convert_passing(
 ):
 
     expected_val = _py_convert(val, i_typ, o_typ)
-    if o_typ == "address" and expected_val == "0x" + "00" * 20:
+    if isinstance(o_typ, AddressT) and expected_val == "0x" + "00" * 20:
         # web3 has special formatter for zero address
         expected_val = None
 
@@ -469,11 +427,11 @@ def test_convert() -> {o_typ}:
 
     # Skip bytes20 literals when there is ambiguity with `address` since address takes precedence.
     # generally happens when there are only digits in the literal.
-    if i_typ == "bytes20" and is_checksum_encoded(_vyper_literal(val, "bytes20")):
+    if i_typ == BYTES20_T and is_checksum_encoded(_vyper_literal(val, BYTES20_T)):
         skip_c1 = True
 
     # typechecker inference borked, ambiguity with bytes20
-    if i_typ == "address" and o_typ == "bytes20" and val == val.lower():
+    if isinstance(i_typ, AddressT) and o_typ == BYTES20_T and val == val.lower():
         skip_c1 = True
 
     if c1_exception is not None:
@@ -585,7 +543,7 @@ def convert_builtin_constant() -> {out_type}:
 
 # uint256 conversion is currently valid due to type inference on literals
 # not quite working yet
-same_type_conversion_blocked = sorted(TEST_TYPES - {"uint256"})
+same_type_conversion_blocked = sorted(TEST_TYPES - {UINT256_T})
 
 
 @pytest.mark.parametrize("typ", same_type_conversion_blocked)
@@ -671,21 +629,21 @@ def foo() -> {o_typ}:
 
     c1_exception = InvalidLiteral
 
-    if i_typ.startswith(("int", "uint")) and o_typ.startswith("bytes"):
+    if isinstance(i_typ, IntegerT) and isinstance(o_typ, BytesM_T):
         # integer literals get upcasted to uint256 / int256 types, so the convert
         # will not compile unless it is bytes32
-        if o_typ != "bytes32":
+        if o_typ != BYTES32_T:
             c1_exception = TypeMismatch
 
     # compile-time folding not implemented for these:
     skip_c1 = False
-    # if o_typ.startswith("int") and i_typ == "address":
+    # if isinstance(o_typ, IntegerT.signeds()) and isinstance(i_typ, Address()):
     #    skip_c1 = True
 
-    if o_typ.startswith("bytes"):
+    if isinstance(o_typ, BytesM_T):
         skip_c1 = True
 
-    # if o_typ in ("address", "bytes20"):
+    # if o_typ in (AddressT(), BYTES20_T):
     #    skip_c1 = True
 
     if not skip_c1:

commit d62125c7aacb7a4eb444761dcb0e51d547c44e1c

fix: codegen for hashmaps with string keys (#3384)
this commit fixes a regression introduced in 3abe588, where the
codegen in parse_Subscript would handle string keys as non-bytestrings
(thus treating the value of the string as word loaded by the string
 pointer - its length value). it also cleans up the logic a little bit,
avoiding a redundant unwrap_location (since get_element_ptr calls
unwrap_location itself), and removing a dead argument from
keccak256_helper.

vyper/builtins/functions.py
@@ -611,7 +611,7 @@ class Keccak256(BuiltinFunction):
     @process_inputs
     def build_IR(self, expr, args, kwargs, context):
         assert len(args) == 1
-        return keccak256_helper(expr, args[0], context)
+        return keccak256_helper(args[0], context)
 
 
 def _make_sha256_call(inp_start, inp_len, out_start, out_len):

vyper/codegen/events.py
@@ -15,7 +15,7 @@ def _encode_log_topics(expr, event_id, arg_nodes, context):
             value = unwrap_location(arg)
 
         elif isinstance(arg.typ, _BytestringT):
-            value = keccak256_helper(expr, arg, context=context)
+            value = keccak256_helper(arg, context=context)
         else:
             # TODO block at higher level
             raise TypeMismatch("Event indexes may only be value types", expr)

vyper/codegen/expr.py
@@ -338,11 +338,10 @@ class Expr:
 
         if isinstance(sub.typ, HashMapT):
             # TODO sanity check we are in a self.my_map[i] situation
-            index = Expr.parse_value_expr(self.expr.slice.value, self.context)
-            if isinstance(index.typ, BytesT):
+            index = Expr(self.expr.slice.value, self.context).ir_node
+            if isinstance(index.typ, _BytestringT):
                 # we have to hash the key to get a storage location
-                assert len(index.args) == 1
-                index = keccak256_helper(self.expr.slice.value, index.args[0], self.context)
+                index = keccak256_helper(index, self.context)
 
         elif is_array_like(sub.typ):
             index = Expr.parse_value_expr(self.expr.slice.value, self.context)
@@ -528,8 +527,8 @@ class Expr:
             left = Expr(self.expr.left, self.context).ir_node
             right = Expr(self.expr.right, self.context).ir_node
 
-            left_keccak = keccak256_helper(self.expr, left, self.context)
-            right_keccak = keccak256_helper(self.expr, right, self.context)
+            left_keccak = keccak256_helper(left, self.context)
+            right_keccak = keccak256_helper(right, self.context)
 
             if op not in ("eq", "ne"):
                 return  # raises

vyper/codegen/keccak256_helper.py
@@ -8,7 +8,7 @@ from vyper.semantics.types.shortcuts import BYTES32_T
 from vyper.utils import SHA3_BASE, SHA3_PER_WORD, MemoryPositions, bytes_to_int, keccak256
 
 
-def _check_byteslike(typ, _expr):
+def _check_byteslike(typ):
     if not isinstance(typ, _BytestringT) and typ != BYTES32_T:
         # NOTE this may be checked at a higher level, but just be safe
         raise CompilerPanic("keccak256 only accepts bytes-like objects")
@@ -18,8 +18,8 @@ def _gas_bound(num_words):
     return SHA3_BASE + num_words * SHA3_PER_WORD
 
 
-def keccak256_helper(expr, to_hash, context):
-    _check_byteslike(to_hash.typ, expr)
+def keccak256_helper(to_hash, context):
+    _check_byteslike(to_hash.typ)
 
     # Can hash literals
     # TODO this is dead code.

tests/parser/features/test_string_map_keys.py
@@ -1,0 +1,25 @@
+def test_string_map_keys(get_contract):
+    code = """
+f:HashMap[String[1], bool]
+@external
+def test() -> bool:
+    a:String[1] = "a"
+    b:String[1] = "b"
+    self.f[a] = True
+    return self.f[b]  # should return False
+    """
+    c = get_contract(code)
+    c.test()
+    assert c.test() is False
+
+
+def test_string_map_keys_literals(get_contract):
+    code = """
+f:HashMap[String[1], bool]
+@external
+def test() -> bool:
+    self.f["a"] = True
+    return self.f["b"]  # should return False
+    """
+    c = get_contract(code)
+    assert c.test() is False