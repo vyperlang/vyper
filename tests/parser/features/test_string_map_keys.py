e741550b9244c23970780ee6b1adcf73ccab34aa
Charles Cooper <cooper.charles.m@gmail.com>
2023-05-04 18:53:18 -0700

    add test for string literals

--- tests/parser/features/test_string_map_keys.py
+++ tests/parser/features/test_string_map_keys.py
@@ -9,4 +9,17 @@ def test() -> bool:
     return self.f[b]  # should return False
     """
     c = get_contract(code)
+    c.test()
+    assert c.test() is False
+
+
+def test_string_map_keys_literals(get_contract):
+    code = """
+f:HashMap[String[1], bool]
+@external
+def test() -> bool:
+    self.f["a"] = True
+    return self.f["b"]  # should return False
+    """
+    c = get_contract(code)
     assert c.test() is False
