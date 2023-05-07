vyperlang/vyper

commit 046ea166d93b8351da7c5d74115893eea2f60d76

refactor: front-end type system (#2974)
This commit refactors the front-end type system.

Currently, there are essentially two classes types for each vyper type.
"Primitives" represented raw types, and "Definitions" represent
the instantiation of types attached to each expression in a program.
"Definitions" additionally carry around annotation information related
to the expressions they tag, such as mutability and location info. This
system also handled dispatching into the correct routines for cases
where a raw type (e.g. "MyStruct") is used in expr land (vs an instance
of a type, e.g.  "my_struct"). This is important because certain types
are callable or attributable - specifically, constructors and enum
members (ex. `MyStruct()` and `MyEnum.FOO`).

This commit reworks the system by factoring the annotation info into
VyperType and ExprInfo. "Primitives" in the old system can be thought of
as VyperTypes, and "Definitions" in the old system are most similar to
the new ExprInfo. To handle cases where types can live in expr land,
this commit also clarifies that usage by promoting raw types into the
special, internal TYPE_T type (which can be thought of as "the type of
a type"). This commit also simplifies the inheritance structure of vyper
types.

Some other miscellaneous things in this commit:
- AST class hierarchy gets more refined - addition of ExprNode
- rename of `vyper/semantics/validation/` -> `vyper/semantics/analysis/`
- type system directory structure simplified
- merge of `builtin_functions/` and `builtin_interfaces/` to `builtins/`

Co-authored-by: z80 <z80dev@mlnl.finance>

vyper/codegen/expr.py
@@ -36,7 +36,6 @@ from vyper.codegen.types import (
 from vyper.codegen.types.convert import new_type_to_old_type
 from vyper.evm.opcodes import version_check
 from vyper.exceptions import (
-    CompilerPanic,
     EvmVersionException,
     StructureException,
     TypeCheckFailure,
@@ -80,10 +79,8 @@ class Expr:
         self.ir_node.source_pos = getpos(self.expr)
 
     def parse_Int(self):
-        typ_ = self.expr._metadata.get("type")
-        if typ_ is None:
-            raise CompilerPanic("Type of integer literal is unknown")
-        new_typ = new_type_to_old_type(typ_)
+        typ = self.expr._metadata["type"]
+        new_typ = new_type_to_old_type(typ)
         new_typ.is_literal = True
         return IRnode.from_list(self.expr.n, typ=new_typ)
 
@@ -185,9 +182,14 @@ class Expr:
                 mutable=var.mutable,
             )
 
-        elif self.expr._metadata["type"].is_immutable:
+        # TODO: use self.expr._expr_info
+        elif self.expr.id in self.context.globals:
             var = self.context.globals[self.expr.id]
-            ofst = self.expr._metadata["type"].position.offset
+            varinfo = var._varinfo
+            if not varinfo.is_immutable:
+                return  # fail
+
+            ofst = varinfo.position.offset
 
             if self.context.sig.is_init_func:
                 mutable = True
@@ -259,10 +261,10 @@ class Expr:
                 return IRnode.from_list(["~extcode", addr], typ=ByteArrayType(0))
         # self.x: global attribute
         elif isinstance(self.expr.value, vy_ast.Name) and self.expr.value.id == "self":
-            type_ = self.expr._metadata["type"]
             var = self.context.globals[self.expr.attr]
+            varinfo = var._varinfo
             return IRnode.from_list(
-                type_.position.position,
+                varinfo.position.position,
                 typ=var.typ,
                 location=STORAGE,
                 annotation="self." + self.expr.attr,
@@ -641,8 +643,8 @@ class Expr:
 
     # Function calls
     def parse_Call(self):
-        # TODO check out this inline import
-        from vyper.builtin_functions import DISPATCH_TABLE
+        # TODO fix cyclic import
+        from vyper.builtins.functions import DISPATCH_TABLE
 
         if isinstance(self.expr.func, vy_ast.Name):
             function_name = self.expr.func.id

vyper/codegen/stmt.py
@@ -2,7 +2,7 @@ import vyper.codegen.events as events
 import vyper.utils as util
 from vyper import ast as vy_ast
 from vyper.address_space import MEMORY, STORAGE
-from vyper.builtin_functions import STMT_DISPATCH_TABLE
+from vyper.builtins.functions import STMT_DISPATCH_TABLE
 from vyper.codegen import external_call, self_call
 from vyper.codegen.context import Constancy, Context
 from vyper.codegen.core import (

commit 3c83947223e19a526e27bbeed69141021b82e060

fix: guard against instantiation of events and hashmaps (#3341)
fix: guard against instantiation of events and hashmaps

this commit fixes a regression introduced in 046ea166d93. as a result of
removing the location kwarg from `type_from_annotation()`, events could
be instantiated in the type system (althought attempts to use them would
fail during codegen). this commit reintroduces the location kwarg, and
also streamlines some logic so that HashMaps and events can all be
handled in the same code paths.

Co-authored-by: Charles Cooper <cooper.charles.m@gmail.com>

vyper/builtins/functions.py
@@ -5,7 +5,6 @@ from decimal import Decimal
 
 from vyper import ast as vy_ast
 from vyper.abi_types import ABI_Tuple
-from vyper.address_space import MEMORY, STORAGE
 from vyper.ast.validation import validate_call_args
 from vyper.codegen.abi_encoder import abi_encode
 from vyper.codegen.context import Context, VariableRecord
@@ -39,6 +38,7 @@ from vyper.codegen.core import (
 from vyper.codegen.expr import Expr
 from vyper.codegen.ir_node import Encoding
 from vyper.codegen.keccak256_helper import keccak256_helper
+from vyper.evm.address_space import MEMORY, STORAGE
 from vyper.exceptions import (
     ArgumentException,
     CompilerPanic,

vyper/codegen/abi_encoder.py
@@ -1,4 +1,3 @@
-from vyper.address_space import MEMORY
 from vyper.codegen.core import (
     STORE,
     add_ofst,
@@ -9,6 +8,7 @@ from vyper.codegen.core import (
     zero_pad,
 )
 from vyper.codegen.ir_node import IRnode
+from vyper.evm.address_space import MEMORY
 from vyper.exceptions import CompilerPanic
 from vyper.semantics.types import DArrayT, SArrayT, _BytestringT
 from vyper.semantics.types.shortcuts import UINT256_T

vyper/codegen/context.py
@@ -3,8 +3,8 @@ import enum
 from dataclasses import dataclass
 from typing import Any, Optional
 
-from vyper.address_space import MEMORY, AddrSpace
 from vyper.codegen.ir_node import Encoding
+from vyper.evm.address_space import MEMORY, AddrSpace
 from vyper.exceptions import CompilerPanic, StateAccessViolation
 from vyper.semantics.types import VyperType
 

vyper/codegen/core.py
@@ -1,6 +1,6 @@
 from vyper import ast as vy_ast
-from vyper.address_space import CALLDATA, DATA, IMMUTABLES, MEMORY, STORAGE
 from vyper.codegen.ir_node import Encoding, IRnode
+from vyper.evm.address_space import CALLDATA, DATA, IMMUTABLES, MEMORY, STORAGE
 from vyper.evm.opcodes import version_check
 from vyper.exceptions import CompilerPanic, StructureException, TypeCheckFailure, TypeMismatch
 from vyper.semantics.types import (

vyper/codegen/expr.py
@@ -3,7 +3,6 @@ import math
 
 import vyper.codegen.arithmetic as arithmetic
 from vyper import ast as vy_ast
-from vyper.address_space import DATA, IMMUTABLES, MEMORY, STORAGE
 from vyper.codegen import external_call, self_call
 from vyper.codegen.core import (
     clamp,
@@ -24,6 +23,7 @@ from vyper.codegen.core import (
 )
 from vyper.codegen.ir_node import IRnode
 from vyper.codegen.keccak256_helper import keccak256_helper
+from vyper.evm.address_space import DATA, IMMUTABLES, MEMORY, STORAGE
 from vyper.evm.opcodes import version_check
 from vyper.exceptions import (
     CompilerPanic,

vyper/codegen/external_call.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 
 import vyper.utils as util
-from vyper.address_space import MEMORY
 from vyper.codegen.abi_encoder import abi_encode
 from vyper.codegen.core import (
     _freshname,
@@ -17,6 +16,7 @@ from vyper.codegen.core import (
     wrap_value_for_external_return,
 )
 from vyper.codegen.ir_node import Encoding, IRnode
+from vyper.evm.address_space import MEMORY
 from vyper.exceptions import TypeCheckFailure
 from vyper.semantics.types import InterfaceT, TupleT
 from vyper.semantics.types.function import StateMutability

vyper/codegen/function_definitions/external_function.py
@@ -1,7 +1,6 @@
 from typing import Any, List
 
 import vyper.utils as util
-from vyper.address_space import CALLDATA, DATA, MEMORY
 from vyper.ast.signatures.function_signature import FunctionSignature
 from vyper.codegen.abi_encoder import abi_encoding_matches_vyper
 from vyper.codegen.context import Context, VariableRecord
@@ -10,6 +9,7 @@ from vyper.codegen.expr import Expr
 from vyper.codegen.function_definitions.utils import get_nonreentrant_lock
 from vyper.codegen.ir_node import Encoding, IRnode
 from vyper.codegen.stmt import parse_body
+from vyper.evm.address_space import CALLDATA, DATA, MEMORY
 from vyper.semantics.types import TupleT
 
 

vyper/codegen/ir_node.py
@@ -3,8 +3,8 @@ from enum import Enum, auto
 from functools import cached_property
 from typing import Any, List, Optional, Tuple, Union
 
-from vyper.address_space import AddrSpace
 from vyper.compiler.settings import VYPER_COLOR_OUTPUT
+from vyper.evm.address_space import AddrSpace
 from vyper.evm.opcodes import get_ir_opcodes
 from vyper.exceptions import CodegenPanic, CompilerPanic
 from vyper.semantics.types import VyperType

vyper/codegen/return_.py
@@ -1,6 +1,5 @@
 from typing import Any, Optional
 
-from vyper.address_space import MEMORY
 from vyper.codegen.abi_encoder import abi_encode, abi_encoding_matches_vyper
 from vyper.codegen.context import Context
 from vyper.codegen.core import (
@@ -13,6 +12,7 @@ from vyper.codegen.core import (
     wrap_value_for_external_return,
 )
 from vyper.codegen.ir_node import IRnode
+from vyper.evm.address_space import MEMORY
 
 Stmt = Any  # mypy kludge
 

vyper/codegen/self_call.py
@@ -1,6 +1,6 @@
-from vyper.address_space import MEMORY
 from vyper.codegen.core import _freshname, eval_once_check, make_setter
 from vyper.codegen.ir_node import IRnode, push_label_to_stack
+from vyper.evm.address_space import MEMORY
 from vyper.exceptions import StateAccessViolation, StructureException
 from vyper.semantics.types.subscriptable import TupleT
 

vyper/codegen/stmt.py
@@ -1,7 +1,6 @@
 import vyper.codegen.events as events
 import vyper.utils as util
 from vyper import ast as vy_ast
-from vyper.address_space import MEMORY, STORAGE
 from vyper.builtins.functions import STMT_DISPATCH_TABLE
 from vyper.codegen import external_call, self_call
 from vyper.codegen.context import Constancy, Context
@@ -23,6 +22,7 @@ from vyper.codegen.core import (
 )
 from vyper.codegen.expr import Expr
 from vyper.codegen.return_ import make_return_stmt
+from vyper.evm.address_space import MEMORY, STORAGE
 from vyper.exceptions import CompilerPanic, StructureException, TypeCheckFailure
 from vyper.semantics.types import DArrayT, MemberFunctionT
 from vyper.semantics.types.shortcuts import INT256_T, UINT256_T

vyper/exceptions.py
@@ -151,6 +151,10 @@ class StructureException(VyperException):
     """Invalid structure for parsable syntax."""
 
 
+class InstantiationException(StructureException):
+    """Variable or expression cannot be instantiated"""
+
+
 class VersionException(VyperException):
     """Version string is malformed or incompatible with this compiler version."""
 

vyper/semantics/analysis/base.py
@@ -9,6 +9,7 @@ from vyper.exceptions import (
     StateAccessViolation,
     VyperInternalException,
 )
+from vyper.semantics.data_locations import DataLocation
 from vyper.semantics.types.base import VyperType
 
 
@@ -90,15 +91,6 @@ class StateMutability(_StringEnum):
             return StateMutability.NONPAYABLE
         # NOTE: The state mutability nonpayable is reflected in Solidity by not
         #       specifying a state mutability modifier at all. Do the same here.
-
-
-# TODO: move me to locations.py?
-class DataLocation(enum.Enum):
-    UNSET = 0
-    MEMORY = 1
-    STORAGE = 2
-    CALLDATA = 3
-    CODE = 4
 
 
 class DataPosition:

vyper/semantics/analysis/local.py
@@ -19,7 +19,7 @@ from vyper.exceptions import (
     VyperException,
 )
 from vyper.semantics.analysis.annotation import StatementAnnotationVisitor
-from vyper.semantics.analysis.base import DataLocation, VarInfo
+from vyper.semantics.analysis.base import VarInfo
 from vyper.semantics.analysis.common import VyperNodeVisitorBase
 from vyper.semantics.analysis.utils import (
     get_common_types,
@@ -28,6 +28,7 @@ from vyper.semantics.analysis.utils import (
     get_possible_types_from_node,
     validate_expected_type,
 )
+from vyper.semantics.data_locations import DataLocation
 
 # TODO consolidate some of these imports
 from vyper.semantics.environment import CONSTANT_ENVIRONMENT_VARS, MUTABLE_ENVIRONMENT_VARS
@@ -232,7 +233,7 @@ class FunctionNodeVisitor(VyperNodeVisitorBase):
                 "Memory variables must be declared with an initial value", node
             )
 
-        type_ = type_from_annotation(node.annotation)
+        type_ = type_from_annotation(node.annotation, DataLocation.MEMORY)
         validate_expected_type(node.value, type_)
 
         try:

vyper/semantics/analysis/module.py
@@ -18,7 +18,7 @@ from vyper.exceptions import (
     VariableDeclarationException,
     VyperException,
 )
-from vyper.semantics.analysis.base import DataLocation, VarInfo
+from vyper.semantics.analysis.base import VarInfo
 from vyper.semantics.analysis.common import VyperNodeVisitorBase
 from vyper.semantics.analysis.levenshtein_utils import get_levenshtein_error_suggestions
 from vyper.semantics.analysis.utils import (
@@ -26,6 +26,7 @@ from vyper.semantics.analysis.utils import (
     validate_expected_type,
     validate_unique_method_ids,
 )
+from vyper.semantics.data_locations import DataLocation
 from vyper.semantics.namespace import Namespace, get_namespace
 from vyper.semantics.types import EnumT, EventT, InterfaceT, StructT
 from vyper.semantics.types.function import ContractFunctionT
@@ -196,7 +197,7 @@ class ModuleAnalyzer(VyperNodeVisitorBase):
             else DataLocation.STORAGE
         )
 
-        type_ = type_from_annotation(node.annotation)
+        type_ = type_from_annotation(node.annotation, data_loc)
         var_info = VarInfo(
             type_,
             decl_node=node,

vyper/semantics/data_locations.py
@@ -1,0 +1,9 @@
+import enum
+
+
+class DataLocation(enum.Enum):
+    UNSET = 0
+    MEMORY = 1
+    STORAGE = 2
+    CALLDATA = 3
+    CODE = 4

vyper/semantics/types/base.py
@@ -40,6 +40,8 @@ class VyperType:
         If `True`, this type can be used as the base member for an array.
     _valid_literal : Tuple
         A tuple of Vyper ast classes that may be assigned this type.
+    _invalid_locations : Tuple
+        A tuple of invalid `DataLocation`s for this type
     _is_prim_word: bool, optional
         This is a word type like uint256, int8, bytesM or address
     """
@@ -47,6 +49,7 @@ class VyperType:
     _id: str
     _type_members: Optional[Dict] = None
     _valid_literal: Tuple = ()
+    _invalid_locations: Tuple = ()
     _is_prim_word: bool = False
     _equality_attrs: Optional[Tuple] = None
     _is_array_type: bool = False

vyper/semantics/types/function.py
@@ -15,13 +15,9 @@ from vyper.exceptions import (
     StateAccessViolation,
     StructureException,
 )
-from vyper.semantics.analysis.base import (
-    DataLocation,
-    FunctionVisibility,
-    StateMutability,
-    StorageSlot,
-)
+from vyper.semantics.analysis.base import FunctionVisibility, StateMutability, StorageSlot
 from vyper.semantics.analysis.utils import check_kwargable, validate_expected_type
+from vyper.semantics.data_locations import DataLocation
 from vyper.semantics.namespace import get_namespace
 from vyper.semantics.types.base import KwargSettings, VyperType
 from vyper.semantics.types.primitives import BoolT
@@ -297,7 +293,7 @@ class ContractFunctionT(VyperType):
             if arg.annotation is None:
                 raise ArgumentException(f"Function argument '{arg.arg}' is missing a type", arg)
 
-            type_ = type_from_annotation(arg.annotation)
+            type_ = type_from_annotation(arg.annotation, DataLocation.CALLDATA)
 
             if value is not None:
                 if not check_kwargable(value):
@@ -316,7 +312,8 @@ class ContractFunctionT(VyperType):
                 "Constructor may not have a return type", node.returns
             )
         elif isinstance(node.returns, (vy_ast.Name, vy_ast.Subscript, vy_ast.Tuple)):
-            return_type = type_from_annotation(node.returns)
+            # note: consider, for cleanliness, adding DataLocation.RETURN_VALUE
+            return_type = type_from_annotation(node.returns, DataLocation.MEMORY)
         else:
             raise InvalidType("Function return value must be a type name or tuple", node.returns)
 
@@ -350,7 +347,7 @@ class ContractFunctionT(VyperType):
         """
         if not node.is_public:
             raise CompilerPanic("getter generated for non-public function")
-        type_ = type_from_annotation(node.annotation)
+        type_ = type_from_annotation(node.annotation, DataLocation.STORAGE)
         arguments, return_type = type_.getter_signature
         args_dict: OrderedDict = OrderedDict()
         for item in arguments:

vyper/semantics/types/subscriptable.py
@@ -4,6 +4,7 @@ from typing import Any, Dict, Optional, Tuple, Union
 from vyper import ast as vy_ast
 from vyper.abi_types import ABI_DynamicArray, ABI_StaticArray, ABI_Tuple, ABIType
 from vyper.exceptions import ArrayIndexException, InvalidType, StructureException
+from vyper.semantics.data_locations import DataLocation
 from vyper.semantics.types.base import VyperType
 from vyper.semantics.types.primitives import IntegerT
 from vyper.semantics.types.shortcuts import UINT256_T
@@ -44,6 +45,14 @@ class HashMapT(_SubscriptableT):
 
     _equality_attrs = ("key_type", "value_type")
 
+    # disallow everything but storage
+    _invalid_locations = (
+        DataLocation.UNSET,
+        DataLocation.CALLDATA,
+        DataLocation.CODE,
+        DataLocation.MEMORY,
+    )
+
     def __repr__(self):
         return f"HashMap[{self.key_type}, {self.value_type}]"
 
@@ -73,15 +82,13 @@ class HashMapT(_SubscriptableT):
                 ),
                 node,
             )
-        # if location != DataLocation.STORAGE or is_immutable:
-        #    raise StructureException("HashMap can only be declared as a storage variable", node)
 
         k_ast, v_ast = node.slice.value.elements
-        key_type = type_from_annotation(k_ast)
+        key_type = type_from_annotation(k_ast, DataLocation.STORAGE)
         if not key_type._as_hashmap_key:
             raise InvalidType("can only use primitive types as HashMap key!", k_ast)
 
-        value_type = type_from_annotation(v_ast)
+        value_type = type_from_annotation(v_ast, DataLocation.STORAGE)
 
         return cls(key_type, value_type)
 
@@ -291,11 +298,18 @@ class TupleT(VyperType):
     """
     Tuple type definition.
 
-    This class is used to represent multiple return values from
-    functions.
+    This class is used to represent multiple return values from functions.
     """
 
     _equality_attrs = ("members",)
+
+    # note: docs say that tuples are not instantiable but they
+    # are in fact instantiable and the codegen works. if we
+    # wanted to be stricter in the typechecker, we could
+    # add _invalid_locations = everything but UNSET and RETURN_VALUE.
+    # (we would need to add a DataLocation.RETURN_VALUE in order for
+    # tuples to be instantiable as return values but not in memory).
+    # _invalid_locations = ...
 
     def __init__(self, member_types: Tuple[VyperType, ...]) -> None:
         super().__init__()

vyper/semantics/types/user.py
@@ -17,6 +17,7 @@ from vyper.exceptions import (
 from vyper.semantics.analysis.base import VarInfo
 from vyper.semantics.analysis.levenshtein_utils import get_levenshtein_error_suggestions
 from vyper.semantics.analysis.utils import validate_expected_type, validate_unique_method_ids
+from vyper.semantics.data_locations import DataLocation
 from vyper.semantics.namespace import get_namespace
 from vyper.semantics.types.base import VyperType
 from vyper.semantics.types.function import ContractFunctionT
@@ -152,6 +153,8 @@ class EventT(_UserType):
     name : str
         Name of the event.
     """
+
+    _invalid_locations = tuple(iter(DataLocation))  # not instantiable in any location
 
     def __init__(self, name: str, arguments: dict, indexed: list) -> None:
         super().__init__(members=arguments)
@@ -470,10 +473,6 @@ class StructT(_UserType):
         self._id = _id
 
         self.ast_def = ast_def
-
-        for n, t in self.members.items():
-            if isinstance(t, HashMapT):
-                raise StructureException(f"Struct contains a mapping '{n}'", ast_def)
 
     @cached_property
     def name(self) -> str:

vyper/semantics/types/utils.py
@@ -1,8 +1,15 @@
 from typing import Dict
 
 from vyper import ast as vy_ast
-from vyper.exceptions import ArrayIndexException, InvalidType, StructureException, UnknownType
+from vyper.exceptions import (
+    ArrayIndexException,
+    InstantiationException,
+    InvalidType,
+    StructureException,
+    UnknownType,
+)
 from vyper.semantics.analysis.levenshtein_utils import get_levenshtein_error_suggestions
+from vyper.semantics.data_locations import DataLocation
 from vyper.semantics.namespace import get_namespace
 from vyper.semantics.types.base import VyperType
 
@@ -60,9 +67,11 @@ def type_from_abi(abi_type: Dict) -> VyperType:
             raise UnknownType(f"ABI contains unknown type: {type_string}") from None
 
 
-def type_from_annotation(node: vy_ast.VyperNode) -> VyperType:
+def type_from_annotation(
+    node: vy_ast.VyperNode, location: DataLocation = DataLocation.UNSET
+) -> VyperType:
     """
-    Return a type object for the given AST node.
+    Return a type object for the given AST node after validating its location.
 
     Arguments
     ---------
@@ -74,6 +83,16 @@ def type_from_annotation(node: vy_ast.VyperNode) -> VyperType:
     VyperType
         Type definition object.
     """
+    typ_ = _type_from_annotation(node)
+
+    if location in typ_._invalid_locations:
+        location_str = "" if location is DataLocation.UNSET else f"in {location.name.lower()}"
+        raise InstantiationException(f"{typ_} is not instantiable {location_str}", node)
+
+    return typ_
+
+
+def _type_from_annotation(node: vy_ast.VyperNode) -> VyperType:
     namespace = get_namespace()
 
     def _failwith(type_name):
@@ -84,7 +103,6 @@ def type_from_annotation(node: vy_ast.VyperNode) -> VyperType:
 
     if isinstance(node, vy_ast.Tuple):
         tuple_t = namespace["$TupleT"]
-
         return tuple_t.from_annotation(node)
 
     if isinstance(node, vy_ast.Subscript):

tests/functional/semantics/types/test_type_from_annotation.py
@@ -6,6 +6,7 @@ from vyper.exceptions import (
     StructureException,
     UndeclaredDefinition,
 )
+from vyper.semantics.data_locations import DataLocation
 from vyper.semantics.types import PRIMITIVE_TYPES, HashMapT, SArrayT
 from vyper.semantics.types.utils import type_from_annotation
 
@@ -14,7 +15,8 @@ BYTESTRING_TYPES = ["String", "Bytes"]
 
 
 @pytest.mark.parametrize("type_str", BASE_TYPES)
-def test_base_types(build_node, type_str):
+@pytest.mark.parametrize("location", iter(DataLocation))
+def test_base_types(build_node, type_str, location):
     node = build_node(type_str)
     base_t = PRIMITIVE_TYPES[type_str]
 
@@ -24,7 +26,8 @@ def test_base_types(build_node, type_str):
 
 
 @pytest.mark.parametrize("type_str", BYTESTRING_TYPES)
-def test_array_value_types(build_node, type_str):
+@pytest.mark.parametrize("location", iter(DataLocation))
+def test_array_value_types(build_node, type_str, location):
     node = build_node(f"{type_str}[1]")
     base_t = PRIMITIVE_TYPES[type_str](1)
 
@@ -34,7 +37,8 @@ def test_array_value_types(build_node, type_str):
 
 
 @pytest.mark.parametrize("type_str", BASE_TYPES)
-def test_base_types_as_arrays(build_node, type_str):
+@pytest.mark.parametrize("location", iter(DataLocation))
+def test_base_types_as_arrays(build_node, type_str, location):
     node = build_node(f"{type_str}[3]")
     base_t = PRIMITIVE_TYPES[type_str]
 
@@ -44,7 +48,8 @@ def test_base_types_as_arrays(build_node, type_str):
 
 
 @pytest.mark.parametrize("type_str", BYTESTRING_TYPES)
-def test_array_value_types_as_arrays(build_node, type_str):
+@pytest.mark.parametrize("location", iter(DataLocation))
+def test_array_value_types_as_arrays(build_node, type_str, location):
     node = build_node(f"{type_str}[1][1]")
 
     with pytest.raises(StructureException):
@@ -52,7 +57,8 @@ def test_array_value_types_as_arrays(build_node, type_str):
 
 
 @pytest.mark.parametrize("type_str", BASE_TYPES)
-def test_base_types_as_multidimensional_arrays(build_node, namespace, type_str):
+@pytest.mark.parametrize("location", iter(DataLocation))
+def test_base_types_as_multidimensional_arrays(build_node, namespace, type_str, location):
     node = build_node(f"{type_str}[3][5]")
     base_t = PRIMITIVE_TYPES[type_str]
 
@@ -63,7 +69,8 @@ def test_base_types_as_multidimensional_arrays(build_node, namespace, type_str):
 
 @pytest.mark.parametrize("type_str", ["int128", "String"])
 @pytest.mark.parametrize("idx", ["0", "-1", "0x00", "'1'", "foo", "[1]", "(1,)"])
-def test_invalid_index(build_node, idx, type_str):
+@pytest.mark.parametrize("location", iter(DataLocation))
+def test_invalid_index(build_node, idx, type_str, location):
     node = build_node(f"{type_str}[{idx}]")
     with pytest.raises(
         (ArrayIndexException, InvalidType, StructureException, UndeclaredDefinition)
@@ -77,7 +84,7 @@ def test_mapping(build_node, type_str, type_str2):
     node = build_node(f"HashMap[{type_str}, {type_str2}]")
     types = PRIMITIVE_TYPES
 
-    ann_t = type_from_annotation(node)
+    ann_t = type_from_annotation(node, DataLocation.STORAGE)
 
     k_t = types[type_str]
     v_t = types[type_str2]
@@ -91,7 +98,7 @@ def test_multidimensional_mapping(build_node, type_str, type_str2):
     node = build_node(f"HashMap[{type_str}, HashMap[{type_str}, {type_str2}]]")
     types = PRIMITIVE_TYPES
 
-    ann_t = type_from_annotation(node)
+    ann_t = type_from_annotation(node, DataLocation.STORAGE)
 
     k_t = types[type_str]
     v_t = types[type_str2]

tests/parser/exceptions/test_instantiation_exception.py
@@ -1,0 +1,81 @@
+import pytest
+
+from vyper.exceptions import InstantiationException
+
+invalid_list = [
+    """
+event Foo:
+    a: uint256
+
+@external
+def foo() -> Foo:
+    return Foo(2)
+    """,
+    """
+event Foo:
+    a: uint256
+
+@external
+def foo() -> (uint256, Foo):
+    return 1, Foo(2)
+    """,
+    """
+a: HashMap[uint256, uint256]
+
+@external
+def foo() -> HashMap[uint256, uint256]:
+    return self.a
+    """,
+    """
+event Foo:
+    a: uint256
+
+@external
+def foo(x: Foo):
+    pass
+    """,
+    """
+@external
+def foo(x: HashMap[uint256, uint256]):
+    pass
+    """,
+    """
+event Foo:
+    a: uint256
+
+foo: Foo
+    """,
+    """
+event Foo:
+    a: uint256
+
+@external
+def foo():
+    f: Foo = Foo(1)
+    pass
+    """,
+    """
+event Foo:
+    a: uint256
+
+b: HashMap[uint256, Foo]
+    """,
+    """
+event Foo:
+    a: uint256
+
+b: HashMap[Foo, uint256]
+    """,
+    """
+b: immutable(HashMap[uint256, uint256])
+
+@external
+def __init__():
+    b = empty(HashMap[uint256, uint256])
+    """,
+]
+
+
+@pytest.mark.parametrize("bad_code", invalid_list)
+def test_instantiation_exception(bad_code, get_contract, assert_compile_failed):
+    assert_compile_failed(lambda: get_contract(bad_code), InstantiationException)

tests/parser/functions/test_empty.py
@@ -1,6 +1,6 @@
 import pytest
 
-from vyper.exceptions import TypeMismatch
+from vyper.exceptions import InstantiationException, TypeMismatch
 
 
 @pytest.mark.parametrize(
@@ -711,4 +711,4 @@ def test():
     ],
 )
 def test_invalid_types(contract, get_contract, assert_compile_failed):
-    assert_compile_failed(lambda: get_contract(contract), TypeMismatch)
+    assert_compile_failed(lambda: get_contract(contract), InstantiationException)

tests/parser/syntax/test_structs.py
@@ -2,6 +2,7 @@ import pytest
 
 from vyper import compiler
 from vyper.exceptions import (
+    InstantiationException,
     InvalidType,
     StructureException,
     TypeMismatch,
@@ -297,7 +298,7 @@ struct Nom:
     a: HashMap[int128, C]
     b: int128
     """,
-        StructureException,
+        InstantiationException,
     ),
     """
 struct C1:
@@ -428,6 +429,16 @@ def foo():
     Foo({a: 1})
     """,
         StructureException,
+    ),
+    (
+        """
+event Foo:
+    a: uint256
+
+struct Bar:
+    a: Foo
+    """,
+        InstantiationException,
     ),
 ]
 