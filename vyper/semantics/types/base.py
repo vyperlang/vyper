ba25230346b836291d67920934a07ae3578d99d7
tserg <8017125+tserg@users.noreply.github.com>
2023-05-07 13:54:19 +0800

    fix lint

--- vyper/semantics/types/base.py
+++ vyper/semantics/types/base.py
@@ -12,7 +12,6 @@ from vyper.exceptions import (
     UnknownAttribute,
 )
 from vyper.semantics.analysis.levenshtein_utils import get_levenshtein_error_suggestions
-from vyper.semantics.data_locations import DataLocation
 from vyper.semantics.namespace import validate_identifier
 
 
