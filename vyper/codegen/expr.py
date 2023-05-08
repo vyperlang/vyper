9fd5d31ea263866eca9535d4f8746c6056f060f0
tserg <8017125+tserg@users.noreply.github.com>
2023-05-08 19:48:35 +0800

    fix type fetching

--- vyper/codegen/expr.py
+++ vyper/codegen/expr.py
@@ -648,19 +648,20 @@ class Expr:
 
         if isinstance(self.expr.func, vy_ast.Name):
             function_name = self.expr.func.id
-            typ = self.expr._metadata["type"]
 
             if function_name in DISPATCH_TABLE:
                 return DISPATCH_TABLE[function_name].build_IR(self.expr, self.context)
 
             # Struct constructors do not need `self` prefix.
-            elif isinstance(typ, StructT):
+            elif isinstance(self.expr._metadata["type"], StructT):
                 args = self.expr.args
                 if len(args) == 1 and isinstance(args[0], vy_ast.Dict):
-                    return Expr.struct_literals(args[0], function_name, self.context, typ)
+                    return Expr.struct_literals(
+                        args[0], function_name, self.context, self.expr._metadata["type"]
+                    )
 
             # Interface assignment. Bar(<address>).
-            elif isinstance(typ, InterfaceT):
+            elif isinstance(self.expr._metadata["type"], InterfaceT):
                 (arg0,) = self.expr.args
                 arg_ir = Expr(arg0, self.context).ir_node
 
