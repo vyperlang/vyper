vyperlang/vyper

commit 046ea166d93b8351da7c5d74115893eea2f60d76

refactor: front-end type system (#2974)
This commit refactors the front-end type system.

Currently, there are essentially two classes types for each vyper type.
"Primitives" represented raw types, and "Definitions" represent
the instantiation of types attached to each expression in a program.
"Definitions" additionally carry around annotation information related
to the expressions they tag, such as mutability and location info. This
system also handled dispatching into the correct routines for cases
where a raw type (e.g. "MyStruct") is used in expr land (vs an instance
of a type, e.g.  "my_struct"). This is important because certain types
are callable or attributable - specifically, constructors and enum
members (ex. `MyStruct()` and `MyEnum.FOO`).

This commit reworks the system by factoring the annotation info into
VyperType and ExprInfo. "Primitives" in the old system can be thought of
as VyperTypes, and "Definitions" in the old system are most similar to
the new ExprInfo. To handle cases where types can live in expr land,
this commit also clarifies that usage by promoting raw types into the
special, internal TYPE_T type (which can be thought of as "the type of
a type"). This commit also simplifies the inheritance structure of vyper
types.

Some other miscellaneous things in this commit:
- AST class hierarchy gets more refined - addition of ExprNode
- rename of `vyper/semantics/validation/` -> `vyper/semantics/analysis/`
- type system directory structure simplified
- merge of `builtin_functions/` and `builtin_interfaces/` to `builtins/`

Co-authored-by: z80 <z80dev@mlnl.finance>

tests/parser/syntax/test_interfaces.py
@@ -4,7 +4,7 @@ from vyper import compiler
 from vyper.exceptions import (
     ArgumentException,
     InvalidReference,
-    StructureException,
+    InvalidType,
     SyntaxException,
     TypeMismatch,
     UnknownAttribute,
@@ -47,7 +47,7 @@ from vyper.interfaces import ERC20
 def test():
     a: address(ERC20) = ZERO_ADDRESS
     """,
-        (StructureException, SyntaxException),
+        InvalidType,
     ),
     (
         """

tests/parser/functions/test_interfaces.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 import pytest
 
 from vyper.ast.signatures.interface import extract_sigs
-from vyper.builtin_interfaces import ERC20, ERC721
+from vyper.builtins.interfaces import ERC20, ERC721
 from vyper.cli.utils import extract_file_interface_imports
 from vyper.compiler import compile_code, compile_codes
 from vyper.exceptions import ArgumentException, InterfaceViolation, StructureException

commit 3f70dd2c8412001c6fa7ab4b2b0613d1c0a13fd1

fix: typechecking for user-defined types (#3418)
non-structs were not type checked correctly. this represents a
regression presumably in 046ea166d9.

this commit fixes the issue and adds some tests.

vyper/semantics/types/user.py
@@ -32,6 +32,10 @@ class _UserType(VyperType):
     def __eq__(self, other):
         return self is other
 
+    # TODO: revisit this once user types can be imported via modules
+    def compare_type(self, other):
+        return super().compare_type(other) and self._id == other._id
+
     def __hash__(self):
         return hash(id(self))
 
@@ -536,10 +540,6 @@ class StructT(_UserType):
 
     def __repr__(self):
         return f"{self._id} declaration object"
-
-    # TODO check me
-    def compare_type(self, other):
-        return super().compare_type(other) and self._id == other._id
 
     @property
     def size_in_bytes(self):

tests/parser/syntax/test_enum.py
@@ -110,6 +110,20 @@ def foo() -> Roles:
     """,
         UnknownAttribute,
     ),
+    (
+        """
+enum A:
+    a
+enum B:
+    a
+    b
+
+@internal
+def foo():
+    a: A = B.b
+    """,
+        TypeMismatch,
+    ),
 ]
 
 

tests/parser/syntax/test_interfaces.py
@@ -106,6 +106,19 @@ implements: Foo
     """,
         StructureException,
     ),
+    (
+        """
+from vyper.interfaces import ERC20
+
+interface A:
+    def f(): view
+
+@internal
+def foo():
+    a: ERC20 = A(empty(address))
+    """,
+        TypeMismatch,
+    ),
 ]
 
 