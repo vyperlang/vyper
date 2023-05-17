a79c4e26c5bf86bbc35e75f59876f00420b47af6
Charles Cooper <cooper.charles.m@gmail.com>
2023-05-17 22:15:07 +0000

    fix error message

--- vyper/semantics/analysis/local.py
+++ vyper/semantics/analysis/local.py
@@ -545,7 +545,7 @@ class FunctionNodeVisitor(VyperNodeVisitorBase):
             raise StructureException("Value is not an event", node.value)
         if self.func.mutability <= StateMutability.VIEW:
             raise StructureException(
-                "Cannot emit logs from {self.func.mutability.lower() functions", node
+                f"Cannot emit logs from {self.func.mutability.lower()} functions", node
             )
         f.fetch_call_return(node.value)
         self.expr_visitor.visit(node.value)
