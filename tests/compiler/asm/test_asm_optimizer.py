vyperlang/vyper

commit c202c4e3ec884c6ddb28f738e45e8b9aa3d209b4

fix: constructor context for internal functions (#3388)
this commit fixes two related issues with initcode generation:

- nested internal functions called from the constructor would cause a
  compiler panic
- internal functions called from the constructor would not read/write
  from the correct immutables space

the relevant examples reproducing each issue are in the tests. this
commit fixes the issue by

- not trying to traverse the call graph to figure out which internal
  functions to include in the initcode. instead, all internal functions
  are included, and we rely on the dead code eliminator to remove unused
  functions
- adding a "constructor" flag to the codegen, so we can distinguish
  between internal calls which are being generated to include in
  initcode or runtime code.

vyper/codegen/module.py
@@ -67,19 +67,21 @@ def _runtime_ir(runtime_functions, all_sigs, global_ctx):
 
     # create a map of the IR functions since they might live in both
     # runtime and deploy code (if init function calls them)
-    internal_functions_map: Dict[str, IRnode] = {}
+    internal_functions_ir: list[IRnode] = []
 
     for func_ast in internal_functions:
         func_ir = generate_ir_for_function(func_ast, all_sigs, global_ctx, False)
-        internal_functions_map[func_ast.name] = func_ir
+        internal_functions_ir.append(func_ir)
 
     # for some reason, somebody may want to deploy a contract with no
     # external functions, or more likely, a "pure data" contract which
     # contains immutables
     if len(external_functions) == 0:
-        # TODO: prune internal functions in this case?
-        runtime = ["seq"] + list(internal_functions_map.values())
-        return runtime, internal_functions_map
+        # TODO: prune internal functions in this case? dead code eliminator
+        # might not eliminate them, since internal function jumpdest is at the
+        # first instruction in the contract.
+        runtime = ["seq"] + internal_functions_ir
+        return runtime
 
     # note: if the user does not provide one, the default fallback function
     # reverts anyway. so it does not hurt to batch the payable check.
@@ -125,10 +127,10 @@ def _runtime_ir(runtime_functions, all_sigs, global_ctx):
         ["label", "fallback", ["var_list"], fallback_ir],
     ]
 
-    # TODO: prune unreachable functions?
-    runtime.extend(internal_functions_map.values())
+    # note: dead code eliminator will clean dead functions
+    runtime.extend(internal_functions_ir)
 
-    return runtime, internal_functions_map
+    return runtime
 
 
 # take a GlobalContext, which is basically
@@ -159,12 +161,15 @@ def generate_ir_for_module(global_ctx: GlobalContext) -> Tuple[IRnode, IRnode, F
     runtime_functions = [f for f in function_defs if not _is_init_func(f)]
     init_function = next((f for f in function_defs if _is_init_func(f)), None)
 
-    runtime, internal_functions = _runtime_ir(runtime_functions, all_sigs, global_ctx)
+    runtime = _runtime_ir(runtime_functions, all_sigs, global_ctx)
 
     deploy_code: List[Any] = ["seq"]
     immutables_len = global_ctx.immutable_section_bytes
     if init_function:
-        init_func_ir = generate_ir_for_function(init_function, all_sigs, global_ctx, False)
+        # TODO might be cleaner to separate this into an _init_ir helper func
+        init_func_ir = generate_ir_for_function(
+            init_function, all_sigs, global_ctx, skip_nonpayable_check=False, is_ctor_context=True
+        )
         deploy_code.append(init_func_ir)
 
         # pass the amount of memory allocated for the init function
@@ -174,8 +179,13 @@ def generate_ir_for_module(global_ctx: GlobalContext) -> Tuple[IRnode, IRnode, F
         deploy_code.append(["deploy", init_mem_used, runtime, immutables_len])
 
         # internal functions come after everything else
-        for f in init_function._metadata["type"].called_functions:
-            deploy_code.append(internal_functions[f.name])
+        internal_functions = [f for f in runtime_functions if _is_internal(f)]
+        for f in internal_functions:
+            func_ir = generate_ir_for_function(
+                f, all_sigs, global_ctx, skip_nonpayable_check=False, is_ctor_context=True
+            )
+            # note: we depend on dead code eliminator to clean dead function defs
+            deploy_code.append(func_ir)
 
     else:
         if immutables_len != 0:

tests/compiler/asm/test_asm_optimizer.py
@@ -1,0 +1,49 @@
+from vyper.compiler.phases import CompilerData
+
+
+def test_dead_code_eliminator():
+    code = """
+s: uint256
+
+@internal
+def foo():
+    self.s = 1
+
+@internal
+def qux():
+    self.s = 2
+
+@external
+def bar():
+    self.foo()
+
+@external
+def __init__():
+    self.qux()
+    """
+
+    c = CompilerData(code, no_optimize=True)
+    initcode_asm = [i for i in c.assembly if not isinstance(i, list)]
+    runtime_asm = c.assembly_runtime
+
+    foo_label = "_sym_internal_foo___"
+    qux_label = "_sym_internal_qux___"
+
+    # all the labels should be in all the unoptimized asms
+    for s in (foo_label, qux_label):
+        assert s in initcode_asm
+        assert s in runtime_asm
+
+    c = CompilerData(code, no_optimize=False)
+    initcode_asm = [i for i in c.assembly if not isinstance(i, list)]
+    runtime_asm = c.assembly_runtime
+
+    # qux should not be in runtime code
+    for instr in runtime_asm:
+        if isinstance(instr, str):
+            assert not instr.startswith(qux_label), instr
+
+    # foo should not be in initcode asm
+    for instr in initcode_asm:
+        if isinstance(instr, str):
+            assert not instr.startswith(foo_label), instr

tests/functional/semantics/analysis/test_for_loop.py
@@ -108,14 +108,14 @@ def main():
     for j in range(3):
         x: uint256 = j
         y: uint16 = j
-    """,  # issue 3212
+    """,  # GH issue 3212
     """
 @external
 def foo():
     for i in [1]:
         a:uint256 = i
         b:uint16 = i
-    """,  # issue 3374
+    """,  # GH issue 3374
     """
 @external
 def foo():
@@ -123,7 +123,7 @@ def foo():
         for j in [1]:
             a:uint256 = i
         b:uint16 = i
-    """,  # issue 3374
+    """,  # GH issue 3374
     """
 @external
 def foo():
@@ -131,7 +131,7 @@ def foo():
         for j in [1,2,3]:
             b:uint256 = j + i
         c:uint16 = i
-    """,  # issue 3374
+    """,  # GH issue 3374
 ]
 
 

tests/parser/features/test_comparison.py
@@ -4,7 +4,7 @@ import pytest
 
 
 def test_3034_verbatim(get_contract):
-    # test issue #3034 exactly
+    # test GH issue 3034 exactly
     code = """
 @view
 @external

tests/parser/features/test_immutable.py
@@ -239,3 +239,90 @@ def get_immutable() -> uint256:
 
     c = get_contract(code, n)
     assert c.get_immutable() == n + 2
+
+
+# GH issue 3292
+def test_internal_functions_called_by_ctor_location(get_contract):
+    code = """
+d: uint256
+x: immutable(uint256)
+
+@external
+def __init__():
+    self.d = 1
+    x = 2
+    self.a()
+
+@external
+def test() -> uint256:
+    return self.d
+
+@internal
+def a():
+    self.d = x
+    """
+    c = get_contract(code)
+    assert c.test() == 2
+
+
+# GH issue 3292, extended to nested internal functions
+def test_nested_internal_function_immutables(get_contract):
+    code = """
+d: public(uint256)
+x: public(immutable(uint256))
+
+@external
+def __init__():
+    self.d = 1
+    x = 2
+    self.a()
+
+@internal
+def a():
+    self.b()
+
+@internal
+def b():
+    self.d = x
+    """
+    c = get_contract(code)
+    assert c.x() == 2
+    assert c.d() == 2
+
+
+# GH issue 3292, test immutable read from both ctor and runtime
+def test_immutable_read_ctor_and_runtime(get_contract):
+    code = """
+d: public(uint256)
+x: public(immutable(uint256))
+
+@external
+def __init__():
+    self.d = 1
+    x = 2
+    self.a()
+
+@internal
+def a():
+    self.d = x
+
+@external
+def thrash():
+    self.d += 5
+
+@external
+def fix():
+    self.a()
+    """
+    c = get_contract(code)
+    assert c.x() == 2
+    assert c.d() == 2
+
+    c.thrash(transact={})
+
+    assert c.x() == 2
+    assert c.d() == 2 + 5
+
+    c.fix(transact={})
+    assert c.x() == 2
+    assert c.d() == 2

tests/parser/features/test_init.py
@@ -53,3 +53,29 @@ def baz() -> uint8:
 
     n = 256
     assert_compile_failed(lambda: get_contract(code, n))
+
+
+# GH issue 3206
+def test_nested_internal_call_from_ctor(get_contract):
+    code = """
+x: uint256
+
+@external
+def __init__():
+    self.a()
+
+@internal
+def a():
+    self.x += 1
+    self.b()
+
+@internal
+def b():
+    self.x += 2
+
+@external
+def test() -> uint256:
+    return self.x
+    """
+    c = get_contract(code)
+    assert c.test() == 3

vyper/codegen/context.py
@@ -54,6 +54,7 @@ class Context:
         forvars=None,
         constancy=Constancy.Mutable,
         sig=None,
+        is_ctor_context=False,
     ):
         # In-memory variables, in the form (name, memory location, type)
         self.vars = vars_ or {}
@@ -91,6 +92,9 @@ class Context:
         # Incremented values, used for internal IDs
         self._internal_var_iter = 0
         self._scope_id_iter = 0
+
+        # either the constructor, or called from the constructor
+        self.is_ctor_context = is_ctor_context
 
     def is_constant(self):
         return self.constancy is Constancy.Constant or self.in_assertion or self.in_range_expr

vyper/codegen/expr.py
@@ -184,7 +184,7 @@ class Expr:
 
             ofst = varinfo.position.offset
 
-            if self.context.sig.is_init_func:
+            if self.context.is_ctor_context:
                 mutable = True
                 location = IMMUTABLES
             else:

commit 510125e0fce389fcc2b9993691696eb0836345b6

fix: initcode codesize regression (#3450)
this commit fixes a regression in c202c4e3ec8. the commit message states
that we rely on the dead code eliminator to prune unused internal
functions in the initcode, but the dead code eliminator does not prune
dead code in all cases, including nested internal functions and loops.
this commit reintroduces the reachability analysis in
`vyper/codegen/module.py` as a stopgap until the dead code eliminator is
more robust.

vyper/codegen/module.py
@@ -123,7 +123,6 @@ def _runtime_ir(runtime_functions, global_ctx):
         ["label", "fallback", ["var_list"], fallback_ir],
     ]
 
-    # note: dead code eliminator will clean dead functions
     runtime.extend(internal_functions_ir)
 
     return runtime
@@ -178,10 +177,14 @@ def generate_ir_for_module(global_ctx: GlobalContext) -> tuple[IRnode, IRnode]:
         # internal functions come after everything else
         internal_functions = [f for f in runtime_functions if _is_internal(f)]
         for f in internal_functions:
+            init_func_t = init_function._metadata["type"]
+            if f.name not in init_func_t.recursive_calls:
+                # unreachable
+                continue
+
             func_ir = generate_ir_for_function(
                 f, global_ctx, skip_nonpayable_check=False, is_ctor_context=True
             )
-            # note: we depend on dead code eliminator to clean dead function defs
             deploy_code.append(func_ir)
 
     else:

vyper/ir/compile_ir.py
@@ -758,6 +758,9 @@ def note_breakpoint(line_number_map, item, pos):
             line_number_map["breakpoints"].add(item.lineno + 1)
 
 
+_TERMINAL_OPS = ("JUMP", "RETURN", "REVERT", "STOP", "INVALID")
+
+
 def _prune_unreachable_code(assembly):
     # In converting IR to assembly we sometimes end up with unreachable
     # instructions - POPing to clear the stack or STOPing execution at the
@@ -766,9 +769,13 @@ def _prune_unreachable_code(assembly):
     # to avoid unnecessary bytecode bloat.
     changed = False
     i = 0
-    while i < len(assembly) - 1:
-        if assembly[i] in ("JUMP", "RETURN", "REVERT", "STOP") and not (
-            is_symbol(assembly[i + 1]) or assembly[i + 1] == "JUMPDEST"
+    while i < len(assembly) - 2:
+        instr = assembly[i]
+        if isinstance(instr, list):
+            instr = assembly[i][-1]
+
+        if assembly[i] in _TERMINAL_OPS and not (
+            is_symbol(assembly[i + 1]) and assembly[i + 2] in ("JUMPDEST", "BLANK")
         ):
             changed = True
             del assembly[i + 1]

tests/compiler/asm/test_asm_optimizer.py
@@ -1,17 +1,42 @@
+import pytest
+
 from vyper.compiler.phases import CompilerData
 
-
-def test_dead_code_eliminator():
-    code = """
+codes = [
+    """
 s: uint256
 
 @internal
-def foo():
+def ctor_only():
     self.s = 1
 
 @internal
-def qux():
+def runtime_only():
     self.s = 2
+
+@external
+def bar():
+    self.runtime_only()
+
+@external
+def __init__():
+    self.ctor_only()
+    """,
+    # code with nested function in it
+    """
+s: uint256
+
+@internal
+def runtime_only():
+    self.s = 1
+
+@internal
+def foo():
+    self.runtime_only()
+
+@internal
+def ctor_only():
+    self.s += 1
 
 @external
 def bar():
@@ -19,31 +44,59 @@ def bar():
 
 @external
 def __init__():
-    self.qux()
+    self.ctor_only()
+    """,
+    # code with loop in it, these are harder for dead code eliminator
     """
+s: uint256
 
+@internal
+def ctor_only():
+    self.s = 1
+
+@internal
+def runtime_only():
+    for i in range(10):
+        self.s += 1
+
+@external
+def bar():
+    self.runtime_only()
+
+@external
+def __init__():
+    self.ctor_only()
+    """,
+]
+
+
+@pytest.mark.parametrize("code", codes)
+def test_dead_code_eliminator(code):
     c = CompilerData(code, no_optimize=True)
     initcode_asm = [i for i in c.assembly if not isinstance(i, list)]
     runtime_asm = c.assembly_runtime
 
-    foo_label = "_sym_internal_foo___"
-    qux_label = "_sym_internal_qux___"
+    ctor_only_label = "_sym_internal_ctor_only___"
+    runtime_only_label = "_sym_internal_runtime_only___"
 
-    # all the labels should be in all the unoptimized asms
-    for s in (foo_label, qux_label):
-        assert s + "_deploy" in initcode_asm
+    # qux reachable from unoptimized initcode, foo not reachable.
+    assert ctor_only_label + "_deploy" in initcode_asm
+    assert runtime_only_label + "_deploy" not in initcode_asm
+
+    # all labels should be in unoptimized runtime asm
+    for s in (ctor_only_label, runtime_only_label):
         assert s + "_runtime" in runtime_asm
 
     c = CompilerData(code, no_optimize=False)
     initcode_asm = [i for i in c.assembly if not isinstance(i, list)]
     runtime_asm = c.assembly_runtime
 
-    # qux should not be in runtime code
+    # ctor only label should not be in runtime code
     for instr in runtime_asm:
         if isinstance(instr, str):
-            assert not instr.startswith(qux_label), instr
+            assert not instr.startswith(ctor_only_label), instr
 
-    # foo should not be in initcode asm
+    # runtime only label should not be in initcode asm
     for instr in initcode_asm:
         if isinstance(instr, str):
-            assert not instr.startswith(foo_label), instr
+            assert not instr.startswith(runtime_only_label), instr

tests/parser/functions/test_create_functions.py
@@ -3,6 +3,8 @@ import rlp
 from eth.codecs import abi
 from hexbytes import HexBytes
 
+import vyper.ir.compile_ir as compile_ir
+from vyper.codegen.ir_node import IRnode
 from vyper.utils import EIP_170_LIMIT, checksum_encode, keccak256
 
 
@@ -224,15 +226,23 @@ def test(code_ofst: uint256) -> address:
     return create_from_blueprint(BLUEPRINT, code_offset=code_ofst)
     """
 
-    # use a bunch of JUMPDEST + STOP instructions as blueprint code
-    # (as any STOP instruction returns valid code, split up with
-    # jumpdests as optimization fence)
     initcode_len = 100
-    f = get_contract_from_ir(["deploy", 0, ["seq"] + ["jumpdest", "stop"] * (initcode_len // 2), 0])
-    blueprint_code = w3.eth.get_code(f.address)
-    print(blueprint_code)
 
-    d = get_contract(deployer_code, f.address)
+    # deploy a blueprint contract whose contained initcode contains only
+    # zeroes (so no matter which offset, create_from_blueprint will
+    # return empty code)
+    ir = IRnode.from_list(["deploy", 0, ["seq"] + ["stop"] * initcode_len, 0])
+    bytecode, _ = compile_ir.assembly_to_evm(compile_ir.compile_to_assembly(ir, no_optimize=True))
+    # manually deploy the bytecode
+    c = w3.eth.contract(abi=[], bytecode=bytecode)
+    deploy_transaction = c.constructor()
+    tx_info = {"from": w3.eth.accounts[0], "value": 0, "gasPrice": 0}
+    tx_hash = deploy_transaction.transact(tx_info)
+    blueprint_address = w3.eth.get_transaction_receipt(tx_hash)["contractAddress"]
+    blueprint_code = w3.eth.get_code(blueprint_address)
+    print("BLUEPRINT CODE:", blueprint_code)
+
+    d = get_contract(deployer_code, blueprint_address)
 
     # deploy with code_ofst=0 fine
     d.test(0)