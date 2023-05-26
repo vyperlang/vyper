20b5b1dcf326d37ae279cdcd50374b6b0e50b1af
Charles Cooper <cooper.charles.m@gmail.com>
2023-05-25 23:44:55 -0400

    small refactor, add bucket datastructure

--- vyper/codegen/jumptable.py
+++ vyper/codegen/jumptable.py
@@ -9,6 +9,16 @@ class Signature:
     method_id: int
     payable: bool
 
+@dataclass
+class Bucket:
+    bucket_id: int
+    magic: int
+    signatures: list[int]
+
+    @property
+    def image(self):
+        return _image_of([s for s in self.signatures], self.magic)
+
 
 # https://stackoverflow.com/a/568618/ with comments removed
 def gen_primes():
@@ -28,13 +38,17 @@ def gen_primes():
 
         q += 1
 
+def _image_of(xs, p):
+    return [((x * p) >> 32) % len(xs) for x in xs]
+
 def find_magic_for(xs):
     gen = gen_primes()
+
     for i in range(0xFFFF):
         p = next(gen)
-        test = [((x * p) >> 32) % len(xs) for x in xs]
+        test = _image_of(xs, p)
         if len(test) == len(set(test)):
-            return p, test
+            return p
 
     raise Exception(f"Could not find hash for {xs}")
 
@@ -43,18 +57,19 @@ def find_magic_for(xs):
 # method_id % len(method_ids)
 # second, get the magic for the bucket.
 def _jumptable(method_ids):
+
     buckets = {}
     for x in method_ids:
         t = x % len(method_ids)
         buckets.setdefault(t, [])
         buckets[t].append(x)
 
-    hashtable = {}
-    for bucket, ids in buckets.items():
-        magic, vals = find_magic_for(ids)
-        hashtable[bucket] = (magic, ids, vals)
+    ret = {}
+    for bucket_id, method_ids in buckets.items():
+        magic = find_magic_for(method_ids)
+        ret[bucket_id] = Bucket(bucket_id, magic, method_ids)
 
-    return hashtable
+    return ret
 
 
 def jumptable_ir(signatures):
