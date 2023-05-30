4394ba4ef2bacfa8cea207baea0247547ac6e509
Charles Cooper <cooper.charles.m@gmail.com>
2023-05-30 16:01:47 -0400

    remove vyper signature from runtime
    it's going at the end of initcode instead, which is cheaper but still
    possible to pick up the vyper version by looking at the create tx.

--- vyper/ir/compile_ir.py
+++ vyper/ir/compile_ir.py
@@ -968,9 +968,7 @@ def adjust_pc_maps(pc_maps, ofst):
     return ret
 
 
-def assembly_to_evm(
-    assembly, pc_ofst=0, insert_vyper_signature=False, disable_bytecode_metadata=False
-):
+def assembly_to_evm(assembly, pc_ofst=0, insert_vyper_signature=False):
     """
     Assembles assembly into EVM
 
@@ -994,7 +992,7 @@ def assembly_to_evm(
     runtime_code, runtime_code_start, runtime_code_end = None, None, None
 
     bytecode_suffix = b""
-    if (not disable_bytecode_metadata) and insert_vyper_signature:
+    if insert_vyper_signature:
         # CBOR encoded: {"vyper": [major,minor,patch]}
         bytecode_suffix += b"\xa1\x65vyper\x83" + bytes(list(version_tuple))
         bytecode_suffix += len(bytecode_suffix).to_bytes(2, "big")
@@ -1011,11 +1009,7 @@ def assembly_to_evm(
     for i, item in enumerate(assembly):
         if isinstance(item, list):
             assert runtime_code is None, "Multiple subcodes"
-            runtime_code, runtime_map = assembly_to_evm(
-                item,
-                insert_vyper_signature=True,
-                disable_bytecode_metadata=disable_bytecode_metadata,
-            )
+            runtime_code, runtime_map = assembly_to_evm(item)
 
             assert item[0].startswith("_DEPLOY_MEM_OFST_")
             assert ctor_mem_size is None
