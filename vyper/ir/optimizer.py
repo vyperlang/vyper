3f117ddd52bbfe24813a9f78f01c46c12e9f0a52
Charles Cooper <cooper.charles.m@gmail.com>
2023-07-19 22:28:43 -0700

    chore: apply merge_dload
    merge_dload was defined in 5dc3ac7ec7, but was not applied. this commit
    fixes that.

--- vyper/ir/optimizer.py
+++ vyper/ir/optimizer.py
@@ -473,6 +473,7 @@ def _optimize(node: IRnode, parent: Optional[IRnode]) -> Tuple[bool, IRnode]:
     if value == "seq":
         changed |= _merge_memzero(argz)
         changed |= _merge_calldataload(argz)
+        changed |= _merge_dload(argz)
         changed |= _merge_mload(argz)
         changed |= _remove_empty_seqs(argz)
 
