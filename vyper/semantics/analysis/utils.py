b56e80616e22c43cd1cb0e63d30c3118f2e395f2
tserg <8017125+tserg@users.noreply.github.com>
2023-07-21 11:12:59 +0800

    fix

--- vyper/semantics/analysis/utils.py
+++ vyper/semantics/analysis/utils.py
@@ -184,21 +184,26 @@ class _ExprAnalyser:
         # variable attribute, e.g. `foo.bar`
         t = self.get_exact_type_from_node(node.value, include_type_exprs=True)
         name = node.attr
+
+        def _raise_invalid_reference(name, node):
+            raise InvalidReference(
+                f"'{name}' is not a storage variable, it should not be prepended with self", node
+            )
+
         try:
             s = t.get_member(name, node)
             if isinstance(s, VyperType):
                 # ex. foo.bar(). bar() is a ContractFunctionT
                 return [s]
             if is_self_reference and (s.is_constant or s.is_immutable):
-                raise InvalidReference(
-                    f"'{name}' is not a storage variable, it should not be prepended with self",
-                    node,
-                )
+                _raise_invalid_reference(name, node)
             # general case. s is a VarInfo, e.g. self.foo
             return [s.typ]
         except UnknownAttribute:
             if not is_self_reference:
                 raise
+            if name in self.namespace:
+                _raise_invalid_reference(name, node)
 
             suggestions_str = get_levenshtein_error_suggestions(name, t.members, 0.4)
             raise UndeclaredDefinition(
