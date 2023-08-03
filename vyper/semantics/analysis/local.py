29953dda6700295c686126233633ce8faf46c9ad
tserg <8017125+tserg@users.noreply.github.com>
2023-08-03 11:36:25 +0800

    improve comment

--- vyper/semantics/analysis/local.py
+++ vyper/semantics/analysis/local.py
@@ -424,7 +424,7 @@ class FunctionNodeVisitor(VyperNodeVisitorBase):
             if assign:
                 raise ImmutableViolation("Cannot modify array during iteration", assign)
 
-        # Check for state-modifying functions in `iter`
+        # Check for state-modifying function calls in `iter`
         iter_call_nodes = node.iter.get_descendants(vy_ast.Call)
         for call_node in iter_call_nodes:
             call_type = get_exact_type_from_node(call_node.func)
