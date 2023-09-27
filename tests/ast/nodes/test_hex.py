fec3a931b21b1a4dc6d45894e6ad56e0d396fae9
tserg <8017125+tserg@users.noreply.github.com>
2023-09-27 12:11:28 +0800

    clarify tests

--- tests/ast/nodes/test_hex.py
+++ tests/ast/nodes/test_hex.py
@@ -2,7 +2,7 @@ import pytest
 
 from vyper import ast as vy_ast
 from vyper import semantics
-from vyper.exceptions import InvalidLiteral
+from vyper.exceptions import BadChecksumAddress, InvalidLiteral
 
 code_invalid_checksum = [
     """
@@ -27,6 +27,19 @@ def foo():
     for i in [0x6b175474e89094c44da98b954eedeac495271d0F]:
         pass
     """,
+]
+
+
+@pytest.mark.parametrize("code", code_invalid_checksum)
+def test_bad_checksum_address(code):
+    vyper_module = vy_ast.parse_to_ast(code)
+
+    with pytest.raises(BadChecksumAddress):
+        vy_ast.validation.validate_literal_nodes(vyper_module)
+        semantics.validate_semantics(vyper_module, {})
+
+
+code_invalid_literal = [
     """
 foo: constant(bytes20) = 0x6b175474e89094c44da98b954eedeac495271d0F
     """,
@@ -36,8 +49,8 @@ foo: constant(bytes4) = 0x12_34_56
 ]
 
 
-@pytest.mark.parametrize("code", code_invalid_checksum)
-def test_invalid_checksum(code):
+@pytest.mark.parametrize("code", code_invalid_literal)
+def test_invalid_literal(code):
     vyper_module = vy_ast.parse_to_ast(code)
 
     with pytest.raises(InvalidLiteral):
