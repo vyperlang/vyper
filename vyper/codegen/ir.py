d7e2bbf02e224a5a958099f47f218094ffdf0d4b
Harry Kalogirou <harkal@nlogn.eu>
2023-10-18 16:01:50 +0300

    ir passes calling

--- vyper/codegen/ir.py
+++ vyper/codegen/ir.py
@@ -8,31 +8,41 @@ from vyper.compiler.settings import OptimizationLevel
 from vyper.ir.bb_optimizer import (
     calculate_in_set,
     calculate_liveness,
-    optimize_empty_blocks,
-    optimize_function,
+    ir_pass_optimize_empty_blocks,
+    ir_pass_remove_unreachable_blocks,
+    ir_pass_optimize_unused_variables,
 )
 from vyper.ir.ir_to_bb_pass import convert_ir_basicblock
 
 
 def generate_ir(ir: IRnode, optimize: Optional[OptimizationLevel] = None) -> IRFunctionBase:
     # Convert "old" IR to "new" IR
-    ctx = convert_ir_basicblock(ir, optimize)
+    ctx = convert_ir_basicblock(ir)
 
     # Run passes on "new" IR
-    if optimize is not OptimizationLevel.NONE:
-        optimize_function(ctx)
+    # if optimize is not OptimizationLevel.NONE:
+    #     optimize_function(ctx)
 
-    optimize_empty_blocks(ctx)
-    calculate_in_set(ctx)
-    calculate_liveness(ctx)
-    convert_ir_to_dfg(ctx)
+    while True:
+        changes = 0
 
-    ir_pass_constant_propagation(ctx)
-    ir_pass_dft(ctx)
+        changes += ir_pass_optimize_empty_blocks(ctx)
+        changes += ir_pass_remove_unreachable_blocks(ctx)
 
-    calculate_in_set(ctx)
-    calculate_liveness(ctx)
+        calculate_liveness(ctx)
 
-    convert_ir_to_dfg(ctx)
+        changes += ir_pass_optimize_unused_variables(ctx)
+
+        convert_ir_to_dfg(ctx)
+
+        changes += ir_pass_constant_propagation(ctx)
+        changes += ir_pass_dft(ctx)
+
+        calculate_in_set(ctx)
+        calculate_liveness(ctx)
+        convert_ir_to_dfg(ctx)
+
+        if changes == 0:
+            break
 
     return ctx
