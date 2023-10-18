4a6a7d9a2080123df194f0534b83e8d4cac1b530
Harry Kalogirou <harkal@nlogn.eu>
2023-10-18 16:01:19 +0300

    ir_pass decorator

--- vyper/utils.py
+++ vyper/utils.py
@@ -462,3 +462,25 @@ def annotate_source_code(
     cleanup_lines += [""] * (num_lines - len(cleanup_lines))
 
     return "\n".join(cleanup_lines)
+
+
+def ir_pass(func):
+    """
+    Decorator for IR passes. This decorator will run the pass repeatedly until
+    no more changes are made.
+    """
+
+    def wrapper(*args, **kwargs):
+        count = 0
+
+        while True:
+            changes = func(*args, **kwargs) or 0
+            if isinstance(changes, list):
+                changes = len(changes)
+            count += changes
+            if changes == 0:
+                break
+
+        return count
+
+    return wrapper
