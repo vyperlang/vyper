97f4053efade450847c98f8470a792987b44adba
tserg <8017125+tserg@users.noreply.github.com>
2023-10-27 10:06:57 +0800

    try removing more tests

--- tests/parser/functions/test_length.py
+++ tests/parser/functions/test_length.py
@@ -1,22 +1,20 @@
 import pytest
 
+# def test_test_length(get_contract_with_gas_estimation):
+#     test_length = """
+# y: Bytes[10]
 
-"""
-def test_test_length(get_contract_with_gas_estimation):
-    test_length = """
-y: Bytes[10]
+# @external
+# def foo(inp: Bytes[10]) -> uint256:
+#     x: Bytes[5] = slice(inp,1, 5)
+#     self.y = slice(inp, 2, 4)
+#     return len(inp) * 100 + len(x) * 10 + len(self.y)
+#     """
 
-@external
-def foo(inp: Bytes[10]) -> uint256:
-    x: Bytes[5] = slice(inp,1, 5)
-    self.y = slice(inp, 2, 4)
-    return len(inp) * 100 + len(x) * 10 + len(self.y)
-    """
+#     c = get_contract_with_gas_estimation(test_length)
+#     assert c.foo(b"badminton") == 954, c.foo(b"badminton")
+#     print("Passed length test")
 
-    c = get_contract_with_gas_estimation(test_length)
-    assert c.foo(b"badminton") == 954, c.foo(b"badminton")
-    print("Passed length test")
-"""
 
 @pytest.mark.parametrize("typ", ["DynArray[uint256, 50]", "Bytes[50]", "String[50]"])
 def test_zero_length(get_contract_with_gas_estimation, typ):
