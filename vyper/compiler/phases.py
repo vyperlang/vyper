2d90a0ce4cba246bce41e097d22befb95db8d00d
Charles Cooper <cooper.charles.m@gmail.com>
2023-10-28 13:36:20 -0400

    always use experimental_codegen - for now

--- vyper/compiler/phases.py
+++ vyper/compiler/phases.py
@@ -85,7 +85,7 @@ class CompilerData:
             Use experimental codegen. Defaults to False
         """
         # to force experimental codegen, uncomment:
-        # experimental_codegen = True
+        experimental_codegen = True
         self.contract_name = contract_name
         self.source_code = source_code
         self.interface_codes = interface_codes
