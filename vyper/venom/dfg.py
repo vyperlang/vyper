979383a91b2dcc60855ed31510cd8b327860b6f1
Charles Cooper <cooper.charles.m@gmail.com>
2023-11-02 09:50:37 -0400

    add review

--- vyper/venom/dfg.py
+++ vyper/venom/dfg.py
@@ -191,6 +191,8 @@ def _stack_reorder(assembly: list, stack: StackModel, stack_ops: OrderedSet[IRVa
         op = stack_ops[i]
         final_stack_depth = -(len(stack_ops) - i - 1)
         depth = stack.get_depth(op)
+
+        # REVIEW: move this assertion into stack.swap
         assert depth is not StackModel.NOT_IN_STACK, f"{op} not in stack: {stack.stack}"
         if depth == final_stack_depth:
             continue
@@ -222,6 +224,7 @@ def _emit_input_operands(
         for op in ops:
             if isinstance(op, IRVariable) and op not in inst.dup_requirements:
                 depth = stack.get_depth(op)
+                # REVIEW: move this assertion into StackModel implementation
                 assert depth is not StackModel.NOT_IN_STACK
                 stack.swap(assembly, depth)
                 break
@@ -248,6 +251,7 @@ def _emit_input_operands(
 
         if op in emited_ops:
             depth = stack.get_depth(op)
+            # REVIEW: move this assertion into StackModel implementation
             assert depth is not StackModel.NOT_IN_STACK
             stack.dup(assembly, depth)
 
@@ -308,6 +312,7 @@ def _generate_evm_for_instruction(
         ret = inst.get_outputs()[0]
         inputs = inst.get_inputs()
         depth = stack.get_shallowest_depth(inputs)
+        # REVIEW: move this into StackModel implementation
         assert depth is not StackModel.NOT_IN_STACK, "Operand not in stack"
         to_be_replaced = stack.peek(depth)
         if to_be_replaced in inst.dup_requirements:
