3bb9a44c669f1695013a88e9fd0fb9e0a010bc7a
francois <fgg67@hotmail.fr>
2023-11-07 21:53:58 +0100

    remove constant CompilerPanic

--- tests/abi_types/test_invalid_abi_types.py
+++ tests/abi_types/test_invalid_abi_types.py
@@ -11,20 +11,16 @@ from vyper.abi_types import (
 from vyper.exceptions import CompilerPanic
 
 cases_invalid_types = [
-    (ABI_GIntM, ((0, False), (7, False), (300, True), (300, False)), CompilerPanic),
-    (
-        ABI_FixedMxN,
-        ((0, 0, False), (8, 0, False), (256, 81, True), (300, 80, False)),
-        CompilerPanic,
-    ),
-    (ABI_BytesM, ((0,), (33,), (-10,)), CompilerPanic),
-    (ABI_Bytes, ((-1,), (-69,)), CompilerPanic),
-    (ABI_DynamicArray, ((ABI_GIntM(256, False), -1), (ABI_String(256), -10)), CompilerPanic),
+    (ABI_GIntM, ((0, False), (7, False), (300, True), (300, False))),
+    (ABI_FixedMxN, ((0, 0, False), (8, 0, False), (256, 81, True), (300, 80, False))),
+    (ABI_BytesM, ((0,), (33,), (-10,))),
+    (ABI_Bytes, ((-1,), (-69,))),
+    (ABI_DynamicArray, ((ABI_GIntM(256, False), -1), (ABI_String(256), -10))),
 ]
 
 
-@pytest.mark.parametrize("typ,params_variants,exc", cases_invalid_types)
-def test_invalid_abi_types(assert_compile_failed, typ, params_variants, exc):
+@pytest.mark.parametrize("typ,params_variants", cases_invalid_types)
+def test_invalid_abi_types(assert_compile_failed, typ, params_variants):
     # double parametrization cannot work because the 2nd dimension is variable
     for params in params_variants:
-        assert_compile_failed(lambda: typ(*params), exc)
+        assert_compile_failed(lambda: typ(*params), CompilerPanic)
