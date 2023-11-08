8cc542f05fe5dc7c8b4907eafa4aa070188fd361
Charles Cooper <cooper.charles.m@gmail.com>
2023-11-08 08:12:42 -0500

    fix lint

--- tests/compiler/venom/test_stack_at_external_return.py
+++ tests/compiler/venom/test_stack_at_external_return.py
@@ -1,9 +1,3 @@
-from vyper.compiler.settings import OptimizationLevel
-from vyper.venom import generate_assembly_experimental
-from vyper.venom.basicblock import IRLiteral
-from vyper.venom.function import IRFunction
-
-
 def test_stack_at_external_return():
     """
     TODO: USE BOA DO GENERATE THIS TEST
