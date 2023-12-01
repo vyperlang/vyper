2109bb2e13f3ad8e016826625c04fecb972b5eba
Harry Kalogirou <harkal@nlogn.eu>
2023-12-01 13:11:00 +0200

    refactor starting point

--- vyper/venom/passes/normalization.py
+++ vyper/venom/passes/normalization.py
@@ -1,6 +1,6 @@
 from vyper.exceptions import CompilerPanic
 from vyper.venom.analysis import calculate_cfg
-from vyper.venom.basicblock import IRBasicBlock, IRInstruction, IRLabel
+from vyper.venom.basicblock import IRBasicBlock, IRInstruction, IRLabel, IRVariable
 from vyper.venom.function import IRFunction
 from vyper.venom.passes.base_pass import IRPass
 
@@ -20,40 +20,48 @@ class NormalizationPass(IRPass):
         # Iterate over the predecessors of the basic block
         for in_bb in bb.cfg_in:
             # We are only splitting on conditional jumps
-            if len(in_bb.cfg_out) < 2:
+            if len(in_bb.cfg_out) <= 1:
                 continue
 
-            # sanity checks. only jnz can product len(cfg_out) > 1
             jump_inst = in_bb.instructions[-1]
-            assert jump_inst.opcode == "jnz"
-            # jnz produces cfg_out with length 2
-            assert len(in_bb.cfg_out) == 2
             assert bb in in_bb.cfg_out
 
             # find which edge of the jnz targets this block
             # jnz condition label1 label2
-            jump_inst.operands[1] == jump_inst.operands[2]
-            for i, op in enumerate(jump_inst.operands):
-                if op == bb.label:
-                    edge = i
-                    break
+            if jump_inst.opcode == "jnz":
+                self._split_for_static_branch(bb, in_bb)
+            elif jump_inst.opcode == "jmp" and isinstance(jump_inst.operands[0], IRVariable):
+                self._split_for_dynamic_branch(bb, in_bb)
             else:
-                # none of the edges points to this bb
-                raise CompilerPanic("bad CFG")
-            assert edge in (1, 2)  # the arguments which can be labels
-
-            # Create an intermediary basic block and append it
-            source = in_bb.label.value
-            target = bb.label.value
-            split_bb = IRBasicBlock(IRLabel(f"{target}_split_{source}"), ctx)
-            split_bb.append_instruction(IRInstruction("jmp", [bb.label]))
-
-            ctx.append_basic_block(split_bb)
-
-            # Redirect the original conditional jump to the intermediary basic block
-            jump_inst.operands[edge] = split_bb.label
+                raise CompilerPanic("Unexpected termination instruction during normalization")
 
             self.changes += 1
+
+    def _split_for_static_branch(self, bb: IRBasicBlock, in_bb: IRBasicBlock) -> None:
+        jump_inst = in_bb.instructions[-1]
+        for i, op in enumerate(jump_inst.operands):
+            if op == bb.label:
+                edge = i
+                break
+        else:
+            # none of the edges points to this bb
+            raise CompilerPanic("bad CFG")
+
+        assert edge in (1, 2)  # the arguments which can be labels
+
+        # Create an intermediary basic block and append it
+        source = in_bb.label.value
+        target = bb.label.value
+        split_bb = IRBasicBlock(IRLabel(f"{target}_split_{source}"), self.ctx)
+        split_bb.append_instruction(IRInstruction("jmp", [bb.label]))
+
+        self.ctx.append_basic_block(split_bb)
+
+        # Redirect the original conditional jump to the intermediary basic block
+        jump_inst.operands[edge] = split_bb.label
+
+    def _split_for_dynamic_branch(self, bb: IRBasicBlock, in_bb: IRBasicBlock) -> None:
+        pass
 
     def _run_pass(self, ctx: IRFunction) -> int:
         self.ctx = ctx
@@ -63,7 +71,7 @@ class NormalizationPass(IRPass):
         calculate_cfg(ctx)
 
         for bb in ctx.basic_blocks:
-            if len(bb.cfg_in) > 1:
+            if len(bb.cfg_in) > 1:  # and len(bb.cfg_out) > 1:
                 self._split_basic_block(bb)
 
         # Recalculate control flow graph
