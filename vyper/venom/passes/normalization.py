fc06f0242de89605e9e8d64e46b5e6ed0346ba0d
Harry Kalogirou <harkal@nlogn.eu>
2023-12-01 15:13:23 +0200

    Remove unused code

--- vyper/venom/passes/normalization.py
+++ vyper/venom/passes/normalization.py
@@ -14,22 +14,6 @@ class NormalizationPass(IRPass):
 
     changes = 0
 
-    def _normalize_basic_block(self, bb: IRBasicBlock) -> None:
-        for out_bb in bb.cfg_out:
-            if len(out_bb.cfg_in) < 2:
-                continue
-
-            split_label = IRLabel(f"{bb.label.value}_split_{out_bb.label.value}")
-            new_out_bb = self.ctx.get_basic_block(split_label.value)
-            if new_out_bb is None:
-                new_out_bb = IRBasicBlock(
-                    IRLabel(f"{bb.label.value}_split_{out_bb.label.value}"), self.ctx
-                )
-                new_out_bb.instructions = out_bb.instructions
-                self.ctx.append_basic_block(new_out_bb)
-
-            out_bb.instructions = [IRInstruction("jmp", [new_out_bb.label])]
-
     def _split_basic_block(self, bb: IRBasicBlock) -> None:
         ctx = self.ctx
 
@@ -42,8 +26,7 @@ class NormalizationPass(IRPass):
             jump_inst = in_bb.instructions[-1]
             assert bb in in_bb.cfg_out
 
-            # find which edge of the jnz targets this block
-            # jnz condition label1 label2
+            # Handle static and dynamic branching
             if jump_inst.opcode == "jnz":
                 self._split_for_static_branch(bb, in_bb)
             elif jump_inst.opcode == "jmp" and isinstance(jump_inst.operands[0], IRVariable):
@@ -85,12 +68,14 @@ class NormalizationPass(IRPass):
         split_bb.append_instruction(IRInstruction("jmp", [bb.label]))
         self.ctx.append_basic_block(split_bb)
 
+        # Rewire the CFG
         split_bb.add_cfg_in(in_bb)
         split_bb.add_cfg_out(bb)
         in_bb.add_cfg_out(split_bb)
         bb.remove_cfg_in(in_bb)
         bb.add_cfg_in(split_bb)
 
+        # Update any affected labels in the data segment
         for inst in self.ctx.data_segment:
             if inst.opcode == "db" and inst.operands[0] == bb.label:
                 inst.operands[0] = split_bb.label
@@ -99,22 +84,10 @@ class NormalizationPass(IRPass):
         self.ctx = ctx
         self.changes = 0
 
-        # Ensure that the CFG is up to date
-        # calculate_cfg(ctx)
-        print(ctx)
-        # for bb in ctx.basic_blocks:
-        #     if len(bb.cfg_out) > 1:
-        #         self._normalize_basic_block(bb)
-
         for bb in ctx.basic_blocks:
             if len(bb.cfg_in) > 1:
                 self._split_basic_block(bb)
 
-        # Recalculate control flow graph
-        # (perf: could do this only when self.changes > 0, but be paranoid)
-        # calculate_cfg(ctx)
-        print("--------------------------")
-        print(ctx)
         # Sanity check
         assert ctx.normalized, "Normalization pass failed"
 
