1208ae13ce8bd370cce3d4fc5700d696e13f8973
Charles Cooper <cooper.charles.m@gmail.com>
2023-12-07 09:51:02 -0500

    fix lint

--- vyper/semantics/analysis/utils.py
+++ vyper/semantics/analysis/utils.py
@@ -259,9 +259,7 @@ class _ExprAnalyser:
                 raise InvalidOperation(
                     "Right operand must be Array for membership comparison", node.right
                 )
-            types_list = [
-                i for i in left if _any_compare_type(i, [i.value_type for i in right])
-            ]
+            types_list = [i for i in left if _any_compare_type(i, [i.value_type for i in right])]
             if not types_list:
                 raise TypeMismatch(
                     "Cannot perform membership comparison between dislike types", node
