86d8fbe33ee255ba5cfb8b13fbcc2b44cbeecfcd
Harry Kalogirou <harkal@nlogn.eu>
2023-12-08 15:56:04 +0200

    Refactor basic block instruction appending  names adopted a shorted name for frequently used methods

--- vyper/venom/passes/normalization.py
+++ vyper/venom/passes/normalization.py
@@ -61,7 +61,7 @@ class NormalizationPass(IRPass):
         source = in_bb.label.value
         target = bb.label.value
         split_bb = IRBasicBlock(IRLabel(f"{target}_split_{source}"), self.ctx)
-        split_bb.add_instruction_no_return("jmp", bb.label)
+        split_bb.append_inst_no_ret("jmp", bb.label)
         self.ctx.append_basic_block(split_bb)
 
         # Rewire the CFG
