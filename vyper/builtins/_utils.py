c58bfa0fed665dff69c4be782d6464aece704978
Charles Cooper <cooper.charles.m@gmail.com>
2023-12-10 01:13:06 +0000

    fix sqrt

--- vyper/builtins/_utils.py
+++ vyper/builtins/_utils.py
@@ -22,14 +22,14 @@ def generate_inline_function(code, variables, variables_2, memory_allocator):
         # Initialise a placeholder `FunctionDef` AST node and corresponding
         # `ContractFunctionT` type to rely on the annotation visitors in semantics
         # module.
-        ast_code.body[0]._metadata["type"] = ContractFunctionT(
+        ast_code.body[0]._metadata["func_type"] = ContractFunctionT(
             "sqrt_builtin", [], [], None, FunctionVisibility.INTERNAL, StateMutability.NONPAYABLE
         )
         # The FunctionNodeVisitor's constructor performs semantic checks
         # annotate the AST as side effects.
         FunctionNodeVisitor(ast_code, ast_code.body[0], namespace)
 
-    new_context = Context(vars_=variables, module_ctx=ModuleT(), memory_allocator=memory_allocator)
+    new_context = Context(vars_=variables, module_ctx=ModuleT(ast_code), memory_allocator=memory_allocator)
     generated_ir = parse_body(ast_code.body[0].body, new_context)
     # strip source position info from the generated_ir since
     # it doesn't make any sense (e.g. the line numbers will start from 0
