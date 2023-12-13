74c630ece2723cf24418acf9c635418ab2c2a461
Charles Cooper <cooper.charles.m@gmail.com>
2023-12-13 18:54:23 -0500

    remove unneeded F401

--- vyper/__init__.py
+++ vyper/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path as _Path
 
-from vyper.compiler import compile_code, compile_from_file_input  # noqa: F401
+from vyper.compiler import compile_code, compile_from_file_input
 
 try:
     from importlib.metadata import PackageNotFoundError  # type: ignore
