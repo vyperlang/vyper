7489e34581bf013d2ba04cf28d847a443a255a34
Daniel Schiavini <daniel.schiavini@gmail.com>
2023-12-24 17:10:45 +0100

    feat: allow `range(x, y, bound=N)` (#3679)
    - allow range where both start and end arguments are variables, so long
      as a bound is supplied
    
    - ban range expressions of the form `range(x, x + N)` since the new form
      is cleaner and supersedes it.
    
    - also do a bit of refactoring of the codegen for range
    
    ---------
    
    Co-authored-by: Charles Cooper <cooper.charles.m@gmail.com>

--- tests/functional/codegen/features/iteration/test_for_range.py
+++ tests/functional/codegen/features/iteration/test_for_range.py
@@ -32,6 +32,102 @@ def repeat(n: uint256) -> uint256:
         c.repeat(7)
 
 
+def test_range_bound_constant_end(get_contract, tx_failed):
+    code = """
+@external
+def repeat(n: uint256) -> uint256:
+    x: uint256 = 0
+    for i in range(n, 7, bound=6):
+        x += i + 1
+    return x
+    """
+    c = get_contract(code)
+    for n in range(1, 5):
+        assert c.repeat(n) == sum(i + 1 for i in range(n, 7))
+
+    # check assertion for `start <= end`
+    with tx_failed():
+        c.repeat(8)
+    # check assertion for `start + bound <= end`
+    with tx_failed():
+        c.repeat(0)
+
+
+def test_range_bound_two_args(get_contract, tx_failed):
+    code = """
+@external
+def repeat(n: uint256) -> uint256:
+    x: uint256 = 0
+    for i in range(1, n, bound=6):
+        x += i + 1
+    return x
+    """
+    c = get_contract(code)
+    for n in range(1, 8):
+        assert c.repeat(n) == sum(i + 1 for i in range(1, n))
+
+    # check assertion for `start <= end`
+    with tx_failed():
+        c.repeat(0)
+
+    # check codegen inserts assertion for `start + bound <= end`
+    with tx_failed():
+        c.repeat(8)
+
+
+def test_range_bound_two_runtime_args(get_contract, tx_failed):
+    code = """
+@external
+def repeat(start: uint256, end: uint256) -> uint256:
+    x: uint256 = 0
+    for i in range(start, end, bound=6):
+        x += i
+    return x
+    """
+    c = get_contract(code)
+    for n in range(0, 7):
+        assert c.repeat(0, n) == sum(range(0, n))
+        assert c.repeat(n, n * 2) == sum(range(n, n * 2))
+
+    # check assertion for `start <= end`
+    with tx_failed():
+        c.repeat(1, 0)
+    with tx_failed():
+        c.repeat(7, 0)
+    with tx_failed():
+        c.repeat(8, 7)
+
+    # check codegen inserts assertion for `start + bound <= end`
+    with tx_failed():
+        c.repeat(0, 7)
+    with tx_failed():
+        c.repeat(14, 21)
+
+
+def test_range_overflow(get_contract, tx_failed):
+    code = """
+@external
+def get_last(start: uint256, end: uint256) -> uint256:
+    x: uint256 = 0
+    for i in range(start, end, bound=6):
+        x = i
+    return x
+    """
+    c = get_contract(code)
+    UINT_MAX = 2**256 - 1
+    assert c.get_last(UINT_MAX, UINT_MAX) == 0  # initial value of x
+
+    for n in range(1, 6):
+        assert c.get_last(UINT_MAX - n, UINT_MAX) == UINT_MAX - 1
+
+    # check for `start + bound <= end`, overflow cases
+    for n in range(1, 7):
+        with tx_failed():
+            c.get_last(UINT_MAX - n, 0)
+        with tx_failed():
+            c.get_last(UINT_MAX, UINT_MAX - n)
+
+
 def test_digit_reverser(get_contract_with_gas_estimation):
     digit_reverser = """
 @external
@@ -89,7 +185,7 @@ def test_offset_repeater_2(get_contract_with_gas_estimation, typ):
 @external
 def sum(frm: {typ}, to: {typ}) -> {typ}:
     out: {typ} = 0
-    for i in range(frm, frm + 101):
+    for i in range(frm, frm + 101, bound=101):
         if i == to:
             break
         out = out + i
@@ -146,26 +242,28 @@ def foo(a: {typ}) -> {typ}:
     assert c.foo(100) == 31337
 
 
-# test that we can get to the upper range of an integer
 @pytest.mark.parametrize("typ", ["uint8", "int128", "uint256"])
 def test_for_range_edge(get_contract, typ):
+    """
+    Check that we can get to the upper range of an integer.
+    Note that to avoid overflow in the bounds check for range(),
+    we need to calculate i+1 inside the loop.
+    """
     code = f"""
 @external
 def test():
     found: bool = False
     x: {typ} = max_value({typ})
-    for i in range(x, x + 1):
-        if i == max_value({typ}):
+    for i in range(x - 1, x, bound=1):
+        if i + 1 == max_value({typ}):
             found = True
-
     assert found
 
     found = False
     x = max_value({typ}) - 1
-    for i in range(x, x + 2):
-        if i == max_value({typ}):
+    for i in range(x - 1, x + 1, bound=2):
+        if i + 1 == max_value({typ}):
             found = True
-
     assert found
     """
     c = get_contract(code)
@@ -178,7 +276,7 @@ def test_for_range_oob_check(get_contract, tx_failed, typ):
 @external
 def test():
     x: {typ} = max_value({typ})
-    for i in range(x, x+2):
+    for i in range(x, x + 2, bound=2):
         pass
     """
     c = get_contract(code)
--- tests/functional/syntax/test_for_range.py
+++ tests/functional/syntax/test_for_range.py
@@ -1,7 +1,9 @@
+import re
+
 import pytest
 
 from vyper import compiler
-from vyper.exceptions import StructureException
+from vyper.exceptions import ArgumentException, StateAccessViolation, StructureException
 
 fail_list = [
     (
@@ -12,33 +14,191 @@ def foo():
         pass
     """,
         StructureException,
+        "Invalid syntax for loop iterator",
+        "a[1]",
     ),
     (
         """
 @external
-def bar():
-    for i in range(1,2,bound=2):
+def foo():
+    x: uint256 = 100
+    for _ in range(10, bound=x):
+        pass
+    """,
+        StateAccessViolation,
+        "Bound must be a literal",
+        "x",
+    ),
+    (
+        """
+@external
+def foo():
+    for _ in range(10, 20, bound=5):
         pass
     """,
         StructureException,
+        "Please remove the `bound=` kwarg when using range with constants",
+        "5",
+    ),
+    (
+        """
+@external
+def foo():
+    for _ in range(10, 20, bound=0):
+        pass
+    """,
+        StructureException,
+        "Bound must be at least 1",
+        "0",
     ),
     (
         """
 @external
 def bar():
     x:uint256 = 1
-    for i in range(x,x+1,bound=2):
+    for i in range(x,x+1,bound=2,extra=3):
+        pass
+    """,
+        ArgumentException,
+        "Invalid keyword argument 'extra'",
+        "extra=3",
+    ),
+    (
+        """
+@external
+def bar():
+    for i in range(0):
         pass
     """,
         StructureException,
+        "End must be greater than start",
+        "0",
+    ),
+    (
+        """
+@external
+def bar():
+    x:uint256 = 1
+    for i in range(x):
+        pass
+    """,
+        StateAccessViolation,
+        "Value must be a literal integer, unless a bound is specified",
+        "x",
+    ),
+    (
+        """
+@external
+def bar():
+    x:uint256 = 1
+    for i in range(0, x):
+        pass
+    """,
+        StateAccessViolation,
+        "Value must be a literal integer, unless a bound is specified",
+        "x",
+    ),
+    (
+        """
+@external
+def repeat(n: uint256) -> uint256:
+    for i in range(0, n * 10):
+        pass
+    return n
+    """,
+        StateAccessViolation,
+        "Value must be a literal integer, unless a bound is specified",
+        "n * 10",
+    ),
+    (
+        """
+@external
+def bar():
+    x:uint256 = 1
+    for i in range(0, x + 1):
+        pass
+    """,
+        StateAccessViolation,
+        "Value must be a literal integer, unless a bound is specified",
+        "x + 1",
+    ),
+    (
+        """
+@external
+def bar():
+    for i in range(2, 1):
+        pass
+    """,
+        StructureException,
+        "End must be greater than start",
+        "1",
+    ),
+    (
+        """
+@external
+def bar():
+    x:uint256 = 1
+    for i in range(x, x):
+        pass
+    """,
+        StateAccessViolation,
+        "Value must be a literal integer, unless a bound is specified",
+        "x",
+    ),
+    (
+        """
+@external
+def foo():
+    x: int128 = 5
+    for i in range(x, x + 10):
+        pass
+    """,
+        StateAccessViolation,
+        "Value must be a literal integer, unless a bound is specified",
+        "x",
+    ),
+    (
+        """
+@external
+def repeat(n: uint256) -> uint256:
+    for i in range(n, 6):
+        pass
+    return x
+    """,
+        StateAccessViolation,
+        "Value must be a literal integer, unless a bound is specified",
+        "n",
+    ),
+    (
+        """
+@external
+def foo(x: int128):
+    y: int128 = 7
+    for i in range(x, x + y):
+        pass
+    """,
+        StateAccessViolation,
+        "Value must be a literal integer, unless a bound is specified",
+        "x",
     ),
 ]
 
+for_code_regex = re.compile(r"for .+ in (.*):")
+fail_test_names = [
+    (
+        f"{i:02d}: {for_code_regex.search(code).group(1)}"  # type: ignore[union-attr]
+        f" raises {type(err).__name__}"
+    )
+    for i, (code, err, msg, src) in enumerate(fail_list)
+]
 
-@pytest.mark.parametrize("bad_code", fail_list)
-def test_range_fail(bad_code):
-    with pytest.raises(bad_code[1]):
-        compiler.compile_code(bad_code[0])
+
+@pytest.mark.parametrize("bad_code,error_type,message,source_code", fail_list, ids=fail_test_names)
+def test_range_fail(bad_code, error_type, message, source_code):
+    with pytest.raises(error_type) as exc_info:
+        compiler.compile_code(bad_code)
+    assert message == exc_info.value.message
+    assert source_code == exc_info.value.args[1].node_source_code
 
 
 valid_list = [
@@ -58,7 +218,21 @@ def foo():
 @external
 def foo():
     x: int128 = 5
-    for i in range(x, x + 10):
+    for i in range(1, x, bound=4):
+        pass
+    """,
+    """
+@external
+def foo():
+    x: int128 = 5
+    for i in range(x, bound=4):
+        pass
+    """,
+    """
+@external
+def foo():
+    x: int128 = 5
+    for i in range(0, x, bound=4):
         pass
     """,
     """
@@ -72,7 +246,12 @@ def kick_foos():
     """,
 ]
 
+valid_test_names = [
+    f"{i} {for_code_regex.search(code).group(1)}"  # type: ignore[union-attr]
+    for i, code in enumerate(valid_list)
+]
 
-@pytest.mark.parametrize("good_code", valid_list)
+
+@pytest.mark.parametrize("good_code", valid_list, ids=valid_test_names)
 def test_range_success(good_code):
     assert compiler.compile_code(good_code) is not None
