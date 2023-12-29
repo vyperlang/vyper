6d2addc54a0555c8653909e2b512ca5e9cf79cdd
tserg <8017125+tserg@users.noreply.github.com>
2023-12-29 16:07:50 +0800

    minor clean up

--- vyper/semantics/analysis/pre_typecheck.py
+++ vyper/semantics/analysis/pre_typecheck.py
@@ -2,53 +2,7 @@ from vyper import ast as vy_ast
 from vyper.exceptions import UnfoldableNode
 
 
-def get_constants(node: vy_ast.Module) -> dict:
-    constants: dict[str, vy_ast.VyperNode] = {}
-    module_nodes = node.body.copy()
-    const_var_decls = [
-        n for n in module_nodes if isinstance(n, vy_ast.VariableDecl) and n.is_constant
-    ]
-
-    while const_var_decls:
-        n_processed = 0
-
-        for c in const_var_decls:
-            name = c.get("target.id")
-            # Handle syntax errors downstream
-            if c.value is None:
-                continue
-
-            for n in c.value.get_descendants(include_self=True, reverse=True):
-                prefold(n, constants)
-
-            try:
-                val = c.value.get_folded_value()
-
-                # note that if a constant is redefined, its value will be overwritten,
-                # but it is okay because the syntax error is handled downstream
-                constants[name] = val
-                n_processed += 1
-                const_var_decls.remove(c)
-            except UnfoldableNode:
-                pass
-
-        if not n_processed:
-            break
-
-    return constants
-
-
-def pre_typecheck(node: vy_ast.Module) -> None:
-    constants = get_constants(node)
-
-    for n in node.get_descendants(reverse=True):
-        if isinstance(n, vy_ast.VariableDecl):
-            continue
-
-        prefold(n, constants)
-
-
-def prefold(node: vy_ast.VyperNode, constants: dict[str, vy_ast.VyperNode]):
+def _prefold(node: vy_ast.VyperNode, constants: dict[str, vy_ast.VyperNode]):
     if isinstance(node, vy_ast.Name):
         var_name = node.id
         if var_name in constants:
@@ -75,3 +29,49 @@ def prefold(node: vy_ast.VyperNode, constants: dict[str, vy_ast.VyperNode]):
         node.get_folded_value()
     except UnfoldableNode:
         pass
+
+
+def _get_constants(node: vy_ast.Module) -> dict:
+    constants: dict[str, vy_ast.VyperNode] = {}
+    module_nodes = node.body.copy()
+    const_var_decls = [
+        n for n in module_nodes if isinstance(n, vy_ast.VariableDecl) and n.is_constant
+    ]
+
+    while const_var_decls:
+        n_processed = 0
+
+        for c in const_var_decls:
+            name = c.get("target.id")
+            # Handle syntax errors downstream
+            if c.value is None:
+                continue
+
+            for n in c.value.get_descendants(include_self=True, reverse=True):
+                _prefold(n, constants)
+
+            try:
+                val = c.value.get_folded_value()
+            except UnfoldableNode:
+                continue
+            
+            # note that if a constant is redefined, its value will be overwritten,
+            # but it is okay because the syntax error is handled downstream
+            constants[name] = val
+            n_processed += 1
+            const_var_decls.remove(c)
+
+        if not n_processed:
+            break
+
+    return constants
+
+
+def pre_typecheck(node: vy_ast.Module) -> None:
+    constants = _get_constants(node)
+
+    for n in node.get_descendants(reverse=True):
+        if isinstance(n, vy_ast.VariableDecl):
+            continue
+
+        _prefold(n, constants)
