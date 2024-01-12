vyperlang/vyper

commit ddfce5273b39a199b194dd74f0f7f741efc03663

feat: require type annotations for loop variables (#3596)
this commit changes the vyper language to require type annotations for
loop variables. that is, before, the following was allowed:
```vyper
for i in [1, 2, 3]:
    pass
```

now, `i` is required to have a type annotation:
```vyper
for i: uint256 in [1, 2, 3]:
    pass
```

this makes the annotation of loop variables consistent with the rest of
vyper (it was previously a special case, that loop variables did not
need to be annotated).

the approach taken in this commit is to add a pre-parsing step which
lifts out the type annotation into a separate data structure, and then
splices it back in during the post-processing steps in
`vyper/ast/parse.py`.

this commit also simplifies a lot of analysis regarding for loops.
notably, the possible types for the loop variable no longer needs to be
iterated over, we can just propagate the type provided by the user. for
this reason we also no longer need to use the typechecker speculation
machinery for inferring the type of the loop variable. however, the
NodeMetadata code is not removed because it might come in handy at a
later date.

---------

Co-authored-by: Charles Cooper <cooper.charles.m@gmail.com>

vyper/ast/parse.py
@@ -54,7 +54,7 @@ def parse_to_ast_with_settings(
     """
     if "\x00" in source_code:
         raise ParserException("No null bytes (\\x00) allowed in the source code.")
-    settings, class_types, reformatted_code = pre_parse(source_code)
+    settings, class_types, for_loop_annotations, reformatted_code = pre_parse(source_code)
     try:
         py_ast = python_ast.parse(reformatted_code)
     except SyntaxError as e:
@@ -73,10 +73,14 @@ def parse_to_ast_with_settings(
         py_ast,
         source_code,
         class_types,
+        for_loop_annotations,
         source_id,
         module_path=module_path,
         resolved_path=resolved_path,
     )
+
+    # postcondition: consumed all the for loop annotations
+    assert len(for_loop_annotations) == 0
 
     # Convert to Vyper AST.
     module = vy_ast.get_node(py_ast)
@@ -113,11 +117,13 @@ def dict_to_ast(ast_struct: Union[Dict, List]) -> Union[vy_ast.VyperNode, List]:
 class AnnotatingVisitor(python_ast.NodeTransformer):
     _source_code: str
     _modification_offsets: ModificationOffsets
+    _loop_var_annotations: dict[int, dict[str, Any]]
 
     def __init__(
         self,
         source_code: str,
-        modification_offsets: Optional[ModificationOffsets],
+        modification_offsets: ModificationOffsets,
+        for_loop_annotations: dict,
         tokens: asttokens.ASTTokens,
         source_id: int,
         module_path: Optional[str] = None,
@@ -127,11 +133,11 @@ class AnnotatingVisitor(python_ast.NodeTransformer):
         self._source_id = source_id
         self._module_path = module_path
         self._resolved_path = resolved_path
-        self._source_code: str = source_code
+        self._source_code = source_code
+        self._modification_offsets = modification_offsets
+        self._for_loop_annotations = for_loop_annotations
+
         self.counter: int = 0
-        self._modification_offsets = {}
-        if modification_offsets is not None:
-            self._modification_offsets = modification_offsets
 
     def generic_visit(self, node):
         """
@@ -211,6 +217,47 @@ class AnnotatingVisitor(python_ast.NodeTransformer):
         self.generic_visit(node)
 
         node.ast_type = self._modification_offsets[(node.lineno, node.col_offset)]
+        return node
+
+    def visit_For(self, node):
+        """
+        Visit a For node, splicing in the loop variable annotation provided by
+        the pre-parser
+        """
+        raw_annotation = self._for_loop_annotations.pop((node.lineno, node.col_offset))
+
+        if not raw_annotation:
+            # a common case for people migrating to 0.4.0, provide a more
+            # specific error message than "invalid type annotation"
+            raise SyntaxException(
+                "missing type annotation\n\n"
+                "(hint: did you mean something like "
+                f"`for {node.target.id}: uint256 in ...`?)\n",
+                self._source_code,
+                node.lineno,
+                node.col_offset,
+            )
+
+        try:
+            annotation = python_ast.parse(raw_annotation, mode="eval")
+            # annotate with token and source code information. `first_token`
+            # and `last_token` attributes are accessed in `generic_visit`.
+            tokens = asttokens.ASTTokens(raw_annotation)
+            tokens.mark_tokens(annotation)
+        except SyntaxError as e:
+            raise SyntaxException(
+                "invalid type annotation", self._source_code, node.lineno, node.col_offset
+            ) from e
+
+        assert isinstance(annotation, python_ast.Expression)
+        annotation = annotation.body
+
+        old_target = node.target
+        new_target = python_ast.AnnAssign(target=old_target, annotation=annotation, simple=1)
+        node.target = new_target
+
+        self.generic_visit(node)
+
         return node
 
     def visit_Expr(self, node):
@@ -355,7 +402,8 @@ class AnnotatingVisitor(python_ast.NodeTransformer):
 def annotate_python_ast(
     parsed_ast: python_ast.AST,
     source_code: str,
-    modification_offsets: Optional[ModificationOffsets] = None,
+    modification_offsets: ModificationOffsets,
+    for_loop_annotations: dict,
     source_id: int = 0,
     module_path: Optional[str] = None,
     resolved_path: Optional[str] = None,
@@ -369,6 +417,9 @@ def annotate_python_ast(
         The AST to be annotated and optimized.
     source_code : str
         The originating source code of the AST.
+    loop_var_annotations: dict, optional
+        A mapping of line numbers of `For` nodes to the type annotation of the iterator
+        extracted during pre-parsing.
     modification_offsets : dict, optional
         A mapping of class names to their original class types.
 
@@ -381,6 +432,7 @@ def annotate_python_ast(
     visitor = AnnotatingVisitor(
         source_code,
         modification_offsets,
+        for_loop_annotations,
         tokens,
         source_id,
         module_path=module_path,

vyper/ast/pre_parser.py
@@ -1,3 +1,4 @@
+import enum
 import io
 import re
 from tokenize import COMMENT, NAME, OP, TokenError, TokenInfo, tokenize, untokenize
@@ -43,6 +44,64 @@ def validate_version_pragma(version_str: str, start: ParserPosition) -> None:
         )
 
 
+class ForParserState(enum.Enum):
+    NOT_RUNNING = enum.auto()
+    START_SOON = enum.auto()
+    RUNNING = enum.auto()
+
+
+# a simple state machine which allows us to handle loop variable annotations
+# (which are rejected by the python parser due to pep-526, so we scoop up the
+# tokens between `:` and `in` and parse them and add them back in later).
+class ForParser:
+    def __init__(self, code):
+        self._code = code
+        self.annotations = {}
+        self._current_annotation = None
+
+        self._state = ForParserState.NOT_RUNNING
+        self._current_for_loop = None
+
+    def consume(self, token):
+        # state machine: we can start slurping tokens soon
+        if token.type == NAME and token.string == "for":
+            # note: self._state should be NOT_RUNNING here, but we don't sanity
+            # check here as that should be an error the parser will handle.
+            self._state = ForParserState.START_SOON
+            self._current_for_loop = token.start
+
+        if self._state == ForParserState.NOT_RUNNING:
+            return False
+
+        # state machine: start slurping tokens
+        if token.type == OP and token.string == ":":
+            self._state = ForParserState.RUNNING
+
+            # sanity check -- this should never really happen, but if it does,
+            # try to raise an exception which pinpoints the source.
+            if self._current_annotation is not None:
+                raise SyntaxException(
+                    "for loop parse error", self._code, token.start[0], token.start[1]
+                )
+
+            self._current_annotation = []
+            return True  # do not add ":" to tokens.
+
+        # state machine: end slurping tokens
+        if token.type == NAME and token.string == "in":
+            self._state = ForParserState.NOT_RUNNING
+            self.annotations[self._current_for_loop] = self._current_annotation or []
+            self._current_annotation = None
+            return False
+
+        if self._state != ForParserState.RUNNING:
+            return False
+
+        # slurp the token
+        self._current_annotation.append(token)
+        return True
+
+
 # compound statements that are replaced with `class`
 # TODO remove enum in favor of flag
 VYPER_CLASS_TYPES = {"flag", "enum", "event", "interface", "struct"}
@@ -51,7 +110,7 @@ VYPER_CLASS_TYPES = {"flag", "enum", "event", "interface", "struct"}
 VYPER_EXPRESSION_TYPES = {"log"}
 
 
-def pre_parse(code: str) -> tuple[Settings, ModificationOffsets, str]:
+def pre_parse(code: str) -> tuple[Settings, ModificationOffsets, dict, str]:
     """
     Re-formats a vyper source string into a python source string and performs
     some validation.  More specifically,
@@ -60,9 +119,11 @@ def pre_parse(code: str) -> tuple[Settings, ModificationOffsets, str]:
     * Validates "@version" pragma against current compiler version
     * Prevents direct use of python "class" keyword
     * Prevents use of python semi-colon statement separator
+    * Extracts type annotation of for loop iterators into a separate dictionary
 
     Also returns a mapping of detected interface and struct names to their
-    respective vyper class types ("interface" or "struct").
+    respective vyper class types ("interface" or "struct"), and a mapping of line numbers
+    of for loops to the type annotation of their iterators.
 
     Parameters
     ----------
@@ -71,21 +132,25 @@ def pre_parse(code: str) -> tuple[Settings, ModificationOffsets, str]:
 
     Returns
     -------
-    dict
-        Mapping of offsets where source was modified.
+    Settings
+        Compilation settings based on the directives in the source code
+    ModificationOffsets
+        A mapping of class names to their original class types.
+    dict[tuple[int, int], str]
+        A mapping of line/column offsets of `For` nodes to the annotation of the for loop target
     str
         Reformatted python source string.
     """
     result = []
     modification_offsets: ModificationOffsets = {}
     settings = Settings()
+    for_parser = ForParser(code)
 
     try:
         code_bytes = code.encode("utf-8")
         token_list = list(tokenize(io.BytesIO(code_bytes).readline))
 
-        for i in range(len(token_list)):
-            token = token_list[i]
+        for token in token_list:
             toks = [token]
 
             typ = token.type
@@ -146,8 +211,18 @@ def pre_parse(code: str) -> tuple[Settings, ModificationOffsets, str]:
 
             if (typ, string) == (OP, ";"):
                 raise SyntaxException("Semi-colon statements not allowed", code, start[0], start[1])
-            result.extend(toks)
+
+            if not for_parser.consume(token):
+                result.extend(toks)
+
     except TokenError as e:
         raise SyntaxException(e.args[0], code, e.args[1][0], e.args[1][1]) from e
 
-    return settings, modification_offsets, untokenize(result).decode("utf-8")
+    for_loop_annotations = {}
+    for k, v in for_parser.annotations.items():
+        v_source = untokenize(v)
+        # untokenize adds backslashes and whitespace, strip them.
+        v_source = v_source.replace("\\", "").strip()
+        for_loop_annotations[k] = v_source
+
+    return settings, modification_offsets, for_loop_annotations, untokenize(result).decode("utf-8")

tests/functional/syntax/test_for_range.py
@@ -15,7 +15,7 @@ fail_list = [
         """
 @external
 def foo():
-    for a[1] in range(10):
+    for a[1]: uint256 in range(10):
         pass
     """,
         StructureException,
@@ -26,7 +26,7 @@ def foo():
         """
 @external
 def bar():
-    for i in range(1,2,bound=0):
+    for i: uint256 in range(1,2,bound=0):
         pass
     """,
         StructureException,
@@ -38,7 +38,7 @@ def bar():
 @external
 def foo():
     x: uint256 = 100
-    for _ in range(10, bound=x):
+    for _: uint256 in range(10, bound=x):
         pass
     """,
         StateAccessViolation,
@@ -49,7 +49,7 @@ def foo():
         """
 @external
 def foo():
-    for _ in range(10, 20, bound=5):
+    for _: uint256 in range(10, 20, bound=5):
         pass
     """,
         StructureException,
@@ -60,7 +60,7 @@ def foo():
         """
 @external
 def foo():
-    for _ in range(10, 20, bound=0):
+    for _: uint256 in range(10, 20, bound=0):
         pass
     """,
         StructureException,
@@ -72,7 +72,7 @@ def foo():
 @external
 def bar():
     x:uint256 = 1
-    for i in range(x,x+1,bound=2,extra=3):
+    for i: uint256 in range(x,x+1,bound=2,extra=3):
         pass
     """,
         ArgumentException,
@@ -83,7 +83,7 @@ def bar():
         """
 @external
 def bar():
-    for i in range(0):
+    for i: uint256 in range(0):
         pass
     """,
         StructureException,
@@ -95,7 +95,7 @@ def bar():
 @external
 def bar():
     x:uint256 = 1
-    for i in range(x):
+    for i: uint256 in range(x):
         pass
     """,
         StateAccessViolation,
@@ -107,7 +107,7 @@ def bar():
 @external
 def bar():
     x:uint256 = 1
-    for i in range(0, x):
+    for i: uint256 in range(0, x):
         pass
     """,
         StateAccessViolation,
@@ -118,7 +118,7 @@ def bar():
         """
 @external
 def repeat(n: uint256) -> uint256:
-    for i in range(0, n * 10):
+    for i: uint256 in range(0, n * 10):
         pass
     return n
     """,
@@ -131,7 +131,7 @@ def repeat(n: uint256) -> uint256:
 @external
 def bar():
     x:uint256 = 1
-    for i in range(0, x + 1):
+    for i: uint256 in range(0, x + 1):
         pass
     """,
         StateAccessViolation,
@@ -142,7 +142,7 @@ def bar():
         """
 @external
 def bar():
-    for i in range(2, 1):
+    for i: uint256 in range(2, 1):
         pass
     """,
         StructureException,
@@ -154,7 +154,7 @@ def bar():
 @external
 def bar():
     x:uint256 = 1
-    for i in range(x, x):
+    for i: uint256 in range(x, x):
         pass
     """,
         StateAccessViolation,
@@ -166,7 +166,7 @@ def bar():
 @external
 def foo():
     x: int128 = 5
-    for i in range(x, x + 10):
+    for i: int128 in range(x, x + 10):
         pass
     """,
         StateAccessViolation,
@@ -177,7 +177,7 @@ def foo():
         """
 @external
 def repeat(n: uint256) -> uint256:
-    for i in range(n, 6):
+    for i: uint256 in range(n, 6):
         pass
     return x
     """,
@@ -190,7 +190,7 @@ def repeat(n: uint256) -> uint256:
 @external
 def foo(x: int128):
     y: int128 = 7
-    for i in range(x, x + y):
+    for i: int128 in range(x, x + y):
         pass
     """,
         StateAccessViolation,
@@ -201,7 +201,7 @@ def foo(x: int128):
         """
 @external
 def bar(x: uint256):
-    for i in range(3, x):
+    for i: uint256 in range(3, x):
         pass
     """,
         StateAccessViolation,
@@ -215,12 +215,12 @@ BAR: constant(uint256) = 7
 
 @external
 def foo():
-    for i in range(FOO, BAR):
+    for i: uint256 in range(FOO, BAR):
         pass
     """,
         TypeMismatch,
-        "Iterator values are of different types",
-        "range(FOO, BAR)",
+        "Given reference has type int128, expected uint256",
+        "FOO",
     ),
     (
         """
@@ -228,12 +228,12 @@ FOO: constant(int128) = -1
 
 @external
 def foo():
-    for i in range(10, bound=FOO):
+    for i: int128 in range(10, bound=FOO):
         pass
         """,
         StructureException,
         "Bound must be at least 1",
-        "-1",
+        "FOO",
     ),
 ]
 
@@ -252,41 +252,41 @@ def test_range_fail(bad_code, error_type, message, source_code):
     with pytest.raises(error_type) as exc_info:
         compiler.compile_code(bad_code)
     assert message == exc_info.value.message
-    assert source_code == exc_info.value.args[1].node_source_code
+    assert source_code == exc_info.value.args[1].get_original_node().node_source_code
 
 
 valid_list = [
     """
 @external
 def foo():
-    for i in range(10):
+    for i: uint256 in range(10):
         pass
     """,
     """
 @external
 def foo():
-    for i in range(10, 20):
-        pass
-    """,
-    """
-@external
-def foo():
-    x: int128 = 5
-    for i in range(1, x, bound=4):
+    for i: uint256 in range(10, 20):
         pass
     """,
     """
 @external
 def foo():
     x: int128 = 5
-    for i in range(x, bound=4):
+    for i: int128 in range(1, x, bound=4):
         pass
     """,
     """
 @external
 def foo():
     x: int128 = 5
-    for i in range(0, x, bound=4):
+    for i: int128 in range(x, bound=4):
+        pass
+    """,
+    """
+@external
+def foo():
+    x: int128 = 5
+    for i: int128 in range(0, x, bound=4):
         pass
     """,
     """
@@ -295,7 +295,7 @@ interface Foo:
 foos: Foo[3]
 @external
 def kick_foos():
-    for foo in self.foos:
+    for foo: Foo in self.foos:
         foo.kick()
     """,
 ]

commit 07ab92f3e287a812a1197182fe6e8b1168016ce3

refactor: for loop target parsing (#3724)
use a parser trick to get always-correct source locations into for-loop
target annotations.

this fixes an issue with parsing the for loop target (introduced in
ddfce5273b3), where the target annotation is not decorated with the
correct line/column offsets. a6dc432db2df partially fixed the issue, but
does not decorate correctly when the annotation is split across multiple
lines. this commit approaches the problem differently, by using
`untokenize()` to produce a source code for the annotation during AST
massaging which "happens" to have the annotation in exactly the same
place as it appeared in the original source.

small refactors:
- move annotate_python_ast to be earlier in the file. it's somehow
  easier to navigate to it when it comes right before the definition of
  AnnotatingVisitor.
- revert changes from a6dc432db2df to ast/pre_parser.py; go back to
  slurping just the annotation as opposed to the target+annotation
  together.

vyper/ast/parse.py
@@ -1,5 +1,4 @@
 import ast as python_ast
-import string
 import tokenize
 from decimal import Decimal
 from typing import Any, Dict, List, Optional, Union, cast
@@ -115,6 +114,50 @@ def dict_to_ast(ast_struct: Union[Dict, List]) -> Union[vy_ast.VyperNode, List]:
     raise CompilerPanic(f'Unknown ast_struct provided: "{type(ast_struct)}".')
 
 
+def annotate_python_ast(
+    parsed_ast: python_ast.AST,
+    source_code: str,
+    modification_offsets: ModificationOffsets,
+    for_loop_annotations: dict,
+    source_id: int = 0,
+    module_path: Optional[str] = None,
+    resolved_path: Optional[str] = None,
+) -> python_ast.AST:
+    """
+    Annotate and optimize a Python AST in preparation conversion to a Vyper AST.
+
+    Parameters
+    ----------
+    parsed_ast : AST
+        The AST to be annotated and optimized.
+    source_code : str
+        The originating source code of the AST.
+    loop_var_annotations: dict
+        A mapping of line numbers of `For` nodes to the tokens of the type
+        annotation of the iterator extracted during pre-parsing.
+    modification_offsets : dict
+        A mapping of class names to their original class types.
+
+    Returns
+    -------
+        The annotated and optimized AST.
+    """
+
+    tokens = asttokens.ASTTokens(source_code, tree=cast(Optional[python_ast.Module], parsed_ast))
+    visitor = AnnotatingVisitor(
+        source_code,
+        modification_offsets,
+        for_loop_annotations,
+        tokens,
+        source_id,
+        module_path=module_path,
+        resolved_path=resolved_path,
+    )
+    visitor.visit(parsed_ast)
+
+    return parsed_ast
+
+
 class AnnotatingVisitor(python_ast.NodeTransformer):
     _source_code: str
     _modification_offsets: ModificationOffsets
@@ -170,6 +213,7 @@ class AnnotatingVisitor(python_ast.NodeTransformer):
         if hasattr(node, "last_token"):
             start_pos = node.first_token.startpos
             end_pos = node.last_token.endpos
+
             if node.last_token.type == 4:
                 # ignore trailing newline once more
                 end_pos -= 1
@@ -241,52 +285,42 @@ class AnnotatingVisitor(python_ast.NodeTransformer):
                 node.col_offset,
             )
 
-        self.generic_visit(node)
+        # some kind of black magic. untokenize preserves the line and column
+        # offsets, giving us something like `\
+        # \
+        # \
+        #   uint8`
+        # that's not a valid python Expr because it is indented.
+        # but it's good because the code is indented to exactly the same
+        # offset as it did in the original source!
+        # (to best understand this, print out annotation_str and
+        # self._source_code and compare them side-by-side).
+        #
+        # what we do here is add in a dummy target which we will remove
+        # in a bit, but for now lets us keep the line/col offset, and
+        # *also* gives us a valid AST. it doesn't matter what the dummy
+        # target name is, since it gets removed in a few lines.
+        annotation_str = tokenize.untokenize(annotation_tokens)
+        annotation_str = "dummy_target:" + annotation_str
 
         try:
-            annotation_str = tokenize.untokenize(annotation_tokens).strip(string.whitespace + "\\")
-            annotation = python_ast.parse(annotation_str)
+            fake_node = python_ast.parse(annotation_str).body[0]
         except SyntaxError as e:
             raise SyntaxException(
                 "invalid type annotation", self._source_code, node.lineno, node.col_offset
             ) from e
 
-        annotation = annotation.body[0]
-        og_target = node.target
+        # fill in with asttokens info. note we can use `self._tokens` because
+        # it is indented to exactly the same position where it appeared
+        # in the original source!
+        self._tokens.mark_tokens(fake_node)
 
-        # annotate with token and source code information. `first_token`
-        # and `last_token` attributes are accessed in `generic_visit`.
-        tokens = asttokens.ASTTokens(annotation_str)
-        tokens.mark_tokens(annotation)
+        # replace the dummy target name with the real target name.
+        fake_node.target = node.target
+        # replace the For node target with the new ann_assign
+        node.target = fake_node
 
-        # decrease line offset by 1 because annotation is on the same line as `For` node
-        # but the spliced expression also starts at line 1
-        adjustment = og_target.first_token.start[0] - 1, og_target.first_token.start[1]
-
-        def _add_pair(x, y):
-            return x[0] + y[0], x[1] + y[1]
-
-        for n in python_ast.walk(annotation):
-            # adjust all offsets
-            if hasattr(n, "first_token"):
-                n.first_token = n.first_token._replace(
-                    start=_add_pair(n.first_token.start, adjustment),
-                    end=_add_pair(n.first_token.end, adjustment),
-                    startpos=n.first_token.startpos + og_target.first_token.startpos,
-                    endpos=n.first_token.startpos + og_target.first_token.startpos,
-                )
-            if hasattr(n, "last_token"):
-                n.last_token = n.last_token._replace(
-                    start=_add_pair(n.last_token.start, adjustment),
-                    end=_add_pair(n.last_token.end, adjustment),
-                    startpos=n.last_token.startpos + og_target.first_token.startpos,
-                    endpos=n.last_token.endpos + og_target.first_token.startpos,
-                )
-
-        node.target = annotation
-        node.target = self.generic_visit(node.target)
-
-        return node
+        return self.generic_visit(node)
 
     def visit_Expr(self, node):
         """
@@ -425,47 +459,3 @@ class AnnotatingVisitor(python_ast.NodeTransformer):
             return node.operand
         else:
             return node
-
-
-def annotate_python_ast(
-    parsed_ast: python_ast.AST,
-    source_code: str,
-    modification_offsets: ModificationOffsets,
-    for_loop_annotations: dict,
-    source_id: int = 0,
-    module_path: Optional[str] = None,
-    resolved_path: Optional[str] = None,
-) -> python_ast.AST:
-    """
-    Annotate and optimize a Python AST in preparation conversion to a Vyper AST.
-
-    Parameters
-    ----------
-    parsed_ast : AST
-        The AST to be annotated and optimized.
-    source_code : str
-        The originating source code of the AST.
-    loop_var_annotations: dict, optional
-        A mapping of line numbers of `For` nodes to the tokens of the type annotation
-        of the iterator extracted during pre-parsing.
-    modification_offsets : dict, optional
-        A mapping of class names to their original class types.
-
-    Returns
-    -------
-        The annotated and optimized AST.
-    """
-
-    tokens = asttokens.ASTTokens(source_code, tree=cast(Optional[python_ast.Module], parsed_ast))
-    visitor = AnnotatingVisitor(
-        source_code,
-        modification_offsets,
-        for_loop_annotations,
-        tokens,
-        source_id,
-        module_path=module_path,
-        resolved_path=resolved_path,
-    )
-    visitor.visit(parsed_ast)
-
-    return parsed_ast

vyper/ast/pre_parser.py
@@ -65,32 +65,27 @@ class ForParser:
     def consume(self, token):
         # state machine: we can start slurping tokens soon
         if token.type == NAME and token.string == "for":
+            # note: self._state should be NOT_RUNNING here, but we don't sanity
+            # check here as that should be an error the parser will handle.
+            self._state = ForParserState.START_SOON
+            self._current_for_loop = token.start
+
+        if self._state == ForParserState.NOT_RUNNING:
+            return False
+
+        # state machine: start slurping tokens
+        if token.type == OP and token.string == ":":
+            self._state = ForParserState.RUNNING
+
             # sanity check -- this should never really happen, but if it does,
             # try to raise an exception which pinpoints the source.
             if self._current_annotation is not None:
                 raise SyntaxException(
                     "for loop parse error", self._code, token.start[0], token.start[1]
                 )
+
             self._current_annotation = []
-
-            assert self._state == ForParserState.NOT_RUNNING
-            self._state = ForParserState.START_SOON
-            self._current_for_loop = token.start
-            return False
-
-        if self._state == ForParserState.NOT_RUNNING:
-            return False
-
-        if self._state == ForParserState.START_SOON:
-            # state machine: start slurping tokens
-
-            self._current_annotation.append(token)
-
-            if token.type == OP and token.string == ":":
-                self._state = ForParserState.RUNNING
-                return True  # do not add ":" to global tokens.
-
-            return False  # add everything before ":" to tokens
+            return True  # do not add ":" to tokens.
 
         # state machine: end slurping tokens
         if token.type == NAME and token.string == "in":
@@ -142,8 +137,7 @@ def pre_parse(code: str) -> tuple[Settings, ModificationOffsets, dict, str]:
     ModificationOffsets
         A mapping of class names to their original class types.
     dict[tuple[int, int], list[TokenInfo]]
-        A mapping of line/column offsets of `For` nodes to the tokens of the annotation of the
-        for loop target
+        A mapping of line/column offsets of `For` nodes to the annotation of the for loop target
     str
         Reformatted python source string.
     """

tests/functional/syntax/test_for_range.py
@@ -247,9 +247,43 @@ def foo():
         "No builtin or user-defined type named 'DynArra'. Did you mean 'DynArray'?",
         "DynArra",
     ),
+    (
+        # test for loop target broken into multiple lines
+        """
+@external
+def foo():
+    for i: \\
+      \\
+        \\
+        \\
+        \\
+        \\
+        uint9 in [1,2,3]:
+        pass
+    """,
+        UnknownType,
+        "No builtin or user-defined type named 'uint9'. Did you mean 'uint96', or maybe 'uint8'?",
+        "uint9",
+    ),
+    (
+        # test an even more deranged example
+        """
+@external
+def foo():
+    for i: \\
+        \\
+          DynArray[\\
+        uint9, 3\\
+        ] in [1,2,3]:
+        pass
+    """,
+        UnknownType,
+        "No builtin or user-defined type named 'uint9'. Did you mean 'uint96', or maybe 'uint8'?",
+        "uint9",
+    ),
 ]
 
-for_code_regex = re.compile(r"for .+ in (.*):")
+for_code_regex = re.compile(r"for .+ in (.*):", re.DOTALL)
 fail_test_names = [
     (
         f"{i:02d}: {for_code_regex.search(code).group(1)}"  # type: ignore[union-attr]