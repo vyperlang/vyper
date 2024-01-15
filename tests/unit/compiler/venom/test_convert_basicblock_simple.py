33e5b5a7bab62b338b80b1084dbdbc3d9e1fed19
Harry Kalogirou <harkal@nlogn.eu>
2024-01-15 14:11:15 +0200

    Add new test cases for simple conversion in test_convert_basicblock_simple.py

--- tests/unit/compiler/venom/test_convert_basicblock_simple.py
+++ tests/unit/compiler/venom/test_convert_basicblock_simple.py
@@ -17,3 +17,39 @@ __global:  IN=[] OUT=[] => {}
     stop"""
 
     assert str(runtime) == correct_venom
+
+
+def test_simple_2():
+    ir = [
+        "seq",
+        [
+            "seq",
+            [
+                "mstore",
+                ["add", 64, 0],
+                [
+                    "with",
+                    "x",
+                    ["calldataload", ["add", 4, 0]],
+                    [
+                        "with",
+                        "ans",
+                        ["add", "x", 1],
+                        ["seq", ["assert", ["ge", "ans", "x"]], "ans"],
+                    ],
+                ],
+            ],
+        ],
+        32,
+    ]
+    ir_node = IRnode.from_list(ir)
+    deploy, runtime = generate_ir(ir_node, OptimizationLevel.NONE)
+    assert deploy is None
+    assert runtime is not None
+
+    print(runtime)
+
+
+if __name__ == "__main__":
+    test_simple()
+    test_simple_2()
