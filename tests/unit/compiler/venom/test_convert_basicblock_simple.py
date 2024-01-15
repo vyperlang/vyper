d6af7fe12ae858029bf0fbe4a36073e2ce37b6c5
Harry Kalogirou <harkal@nlogn.eu>
2024-01-15 09:00:03 +0200

    Clean up whitespace test_convert_basicblock_simple.py

--- tests/unit/compiler/venom/test_convert_basicblock_simple.py
+++ tests/unit/compiler/venom/test_convert_basicblock_simple.py
@@ -3,20 +3,16 @@ from vyper.compiler.settings import OptimizationLevel
 from vyper.venom import generate_ir
 
 
-def test_simple(get_contract_from_ir):
+def test_simple():
     ir = ["calldatacopy", 32, 0, ["calldatasize"]]
-    ir = IRnode.from_list(ir)
-    print(ir)
-    deploy, runtime = generate_ir(ir, OptimizationLevel.NONE)
+    ir_node = IRnode.from_list(ir)
+    deploy, runtime = generate_ir(ir_node, OptimizationLevel.NONE)
     assert deploy is None
     assert runtime is not None
-    assert len(runtime.basic_blocks) == 1
-    bb = runtime.basic_blocks[0]
-    assert len(bb.instructions) == 3
 
     correct_venom = """IRFunction: __global
-__global:  IN=[] OUT=[] => {} 
-    %1 = calldatasize 
+__global:  IN=[] OUT=[] => {}
+    %1 = calldatasize
     calldatacopy %1, 0, 32
     stop"""
 
