353fefa599e95e6bcc693e969fb57f11383d08a4
Harry Kalogirou <harkal@nlogn.eu>
2024-01-17 16:52:58 +0200

    Add test for liveness in a simple loop

---
+++ tests/unit/compiler/venom/test_liveness_simple_loop.py
@@ -0,0 +1,17 @@
+import pytest
+import vyper
+
+source = """
+@external
+def foo(a: uint256):
+    _numBids: uint256 = 20
+    b: uint256 = 10
+
+    for i: uint256 in range(128):
+        b = 1 + _numBids
+"""
+
+
+def test_liveness_simple_loop():
+    vyper.compile_code(source, ["opcodes"])
+    assert True
