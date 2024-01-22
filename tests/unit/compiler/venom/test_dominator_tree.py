73b81632dd18554797d67b1bc0a385075b5655ed
Harry Kalogirou <harkal@nlogn.eu>
2024-01-22 13:52:11 +0200

    Add test for dominator tree calculation

---
+++ tests/unit/compiler/venom/test_dominator_tree.py
@@ -0,0 +1,53 @@
+from typing import Optional
+from vyper.venom.analysis import calculate_cfg
+from vyper.venom.basicblock import IRBasicBlock, IRLabel, IRLiteral
+from vyper.venom.dominators import DominatorTree
+from vyper.venom.function import IRFunction
+
+
+def _add_bb(
+    ctx: IRFunction, label: IRLabel, cfg_outs: [IRLabel], bb: Optional[IRBasicBlock] = None
+) -> IRBasicBlock:
+    bb = bb if bb is not None else IRBasicBlock(label, ctx)
+    ctx.append_basic_block(bb)
+    cgf_outs_len = len(cfg_outs)
+    if cgf_outs_len == 0:
+        bb.append_instruction("stop")
+    elif cgf_outs_len == 1:
+        bb.append_instruction("jmp", cfg_outs[0])
+    elif cgf_outs_len == 2:
+        bb.append_instruction("jnz", IRLiteral(1), cfg_outs[0], cfg_outs[1])
+    else:
+        assert False, cgf_outs_len
+    return bb
+
+
+def test_deminator_frontier_calculation():
+    l = [IRLabel(str(i)) for i in range(0, 9)]
+
+    ctx = IRFunction(l[1])
+
+    bb1 = ctx.basic_blocks[0]
+    bb1.append_instruction("jmp", l[2])
+
+    bb7 = _add_bb(ctx, l[7], [])
+    bb6 = _add_bb(ctx, l[6], [l[7], l[2]])
+    bb5 = _add_bb(ctx, l[5], [l[6], l[3]])
+    bb4 = _add_bb(ctx, l[4], [l[6]])
+    bb3 = _add_bb(ctx, l[3], [l[5]])
+    bb2 = _add_bb(ctx, l[2], [l[3], l[4]])
+
+    calculate_cfg(ctx)
+    dom = DominatorTree(ctx, bb1)
+
+    assert dom.df[bb1] == set(), dom.df[bb1]
+    assert dom.df[bb2] == {bb2}, dom.df[bb2]
+    assert dom.df[bb3] == {bb3, bb6}, dom.df[bb3]
+    assert dom.df[bb4] == {bb6}, dom.df[bb4]
+    assert dom.df[bb5] == {bb3, bb6}, dom.df[bb5]
+    assert dom.df[bb6] == {bb2}, dom.df[bb6]
+    assert dom.df[bb7] == set(), dom.df[bb7]
+
+
+if __name__ == "__main__":
+    test_deminator_frontier_calculation()
