109c697acedc901830ef8405e7faa23938586c85
Harry Kalogirou <harkal@nlogn.eu>
2024-01-22 21:13:54 +0200

    renaming start

--- vyper/venom/passes/make_ssa.py
+++ vyper/venom/passes/make_ssa.py
@@ -6,27 +6,42 @@ from vyper.venom.passes.base_pass import IRPass
 
 
 class MakeSSA(IRPass):
+    dom: DominatorTree
+
     def _run_pass(self, ctx: IRFunction) -> int:
         self.ctx = ctx
 
         calculate_cfg(ctx)
         entry = ctx.get_basic_block(ctx.entry_points[0].value)
         dom = DominatorTree(ctx, entry)
+        self.dom = dom
 
-        defs = {}
-        for bb in ctx.basic_blocks:
-            assignments = bb.get_assignments()
-            for var in assignments:
-                if var not in defs:
-                    defs[var] = set()
-                defs[var].add(bb)
+        # self._dfs_dom(entry, set())
 
-        for var, d in defs.items():
-            df = dom.dominance_frontier(d)
-            for bb in df:
-                self._add_phi(var, bb)
+        print(ctx.as_graph())
 
         self.changes = 0
+
+    def _dfs_dom(self, basic_block: IRBasicBlock, visited: set):
+        visited.add(basic_block)
+        for bb in self.dom.dominated[basic_block]:
+            if bb not in visited:
+                self._dfs_dom(bb, visited)
+
+        self._process_basic_block(basic_block)
+
+    def _process_basic_block(self, basic_block: IRBasicBlock):
+        defs = {}
+        assignments = basic_block.get_assignments()
+        for var in assignments:
+            if var not in defs:
+                defs[var] = set()
+            defs[var].add(basic_block)
+
+        for var, d in defs.items():
+            df = self.dom.dominance_frontier(d)
+            for bb in df:
+                self._add_phi(var, bb)
 
     def _add_phi(self, var: IRVariable, basic_block: IRBasicBlock):
         args = []
