c701de729d6d73319921f8e8186cdc09501a671a
Harry Kalogirou <harkal@nlogn.eu>
2024-01-23 11:59:22 +0200

    Test update

--- tests/unit/compiler/venom/test_dominator_tree.py
+++ tests/unit/compiler/venom/test_dominator_tree.py
@@ -67,9 +67,7 @@ def test_phi_placement():
     bb2.insert_instruction(IRInstruction("add", [x, IRLiteral(1)], x), 0)
     bb7.insert_instruction(IRInstruction("mstore", [x, IRLiteral(0)]), 0)
 
-    MakeSSA.run_pass(ctx)
-
-    # print(ctx)
+    MakeSSA.run_pass(ctx, bb1)
 
 
 if __name__ == "__main__":
