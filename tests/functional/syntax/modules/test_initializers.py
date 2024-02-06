c8deffda48488759cc5c5260d8a0e68c2f7cbf8d
Charles Cooper <cooper.charles.m@gmail.com>
2024-02-06 13:05:52 -0500

    add tests for initializer constraints

---
+++ tests/functional/syntax/modules/test_initializers.py
@@ -0,0 +1,512 @@
+import pytest
+
+from vyper.compiler import compile_code
+from vyper.exceptions import (
+    BorrowException,
+    ImmutableViolation,
+    InitializerException,
+    StructureException,
+    UndeclaredDefinition,
+)
+
+
+def test_initialize_uses(make_input_bundle):
+    lib1 = """
+counter: uint256
+
+@deploy
+def __init__():
+    pass
+    """
+    lib2 = """
+import lib1
+
+uses: lib1
+
+counter: uint256
+
+@deploy
+def __init__():
+    pass
+
+@internal
+def foo():
+    lib1.counter += 1
+    """
+    main = """
+import lib2
+import lib1
+
+initializes: lib2[lib1 := lib1]
+initializes: lib1
+
+@deploy
+def __init__():
+    lib1.__init__()
+    lib2.__init__()
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1, "lib2.vy": lib2})
+
+    assert compile_code(main, input_bundle=input_bundle) is not None
+
+
+def test_initialize_uses_attribute(make_input_bundle):
+    lib1 = """
+counter: uint256
+
+@deploy
+def __init__():
+    pass
+    """
+    lib2 = """
+import lib1
+
+uses: lib1
+
+counter: uint256
+
+@deploy
+def __init__():
+    pass
+
+@internal
+def foo():
+    lib1.counter += 1
+    """
+    main = """
+import lib1
+import lib2
+
+initializes: lib2[lib1 := lib1]
+initializes: lib1
+
+@deploy
+def __init__():
+    lib2.__init__()
+    # demonstrate we can call lib1.__init__ through lib2.lib1
+    # (not sure this should be allowed, really.
+    lib2.lib1.__init__()
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1, "lib2.vy": lib2})
+
+    assert compile_code(main, input_bundle=input_bundle) is not None
+
+
+def test_initializes_without_init_function(make_input_bundle):
+    lib1 = """
+counter: uint256
+    """
+    lib2 = """
+import lib1
+
+uses: lib1
+
+counter: uint256
+
+@internal
+def foo():
+    lib1.counter += 1
+    """
+    main = """
+import lib1
+import lib2
+
+initializes: lib2[lib1 := lib1]
+initializes: lib1
+
+@deploy
+def __init__():
+    pass
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1, "lib2.vy": lib2})
+
+    assert compile_code(main, input_bundle=input_bundle) is not None
+
+
+def test_imported_as_different_names(make_input_bundle):
+    lib1 = """
+counter: uint256
+    """
+    lib2 = """
+import lib1 as m
+
+uses: m
+
+counter: uint256
+
+@internal
+def foo():
+    m.counter += 1
+    """
+    main = """
+import lib1
+import lib2
+
+initializes: lib2[m := lib1]
+initializes: lib1
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1, "lib2.vy": lib2})
+
+    assert compile_code(main, input_bundle=input_bundle) is not None
+
+
+def test_imported_as_different_names_error(make_input_bundle):
+    lib1 = """
+counter: uint256
+    """
+    lib2 = """
+import lib1 as m
+
+uses: m
+
+counter: uint256
+
+@internal
+def foo():
+    m.counter += 1
+    """
+    main = """
+import lib1
+import lib2
+
+initializes: lib2[lib1 := lib1]
+initializes: lib1
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1, "lib2.vy": lib2})
+
+    with pytest.raises(UndeclaredDefinition) as e:
+        compile_code(main, input_bundle=input_bundle)
+
+    assert e.value._message == "unknown module `lib1`"
+    assert e.value._hint == "did you mean `m := lib1`?"
+
+
+def test_global_initializer_constraint(make_input_bundle):
+    lib1 = """
+counter: uint256
+    """
+    lib2 = """
+import lib1
+
+uses: lib1
+
+counter: uint256
+
+@internal
+def foo():
+    lib1.counter += 1
+    """
+    main = """
+import lib1
+import lib2
+
+initializes: lib2[lib1 := lib1]
+# forgot to initialize lib1!
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1, "lib2.vy": lib2})
+
+    with pytest.raises(InitializerException) as e:
+        compile_code(main, input_bundle=input_bundle)
+    assert e.value._message == "module `lib1.vy` is used but never initialized!"
+    assert e.value._hint == "add `initializes: lib1` to the top level of your main contract"
+
+
+def test_initializer_no_references(make_input_bundle):
+    lib1 = """
+counter: uint256
+    """
+    lib2 = """
+import lib1
+
+uses: lib1
+
+counter: uint256
+
+@internal
+def foo():
+    lib1.counter += 1
+    """
+    main = """
+import lib1
+import lib2
+
+initializes: lib2
+initializes: lib1
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1, "lib2.vy": lib2})
+
+    with pytest.raises(InitializerException) as e:
+        compile_code(main, input_bundle=input_bundle)
+    assert e.value._message == "`lib2` uses `lib1`, but it is not initialized with `lib1`"
+    assert e.value._hint == "add `lib1` to its initializer list"
+
+
+def test_missing_uses(make_input_bundle):
+    lib1 = """
+counter: uint256
+    """
+    lib2 = """
+import lib1
+
+# forgot `uses: lib1`!
+
+counter: uint256
+
+@internal
+def foo():
+    lib1.counter += 1
+    """
+    main = """
+import lib1
+import lib2
+
+initializes: lib2
+initializes: lib1
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1, "lib2.vy": lib2})
+
+    with pytest.raises(ImmutableViolation) as e:
+        compile_code(main, input_bundle=input_bundle)
+
+    assert e.value._message == "Constant value cannot be written to."
+
+    expected_hint = "add `uses: lib1` or `initializes: lib1` as a "
+    expected_hint += "top-level statement to your contract"
+    assert e.value._hint == expected_hint
+
+
+def test_invalid_uses(make_input_bundle):
+    lib1 = """
+counter: uint256
+    """
+    lib2 = """
+import lib1
+
+uses: lib1  # not necessary!
+
+counter: uint256
+
+@internal
+def foo():
+    pass
+    """
+    main = """
+import lib1
+import lib2
+
+initializes: lib2[lib1 := lib1]
+initializes: lib1
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1, "lib2.vy": lib2})
+
+    with pytest.raises(BorrowException) as e:
+        compile_code(main, input_bundle=input_bundle)
+    assert e.value._message == "`lib1` is declared as used, but it is not actually used in lib2.vy!"
+    assert e.value._hint == "delete `uses: lib1`"
+
+
+def test_initializes_uses_conflict(make_input_bundle):
+    lib1 = """
+counter: uint256
+    """
+    main = """
+import lib1
+
+initializes: lib1
+uses: lib1
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1})
+
+    with pytest.raises(StructureException) as e:
+        compile_code(main, input_bundle=input_bundle)
+    assert e.value._message == "ownership already set to `initializes`"
+
+
+def test_uses_initializes_conflict(make_input_bundle):
+    lib1 = """
+counter: uint256
+    """
+    main = """
+import lib1
+
+uses: lib1
+initializes: lib1
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1})
+
+    with pytest.raises(StructureException) as e:
+        compile_code(main, input_bundle=input_bundle)
+    assert e.value._message == "ownership already set to `uses`"
+
+
+def test_uses_twice(make_input_bundle):
+    lib1 = """
+counter: uint256
+    """
+    main = """
+import lib1
+
+uses: lib1
+
+random_variable: constant(uint256) = 3
+
+uses: lib1
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1})
+
+    with pytest.raises(StructureException) as e:
+        compile_code(main, input_bundle=input_bundle)
+    assert e.value._message == "ownership already set to `uses`"
+
+
+def test_initializes_twice(make_input_bundle):
+    lib1 = """
+counter: uint256
+    """
+    main = """
+import lib1
+
+initializes: lib1
+
+random_variable: constant(uint256) = 3
+
+initializes: lib1
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1})
+
+    with pytest.raises(StructureException) as e:
+        compile_code(main, input_bundle=input_bundle)
+    assert e.value._message == "ownership already set to `initializes`"
+
+
+def test_no_initialize_unused_module(make_input_bundle):
+    lib1 = """
+counter: uint256
+
+@internal
+def set_counter(new_value: uint256):
+    self.counter = new_value
+
+@internal
+@pure
+def add(x: uint256, y: uint256) -> uint256:
+    return x + y
+    """
+    main = """
+import lib1
+
+# not needed: `initializes: lib1`
+
+@external
+def do_add(x: uint256, y: uint256) -> uint256:
+    return lib1.add(x, y)
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1})
+    assert compile_code(main, input_bundle=input_bundle) is not None
+
+
+def test_no_initialize_unused_module2(make_input_bundle):
+    # slightly more complicated
+    lib1 = """
+counter: uint256
+
+@internal
+def set_counter(new_value: uint256):
+    self.counter = new_value
+
+@internal
+@pure
+def add(x: uint256, y: uint256) -> uint256:
+    return x + y
+    """
+    lib2 = """
+import lib1
+
+@internal
+@pure
+def addmul(x: uint256, y: uint256, z: uint256) -> uint256:
+    return lib1.add(x, y) * z
+    """
+    main = """
+import lib1
+import lib2
+
+@external
+def do_addmul(x: uint256, y: uint256) -> uint256:
+    return lib2.addmul(x, y, 5)
+    """
+    input_bundle = make_input_bundle({"lib1.vy": lib1, "lib2.vy": lib2})
+    assert compile_code(main, input_bundle=input_bundle) is not None
+
+
+def test_init_uninitialized_function(make_input_bundle):
+    lib1 = """
+counter: uint256
+
+@deploy
+def __init__():
+    self.counter = 5
+    """
+    main = """
+import lib1
+
+# missing `initializes: lib1`!
+
+@deploy
+def __init__():
+    lib1.__init__()
+    """
+
+    input_bundle = make_input_bundle({"lib1.vy": lib1})
+    with pytest.raises(InitializerException) as e:
+        compile_code(main, input_bundle=input_bundle)
+    assert e.value._message == "tried to initialize `lib1`, but it is not in initializer list!"
+    assert e.value._hint == "add `initializes: lib1` as a top-level statement to your contract"
+
+
+def test_noinit_initialized_function(make_input_bundle):
+    lib1 = """
+counter: uint256
+
+@deploy
+def __init__():
+    self.counter = 5
+    """
+    main = """
+import lib1
+
+initializes: lib1
+
+@deploy
+def __init__():
+    pass  # missing `lib1.__init__()`!
+    """
+
+    input_bundle = make_input_bundle({"lib1.vy": lib1})
+    with pytest.raises(InitializerException) as e:
+        compile_code(main, input_bundle=input_bundle)
+    assert e.value._message == "not initialized!"
+    assert e.value._hint == "add `lib1.__init__()` to your `__init__()` function"
+
+
+def test_noinit_initialized_function2(make_input_bundle):
+    lib1 = """
+counter: uint256
+
+@deploy
+def __init__():
+    self.counter = 5
+    """
+    main = """
+import lib1
+
+initializes: lib1
+
+# missing `lib1.__init__()`!
+    """
+
+    input_bundle = make_input_bundle({"lib1.vy": lib1})
+    with pytest.raises(InitializerException) as e:
+        compile_code(main, input_bundle=input_bundle)
+    assert e.value._message == "not initialized!"
+    assert e.value._hint == "add `lib1.__init__()` to your `__init__()` function"
