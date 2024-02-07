cbef6bc3f656056b167aba6850b4564cb0b10379
Charles Cooper <cooper.charles.m@gmail.com>
2024-02-06 20:00:18 -0500

    add more mutability tests

--- tests/functional/syntax/exceptions/test_constancy_exception.py
+++ tests/functional/syntax/exceptions/test_constancy_exception.py
@@ -122,6 +122,27 @@ def test(to:address):
     a:A = A(to)
     a.bar()
     """,
+    """
+a:DynArray[uint16,3]
+@deploy
+def __init__():
+    self.a = [1,2,3]
+@view
+@external
+def bar()->DynArray[uint16,3]:
+    x:uint16 = self.a.pop()
+    return self.a # return [1,2]
+    """,
+    """
+from ethereum.ercs import ERC20
+
+token: ERC20
+
+@external
+@view
+def topup(amount: uint256):
+    assert self.token.transferFrom(msg.sender, self, amount)
+    """,
     ],
 )
 def test_statefulness_violations(bad_code):
