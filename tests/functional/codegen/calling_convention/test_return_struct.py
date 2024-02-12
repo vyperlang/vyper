194f0316277e714fb86258a53bb0643ea74e1602
tserg <8017125+tserg@users.noreply.github.com>
2024-02-12 16:02:35 +0800

    deduplicate

--- tests/functional/codegen/calling_convention/test_return_struct.py
+++ tests/functional/codegen/calling_convention/test_return_struct.py
@@ -505,3 +505,61 @@ def foo(addr: address) -> Foo:
     c2 = get_contract(code2)
 
     assert c2.foo(c.address) == (4,)
+
+
+@pytest.mark.parametrize("string", ["a", "abc", "abcde", "potato"])
+def test_string_inside_struct(get_contract, string):
+    code = f"""
+struct Person:
+    name: String[6]
+    age: uint256
+
+@external
+def test_return() -> Person:
+    return Person({{ name:"{string}", age:42 }})
+    """
+    c1 = get_contract(code)
+
+    code = """
+struct Person:
+    name: String[6]
+    age: uint256
+
+interface jsonabi:
+    def test_return() -> Person: view
+
+@external
+def test_values(a: address) -> Person:
+    return jsonabi(a).test_return()
+    """
+
+    c2 = get_contract(code)
+    assert c2.test_values(c1.address) == (string, 42)
+
+
+@pytest.mark.parametrize("string", ["a", "abc", "abcde", "potato"])
+def test_string_inside_single_struct(get_contract, string):
+    code = f"""
+struct Person:
+    name: String[6]
+
+@external
+def test_return() -> Person:
+    return Person({{ name:"{string}"}})
+    """
+    c1 = get_contract(code)
+
+    code = """
+struct Person:
+    name: String[6]
+
+interface jsonabi:
+    def test_return() -> Person: view
+
+@external
+def test_values(a: address) -> Person:
+    return jsonabi(a).test_return()
+    """
+
+    c2 = get_contract(code)
+    assert c2.test_values(c1.address) == (string,)
--- tests/functional/codegen/calling_convention/test_return_tuple.py
+++ tests/functional/codegen/calling_convention/test_return_tuple.py
@@ -153,6 +153,50 @@ def test3() -> (address, int128):
     assert c.test3() == [c.out_literals()[2], 1]
 
 
+@pytest.mark.parametrize("string", ["a", "abc", "abcde", "potato"])
+def test_string_inside_tuple(get_contract, string):
+    code = f"""
+@external
+def test_return() -> (String[6], uint256):
+    return "{string}", 42
+    """
+    c1 = get_contract(code)
+
+    code = """
+interface jsonabi:
+    def test_return() -> (String[6], uint256): view
+
+@external
+def test_values(a: address) -> (String[6], uint256):
+    return jsonabi(a).test_return()
+    """
+
+    c2 = get_contract(code)
+    assert c2.test_values(c1.address) == [string, 42]
+
+
+@pytest.mark.parametrize("string", ["a", "abc", "abcde", "potato"])
+def test_bytes_inside_tuple(get_contract, string):
+    code = f"""
+@external
+def test_return() -> (Bytes[6], uint256):
+    return b"{string}", 42
+    """
+    c1 = get_contract(code)
+
+    code = """
+interface jsonabi:
+    def test_return() -> (Bytes[6], uint256): view
+
+@external
+def test_values(a: address) -> (Bytes[6], uint256):
+    return jsonabi(a).test_return()
+    """
+
+    c2 = get_contract(code)
+    assert c2.test_values(c1.address) == [bytes(string, "utf-8"), 42]
+
+
 def test_tuple_return_typecheck(tx_failed, get_contract_with_gas_estimation):
     code = """
 @external
