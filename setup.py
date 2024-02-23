867d0d24494677f3743a3b68382120855ee4dcac
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-02-23 14:08:09 +0100

    Add profiling

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ extras_require = {
         "pytest-instafail>=0.4,<1.0",
         "pytest-xdist>=2.5,<3.0",
         "pytest-split>=0.7.0,<1.0",
+        "pytest_profiling>=1.7.0,<2.0",
         "eth-tester[py-evm]>=0.9.0b1,<0.10",
         "py-evm>=0.7.0a1,<0.8",
         "web3==6.0.0",
