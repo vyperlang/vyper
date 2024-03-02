92eed811efc89f3bd11fed70be3329d5456c6e94
Harry Kalogirou <harkal@nlogn.eu>
2024-03-02 22:43:30 +0200

    gg

--- vyper/venom/passes/sccp.py
+++ vyper/venom/passes/sccp.py
@@ -1,9 +1,21 @@
-from vyper.venom.basicblock import IRBasicBlock
+from dataclasses import dataclass
+from enum import Enum
+from vyper.venom.basicblock import IRBasicBlock, IRLiteral, IRVariable
 from vyper.venom.function import IRFunction
 from vyper.venom.passes.base_pass import IRPass
 
 
-class SCCP(IRPass):
+class LatticeEnum(Enum):
+    TOP = 1
+    BOTTOM = 2
 
-    def _run_pass(self, ctx: IRFunction, entry: IRBasicBlock) -> int:
+
+type LatticeItem = LatticeEnum | IRLiteral
+
+
+class SCCP(IRPass):
+    def _run_pass(self, ctx: IRFunction, entry: IRBasicBlock, defs, uses) -> int:
+        pass
+
+    def _calculate_sccp(ctx: IRFunction, entry: IRBasicBlock, uses) -> map[IRVariable, LatticeItem]:
         pass
