9c1e5330081e808543dc687e0357a38f659b0cdd
Harry Kalogirou <harkal@nlogn.eu>
2024-03-03 19:34:21 +0200

    data structures

--- vyper/venom/passes/sccp.py
+++ vyper/venom/passes/sccp.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
-from vyper.venom.basicblock import IRBasicBlock, IRLiteral, IRVariable
+from vyper.venom.basicblock import IRBasicBlock, IRInstruction, IRLabel, IRLiteral, IRVariable
 from vyper.venom.function import IRFunction
 from vyper.venom.passes.base_pass import IRPass
 
@@ -10,6 +10,20 @@ class LatticeEnum(Enum):
     BOTTOM = 2
 
 
+@dataclass
+class SSAWorkListItem:
+    inst: IRInstruction
+    basic_block: IRBasicBlock
+
+
+@dataclass
+class FlowWorkItem:
+    start: IRBasicBlock
+    end: IRBasicBlock
+
+
+type WorkListItem = FlowWorkItem | SSAWorkListItem
+
 type LatticeItem = LatticeEnum | IRLiteral
 
 
@@ -18,4 +32,10 @@ class SCCP(IRPass):
         pass
 
     def _calculate_sccp(ctx: IRFunction, entry: IRBasicBlock, uses) -> map[IRVariable, LatticeItem]:
-        pass
+        ret: map[IRVariable, LatticeItem] = {}
+        work_list: list[WorkListItem] = []
+
+        dummy = IRBasicBlock(IRLabel("__dummy_start"), ctx)
+        work_list.append(SSAWorkListItem(dummy, ctx.basic_blocks[0]))
+
+        return ret
