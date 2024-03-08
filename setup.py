e81a5e2f775b4944298d233b301809ea727cb732
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-03-08 18:02:11 +0100

    Version specifier

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ extras_require = {
         "lark==1.1.9",
         "hypothesis[lark]>=5.37.1,<6.0",
         "eth-stdlib==0.2.6",
-        "git+https://github.com/gakonst/pyrevm",
+        "pyrevm @ git+https://github.com/gakonst/pyrevm",
     ],
     "lint": [
         "black==23.12.0",
