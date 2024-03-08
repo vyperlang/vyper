1e4ea235e63f9a072b373d32d749e3ff9703036c
Charles Cooper <cooper.charles.m@gmail.com>
2024-03-08 09:37:25 -0800

    fix lint

--- tests/conftest.py
+++ tests/conftest.py
@@ -61,7 +61,10 @@ def pytest_addoption(parser):
     parser.addoption("--enable-compiler-debug-mode", action="store_true")
 
     parser.addoption(
-        "--evm-version", choices=list(evm.EVM_VERSIONS.keys()), default="shanghai", help="set evm version"
+        "--evm-version",
+        choices=list(evm.EVM_VERSIONS.keys()),
+        default="shanghai",
+        help="set evm version",
     )
 
 
