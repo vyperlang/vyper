c017371256ae45be1334bb54568efb5eb28d64c0
Harry Kalogirou <harkal@nlogn.eu>
2024-03-09 23:00:26 +0200

    update test for venom

--- tests/unit/compiler/asm/test_asm_optimizer.py
+++ tests/unit/compiler/asm/test_asm_optimizer.py
@@ -95,7 +95,7 @@ def test_dead_code_eliminator(code):
     assert all(ctor_only not in instr for instr in runtime_asm)
 
 
-def test_library_code_eliminator(make_input_bundle):
+def test_library_code_eliminator(make_input_bundle, venom_pipeline):
     library = """
 @internal
 def unused1():
@@ -119,6 +119,7 @@ def foo():
     input_bundle = make_input_bundle({"library.vy": library})
     res = compile_code(code, input_bundle=input_bundle, output_formats=["asm"])
     asm = res["asm"]
-    assert "some_function()" in asm
+    if not venom_pipeline:
+        assert "some_function()" in asm
     assert "unused1()" not in asm
     assert "unused2()" not in asm
