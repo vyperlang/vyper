aaa419c8e2a94fb46427fbc9448dbd775c35b2d6
Charles Cooper <cooper.charles.m@gmail.com>
2024-03-15 08:26:05 -0400

    update pytest while we are at it

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 extras_require = {
     "test": [
-        "pytest>=6.2.5,<7.0",
+        "pytest>=8.0,<9.0",
         "pytest-cov>=2.10,<3.0",
         "pytest-instafail>=0.4,<1.0",
         "pytest-xdist>=2.5,<3.0",
