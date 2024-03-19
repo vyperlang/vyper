f184f1850624e1ebb7386936dfbde8e28deaeda1
Charles Cooper <cooper.charles.m@gmail.com>
2024-03-19 09:23:42 -0400

    fix a comment

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ extras_require = {
         "hypothesis[lark]>=6.0,<7.0",
         "eth-stdlib==0.2.7",
         "setuptools",
-        "typing_extensions",  # eth-rlp>2 asks for this properly but it has a conflict
+        "typing_extensions",  # we can remove this once dependencies upgrade to eth-rlp>=2.0
     ],
     "lint": [
         "black==23.12.0",
