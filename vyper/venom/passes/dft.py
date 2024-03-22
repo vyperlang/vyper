ef78ccf9e38bbf8305e920dc32113e7f6ea806c1
Harry Kalogirou <harkal@nlogn.eu>
2024-03-22 15:59:15 +0200

    before cleanup

--- vyper/venom/passes/dft.py
+++ vyper/venom/passes/dft.py
@@ -1,12 +1,12 @@
 from vyper.utils import OrderedSet
 from vyper.venom.analysis import DFG
-from vyper.venom.basicblock import IRBasicBlock, IRInstruction, IRVariable
+from vyper.venom.basicblock import BB_TERMINATORS, IRBasicBlock, IRInstruction, IRVariable
 from vyper.venom.function import IRFunction
 from vyper.venom.passes.base_pass import IRPass
 
 
 class DFTPass(IRPass):
-    def _process_instruction_r(self, bb: IRBasicBlock, inst: IRInstruction):
+    def _process_instruction_r(self, bb: IRBasicBlock, inst: IRInstruction, offset: int = 0):
         for op in inst.get_outputs():
             assert isinstance(op, IRVariable), f"expected variable, got {op}"
             uses = self.dfg.get_uses(op)
@@ -15,7 +15,9 @@ class DFTPass(IRPass):
                 if uses_this.parent != inst.parent or uses_this.fence_id != inst.fence_id:
                     # don't reorder across basic block or fence boundaries
                     continue
-                self._process_instruction_r(bb, uses_this)
+                self._process_instruction_r(
+                    bb, uses_this, 1000 if uses_this.opcode in BB_TERMINATORS else offset
+                )
 
         if inst in self.visited_instructions:
             return
@@ -24,7 +26,9 @@ class DFTPass(IRPass):
         if inst.opcode == "phi":
             # phi instructions stay at the beginning of the basic block
             # and no input processing is needed
-            bb.instructions.append(inst)
+            # bb.instructions.append(inst)
+            self.inst_order_num += 1
+            self.inst_order[inst] = self.inst_order_num + offset
             return
 
         for op in inst.get_inputs():
@@ -33,23 +37,27 @@ class DFTPass(IRPass):
             if target.parent != inst.parent or target.fence_id != inst.fence_id:
                 # don't reorder across basic block or fence boundaries
                 continue
-            self._process_instruction_r(bb, target)
+            self._process_instruction_r(bb, target, offset)
 
-        bb.instructions.append(inst)
+        # bb.instructions.append(inst)
+        self.inst_order_num += 1
+        self.inst_order[inst] = self.inst_order_num + offset
 
     def _process_basic_block(self, bb: IRBasicBlock) -> None:
         self.ctx.append_basic_block(bb)
 
-        instructions = bb.instructions
-        bb.instructions = []
-
-        for inst in instructions:
+        for inst in bb.instructions:
             inst.fence_id = self.fence_id
             if inst.volatile:
                 self.fence_id += 1
 
-        for inst in instructions:
+        self.inst_order = {}
+        self.inst_order_num = 0
+        for inst in bb.instructions:
             self._process_instruction_r(bb, inst)
+
+        bb.instructions.sort(key=lambda x: self.inst_order[x])
+        pass
 
     def _run_pass(self, ctx: IRFunction) -> None:
         self.ctx = ctx
