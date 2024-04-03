ce220b5c5306e14a5e95224dbad7f3cd380c5362
Charles Cooper <cooper.charles.m@gmail.com>
2024-04-03 15:21:23 +0000

    fix a defaults test

--- tests/unit/compiler/test_default_settings.py
+++ tests/unit/compiler/test_default_settings.py
@@ -15,10 +15,10 @@ def test_default_opt_level():
     assert OptimizationLevel.default() == OptimizationLevel.GAS
 
 
-def test_codegen_opt_level():
-    assert core._opt_gas() is True  # check the default
-    assert core._opt_none() is False
-    assert core._opt_codesize() is False
+def test_codegen_opt_level(optimize):
+    assert core._opt_gas() == (optimize == OptimizationLevel.GAS)
+    assert core._opt_none() == (optimize == OptimizationLevel.NONE)
+    assert core._opt_codesize() == (optimize == OptimizationLevel.CODESIZE)
 
 
 def test_debug_mode(pytestconfig):
