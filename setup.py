dc62e7a63e6857ed73480f16de62aa00fd00c96b
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-04-04 16:33:12 +0200

    chore[test]: update hexbytes version and tests (#3904)
    update some tests after they were broken by an upstream update.
    
    `eth-account` got bumped from 0.11.0 to 0.12.0, which in turn bumped
    `hexbytes` to 1.2.0. `hexbytes` 1.0.0 had a (long overdue) breaking
    change, which was to change the `.hex()` method to not prepend "0x".
    
    this commit updates our tests accordingly, and does a bit of refactoring
    in some places to use `bytes` directly instead of `HexBytes` (including
    that the `keccak` fixture is changed to use vyper.utils.keccak256
    instead of the one provided by web3, which returns `HexBytes`). it also
    cleans up some manual method id calculations with the utility functions.
    
    ---------
    
    Co-authored-by: Charles Cooper <cooper.charles.m@gmail.com>

--- setup.py
+++ setup.py
@@ -21,8 +21,7 @@ extras_require = {
         "hypothesis[lark]>=6.0,<7.0",
         "eth-stdlib==0.2.7",
         "setuptools",
-        "hexbytes<1.0",
-        "typing_extensions",  # we can remove this once dependencies upgrade to eth-rlp>=2.0
+        "hexbytes>=1.2",
     ],
     "lint": [
         "black==23.12.0",
