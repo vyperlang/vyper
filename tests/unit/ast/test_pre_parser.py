0a481c37ebac713a4abd7b5e36de4334f96fb3e8
tserg <8017125+tserg@users.noreply.github.com>
2024-04-08 16:17:30 +0800

    add test

--- tests/unit/ast/test_pre_parser.py
+++ tests/unit/ast/test_pre_parser.py
@@ -1,5 +1,6 @@
 import pytest
 
+from vyper import compile_code
 from vyper.ast.pre_parser import pre_parse, validate_version_pragma
 from vyper.compiler.phases import CompilerData
 from vyper.compiler.settings import OptimizationLevel, Settings
@@ -45,14 +46,14 @@ invalid_versions = [
 @pytest.mark.parametrize("file_version", valid_versions)
 def test_valid_version_pragma(file_version, mock_version):
     mock_version(COMPILER_VERSION)
-    validate_version_pragma(f"{file_version}", (SRC_LINE))
+    validate_version_pragma(f"{file_version}", file_version, (SRC_LINE))
 
 
 @pytest.mark.parametrize("file_version", invalid_versions)
 def test_invalid_version_pragma(file_version, mock_version):
     mock_version(COMPILER_VERSION)
     with pytest.raises(VersionException):
-        validate_version_pragma(f"{file_version}", (SRC_LINE))
+        validate_version_pragma(f"{file_version}", file_version, (SRC_LINE))
 
 
 prerelease_valid_versions = [
@@ -82,14 +83,14 @@ prerelease_invalid_versions = [
 @pytest.mark.parametrize("file_version", prerelease_valid_versions)
 def test_prerelease_valid_version_pragma(file_version, mock_version):
     mock_version(PRERELEASE_COMPILER_VERSION)
-    validate_version_pragma(file_version, (SRC_LINE))
+    validate_version_pragma(file_version, file_version, (SRC_LINE))
 
 
 @pytest.mark.parametrize("file_version", prerelease_invalid_versions)
 def test_prerelease_invalid_version_pragma(file_version, mock_version):
     mock_version(PRERELEASE_COMPILER_VERSION)
     with pytest.raises(VersionException):
-        validate_version_pragma(file_version, (SRC_LINE))
+        validate_version_pragma(file_version, file_version, (SRC_LINE))
 
 
 pragma_examples = [
@@ -224,3 +225,37 @@ invalid_pragmas = [
 def test_invalid_pragma(code):
     with pytest.raises(StructureException):
         pre_parse(code)
+
+
+def test_version_conflict_with_imports(make_input_bundle, mock_version):
+    lib_version = "~=0.3.10"
+    contract_version = "0.4.0"
+
+    lib_pragma = f"#pragma version {lib_version}\n"
+    lib = f"""
+{lib_pragma}
+@external
+def foo():
+    pass
+    """
+
+    contract_pragma = f"#pragma version {contract_version}\n"
+    code = f"""
+{contract_pragma}
+import lib
+
+uses: lib
+
+@external
+def bar():
+    pass
+    """
+    input_bundle = make_input_bundle({"lib.vy": lib})
+
+    mock_version(contract_version)
+    with pytest.raises(VersionException) as excinfo:
+        compile_code(code, input_bundle=input_bundle)
+    annotation = excinfo.value.annotations[0]
+    assert annotation.lineno == 2
+    assert annotation.col_offset == 0
+    assert annotation.full_source_code == lib_pragma
