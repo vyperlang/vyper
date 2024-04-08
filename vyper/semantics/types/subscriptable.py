vyperlang/vyper

commit 56c4c9dbc09d6310bf132cfde3fdbe1431189a9b

refactor: reimplement AST folding (#3669)
this commit reimplements AST folding. fundamentally, it changes AST
folding from a mutating pass to be an annotation pass. this brings
several benefits:
- typechecking is easier, because folding does not have to reason at all
  about types. type checking happens on both the folded and unfolded
  nodes, so intermediate values are type-checked.
- correctness in general is easier, because the AST is not mutated.
  there is also some incidental performance benefit, although that is
  not necessarily the focus here.
- the vyper frontend is now nearly mutation-free. only the getter AST
  expansion pass remains.

note that we cannot push folding past the typechecking stage entirely,
because some type checking operations depend on having folded values
(e.g., `range()` expressions, or type expressions with integer
parameters).

the approach taken in this commit is to change constant folding to be
annotating, rather than mutating. this way, type-checking can operate on
the original AST (and check for the folded values where needed).
intermediate values are also type-checked, so expressions like
`x: uint128 = 2**128 + 1 - 1` are caught by the typechecker.

summary of changes:
- `evaluate()` is renamed to `_try_fold()`. a new utility function
  called `get_folded_value()` caches folded values and is threaded
  through the codebase.
- `pre_typecheck` is added, which extracts `constant` variables and runs
  `get_folded_value()` on all nodes.
- a new `Modifiability` enum replaces the old (confusing) `is_constant`
  and `is_immutable` attributes on ExprInfo.
- `ExprInfo.is_transient` is removed, and handled by adding `TRANSIENT`
  to the `DataLocation` enum.
- the old `check_literal` and `check_kwargable` utility functions are
  replaced with a more general (and more correct) `check_modifiability`
  function
- several utility functions (ex. `_validate_numeric_bounds()`) related

vyper/semantics/types/subscriptable.py
@@ -288,6 +288,8 @@ class DArrayT(_SequenceT):
             raise StructureException(err_msg, node.slice)
 
         length_node = node.slice.value.elements[1]
+        if length_node.has_folded_value:
+            length_node = length_node.get_folded_value()
 
         if not isinstance(length_node, vy_ast.Int):
             raise StructureException(err_msg, length_node)

commit d9544a1bcd0e613dd459d18af03cef2425313fe4

fix[lang]: fix array index checks when the subscript is folded
this commit fixes a regression introduced in 56c4c9dbc. prior to
56c4c9dbc, the folded subscript would be checked for OOB access, but
after 56c4c9dbc, expressions like `foo[0 - 1]` can slip past the
typechecker (getting demoted to a runtime check).

vyper/semantics/types/subscriptable.py
@@ -128,6 +128,8 @@ class _SequenceT(_SubscriptableT):
         # TODO break this cycle
         from vyper.semantics.analysis.utils import validate_expected_type
 
+        node = node.get_folded_value()
+
         if isinstance(node, vy_ast.Int):
             if node.value < 0:
                 raise ArrayIndexException("Vyper does not support negative indexing", node)