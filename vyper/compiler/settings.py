ecde9ca336a7989c2d735a59ab4324583b56202e
Charles Cooper <cooper.charles.m@gmail.com>
2024-04-09 16:15:55 -0400

    fix set_global_settings default evm version

--- vyper/compiler/settings.py
+++ vyper/compiler/settings.py
@@ -57,7 +57,7 @@ def get_global_settings() -> Optional[Settings]:
 def set_global_settings(new_settings: Optional[Settings]) -> None:
     assert isinstance(new_settings, Settings) or new_settings is None
     # TODO evil circular import
-    from vyper.evm.opcodes import DEFAULT_EVM_VERSION, EVM_VERSIONS, set_global_evm_version
+    from vyper.evm.opcodes import EVM_VERSIONS, set_global_evm_version
 
     global _settings
     _settings = new_settings
@@ -65,10 +65,9 @@ def set_global_settings(new_settings: Optional[Settings]) -> None:
     # set the global evm version so that version_check picks it up.
     # this is a bit spooky, but it's generally always what we want
     # when set_global_settings is called.
-    evm_version = DEFAULT_EVM_VERSION
     if new_settings is not None and new_settings.evm_version is not None:
         evm_version = new_settings.evm_version
-    set_global_evm_version(EVM_VERSIONS[evm_version])
+        set_global_evm_version(EVM_VERSIONS[evm_version])
 
 
 # could maybe refactor this, but it is easier for now than threading settings
