e4f1c2479ee8a82777aecb929945f641750b1536
Charles Cooper <cooper.charles.m@gmail.com>
2024-04-10 11:57:36 -0400

    refactor[codegen]: make settings into a global object (#3929)
    this commit adds a global settings object, unifying various functions
    which modify global settings:
    anchor_opt_level, anchor_evm_version, _set_debug

--- tests/unit/cli/vyper_json/test_get_settings.py
+++ tests/unit/cli/vyper_json/test_get_settings.py
@@ -10,7 +10,7 @@ def test_unknown_evm():
 
 
 @pytest.mark.parametrize(
-    "evm_version",
+    "evm_version_str",
     [
         "homestead",
         "tangerineWhistle",
@@ -22,11 +22,11 @@ def test_unknown_evm():
         "berlin",
     ],
 )
-def test_early_evm(evm_version):
+def test_early_evm(evm_version_str):
     with pytest.raises(JSONError):
-        get_evm_version({"settings": {"evmVersion": evm_version}})
+        get_evm_version({"settings": {"evmVersion": evm_version_str}})
 
 
-@pytest.mark.parametrize("evm_version", ["london", "paris", "shanghai", "cancun"])
-def test_valid_evm(evm_version):
-    assert evm_version == get_evm_version({"settings": {"evmVersion": evm_version}})
+@pytest.mark.parametrize("evm_version_str", ["london", "paris", "shanghai", "cancun"])
+def test_valid_evm(evm_version_str):
+    assert evm_version_str == get_evm_version({"settings": {"evmVersion": evm_version_str}})
