5ae8edea5e35a2fb56a25f30f8c6c350a15758b0
Harry Kalogirou <harkal@nlogn.eu>
2024-04-11 00:29:48 +0300

    Refactor Mem2Stack pass and add variable propagation

--- vyper/venom/passes/mem2stack.py
+++ vyper/venom/passes/mem2stack.py
@@ -9,15 +9,20 @@ from vyper.venom.passes.dft import DFTPass
 
 class Mem2Stack(IRPass):
     """ """
-
+    ctx: IRFunction
     dom: DominatorTree
     defs: dict[IRVariable, OrderedSet[IRBasicBlock]]
+    dfg: DFG
 
     def _run_pass(self, ctx: IRFunction, entry: IRBasicBlock, dfg: DFG) -> int:
         self.ctx = ctx
+        self.dfg = dfg
 
         calculate_cfg(ctx)
         self.dom = DominatorTree.build_dominator_tree(ctx, entry)
+        #self._propagate_variables()
+        dfg = DFG.build_dfg(ctx)
+        self.dfg = dfg
 
         calculate_liveness(ctx)
 
@@ -33,13 +38,30 @@ class Mem2Stack(IRPass):
 
         return 0
 
+    def _propagate_variables(self):
+        for bb in self.dom.dfs_walk:
+            for inst in bb.instructions:
+                if inst.opcode == "store":
+                    uses = self.dfg.get_uses(inst.output)
+                    remove_inst = True
+                    for usage_inst in uses:
+                        if usage_inst.opcode == "phi":
+                            remove_inst = False
+                            continue
+                        for i, op in enumerate(usage_inst.operands):
+                            if op == inst.output:
+                                usage_inst.operands[i] = inst.operands[0]
+                    if remove_inst:
+                        inst.opcode = "nop"
+                        inst.operands = []
+
     def _process_alloca_var(self, dfg: DFG, var: IRVariable, alloca_inst: IRInstruction):
         uses = dfg.get_uses(var)
         if all([inst.opcode == "mload" for inst in uses]):
             return
         elif all([inst.opcode == "mstore" for inst in uses]):
             return
-        elif all([inst.opcode == "mstore" or inst.opcode == "mload" for inst in uses]):
+        elif all([inst.opcode in ["mstore", "mload", "return"] for inst in uses]):
             var_name = f"addr{var.name}_{self.var_name_count}"
             self.var_name_count += 1
             # print(f"Processing alloca var {var_name}")
@@ -52,6 +74,11 @@ class Mem2Stack(IRPass):
                 elif inst.opcode == "mload":
                     inst.opcode = "store"
                     inst.operands = [IRVariable(var_name)]
+                elif inst.opcode == "return":
+                    bb = inst.parent
+                    new_var = self.ctx.get_next_variable()
+                    bb.insert_instruction(IRInstruction("mstore", [IRVariable(var_name), inst.operands[1]], new_var), -1)
+                    inst.operands[1] = new_var
 
     def _compute_stores(self):
         self.defs = {}
