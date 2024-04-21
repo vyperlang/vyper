ea40cde36c94648f52d70d2f6d0bd6b108fb2606
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-04-21 11:51:30 +0200

    not sure this will work...
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- tests/functional/builtins/codegen/test_blobhash.py
+++ tests/functional/builtins/codegen/test_blobhash.py
@@ -42,3 +42,44 @@ def test_blobhash_success(good_code):
     assert compiler.compile_code(good_code) is not None
     assembly = compiler.compile_code(good_code, output_formats=["asm"])["asm"].split(" ")
     assert "BLOBHASH" in assembly
+
+
+@pytest.mark.requires_evm_version("cancun")
+def test_get_blobhashes(get_contract_with_gas_estimation, w3):
+    code = """
+@external
+@view
+def get_blobhashes() -> bytes32[6]:
+    return [blobhash(0), blobhash(1), blobhash(2), blobhash(3), blobhash(4), blobhash(5)]
+"""
+    c = get_contract_with_gas_estimation(code)
+
+    random_account = w3.eth.account.from_key(f"0x{'00' * 31}01")
+
+    text = b"Long live the BLOBs!"
+    # BLOBs contain 4096 32-byte field elements.
+    # (32 * 4096) / 2 ** 10 = 128.0 -> Each BLOB can store up to 128kb.
+    blob_data = text.rjust(32 * 4096)
+
+    tx = {
+        "type": 3,
+        "chainId": 1337,
+        "from": random_account.address,
+        "to": "0xBa5EdBA5eDBA5EdbA5edbA5EDBA5eDbA5edBa5Ed",  # random address
+        "value": 0,
+        "gas": 21000,
+        "maxFeePerGas": 10**10,
+        "maxPriorityFeePerGas": 10**10,
+        "maxFeePerBlobGas": 10**10,
+        "nonce": w3.eth.get_transaction_count(random_account.address),
+    }
+
+    signed = random_account.sign_transaction(tx, blobs=[blob_data] * 6)
+    tx_hash = w3.eth.send_raw_transaction(signed.rawTransaction)
+    tx = w3.eth.get_transaction(tx_hash)
+
+    # the attribute `blobVersionedHashes` are currently only available at the tx-level:
+    # https://github.com/ethereum/web3.py/blob/332ff99ae5dbe0209254619801c4848e528f7851/web3/_utils/method_formatters.py#L233
+    expected_blob_versioned_hash = tx["blobVersionedHashes"]
+
+    assert c.get_blobhashes() == [expected_blob_versioned_hash for _ in range(6)]
