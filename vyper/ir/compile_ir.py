d246f16298a27f217b3eb896fd989a4b78bf2b79
Harry Kalogirou <harkal@nlogn.eu>
2024-04-30 11:47:15 +0300

    fix symbol replacement bug in _merge_jumpdests function

--- vyper/ir/compile_ir.py
+++ vyper/ir/compile_ir.py
@@ -892,10 +892,14 @@ def _merge_jumpdests(assembly):
                 # replace all instances of _sym_x with _sym_y
                 # (except for _sym_x JUMPDEST - don't want duplicate labels)
                 new_symbol = assembly[i + 2]
-                for j in range(len(assembly)):
-                    if assembly[j] == current_symbol and i != j:
-                        assembly[j] = new_symbol
-                        changed = True
+                if new_symbol == current_symbol:
+                    del assembly[i : i + 2]
+                    changed = True
+                else:
+                    for j in range(len(assembly)):
+                        if assembly[j] == current_symbol and i != j:
+                            assembly[j] = new_symbol
+                            changed = True
             elif is_symbol(assembly[i + 2]) and assembly[i + 3] == "JUMP":
                 # _sym_x JUMPDEST _sym_y JUMP
                 # replace all instances of _sym_x with _sym_y
