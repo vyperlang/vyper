c18f6891d326b9de2e0d910a9883c0ec078888e6
Charles Cooper <cooper.charles.m@gmail.com>
2024-05-07 17:06:17 -0400

    fix solc json: experimentalCodegen in output bundle

--- vyper/compiler/output_bundle.py
+++ vyper/compiler/output_bundle.py
@@ -180,6 +180,9 @@ class SolcJSONWriter(OutputBundleWriter):
             s = settings.as_dict()
             if "evm_version" in s:
                 s["evmVersion"] = s.pop("evm_version")
+            if "experimental_codegen" in s:
+                s["experimentalCodegen"] = s.pop("experimental_codegen")
+
             self._output["settings"].update(s)
 
     def write_integrity(self, integrity_sum: str):
