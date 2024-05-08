8f7a4fc04c255a869d7030544d0e4f1b05b2c8cf
tserg <8017125+tserg@users.noreply.github.com>
2024-05-08 22:51:12 +0800

    add source map for deployable

--- vyper/compiler/__init__.py
+++ vyper/compiler/__init__.py
@@ -35,6 +35,8 @@ OUTPUT_FORMATS = {
     "asm": output.build_asm_output,
     "source_map": output.build_source_map_output,
     "source_map_full": output.build_source_map_output,
+    "source_map_runtime": output.build_source_map_runtime_output,
+    "source_map_full_runtime": output.build_source_map_runtime_output,
     # requires bytecode
     "bytecode": output.build_bytecode_output,
     "bytecode_runtime": output.build_bytecode_runtime_output,
