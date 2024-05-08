75c75c5631222dd1b98c23f8cfeedc080e47a9e3
Charles Cooper <cooper.charles.m@gmail.com>
2024-05-08 08:37:15 -0400

    feat[tool]: archive format (#3891)
    this commit adds several output formats to aid with build
    reproducibility and source code verification:
    - `-f archive`
    - `-f solc_json`
    - `-f integrity`
    - `--base64`
    
    `-f archive` creates a "vyper archive" using the zipfile format. it
    emits the metadata associated with the build (settings, search path,
    compiler version, integrity hash) in the `MANIFEST/` folder inside the
    archive. `--base64` is only usable with `-f archive` and produces a
    base64-encoded archive (which is easier to copy-paste).
    
    both the base64 and binary versions of the archive round-trip. that is,
    if you provide an archive directly to the vyper compiler (e.g.
    `vyper contract.zip` or `vyper contract.zip.b64`), it should produce
    exactly the same output as running `vyper contract.vy` on the local
    machine with the same settings+environment that produced the archive.
    
    `-f solc_json` is for compatibility with standard json that a lot of
    verifiers and tooling currently uses. it uses the same "output bundle"
    machinery as `-f archive`, but spits out in "standard-json" format
    (consumable by `--standard-json`).
    
    both of these use an `OutputBundle` abstraction, which abstracts
    collecting the inputs to the build. these include
    - settings (whatever is on the Settings object)
    - search path
    - compiler version
    - integrity hash
    
    importantly, `OutputBundle` recovers and anonymizes search paths used
    during compilation. this is done to minimize leaking of user information
    in archives. however, it comes with a tradeoff -- because of how the
    anonymization works, it is possible to have a build where search paths
    are not recoverable (specifically, if a module "escapes" its package
    with too many `..`, the resulting anonymized path will be bogus).
    several methods were tried to prevent this, but in the end this method
    was chosen, which prioritizes minimizing leakage over handling edge
    cases.
    
    `-f integrity` produces an "integrity hash", which is basically a
    checksum over the source file inputs. it is intended to let consumers
    of the compiler know when any input in the dependency tree has changed
    and recompilation is necessary. it is conservative by design; it works
    by recursively hashing source text as opposed to AST or any other
    semantic representation of source code. it can also be used by tooling
    as a check to determine if the source tree in an archive is the same as
    expected. this would likely be an additional check in addition to
    bytecode comparison, since there could differences in source code (e.g.
    comments) which affect the integrity hash but not the bytecode.
    
    the integrity hash computation currently depends on all frontend
    analysis to complete. in theory, since it only depends on source code,
    it could be refactored into another preliminary pass in the compiler,
    whose sole job is to resolve (and hash) imports. however, it would be
    additional maintenance work. we could revisit if the performance of this
    method becomes reported as an issue (note: current numbers are that this
    method operates at roughly 2500 lloc per second).
    
    currently, there are two places where build reproducibility might fail -
    in checking the integrity hash of an archive or during archive
    construction itself (if there is a compile-time failure, this could
    happen for example if the user is trying to send a reproduction of an
    error). it was decided that the most user-friendly thing to do is to
    emit a warning in these cases, rather than adding additional compilation
    flags that control whether to bail out or not.
    
    the tentative canonical suffix for vyper archive (the zipfile version)
    is `.vyz`, although this is subject to change (several alternatives were
    also considered, including `.den` - as in "a den of vypers"!).

--- vyper/compiler/settings.py
+++ vyper/compiler/settings.py
@@ -1,4 +1,5 @@
 import contextlib
+import dataclasses
 import os
 from dataclasses import dataclass
 from enum import Enum
@@ -17,6 +18,7 @@ else:
     VYPER_TRACEBACK_LIMIT = None
 
 
+# TODO: use StringEnum (requires refactoring vyper.utils to avoid import cycle)
 class OptimizationLevel(Enum):
     NONE = 1
     GAS = 2
@@ -37,6 +39,9 @@ class OptimizationLevel(Enum):
     def default(cls):
         return cls.GAS
 
+    def __str__(self):
+        return self._name_.lower()
+
 
 DEFAULT_ENABLE_DECIMALS = False
 
@@ -50,11 +55,79 @@ class Settings:
     debug: Optional[bool] = None
     enable_decimals: Optional[bool] = None
 
+    def __post_init__(self):
+        # sanity check inputs
+        if self.optimize is not None:
+            assert isinstance(self.optimize, OptimizationLevel)
+        if self.experimental_codegen is not None:
+            assert isinstance(self.experimental_codegen, bool)
+        if self.debug is not None:
+            assert isinstance(self.debug, bool)
+        if self.enable_decimals is not None:
+            assert isinstance(self.enable_decimals, bool)
+
     # CMC 2024-04-10 consider hiding the `enable_decimals` member altogether
     def get_enable_decimals(self) -> bool:
         if self.enable_decimals is None:
             return DEFAULT_ENABLE_DECIMALS
         return self.enable_decimals
+
+    def as_cli(self):
+        ret = []
+        if self.optimize is not None:
+            ret.append(" --optimize " + str(self.optimize))
+        if self.experimental_codegen is True:
+            ret.append(" --experimental-codegen")
+        if self.evm_version is not None:
+            ret.append(" --evm-version " + self.evm_version)
+        if self.debug is True:
+            ret.append(" --debug")
+        if self.enable_decimals is True:
+            ret.append(" --enable-decimals")
+
+        return "".join(ret)
+
+    def as_dict(self):
+        ret = dataclasses.asdict(self)
+        # compiler_version is not a compiler input, it can only come from
+        # source code pragma.
+        ret.pop("compiler_version", None)
+        ret = {k: v for (k, v) in ret.items() if v is not None}
+        if "optimize" in ret:
+            ret["optimize"] = str(ret["optimize"])
+        return ret
+
+    @classmethod
+    def from_dict(cls, data):
+        data = data.copy()
+        if "optimize" in data:
+            data["optimize"] = OptimizationLevel.from_string(data["optimize"])
+        return cls(**data)
+
+
+def merge_settings(
+    one: Settings, two: Settings, lhs_source="compiler settings", rhs_source="source pragma"
+) -> Settings:
+    def _merge_one(lhs, rhs, helpstr):
+        if lhs is not None and rhs is not None and lhs != rhs:
+            # aesthetics, conjugate the verbs per english rules
+            s1 = "" if lhs_source.endswith("s") else "s"
+            s2 = "" if rhs_source.endswith("s") else "s"
+            raise ValueError(
+                f"settings conflict!\n\n  {lhs_source}: {one}\n  {rhs_source}: {two}\n\n"
+                f"({lhs_source} indicate{s1} {helpstr} {lhs}, but {rhs_source} indicate{s2} {rhs}.)"
+            )
+        return lhs if rhs is None else rhs
+
+    ret = Settings()
+    ret.evm_version = _merge_one(one.evm_version, two.evm_version, "evm version")
+    ret.optimize = _merge_one(one.optimize, two.optimize, "optimize")
+    ret.experimental_codegen = _merge_one(
+        one.experimental_codegen, two.experimental_codegen, "experimental codegen"
+    )
+    ret.enable_decimals = _merge_one(one.enable_decimals, two.enable_decimals, "enable-decimals")
+
+    return ret
 
 
 # CMC 2024-04-10 do we need it to be Optional?
