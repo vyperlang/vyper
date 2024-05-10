vyperlang/vyper

commit ace3789e4b9e23079579695070d52e7dc8c26aad

refactor[venom]: introduce `IRContext` and `IRAnalysisCache` (#3983)
this is a refactoring commit. it introduces `IRContext` and
`IRAnalysisCache`. `IRContext` is a global compilation context for a
compilation run, it includes global information like the deploy- and
runtime-code. meanwhile, `IRFunction` now corresponds directly to
"internal" functions passed from the frontend.

this commit refactors the `IRAnalysis` and `IRPass` classes by adding
`IRAnalysisCache` (kind of a manager for analyses) which determines
which analyses are needed for a given pass.

additionally, there is a compilation-time performance improvement which
is due to optimizations running over smaller sections of code.

this commit introduces some performance regressions due to
`remove_unused_variables` running in one-shot instead of in a loop now.
these performance regressions will be investigated and tuned in a
follow-up PR.

vyper/venom/context.py
@@ -1,0 +1,67 @@
+from typing import Optional
+
+from vyper.venom.basicblock import IRInstruction, IRLabel, IROperand
+from vyper.venom.function import IRFunction
+
+
+class IRContext:
+    functions: dict[IRLabel, IRFunction]
+    ctor_mem_size: Optional[int]
+    immutables_len: Optional[int]
+    data_segment: list[IRInstruction]
+    last_label: int
+
+    def __init__(self) -> None:
+        self.functions = {}
+        self.ctor_mem_size = None
+        self.immutables_len = None
+        self.data_segment = []
+        self.last_label = 0
+
+    def add_function(self, fn: IRFunction) -> None:
+        fn.ctx = self
+        self.functions[fn.name] = fn
+
+    def create_function(self, name: str) -> IRFunction:
+        label = IRLabel(name, True)
+        fn = IRFunction(label, self)
+        self.add_function(fn)
+        return fn
+
+    def get_function(self, name: IRLabel) -> IRFunction:
+        if name in self.functions:
+            return self.functions[name]
+        raise Exception(f"Function {name} not found in context")
+
+    def get_next_label(self, suffix: str = "") -> IRLabel:
+        if suffix != "":
+            suffix = f"_{suffix}"
+        self.last_label += 1
+        return IRLabel(f"{self.last_label}{suffix}")
+
+    def chain_basic_blocks(self) -> None:
+        """
+        Chain basic blocks together. This is necessary for the IR to be valid, and is done after
+        the IR is generated.
+        """
+        for fn in self.functions.values():
+            fn.chain_basic_blocks()
+
+    def append_data(self, opcode: str, args: list[IROperand]) -> None:
+        """
+        Append data
+        """
+        self.data_segment.append(IRInstruction(opcode, args))  # type: ignore
+
+    def __repr__(self) -> str:
+        s = ["IRContext:"]
+        for fn in self.functions.values():
+            s.append(fn.__repr__())
+            s.append("\n")
+
+        if len(self.data_segment) > 0:
+            s += "\nData segment:\n"
+            for inst in self.data_segment:
+                s += f"{inst}\n"
+
+        return "\n".join(s)

vyper/venom/function.py
@@ -2,16 +2,7 @@ from typing import Iterator, Optional
 
 from vyper.codegen.ir_node import IRnode
 from vyper.utils import OrderedSet
-from vyper.venom.basicblock import (
-    CFG_ALTERING_INSTRUCTIONS,
-    IRBasicBlock,
-    IRInstruction,
-    IRLabel,
-    IROperand,
-    IRVariable,
-)
-
-GLOBAL_LABEL = IRLabel("__global")
+from vyper.venom.basicblock import CFG_ALTERING_INSTRUCTIONS, IRBasicBlock, IRLabel, IRVariable
 
 
 class IRFunction:
@@ -20,12 +11,9 @@ class IRFunction:
     """
 
     name: IRLabel  # symbol name
-    entry_points: list[IRLabel]  # entry points
+    ctx: "IRContext"  # type: ignore # noqa: F821
     args: list
-    ctor_mem_size: Optional[int]
-    immutables_len: Optional[int]
     basic_blocks: list[IRBasicBlock]
-    data_segment: list[IRInstruction]
     last_label: int
     last_variable: int
 
@@ -34,37 +22,23 @@ class IRFunction:
     _error_msg_stack: list[str]
     _bb_index: dict[str, int]
 
-    def __init__(self, name: IRLabel = None) -> None:
-        if name is None:
-            name = GLOBAL_LABEL
+    def __init__(self, name: IRLabel, ctx: "IRContext" = None) -> None:  # type: ignore # noqa: F821
+        self.ctx = ctx
         self.name = name
-        self.entry_points = []
         self.args = []
-        self.ctor_mem_size = None
-        self.immutables_len = None
         self.basic_blocks = []
-        self.data_segment = []
-        self.last_label = 0
+
         self.last_variable = 0
 
         self._ast_source_stack = []
         self._error_msg_stack = []
         self._bb_index = {}
 
-        self.add_entry_point(name)
         self.append_basic_block(IRBasicBlock(name, self))
 
-    def add_entry_point(self, label: IRLabel) -> None:
-        """
-        Add entry point.
-        """
-        self.entry_points.append(label)
-
-    def remove_entry_point(self, label: IRLabel) -> None:
-        """
-        Remove entry point.
-        """
-        self.entry_points.remove(label)
+    @property
+    def entry(self) -> IRBasicBlock:
+        return self.basic_blocks[0]
 
     def append_basic_block(self, bb: IRBasicBlock) -> IRBasicBlock:
         """
@@ -86,7 +60,9 @@ class IRFunction:
         # do a reindex
         self._bb_index = dict((bb.label.name, ix) for ix, bb in enumerate(self.basic_blocks))
         # sanity check - no duplicate labels
-        assert len(self._bb_index) == len(self.basic_blocks)
+        assert len(self._bb_index) == len(
+            self.basic_blocks
+        ), f"Duplicate labels in function '{self.name}' {self._bb_index} {self.basic_blocks}"
         return self._bb_index[label]
 
     def get_basic_block(self, label: Optional[str] = None) -> IRBasicBlock:
@@ -121,12 +97,6 @@ class IRFunction:
         Get basic blocks that point to the given basic block
         """
         return [bb for bb in self.basic_blocks if basic_block.label in bb.cfg_in]
-
-    def get_next_label(self, suffix: str = "") -> IRLabel:
-        if suffix != "":
-            suffix = f"_{suffix}"
-        self.last_label += 1
-        return IRLabel(f"{self.last_label}{suffix}")
 
     def get_next_variable(self) -> IRVariable:
         self.last_variable += 1
@@ -176,9 +146,7 @@ class IRFunction:
             bb.reachable = OrderedSet()
             bb.is_reachable = False
 
-        for entry in self.entry_points:
-            entry_bb = self.get_basic_block(entry.value)
-            self._compute_reachability_from(entry_bb)
+        self._compute_reachability_from(self.entry)
 
     def _compute_reachability_from(self, bb: IRBasicBlock) -> None:
         """
@@ -188,17 +156,11 @@ class IRFunction:
             return
         bb.is_reachable = True
         for inst in bb.instructions:
-            if inst.opcode in CFG_ALTERING_INSTRUCTIONS or inst.opcode == "invoke":
+            if inst.opcode in CFG_ALTERING_INSTRUCTIONS:
                 for op in inst.get_label_operands():
                     out_bb = self.get_basic_block(op.value)
                     bb.reachable.add(out_bb)
                     self._compute_reachability_from(out_bb)
-
-    def append_data(self, opcode: str, args: list[IROperand]) -> None:
-        """
-        Append data
-        """
-        self.data_segment.append(IRInstruction(opcode, args))  # type: ignore
 
     @property
     def normalized(self) -> bool:
@@ -243,10 +205,28 @@ class IRFunction:
     def error_msg(self) -> Optional[str]:
         return self._error_msg_stack[-1] if len(self._error_msg_stack) > 0 else None
 
+    def chain_basic_blocks(self) -> None:
+        """
+        Chain basic blocks together. If a basic block is not terminated, jump to the next one.
+        Otherwise, append a stop instruction. This is necessary for the IR to be valid, and is
+        done after the IR is generated.
+        """
+        for i, bb in enumerate(self.basic_blocks):
+            if not bb.is_terminated:
+                if len(self.basic_blocks) - 1 > i:
+                    # TODO: revisit this. When contructor calls internal functions they
+                    # are linked to the last ctor block. Should separate them before this
+                    # so we don't have to handle this here
+                    if self.basic_blocks[i + 1].label.value.startswith("internal"):
+                        bb.append_instruction("stop")
+                    else:
+                        bb.append_instruction("jmp", self.basic_blocks[i + 1].label)
+                else:
+                    bb.append_instruction("exit")
+
     def copy(self):
         new = IRFunction(self.name)
         new.basic_blocks = self.basic_blocks.copy()
-        new.data_segment = self.data_segment.copy()
         new.last_label = self.last_label
         new.last_variable = self.last_variable
         return new
@@ -281,8 +261,4 @@ class IRFunction:
         str = f"IRFunction: {self.name}\n"
         for bb in self.basic_blocks:
             str += f"{bb}\n"
-        if len(self.data_segment) > 0:
-            str += "Data segment:\n"
-            for inst in self.data_segment:
-                str += f"{inst}\n"
         return str.strip()

tests/unit/compiler/venom/test_convert_basicblock_simple.py
@@ -8,7 +8,9 @@ def test_simple():
     venom = ir_node_to_venom(ir_node)
     assert venom is not None
 
-    bb = venom.basic_blocks[0]
+    fn = list(venom.functions.values())[0]
+
+    bb = fn.entry
     assert bb.instructions[0].opcode == "calldatasize"
     assert bb.instructions[1].opcode == "calldatacopy"
 

tests/unit/compiler/venom/test_dominator_tree.py
@@ -2,18 +2,19 @@ from typing import Optional
 
 from vyper.exceptions import CompilerPanic
 from vyper.utils import OrderedSet
-from vyper.venom.analysis import calculate_cfg
+from vyper.venom.analysis.analysis import IRAnalysesCache
+from vyper.venom.analysis.dominators import DominatorTreeAnalysis
 from vyper.venom.basicblock import IRBasicBlock, IRInstruction, IRLabel, IRLiteral, IRVariable
-from vyper.venom.dominators import DominatorTree
+from vyper.venom.context import IRContext
 from vyper.venom.function import IRFunction
 from vyper.venom.passes.make_ssa import MakeSSA
 
 
 def _add_bb(
-    ctx: IRFunction, label: IRLabel, cfg_outs: [IRLabel], bb: Optional[IRBasicBlock] = None
+    fn: IRFunction, label: IRLabel, cfg_outs: list[IRLabel], bb: Optional[IRBasicBlock] = None
 ) -> IRBasicBlock:
-    bb = bb if bb is not None else IRBasicBlock(label, ctx)
-    ctx.append_basic_block(bb)
+    bb = bb if bb is not None else IRBasicBlock(label, fn)
+    fn.append_basic_block(bb)
     cfg_outs_len = len(cfg_outs)
     if cfg_outs_len == 0:
         bb.append_instruction("stop")
@@ -29,27 +30,27 @@ def _add_bb(
 def _make_test_ctx():
     lab = [IRLabel(str(i)) for i in range(0, 9)]
 
-    ctx = IRFunction(lab[1])
+    ctx = IRContext()
+    fn = ctx.create_function(lab[1].value)
 
-    bb1 = ctx.basic_blocks[0]
-    bb1.append_instruction("jmp", lab[2])
+    fn.entry.append_instruction("jmp", lab[2])
 
-    _add_bb(ctx, lab[7], [])
-    _add_bb(ctx, lab[6], [lab[7], lab[2]])
-    _add_bb(ctx, lab[5], [lab[6], lab[3]])
-    _add_bb(ctx, lab[4], [lab[6]])
-    _add_bb(ctx, lab[3], [lab[5]])
-    _add_bb(ctx, lab[2], [lab[3], lab[4]])
+    _add_bb(fn, lab[7], [])
+    _add_bb(fn, lab[6], [lab[7], lab[2]])
+    _add_bb(fn, lab[5], [lab[6], lab[3]])
+    _add_bb(fn, lab[4], [lab[6]])
+    _add_bb(fn, lab[3], [lab[5]])
+    _add_bb(fn, lab[2], [lab[3], lab[4]])
 
-    return ctx
+    return fn
 
 
 def test_deminator_frontier_calculation():
-    ctx = _make_test_ctx()
-    bb1, bb2, bb3, bb4, bb5, bb6, bb7 = [ctx.get_basic_block(str(i)) for i in range(1, 8)]
+    fn = _make_test_ctx()
+    bb1, bb2, bb3, bb4, bb5, bb6, bb7 = [fn.get_basic_block(str(i)) for i in range(1, 8)]
 
-    calculate_cfg(ctx)
-    dom = DominatorTree.build_dominator_tree(ctx, bb1)
+    ac = IRAnalysesCache(fn)
+    dom = ac.request_analysis(DominatorTreeAnalysis)
     df = dom.dominator_frontiers
 
     assert len(df[bb1]) == 0, df[bb1]
@@ -62,12 +63,13 @@ def test_deminator_frontier_calculation():
 
 
 def test_phi_placement():
-    ctx = _make_test_ctx()
-    bb1, bb2, bb3, bb4, bb5, bb6, bb7 = [ctx.get_basic_block(str(i)) for i in range(1, 8)]
+    fn = _make_test_ctx()
+    bb1, bb2, bb3, bb4, bb5, bb6, bb7 = [fn.get_basic_block(str(i)) for i in range(1, 8)]
 
     x = IRVariable("%x")
     bb1.insert_instruction(IRInstruction("mload", [IRLiteral(0)], x), 0)
     bb2.insert_instruction(IRInstruction("add", [x, IRLiteral(1)], x), 0)
     bb7.insert_instruction(IRInstruction("mstore", [x, IRLiteral(0)]), 0)
 
-    MakeSSA().run_pass(ctx, bb1)
+    ac = IRAnalysesCache(fn)
+    MakeSSA(ac, fn).run_pass()

tests/unit/compiler/venom/test_duplicate_operands.py
@@ -1,6 +1,6 @@
 from vyper.compiler.settings import OptimizationLevel
 from vyper.venom import generate_assembly_experimental
-from vyper.venom.function import IRFunction
+from vyper.venom.context import IRContext
 
 
 def test_duplicate_operands():
@@ -15,8 +15,9 @@ def test_duplicate_operands():
 
     Should compile to: [PUSH1, 10, DUP1, DUP1, DUP1, ADD, MUL, STOP]
     """
-    ctx = IRFunction()
-    bb = ctx.get_basic_block()
+    ctx = IRContext()
+    fn = ctx.create_function("test")
+    bb = fn.get_basic_block()
     op = bb.append_instruction("store", 10)
     sum_ = bb.append_instruction("add", op, op)
     bb.append_instruction("mul", sum_, op)

tests/unit/compiler/venom/test_make_ssa.py
@@ -1,22 +1,23 @@
-from vyper.venom.analysis import calculate_cfg, calculate_liveness
+from vyper.venom.analysis.analysis import IRAnalysesCache
 from vyper.venom.basicblock import IRBasicBlock, IRLabel
-from vyper.venom.function import IRFunction
+from vyper.venom.context import IRContext
 from vyper.venom.passes.make_ssa import MakeSSA
 
 
 def test_phi_case():
-    ctx = IRFunction(IRLabel("_global"))
+    ctx = IRContext()
+    fn = ctx.create_function("_global")
 
-    bb = ctx.get_basic_block()
+    bb = fn.get_basic_block()
 
-    bb_cont = IRBasicBlock(IRLabel("condition"), ctx)
-    bb_then = IRBasicBlock(IRLabel("then"), ctx)
-    bb_else = IRBasicBlock(IRLabel("else"), ctx)
-    bb_if_exit = IRBasicBlock(IRLabel("if_exit"), ctx)
-    ctx.append_basic_block(bb_cont)
-    ctx.append_basic_block(bb_then)
-    ctx.append_basic_block(bb_else)
-    ctx.append_basic_block(bb_if_exit)
+    bb_cont = IRBasicBlock(IRLabel("condition"), fn)
+    bb_then = IRBasicBlock(IRLabel("then"), fn)
+    bb_else = IRBasicBlock(IRLabel("else"), fn)
+    bb_if_exit = IRBasicBlock(IRLabel("if_exit"), fn)
+    fn.append_basic_block(bb_cont)
+    fn.append_basic_block(bb_then)
+    fn.append_basic_block(bb_else)
+    fn.append_basic_block(bb_if_exit)
 
     v = bb.append_instruction("mload", 64)
     bb_cont.append_instruction("jnz", v, bb_then.label, bb_else.label)
@@ -30,11 +31,10 @@ def test_phi_case():
 
     bb.append_instruction("jmp", bb_cont.label)
 
-    calculate_cfg(ctx)
-    MakeSSA().run_pass(ctx, ctx.basic_blocks[0])
-    calculate_liveness(ctx)
+    ac = IRAnalysesCache(fn)
+    MakeSSA(ac, fn).run_pass()
 
-    condition_block = ctx.get_basic_block("condition")
+    condition_block = fn.get_basic_block("condition")
     assert len(condition_block.instructions) == 2
 
     phi_inst = condition_block.instructions[0]

tests/unit/compiler/venom/test_multi_entry_block.py
@@ -1,44 +1,48 @@
-from vyper.venom.analysis import calculate_cfg
-from vyper.venom.function import IRBasicBlock, IRFunction, IRLabel
+from vyper.venom.analysis.analysis import IRAnalysesCache
+from vyper.venom.analysis.cfg import CFGAnalysis
+from vyper.venom.context import IRContext
+from vyper.venom.function import IRBasicBlock, IRLabel
 from vyper.venom.passes.normalization import NormalizationPass
 
 
 def test_multi_entry_block_1():
-    ctx = IRFunction()
+    ctx = IRContext()
+    fn = ctx.create_function("__global")
 
     finish_label = IRLabel("finish")
     target_label = IRLabel("target")
-    block_1_label = IRLabel("block_1", ctx)
+    block_1_label = IRLabel("block_1", fn)
 
-    bb = ctx.get_basic_block()
+    bb = fn.get_basic_block()
     op = bb.append_instruction("store", 10)
     acc = bb.append_instruction("add", op, op)
     bb.append_instruction("jnz", acc, finish_label, block_1_label)
 
-    block_1 = IRBasicBlock(block_1_label, ctx)
-    ctx.append_basic_block(block_1)
+    block_1 = IRBasicBlock(block_1_label, fn)
+    fn.append_basic_block(block_1)
     acc = block_1.append_instruction("add", acc, op)
     op = block_1.append_instruction("store", 10)
     block_1.append_instruction("mstore", acc, op)
     block_1.append_instruction("jnz", acc, finish_label, target_label)
 
-    target_bb = IRBasicBlock(target_label, ctx)
-    ctx.append_basic_block(target_bb)
+    target_bb = IRBasicBlock(target_label, fn)
+    fn.append_basic_block(target_bb)
     target_bb.append_instruction("mul", acc, acc)
     target_bb.append_instruction("jmp", finish_label)
 
-    finish_bb = IRBasicBlock(finish_label, ctx)
-    ctx.append_basic_block(finish_bb)
+    finish_bb = IRBasicBlock(finish_label, fn)
+    fn.append_basic_block(finish_bb)
     finish_bb.append_instruction("stop")
 
-    calculate_cfg(ctx)
-    assert not ctx.normalized, "CFG should not be normalized"
+    ac = IRAnalysesCache(fn)
+    ac.request_analysis(CFGAnalysis)
+    assert not fn.normalized, "CFG should not be normalized"
 
-    NormalizationPass().run_pass(ctx)
+    NormalizationPass(ac, fn).run_pass()
 
-    assert ctx.normalized, "CFG should be normalized"
+    assert fn.normalized, "CFG should be normalized"
 
-    finish_bb = ctx.get_basic_block(finish_label.value)
+    finish_bb = fn.get_basic_block(finish_label.value)
     cfg_in = list(finish_bb.cfg_in)
     assert cfg_in[0].label.value == "target", "Should contain target"
     assert cfg_in[1].label.value == "__global_split_finish", "Should contain __global_split_finish"
@@ -47,50 +51,52 @@ def test_multi_entry_block_1():
 
 # more complicated one
 def test_multi_entry_block_2():
-    ctx = IRFunction()
+    ctx = IRContext()
+    fn = ctx.create_function("__global")
 
     finish_label = IRLabel("finish")
     target_label = IRLabel("target")
-    block_1_label = IRLabel("block_1", ctx)
-    block_2_label = IRLabel("block_2", ctx)
+    block_1_label = IRLabel("block_1", fn)
+    block_2_label = IRLabel("block_2", fn)
 
-    bb = ctx.get_basic_block()
+    bb = fn.get_basic_block()
     op = bb.append_instruction("store", 10)
     acc = bb.append_instruction("add", op, op)
     bb.append_instruction("jnz", acc, finish_label, block_1_label)
 
-    block_1 = IRBasicBlock(block_1_label, ctx)
-    ctx.append_basic_block(block_1)
+    block_1 = IRBasicBlock(block_1_label, fn)
+    fn.append_basic_block(block_1)
     acc = block_1.append_instruction("add", acc, op)
     op = block_1.append_instruction("store", 10)
     block_1.append_instruction("mstore", acc, op)
     block_1.append_instruction("jnz", acc, target_label, finish_label)
 
-    block_2 = IRBasicBlock(block_2_label, ctx)
-    ctx.append_basic_block(block_2)
+    block_2 = IRBasicBlock(block_2_label, fn)
+    fn.append_basic_block(block_2)
     acc = block_2.append_instruction("add", acc, op)
     op = block_2.append_instruction("store", 10)
     block_2.append_instruction("mstore", acc, op)
     # switch the order of the labels, for fun and profit
     block_2.append_instruction("jnz", acc, finish_label, target_label)
 
-    target_bb = IRBasicBlock(target_label, ctx)
-    ctx.append_basic_block(target_bb)
+    target_bb = IRBasicBlock(target_label, fn)
+    fn.append_basic_block(target_bb)
     target_bb.append_instruction("mul", acc, acc)
     target_bb.append_instruction("jmp", finish_label)
 
-    finish_bb = IRBasicBlock(finish_label, ctx)
-    ctx.append_basic_block(finish_bb)
+    finish_bb = IRBasicBlock(finish_label, fn)
+    fn.append_basic_block(finish_bb)
     finish_bb.append_instruction("stop")
 
-    calculate_cfg(ctx)
-    assert not ctx.normalized, "CFG should not be normalized"
+    ac = IRAnalysesCache(fn)
+    ac.request_analysis(CFGAnalysis)
+    assert not fn.normalized, "CFG should not be normalized"
 
-    NormalizationPass().run_pass(ctx)
+    NormalizationPass(ac, fn).run_pass()
 
-    assert ctx.normalized, "CFG should be normalized"
+    assert fn.normalized, "CFG should be normalized"
 
-    finish_bb = ctx.get_basic_block(finish_label.value)
+    finish_bb = fn.get_basic_block(finish_label.value)
     cfg_in = list(finish_bb.cfg_in)
     assert cfg_in[0].label.value == "target", "Should contain target"
     assert cfg_in[1].label.value == "__global_split_finish", "Should contain __global_split_finish"
@@ -98,40 +104,42 @@ def test_multi_entry_block_2():
 
 
 def test_multi_entry_block_with_dynamic_jump():
-    ctx = IRFunction()
+    ctx = IRContext()
+    fn = ctx.create_function("__global")
 
     finish_label = IRLabel("finish")
     target_label = IRLabel("target")
-    block_1_label = IRLabel("block_1", ctx)
+    block_1_label = IRLabel("block_1", fn)
 
-    bb = ctx.get_basic_block()
+    bb = fn.get_basic_block()
     op = bb.append_instruction("store", 10)
     acc = bb.append_instruction("add", op, op)
     bb.append_instruction("djmp", acc, finish_label, block_1_label)
 
-    block_1 = IRBasicBlock(block_1_label, ctx)
-    ctx.append_basic_block(block_1)
+    block_1 = IRBasicBlock(block_1_label, fn)
+    fn.append_basic_block(block_1)
     acc = block_1.append_instruction("add", acc, op)
     op = block_1.append_instruction("store", 10)
     block_1.append_instruction("mstore", acc, op)
     block_1.append_instruction("jnz", acc, finish_label, target_label)
 
-    target_bb = IRBasicBlock(target_label, ctx)
-    ctx.append_basic_block(target_bb)
+    target_bb = IRBasicBlock(target_label, fn)
+    fn.append_basic_block(target_bb)
     target_bb.append_instruction("mul", acc, acc)
     target_bb.append_instruction("jmp", finish_label)
 
-    finish_bb = IRBasicBlock(finish_label, ctx)
-    ctx.append_basic_block(finish_bb)
+    finish_bb = IRBasicBlock(finish_label, fn)
+    fn.append_basic_block(finish_bb)
     finish_bb.append_instruction("stop")
 
-    calculate_cfg(ctx)
-    assert not ctx.normalized, "CFG should not be normalized"
+    ac = IRAnalysesCache(fn)
+    ac.request_analysis(CFGAnalysis)
+    assert not fn.normalized, "CFG should not be normalized"
 
-    NormalizationPass().run_pass(ctx)
-    assert ctx.normalized, "CFG should be normalized"
+    NormalizationPass(ac, fn).run_pass()
+    assert fn.normalized, "CFG should be normalized"
 
-    finish_bb = ctx.get_basic_block(finish_label.value)
+    finish_bb = fn.get_basic_block(finish_label.value)
     cfg_in = list(finish_bb.cfg_in)
     assert cfg_in[0].label.value == "target", "Should contain target"
     assert cfg_in[1].label.value == "__global_split_finish", "Should contain __global_split_finish"

commit dea5d2b5f6b240fbb90ee55ddf417e9c64a61c36

fix[venom]: fix `cfg` output format (#4010)
this commit fixes a regression introduced in ace3789e4b9e2307, as we now
have multiple `IRFunction` objects grouped in an `IRContext`. This
commit adds the required `as_graph()` method to `IRContext`. At the same
time the `IRFuntions.as_graph()` is updated to support emitting only a
subgraph without wrapping it into a graphviz `digraph`.

vyper/venom/context.py
@@ -53,6 +53,14 @@ class IRContext:
         """
         self.data_segment.append(IRInstruction(opcode, args))  # type: ignore
 
+    def as_graph(self) -> str:
+        s = ["digraph G {"]
+        for fn in self.functions.values():
+            s.append(fn.as_graph(True))
+            s.append("\n")
+        s.append("}")
+        return "\n".join(s)
+
     def __repr__(self) -> str:
         s = ["IRContext:"]
         for fn in self.functions.values():

vyper/venom/function.py
@@ -214,7 +214,11 @@ class IRFunction:
         new.last_variable = self.last_variable
         return new
 
-    def as_graph(self) -> str:
+    def as_graph(self, only_subgraph=False) -> str:
+        """
+        Return the function as a graphviz dot string. If only_subgraph is True, only return the
+        subgraph, not the full digraph -for embedding in a larger graph-
+        """
         import html
 
         def _make_label(bb):
@@ -227,18 +231,25 @@ class IRFunction:
             return ret
             # return f"{bb.label.value}:\n" + "\n".join([f"    {inst}" for inst in bb.instructions])
 
-        ret = "digraph G {\n"
+        ret = []
+
+        if not only_subgraph:
+            ret.append("digraph G {{")
+        ret.append(f'subgraph "{self.name}" {{')
 
         for bb in self.get_basic_blocks():
             for out_bb in bb.cfg_out:
-                ret += f'    "{bb.label.value}" -> "{out_bb.label.value}"\n'
+                ret.append(f'    "{bb.label.value}" -> "{out_bb.label.value}"')
 
         for bb in self.get_basic_blocks():
-            ret += f'    "{bb.label.value}" [shape=plaintext, '
-            ret += f'label={_make_label(bb)}, fontname="Courier" fontsize="8"]\n'
+            ret.append(f'    "{bb.label.value}" [shape=plaintext, ')
+            ret.append(f'label={_make_label(bb)}, fontname="Courier" fontsize="8"]')
 
-        ret += "}\n"
-        return ret
+        ret.append("}\n")
+        if not only_subgraph:
+            ret.append("}\n")
+
+        return "\n".join(ret)
 
     def __repr__(self) -> str:
         str = f"IRFunction: {self.name}\n"