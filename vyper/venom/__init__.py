7ca1a3954e8ce0dd0bbbd4a41a3f7b1e34da97f9
Harry Kalogirou <harkal@nlogn.eu>
2024-05-24 10:10:16 +0300

    implement branch optimization pass

--- vyper/venom/__init__.py
+++ vyper/venom/__init__.py
@@ -10,6 +10,7 @@ from vyper.venom.analysis.liveness import LivenessAnalysis
 from vyper.venom.context import IRContext
 from vyper.venom.function import IRFunction
 from vyper.venom.ir_node_to_venom import ir_node_to_venom
+from vyper.venom.passes.branch_optimization import BranchOptimizationPass
 from vyper.venom.passes.dft import DFTPass
 from vyper.venom.passes.make_ssa import MakeSSA
 from vyper.venom.passes.mem2var import Mem2Var
@@ -51,6 +52,7 @@ def _run_passes(fn: IRFunction, optimize: OptimizationLevel) -> None:
     StoreElimination(ac, fn).run_pass()
     SimplifyCFGPass(ac, fn).run_pass()
     RemoveUnusedVariablesPass(ac, fn).run_pass()
+    BranchOptimizationPass(ac, fn).run_pass()
     DFTPass(ac, fn).run_pass()
 
 
---
+++ vyper/venom/passes/branch_optimization.py
@@ -0,0 +1,35 @@
+from vyper.venom.analysis.cfg import CFGAnalysis
+from vyper.venom.analysis.dfg import DFGAnalysis
+from vyper.venom.passes.base_pass import IRPass
+
+
+class BranchOptimizationPass(IRPass):
+    """
+    This pass optimizes branches inverting jnz instructions where appropriate
+    """
+
+    def _optimize_branches(self) -> None:
+        fn = self.function
+        for bb in fn.get_basic_blocks():
+            if len(bb.instructions) == 1:
+                continue
+
+            term_inst = bb.instructions[-1]
+            if term_inst.opcode != "jnz":
+                continue
+
+            prev_inst = self.dfg.get_producing_instruction(term_inst.operands[0])
+            if prev_inst.opcode == "iszero":
+                prev_inst.parent.remove_instruction(prev_inst)
+                term_inst.operands = [
+                    prev_inst.operands[0],
+                    term_inst.operands[2],
+                    term_inst.operands[1],
+                ]
+
+    def run_pass(self):
+        self.dfg = self.analyses_cache.request_analysis(DFGAnalysis)
+
+        self._optimize_branches()
+
+        self.analyses_cache.invalidate_analysis(CFGAnalysis)
