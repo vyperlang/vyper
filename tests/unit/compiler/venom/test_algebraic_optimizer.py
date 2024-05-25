a51a7d42982f61d0bb3aebc980c374dceaf7c2ce
Harry Kalogirou <harkal@nlogn.eu>
2024-05-25 19:02:59 +0300

    iszero sequences optimization

---
+++ tests/unit/compiler/venom/test_algebraic_optimizer.py
@@ -0,0 +1,99 @@
+import pytest
+
+from vyper.venom.analysis.analysis import IRAnalysesCache
+from vyper.venom.basicblock import IRBasicBlock, IRLabel
+from vyper.venom.context import IRContext
+from vyper.venom.passes.algebraic_optimization import AlgebraicOptimizationPass
+from vyper.venom.passes.make_ssa import MakeSSA
+from vyper.venom.passes.remove_unused_variables import RemoveUnusedVariablesPass
+
+
+@pytest.mark.parametrize("iszero_count", range(5))
+def test_simple_jump_case(iszero_count):
+    ctx = IRContext()
+    fn = ctx.create_function("_global")
+
+    bb = fn.get_basic_block()
+
+    br1 = IRBasicBlock(IRLabel("then"), fn)
+    fn.append_basic_block(br1)
+    br2 = IRBasicBlock(IRLabel("else"), fn)
+    fn.append_basic_block(br2)
+
+    p1 = bb.append_instruction("param")
+    op1 = bb.append_instruction("store", p1)
+    op2 = bb.append_instruction("store", 64)
+    op3 = bb.append_instruction("add", op1, op2)
+    jnz_input = op3
+
+    for _ in range(iszero_count):
+        jnz_input = bb.append_instruction("iszero", jnz_input)  
+
+    bb.append_instruction("jnz", jnz_input, br1.label, br2.label)
+
+    br1.append_instruction("add", op3, 10)
+    br1.append_instruction("stop")
+    br2.append_instruction("add", op3, p1)
+    br2.append_instruction("stop")
+
+    ac = IRAnalysesCache(fn)
+    MakeSSA(ac, fn).run_pass()
+    AlgebraicOptimizationPass(ac, fn).run_pass()
+    RemoveUnusedVariablesPass(ac, fn).run_pass()
+
+    iszeros = [inst for inst in bb.instructions if inst.opcode == "iszero"]
+    removed_iszeros = iszero_count - len(iszeros)
+    
+    assert removed_iszeros % 2 == 0
+    assert len(iszeros) % 2 == iszero_count % 2
+    
+@pytest.mark.parametrize("interleave_point", range(1,5))
+def test_interleaved_case(interleave_point):
+    iszeros_after_interleave_point = interleave_point // 2
+    ctx = IRContext()
+    fn = ctx.create_function("_global")
+
+    bb = fn.get_basic_block()
+
+    br1 = IRBasicBlock(IRLabel("then"), fn)
+    fn.append_basic_block(br1)
+    br2 = IRBasicBlock(IRLabel("else"), fn)
+    fn.append_basic_block(br2)
+
+    p1 = bb.append_instruction("param")
+    op1 = bb.append_instruction("store", p1)
+    op2 = bb.append_instruction("store", 64)
+    op3 = bb.append_instruction("add", op1, op2)
+    op3_inv = bb.append_instruction("iszero", op3)
+    jnz_input = op3_inv
+    for _ in range(interleave_point):
+        jnz_input = bb.append_instruction("iszero", jnz_input)  
+    bb.append_instruction("mstore", jnz_input, p1)
+    for _ in range(iszeros_after_interleave_point):
+        jnz_input = bb.append_instruction("iszero", jnz_input)  
+    bb.append_instruction("jnz", jnz_input, br1.label, br2.label)
+
+    br1.append_instruction("add", op3, 10)
+    br1.append_instruction("stop")
+    br2.append_instruction("add", op3, p1)
+    br2.append_instruction("stop")
+
+    ac = IRAnalysesCache(fn)
+    MakeSSA(ac, fn).run_pass()
+    print(ctx)
+    AlgebraicOptimizationPass(ac, fn).run_pass()    
+    RemoveUnusedVariablesPass(ac, fn).run_pass()
+    
+    print("-----------------")
+    print(ctx)
+
+    assert bb.instructions[-1].opcode == "jnz"
+    if (interleave_point + iszeros_after_interleave_point) % 2 == 0:
+        assert bb.instructions[-1].operands[0] == op3_inv
+    else:
+        assert bb.instructions[-1].operands[0] == op3
+        
+if __name__ == '__main__':
+     test_interleaved_case(5)
+
+
---
+++ vyper/venom/passes/algebraic_optimization.py
@@ -0,0 +1,48 @@
+from vyper.venom.analysis.cfg import CFGAnalysis
+from vyper.venom.analysis.dfg import DFGAnalysis
+from vyper.venom.basicblock import IRInstruction, IROperand
+from vyper.venom.passes.base_pass import IRPass
+
+
+class AlgebraicOptimizationPass(IRPass):
+    """
+    """
+
+    def _optimize_iszero_chains(self) -> None:
+        fn = self.function
+        for bb in fn.get_basic_blocks():
+            for inst in bb.instructions:
+                if inst.opcode != "iszero":
+                    continue
+
+                iszero_chain = self._get_iszero_chain(inst.operands[0])
+                if len(iszero_chain) == 0:
+                    continue
+
+                if len(iszero_chain) % 2 == 1:    
+                    out_var = iszero_chain[-1].operands[0]
+                else:
+                    out_var = iszero_chain[-1].output
+
+                for use_inst in self.dfg.get_uses(inst.output):
+                    if use_inst.opcode != "iszero":
+                        use_inst.replace_operands({inst.output: out_var})
+            
+    def _get_iszero_chain(self, op: IROperand) -> list[IRInstruction]:
+        chain = []
+
+        while True:
+            inst = self.dfg.get_producing_instruction(op)
+            if inst.opcode != "iszero":
+                break
+            op = inst.operands[0]
+            chain.append(inst)
+
+        return chain
+
+    def run_pass(self):
+        self.dfg = self.analyses_cache.request_analysis(DFGAnalysis)
+
+        self._optimize_iszero_chains()
+
+        self.analyses_cache.invalidate_analysis(CFGAnalysis)
