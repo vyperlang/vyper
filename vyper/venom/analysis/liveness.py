9478690e928171c03aeacfeea6f4a171199454d3
Charles Cooper <cooper.charles.m@gmail.com>
2024-05-31 20:27:18 +0000

    improve generated code

--- vyper/venom/analysis/liveness.py
+++ vyper/venom/analysis/liveness.py
@@ -1,4 +1,4 @@
-from collections import OrderedDict
+from collections import deque
 
 from vyper.exceptions import CompilerPanic
 from vyper.utils import OrderedSet
@@ -16,21 +16,18 @@ class LivenessAnalysis(IRAnalysis):
         self.analyses_cache.request_analysis(CFGAnalysis)
         self._reset_liveness()
 
-        self._worklist = OrderedSet()
-        self._worklist.addmany(self.function.get_basic_blocks())
+        self._worklist = deque()
+        self._worklist.extend(self.function.get_basic_blocks())
 
         while len(self._worklist) > 0:
             changed = False
-            bb = next(iter(self._worklist))  # fifo "popleft"
-            self._worklist.remove(bb)
+            bb = self._worklist.popleft()
             changed |= self._calculate_out_vars(bb)
             changed |= self._calculate_liveness(bb)
             # recompute liveness for basic blocks pointing into
             # this basic block
             if changed:
-                # queue semantics: add to end of worklist
-                self._worklist.dropmany(bb.cfg_in)
-                self._worklist.addmany(bb.cfg_in)
+                self._worklist.extend(bb.cfg_in)
 
     def _reset_liveness(self) -> None:
         for bb in self.function.get_basic_blocks():
