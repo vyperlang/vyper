c502e99cca1a7545bfda45ad97bb07ac2a7cb8a6
Harry Kalogirou <harkal@nlogn.eu>
2024-06-12 11:49:28 +0300

    cleanup

--- tests/functional/codegen/venom/test_double_eval.py
+++ tests/functional/codegen/venom/test_double_eval.py
@@ -1,14 +1,13 @@
-
 def test_double_eval_pop(get_contract):
-    code = f"""
+    code = """
 m: HashMap[uint256, String[33]]
 
 @external
 def foo() -> uint256:
-    x: DynArray[uint256, 32] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    x: DynArray[uint256, 16] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     self.m[x.pop()] = "Hello world"
     return len(x)
 """
-    
+
     c = get_contract(code)
-    assert c.foo() == 31
+    assert c.foo() == 15
