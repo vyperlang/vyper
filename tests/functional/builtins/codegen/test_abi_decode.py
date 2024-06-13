44bb281ccaac89dc3bd66030702473c386bceae6
Charles Cooper <cooper.charles.m@gmail.com>
2024-06-13 09:22:58 -0700

    fix[codegen]: add back in `returndatasize` check (#4144)
    add back in `returndatasize` check for external calls in the case
    that `make_setter()` is not called (i.e. when `needs_clamp()` is
    `True`). the check was removed (i.e. there was a regression) in
    21f7172274e
    
    test case and poc contributed by @cyberthirst
    
    ---------
    
    Co-authored-by: cyberthirst <cyberthirst.eth@gmail.com>

--- tests/functional/builtins/codegen/test_abi_decode.py
+++ tests/functional/builtins/codegen/test_abi_decode.py
@@ -1421,3 +1421,23 @@ def foo(a:Bytes[1000]):
 
     with tx_failed():
         c.foo(_abi_payload_from_tuple(payload))
+
+
+# returndatasize check for uint256
+def test_returndatasize_check(get_contract, tx_failed):
+    code = """
+@external
+def bar():
+    pass
+
+interface A:
+    def bar() -> uint256: nonpayable
+
+@external
+def run() -> uint256:
+    return extcall A(self).bar()
+    """
+    c = get_contract(code)
+
+    with tx_failed():
+        c.run()
