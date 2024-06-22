7779b6a675db7069bd520ba731746599556a8bbd
Charles Cooper <cooper.charles.m@gmail.com>
2024-06-22 08:54:16 -0700

    fix VarInfo for function arguments

--- vyper/semantics/analysis/local.py
+++ vyper/semantics/analysis/local.py
@@ -317,7 +317,7 @@ class FunctionAnalyzer(VyperNodeVisitorBase):
 
         for arg in self.func.arguments:
             self.namespace[arg.name] = VarInfo(
-                arg.typ, location=location, modifiability=modifiability
+                arg.typ, location=location, modifiability=modifiability, decl_node=arg
             )
 
         for node in self.fn_node.body:
