1062fc7dda1b0be635101194d4b24dcb80d9b1c5
Ramana Kumar <ramana@member.fsf.org>
2024-09-01 10:28:48 +0100

    Also test get_settings when setting experimental codegen

--- tests/unit/cli/vyper_json/test_compile_json.py
+++ tests/unit/cli/vyper_json/test_compile_json.py
@@ -9,6 +9,7 @@ from vyper.cli.vyper_json import (
     compile_json,
     exc_handler_to_dict,
     get_inputs,
+    get_settings,
 )
 from vyper.compiler import OUTPUT_FORMATS, compile_code, compile_from_file_input
 from vyper.compiler.input_bundle import JSONInputBundle
@@ -297,15 +298,19 @@ def test_relative_import_paths(input_json):
     compile_from_input_dict(input_json)
 
 
-def test_experimental_codegen_default(input_json):
+def test_experimental_codegen_settings(input_json):
     assert input_json["settings"]["experimentalCodegen"] is None
+    assert get_settings(input_json).experimental_codegen is None
     default_output = compile_from_input_dict(input_json)
     input_json["source"]["contracts/foo.vy"]["content"] = "#pragma experimental_codegen\n\n" + FOO_CODE
+    assert get_settings(input_json).experimental_codegen is None
     pragma_output = compile_from_input_dict(input_json)
     input_json["source"]["contracts/foo.vy"]["content"] = FOO_CODE
     input_json["settings"]["experimentalCodegen"] = True
+    assert get_settings(input_json).experimental_codegen == True
     setting_true_output = compile_from_input_dict(input_json)
     input_json["settings"]["experimentalCodegen"] = False
+    assert get_settings(input_json).experimental_codegen == False
     setting_false_output = compile_from_input_dict(input_json)
     assert default_output == setting_false_output
     assert pragma_output == setting_true_output
