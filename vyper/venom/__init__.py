a22f5c92eaa6d3d2ce33d3d4f1252241c18baa0c
Charles Cooper <cooper.charles.m@gmail.com>
2024-09-25 19:45:42 -0400

    reorder store expansion and unused var elimination

--- vyper/venom/__init__.py
+++ vyper/venom/__init__.py
@@ -54,9 +54,9 @@ def _run_passes(fn: IRFunction, optimize: OptimizationLevel) -> None:
     SimplifyCFGPass(ac, fn).run_pass()
     AlgebraicOptimizationPass(ac, fn).run_pass()
     BranchOptimizationPass(ac, fn).run_pass()
+    RemoveUnusedVariablesPass(ac, fn).run_pass()
 
     StoreExpansionPass(ac, fn).run_pass()
-    RemoveUnusedVariablesPass(ac, fn).run_pass()
     DFTPass(ac, fn).run_pass()
 
 
