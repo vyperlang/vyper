b1425cac95e3597eb126c42e2dcb4d8d2c947c5f
Harry Kalogirou <harkal@nlogn.eu>
2024-09-27 22:52:21 +0300

    wip

--- vyper/venom/passes/dft.py
+++ vyper/venom/passes/dft.py
@@ -1,5 +1,4 @@
 import random
-from dataclasses import dataclass
 import sys
 
 from vyper.utils import OrderedSet
@@ -13,15 +12,28 @@ from vyper.venom.passes.base_pass import IRPass
 
 class DFTPass(IRPass):
     function: IRFunction
-    inst_order: dict[IRInstruction, int]
-    inst_order_num: int
+    inst_count: dict[IRInstruction, int]
 
     def __init__(self, analyses_cache: IRAnalysesCache, function: IRFunction):
         super().__init__(analyses_cache, function)
         self.inst_groups = {}
+        self.inst_count = {}
 
     def _permutate(self, instructions: list[IRInstruction]):
         return random.shuffle(instructions)
+    
+    def _calculate_depth_instruction_r(self, inst: IRInstruction):
+        if inst in self.visited_instructions:
+            return
+
+        self.visited_instructions.add(inst)
+        self.inst_count[inst] = 1
+
+        for dep_inst in self.ida[inst]:
+            if inst.parent != dep_inst.parent:
+                continue
+            self._calculate_depth_instruction_r(dep_inst)
+            self.inst_count[inst] += self.inst_count[dep_inst]
 
     def _process_instruction_r(self, instructions: list[IRInstruction], inst: IRInstruction):
         if inst in self.visited_instructions:
@@ -31,7 +43,7 @@ class DFTPass(IRPass):
         if inst.is_pseudo:
             return
 
-        children = sorted(self.ida[inst], key=lambda x: x.is_volatile, reverse=True)
+        children = sorted(self.ida[inst], key=lambda x: -self.inst_count[x])
 
         for dep_inst in children:
             if inst.parent != dep_inst.parent:
@@ -48,8 +60,17 @@ class DFTPass(IRPass):
         self._calculate_dependency_graphs(bb)
         self.instructions = list(bb.pseudo_instructions)
 
-        for inst in reversed(list(bb.instructions)):
+        self.visited_instructions = OrderedSet()
+        for inst in reversed(list(bb.non_phi_instructions)):
+            self._calculate_depth_instruction_r(inst)
+
+        self.visited_instructions = OrderedSet()
+        for inst in reversed(list(bb.non_phi_instructions)):
             self._process_instruction_r(self.instructions, inst)
+
+        # if bb.label.value == "26_then":
+        #     print(self.ida_as_graph())
+        #     sys.exit(0)
 
         bb.instructions = self.instructions
         assert bb.is_terminated, f"Basic block should be terminated {bb}"
@@ -65,8 +86,6 @@ class DFTPass(IRPass):
         for inst in non_phis:
             self.ida[inst] = list()
 
-        self.inst_groups = {}
-
         #
         # Calculate instruction groups and instruction dependencies
         #
@@ -81,10 +100,6 @@ class DFTPass(IRPass):
 
             for use in uses:
                 self.ida[use].append(inst)
-
-            # if bb.label.value == "26_then":
-            #     print(self.ida_as_graph())
-            #     sys.exit(0)
 
     def run_pass(self) -> None:
         self.visited_instructions: OrderedSet[IRInstruction] = OrderedSet()
@@ -107,10 +122,8 @@ class DFTPass(IRPass):
             for dep in deps:
                 a = inst.str_short()
                 b = dep.str_short()
-                if s := self.inst_groups.get(inst):
-                    a += f" {s.group_id}"
-                if s := self.inst_groups.get(dep):
-                    b += f" {s.group_id}"
+                a += f" {self.inst_count.get(inst, "-")}"
+                b += f" {self.inst_count.get(dep, "-")}"
                 a = a.replace("%", "\\%")
                 b = b.replace("%", "\\%")
                 lines.append(f'"{a}" -> "{b}"')
