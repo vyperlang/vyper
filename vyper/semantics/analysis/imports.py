vyperlang/vyper

commit 6843e7915729f3a3ea0d8c765dffa52033f5818e

feat[tool]: separate import resolution pass (#4229)
this commit separates import resolution into its own pass. there are
two reasons for this:

a) improved performance on certain operations, specifically,
computation of the integrity hash. this is a hotspot for tooling, which
needs to compute the integrity hash of a contract quickly to check if
it needs to be recompiled or not

b) logical separation. after this commit, all the I/O and filepath
computation of the module analysis is moved into its own pass, making
the module analyzer "pure" - it no longer needs to deal about input
bundles or filepaths or anything like that.

misc/refactor: this commit moves integrity hash computation into
a compiler pass. besides that, some some minor code touchups were
performed, but because of moving code into a new file, an effort
was made to keep the semantics of the code largely the same, to ease
reasoning about the diff.

vyper/semantics/analysis/imports.py
@@ -1,0 +1,332 @@
+import contextlib
+from dataclasses import dataclass, field
+from pathlib import Path, PurePath
+from typing import Any, Iterator
+
+import vyper.builtins.interfaces
+from vyper import ast as vy_ast
+from vyper.compiler.input_bundle import (
+    ABIInput,
+    CompilerInput,
+    FileInput,
+    FilesystemInputBundle,
+    InputBundle,
+    PathLike,
+)
+from vyper.exceptions import (
+    CompilerPanic,
+    DuplicateImport,
+    ImportCycle,
+    ModuleNotFound,
+    StructureException,
+)
+from vyper.semantics.analysis.base import ImportInfo
+from vyper.utils import safe_relpath, sha256sum
+
+"""
+collect import statements and validate the import graph.
+this module is separated into its own pass so that we can resolve the import
+graph quickly (without doing semantic analysis) and for cleanliness, to
+segregate the I/O portion of semantic analysis into its own pass.
+"""
+
+
+@dataclass
+class _ImportGraph:
+    # the current path in the import graph traversal
+    _path: list[vy_ast.Module] = field(default_factory=list)
+
+    # stack of dicts, each item in the stack is a dict keeping
+    # track of imports in the current module
+    _imports: list[dict] = field(default_factory=list)
+
+    @property
+    def imported_modules(self):
+        return self._imports[-1]
+
+    @property
+    def current_module(self):
+        return self._path[-1]
+
+    def push_path(self, module_ast: vy_ast.Module) -> None:
+        if module_ast in self._path:
+            cycle = self._path + [module_ast]
+            raise ImportCycle(" imports ".join(f'"{t.path}"' for t in cycle))
+
+        self._path.append(module_ast)
+        self._imports.append({})
+
+    def pop_path(self, expected: vy_ast.Module) -> None:
+        popped = self._path.pop()
+        if expected != popped:
+            raise CompilerPanic("unreachable")
+        self._imports.pop()
+
+    @contextlib.contextmanager
+    def enter_path(self, module_ast: vy_ast.Module) -> Iterator[None]:
+        self.push_path(module_ast)
+        try:
+            yield
+        finally:
+            self.pop_path(module_ast)
+
+
+class ImportAnalyzer:
+    def __init__(self, input_bundle: InputBundle, graph: _ImportGraph):
+        self.input_bundle = input_bundle
+        self.graph = graph
+        self._ast_of: dict[int, vy_ast.Module] = {}
+
+        self.seen: set[int] = set()
+
+        self.integrity_sum = None
+
+    def resolve_imports(self, module_ast: vy_ast.Module):
+        self._resolve_imports_r(module_ast)
+        self.integrity_sum = self._calculate_integrity_sum_r(module_ast)
+
+    def _calculate_integrity_sum_r(self, module_ast: vy_ast.Module):
+        acc = [sha256sum(module_ast.full_source_code)]
+        for s in module_ast.get_children((vy_ast.Import, vy_ast.ImportFrom)):
+            info = s._metadata["import_info"]
+
+            if info.compiler_input.path.suffix in (".vyi", ".json"):
+                # NOTE: this needs to be redone if interfaces can import other interfaces
+                acc.append(info.compiler_input.sha256sum)
+            else:
+                acc.append(self._calculate_integrity_sum_r(info.parsed))
+
+        return sha256sum("".join(acc))
+
+    def _resolve_imports_r(self, module_ast: vy_ast.Module):
+        if id(module_ast) in self.seen:
+            return
+        with self.graph.enter_path(module_ast):
+            for node in module_ast.body:
+                if isinstance(node, vy_ast.Import):
+                    self._handle_Import(node)
+                elif isinstance(node, vy_ast.ImportFrom):
+                    self._handle_ImportFrom(node)
+        self.seen.add(id(module_ast))
+
+    def _handle_Import(self, node: vy_ast.Import):
+        # import x.y[name] as y[alias]
+
+        alias = node.alias
+
+        if alias is None:
+            alias = node.name
+
+        # don't handle things like `import x.y`
+        if "." in alias:
+            msg = "import requires an accompanying `as` statement"
+            suggested_alias = node.name[node.name.rfind(".") :]
+            hint = f"try `import {node.name} as {suggested_alias}`"
+            raise StructureException(msg, node, hint=hint)
+
+        self._add_import(node, 0, node.name, alias)
+
+    def _handle_ImportFrom(self, node: vy_ast.ImportFrom):
+        # from m.n[module] import x[name] as y[alias]
+
+        alias = node.alias
+
+        if alias is None:
+            alias = node.name
+
+        module = node.module or ""
+        if module:
+            module += "."
+
+        qualified_module_name = module + node.name
+        self._add_import(node, node.level, qualified_module_name, alias)
+
+    def _add_import(
+        self, node: vy_ast.VyperNode, level: int, qualified_module_name: str, alias: str
+    ) -> None:
+        compiler_input, ast = self._load_import(node, level, qualified_module_name, alias)
+        node._metadata["import_info"] = ImportInfo(
+            alias, qualified_module_name, compiler_input, ast
+        )
+
+    # load an InterfaceT or ModuleInfo from an import.
+    # raises FileNotFoundError
+    def _load_import(self, node: vy_ast.VyperNode, level: int, module_str: str, alias: str) -> Any:
+        # the directory this (currently being analyzed) module is in
+        ast = self.graph.current_module
+        self_search_path = Path(ast.resolved_path).parent
+
+        with self.input_bundle.poke_search_path(self_search_path):
+            return self._load_import_helper(node, level, module_str, alias)
+
+    def _load_import_helper(
+        self, node: vy_ast.VyperNode, level: int, module_str: str, alias: str
+    ) -> tuple[CompilerInput, Any]:
+        if _is_builtin(module_str):
+            return _load_builtin_import(level, module_str)
+
+        path = _import_to_path(level, module_str)
+
+        if path in self.graph.imported_modules:
+            previous_import_stmt = self.graph.imported_modules[path]
+            raise DuplicateImport(f"{alias} imported more than once!", previous_import_stmt, node)
+
+        self.graph.imported_modules[path] = node
+
+        err = None
+
+        try:
+            path_vy = path.with_suffix(".vy")
+            file = self.input_bundle.load_file(path_vy)
+            assert isinstance(file, FileInput)  # mypy hint
+
+            module_ast = self._ast_from_file(file)
+            self.resolve_imports(module_ast)
+
+            return file, module_ast
+
+        except FileNotFoundError as e:
+            # escape `e` from the block scope, it can make things
+            # easier to debug.
+            err = e
+
+        try:
+            file = self.input_bundle.load_file(path.with_suffix(".vyi"))
+            assert isinstance(file, FileInput)  # mypy hint
+            module_ast = self._ast_from_file(file)
+
+            # language does not yet allow recursion for vyi files
+            # self.resolve_imports(module_ast)
+
+            return file, module_ast
+
+        except FileNotFoundError:
+            pass
+
+        try:
+            file = self.input_bundle.load_file(path.with_suffix(".json"))
+            assert isinstance(file, ABIInput)  # mypy hint
+            return file, file.abi
+        except FileNotFoundError:
+            pass
+
+        hint = None
+        if module_str.startswith("vyper.interfaces"):
+            hint = "try renaming `vyper.interfaces` to `ethereum.ercs`"
+
+        # copy search_paths, makes debugging a bit easier
+        search_paths = self.input_bundle.search_paths.copy()  # noqa: F841
+        raise ModuleNotFound(module_str, hint=hint) from err
+
+    def _ast_from_file(self, file: FileInput) -> vy_ast.Module:
+        # cache ast if we have seen it before.
+        # this gives us the additional property of object equality on
+        # two ASTs produced from the same source
+        ast_of = self._ast_of
+        if file.source_id not in ast_of:
+            ast_of[file.source_id] = _parse_ast(file)
+
+        return ast_of[file.source_id]
+
+
+def _parse_ast(file: FileInput) -> vy_ast.Module:
+    module_path = file.resolved_path  # for error messages
+    try:
+        # try to get a relative path, to simplify the error message
+        cwd = Path(".")
+        if module_path.is_absolute():
+            cwd = cwd.resolve()
+        module_path = module_path.relative_to(cwd)
+    except ValueError:
+        # we couldn't get a relative path (cf. docs for Path.relative_to),
+        # use the resolved path given to us by the InputBundle
+        pass
+
+    ret = vy_ast.parse_to_ast(
+        file.source_code,
+        source_id=file.source_id,
+        module_path=module_path.as_posix(),
+        resolved_path=file.resolved_path.as_posix(),
+    )
+    return ret
+
+
+# convert an import to a path (without suffix)
+def _import_to_path(level: int, module_str: str) -> PurePath:
+    base_path = ""
+    if level > 1:
+        base_path = "../" * (level - 1)
+    elif level == 1:
+        base_path = "./"
+    return PurePath(f"{base_path}{module_str.replace('.', '/')}/")
+
+
+# can add more, e.g. "vyper.builtins.interfaces", etc.
+BUILTIN_PREFIXES = ["ethereum.ercs"]
+
+
+# TODO: could move this to analysis/common.py or something
+def _is_builtin(module_str):
+    return any(module_str.startswith(prefix) for prefix in BUILTIN_PREFIXES)
+
+
+_builtins_cache: dict[PathLike, tuple[CompilerInput, vy_ast.Module]] = {}
+
+
+def _load_builtin_import(level: int, module_str: str) -> tuple[CompilerInput, vy_ast.Module]:
+    if not _is_builtin(module_str):  # pragma: nocover
+        raise CompilerPanic("unreachable!")
+
+    builtins_path = vyper.builtins.interfaces.__path__[0]
+    # hygiene: convert to relpath to avoid leaking user directory info
+    # (note Path.relative_to cannot handle absolute to relative path
+    # conversion, so we must use the `os` module).
+    builtins_path = safe_relpath(builtins_path)
+
+    search_path = Path(builtins_path).parent.parent.parent
+    # generate an input bundle just because it knows how to build paths.
+    input_bundle = FilesystemInputBundle([search_path])
+
+    # remap builtins directory --
+    # ethereum/ercs => vyper/builtins/interfaces
+    remapped_module = module_str
+    if remapped_module.startswith("ethereum.ercs"):
+        remapped_module = remapped_module.removeprefix("ethereum.ercs")
+        remapped_module = vyper.builtins.interfaces.__package__ + remapped_module
+
+    path = _import_to_path(level, remapped_module).with_suffix(".vyi")
+
+    # builtins are globally the same, so we can safely cache them
+    # (it is also *correct* to cache them, so that types defined in builtins
+    # compare correctly using pointer-equality.)
+    if path in _builtins_cache:
+        file, ast = _builtins_cache[path]
+        return file, ast
+
+    try:
+        file = input_bundle.load_file(path)
+        assert isinstance(file, FileInput)  # mypy hint
+    except FileNotFoundError as e:
+        hint = None
+        components = module_str.split(".")
+        # common issue for upgrading codebases from v0.3.x to v0.4.x -
+        # hint: rename ERC20 to IERC20
+        if components[-1].startswith("ERC"):
+            module_prefix = components[-1]
+            hint = f"try renaming `{module_prefix}` to `I{module_prefix}`"
+        raise ModuleNotFound(module_str, hint=hint) from e
+
+    interface_ast = _parse_ast(file)
+
+    # no recursion needed since builtins don't have any imports
+
+    _builtins_cache[path] = file, interface_ast
+    return file, interface_ast
+
+
+def resolve_imports(module_ast: vy_ast.Module, input_bundle: InputBundle):
+    graph = _ImportGraph()
+    analyzer = ImportAnalyzer(input_bundle, graph)
+    analyzer.resolve_imports(module_ast)
+
+    return analyzer

tests/conftest.py
@@ -14,7 +14,7 @@ from tests.evm_backends.revm_env import RevmEnv
 from tests.utils import working_directory
 from vyper import compiler
 from vyper.codegen.ir_node import IRnode
-from vyper.compiler.input_bundle import FilesystemInputBundle, InputBundle
+from vyper.compiler.input_bundle import FilesystemInputBundle
 from vyper.compiler.settings import OptimizationLevel, Settings, set_global_settings
 from vyper.exceptions import EvmVersionException
 from vyper.ir import compile_ir, optimizer
@@ -164,12 +164,6 @@ def make_input_bundle(tmp_path, make_file):
         return FilesystemInputBundle([tmp_path])
 
     return fn
-
-
-# for tests which just need an input bundle, doesn't matter what it is
-@pytest.fixture
-def dummy_input_bundle():
-    return InputBundle([])
 
 
 @pytest.fixture(scope="module")

tests/functional/codegen/types/numbers/test_decimals.py
@@ -299,7 +299,7 @@ def foo():
         compile_code(code)
 
 
-def test_replace_decimal_nested_intermediate_underflow(dummy_input_bundle):
+def test_replace_decimal_nested_intermediate_underflow():
     code = """
 @external
 def foo():

tests/unit/ast/nodes/test_hex.py
@@ -40,7 +40,7 @@ foo: constant(bytes4) = 0X12345678
 
 
 @pytest.mark.parametrize("code", code_invalid_checksum)
-def test_invalid_checksum(code, dummy_input_bundle):
+def test_invalid_checksum(code):
     with pytest.raises(InvalidLiteral):
         vyper_module = vy_ast.parse_to_ast(code)
-        semantics.analyze_module(vyper_module, dummy_input_bundle)
+        semantics.analyze_module(vyper_module)

tests/unit/semantics/analysis/test_array_index.py
@@ -11,7 +11,7 @@ from vyper.semantics.analysis import analyze_module
 
 
 @pytest.mark.parametrize("value", ["address", "Bytes[10]", "decimal", "bool"])
-def test_type_mismatch(namespace, value, dummy_input_bundle):
+def test_type_mismatch(namespace, value):
     code = f"""
 
 a: uint256[3]
@@ -22,11 +22,11 @@ def foo(b: {value}):
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(TypeMismatch):
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
 
 @pytest.mark.parametrize("value", ["1.0", "0.0", "'foo'", "0x00", "b'\x01'", "False"])
-def test_invalid_literal(namespace, value, dummy_input_bundle):
+def test_invalid_literal(namespace, value):
     code = f"""
 
 a: uint256[3]
@@ -37,11 +37,11 @@ def foo():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(TypeMismatch):
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
 
 @pytest.mark.parametrize("value", [-1, 3, -(2**127), 2**127 - 1, 2**256 - 1])
-def test_out_of_bounds(namespace, value, dummy_input_bundle):
+def test_out_of_bounds(namespace, value):
     code = f"""
 
 a: uint256[3]
@@ -52,11 +52,11 @@ def foo():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(ArrayIndexException):
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
 
 @pytest.mark.parametrize("value", ["b", "self.b"])
-def test_undeclared_definition(namespace, value, dummy_input_bundle):
+def test_undeclared_definition(namespace, value):
     code = f"""
 
 a: uint256[3]
@@ -67,11 +67,11 @@ def foo():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(UndeclaredDefinition):
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
 
 @pytest.mark.parametrize("value", ["a", "foo", "int128"])
-def test_invalid_reference(namespace, value, dummy_input_bundle):
+def test_invalid_reference(namespace, value):
     code = f"""
 
 a: uint256[3]
@@ -82,4 +82,4 @@ def foo():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(InvalidReference):
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)

tests/unit/semantics/analysis/test_cyclic_function_calls.py
@@ -5,7 +5,7 @@ from vyper.exceptions import CallViolation, StructureException
 from vyper.semantics.analysis import analyze_module
 
 
-def test_self_function_call(dummy_input_bundle):
+def test_self_function_call():
     code = """
 @internal
 def foo():
@@ -13,12 +13,12 @@ def foo():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(CallViolation) as e:
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
     assert e.value.message == "Contract contains cyclic function call: foo -> foo"
 
 
-def test_self_function_call2(dummy_input_bundle):
+def test_self_function_call2():
     code = """
 @external
 def foo():
@@ -30,12 +30,12 @@ def bar():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(CallViolation) as e:
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
     assert e.value.message == "Contract contains cyclic function call: foo -> bar -> bar"
 
 
-def test_cyclic_function_call(dummy_input_bundle):
+def test_cyclic_function_call():
     code = """
 @internal
 def foo():
@@ -47,12 +47,12 @@ def bar():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(CallViolation) as e:
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
     assert e.value.message == "Contract contains cyclic function call: foo -> bar -> foo"
 
 
-def test_multi_cyclic_function_call(dummy_input_bundle):
+def test_multi_cyclic_function_call():
     code = """
 @internal
 def foo():
@@ -72,14 +72,14 @@ def potato():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(CallViolation) as e:
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
     expected_message = "Contract contains cyclic function call: foo -> bar -> baz -> potato -> foo"
 
     assert e.value.message == expected_message
 
 
-def test_multi_cyclic_function_call2(dummy_input_bundle):
+def test_multi_cyclic_function_call2():
     code = """
 @internal
 def foo():
@@ -99,14 +99,14 @@ def potato():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(CallViolation) as e:
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
     expected_message = "Contract contains cyclic function call: foo -> bar -> baz -> potato -> bar"
 
     assert e.value.message == expected_message
 
 
-def test_global_ann_assign_callable_no_crash(dummy_input_bundle):
+def test_global_ann_assign_callable_no_crash():
     code = """
 balanceOf: public(HashMap[address, uint256])
 
@@ -116,5 +116,5 @@ def foo(to : address):
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(StructureException) as excinfo:
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
     assert excinfo.value.message == "HashMap[address, uint256] is not callable"

tests/unit/semantics/analysis/test_for_loop.py
@@ -5,7 +5,7 @@ from vyper.exceptions import ArgumentException, ImmutableViolation, StructureExc
 from vyper.semantics.analysis import analyze_module
 
 
-def test_modify_iterator_function_outside_loop(dummy_input_bundle):
+def test_modify_iterator_function_outside_loop():
     code = """
 
 a: uint256[3]
@@ -21,10 +21,10 @@ def bar():
         pass
     """
     vyper_module = parse_to_ast(code)
-    analyze_module(vyper_module, dummy_input_bundle)
+    analyze_module(vyper_module)
 
 
-def test_pass_memory_var_to_other_function(dummy_input_bundle):
+def test_pass_memory_var_to_other_function():
     code = """
 
 @internal
@@ -41,10 +41,10 @@ def bar():
         self.foo(a)
     """
     vyper_module = parse_to_ast(code)
-    analyze_module(vyper_module, dummy_input_bundle)
+    analyze_module(vyper_module)
 
 
-def test_modify_iterator(dummy_input_bundle):
+def test_modify_iterator():
     code = """
 
 a: uint256[3]
@@ -56,10 +56,10 @@ def bar():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(ImmutableViolation):
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
 
-def test_bad_keywords(dummy_input_bundle):
+def test_bad_keywords():
     code = """
 
 @internal
@@ -70,10 +70,10 @@ def bar(n: uint256):
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(ArgumentException):
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
 
-def test_bad_bound(dummy_input_bundle):
+def test_bad_bound():
     code = """
 
 @internal
@@ -84,10 +84,10 @@ def bar(n: uint256):
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(StructureException):
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
 
-def test_modify_iterator_function_call(dummy_input_bundle):
+def test_modify_iterator_function_call():
     code = """
 
 a: uint256[3]
@@ -103,10 +103,10 @@ def bar():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(ImmutableViolation):
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
 
-def test_modify_iterator_recursive_function_call(dummy_input_bundle):
+def test_modify_iterator_recursive_function_call():
     code = """
 
 a: uint256[3]
@@ -126,10 +126,10 @@ def baz():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(ImmutableViolation):
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
 
-def test_modify_iterator_recursive_function_call_topsort(dummy_input_bundle):
+def test_modify_iterator_recursive_function_call_topsort():
     # test the analysis works no matter the order of functions
     code = """
 a: uint256[3]
@@ -149,12 +149,12 @@ def foo():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(ImmutableViolation) as e:
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
     assert e.value._message == "Cannot modify loop variable `a`"
 
 
-def test_modify_iterator_through_struct(dummy_input_bundle):
+def test_modify_iterator_through_struct():
     # GH issue 3429
     code = """
 struct A:
@@ -170,12 +170,12 @@ def foo():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(ImmutableViolation) as e:
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
     assert e.value._message == "Cannot modify loop variable `a`"
 
 
-def test_modify_iterator_complex_expr(dummy_input_bundle):
+def test_modify_iterator_complex_expr():
     # GH issue 3429
     # avoid false positive!
     code = """
@@ -189,10 +189,10 @@ def foo():
         self.b[self.a[1]] = i
     """
     vyper_module = parse_to_ast(code)
-    analyze_module(vyper_module, dummy_input_bundle)
+    analyze_module(vyper_module)
 
 
-def test_modify_iterator_siblings(dummy_input_bundle):
+def test_modify_iterator_siblings():
     # test we can modify siblings in an access tree
     code = """
 struct Foo:
@@ -207,10 +207,10 @@ def foo():
         self.f.b += i
     """
     vyper_module = parse_to_ast(code)
-    analyze_module(vyper_module, dummy_input_bundle)
+    analyze_module(vyper_module)
 
 
-def test_modify_subscript_barrier(dummy_input_bundle):
+def test_modify_subscript_barrier():
     # test that Subscript nodes are a barrier for analysis
     code = """
 struct Foo:
@@ -229,7 +229,7 @@ def foo():
     """
     vyper_module = parse_to_ast(code)
     with pytest.raises(ImmutableViolation) as e:
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)
 
     assert e.value._message == "Cannot modify loop variable `b`"
 
@@ -269,7 +269,7 @@ def foo():
 
 
 @pytest.mark.parametrize("code", iterator_inference_codes)
-def test_iterator_type_inference_checker(code, dummy_input_bundle):
+def test_iterator_type_inference_checker(code):
     vyper_module = parse_to_ast(code)
     with pytest.raises(TypeMismatch):
-        analyze_module(vyper_module, dummy_input_bundle)
+        analyze_module(vyper_module)

vyper/compiler/output.py
@@ -102,7 +102,7 @@ def build_archive_b64(compiler_data: CompilerData) -> str:
 
 
 def build_integrity(compiler_data: CompilerData) -> str:
-    return compiler_data.compilation_target._metadata["type"].integrity_sum
+    return compiler_data.resolved_imports.integrity_sum
 
 
 def build_external_interface_output(compiler_data: CompilerData) -> str:

commit 13c659b6a52545eee35de099c3d16314df5ff14d

fix[lang]: fix recursive interface imports
fix a bug where imports inside of interfaces are not recursed into in
the import resolution pass. this is a regression in
6843e7915729f3a3ea0d8c765dffa52033f5818e.

vyper/semantics/analysis/imports.py
@@ -194,6 +194,7 @@ class ImportAnalyzer:
             file = self.input_bundle.load_file(path.with_suffix(".vyi"))
             assert isinstance(file, FileInput)  # mypy hint
             module_ast = self._ast_from_file(file)
+            self.resolve_imports(module_ast)
 
             # language does not yet allow recursion for vyi files
             # self.resolve_imports(module_ast)