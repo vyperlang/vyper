932e09aae2b93a1a211a776a994dcb0e6b99d753
Sand Bubbles <sandbubbles07@gmail.com>
2024-10-15 11:00:07 +0200

    add tests

--- tests/functional/syntax/test_import.py
+++ tests/functional/syntax/test_import.py
@@ -7,7 +7,6 @@ from vyper.exceptions import ModuleNotFound
 def test_implicitly_relative_import_crashes(make_input_bundle):
     top = """
 import subdir0.lib0 as lib0
-
 @external
 def foo():
     lib0.foo()
@@ -15,13 +14,11 @@ def foo():
 
     lib0 = """
 import subdir1.lib1 as lib1
-
 def foo():
     lib1.foo()
     """
 
     lib1 = """
-
 def foo():
     pass
     """
@@ -35,7 +32,6 @@ def foo():
 
     lib0 = """
 from subdir1 import lib1 as lib1
-
 def foo():
     lib1.foo()
     """
@@ -48,46 +44,7 @@ def foo():
         compiler.compile_code(top, input_bundle=input_bundle)
 
 
-def test_absolute_path_passes(make_input_bundle):
-    top = """
-import subdir0.lib0 as lib0
-
-@external
-def foo():
-    lib0.foo()
-    """
-
-    lib0 = """
-import subdir0.subdir1.lib1 as lib1
-
-def foo():
-    lib1.foo()
-    """
-
-    lib1 = """
-
-def foo():
-    pass
-    """
-
-    input_bundle = make_input_bundle(
-        {"top.vy": top, "subdir0/lib0.vy": lib0, "subdir0/subdir1/lib1.vy": lib1}
-    )
-    compiler.compile_code(top, input_bundle=input_bundle)
-
-    lib0 = """
-from .subdir1 import lib1 as lib1
-
-def foo():
-    lib1.foo()
-    """
-
-    input_bundle = make_input_bundle(
-        {"top.vy": top, "subdir0/lib0.vy": lib0, "subdir0/subdir1/lib1.vy": lib1}
-    )
-    compiler.compile_code(top, input_bundle=input_bundle)
-
-def test_relative_paths_stay_in_current_directory(make_input_bundle):
+def test_relative_import_searches_only_current_path(make_input_bundle):
     top = """
 from subdir import b as b
 @external
@@ -102,7 +59,6 @@ def foo():
 
     b = """
 from . import a as a
-
 def foo():
     a.foo()
     """
@@ -113,3 +69,67 @@ def foo():
 
     with pytest.raises(ModuleNotFound):
         compiler.compile_code(top, input_bundle=input_bundle)
+
+def test_absolute_import_within_relative_import(make_input_bundle):
+    top = """
+import subdir0.subdir1.c as c
+@external
+def foo():
+    c.foo()
+        """
+    a = """
+import subdir0.b as b
+def foo():
+    b.foo()
+            """
+    b = """
+def foo():
+    pass
+        """
+
+    c = """
+from .. import a as a
+def foo():
+    a.foo()
+        """
+
+    input_bundle = make_input_bundle(
+        {"top.vy": top, "subdir0/a.vy": a, "subdir0/b.vy": b, "subdir0/subdir1/c.vy": c}
+    )
+    compiler.compile_code(top, input_bundle=input_bundle)
+
+
+def test_absolute_path_passes(make_input_bundle):
+    top = """
+import subdir0.lib0 as lib0
+@external
+def foo():
+    lib0.foo()
+    """
+
+    lib0 = """
+import subdir0.subdir1.lib1 as lib1
+def foo():
+    lib1.foo()
+    """
+
+    lib1 = """
+def foo():
+    pass
+    """
+
+    input_bundle = make_input_bundle(
+        {"top.vy": top, "subdir0/lib0.vy": lib0, "subdir0/subdir1/lib1.vy": lib1}
+    )
+    compiler.compile_code(top, input_bundle=input_bundle)
+
+    lib0 = """
+from .subdir1 import lib1 as lib1
+def foo():
+    lib1.foo()
+    """
+
+    input_bundle = make_input_bundle(
+        {"top.vy": top, "subdir0/lib0.vy": lib0, "subdir0/subdir1/lib1.vy": lib1}
+    )
+    compiler.compile_code(top, input_bundle=input_bundle)
