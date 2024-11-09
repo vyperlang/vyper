796794d690d9b35eb819770138f1d40455cab8ad
Sand Bubbles <sandbubbles07@gmail.com>
2024-11-09 09:10:40 +0000

    revert hint check to handle None only

--- vyper/exceptions.py
+++ vyper/exceptions.py
@@ -147,7 +147,7 @@ class _BaseVyperException(Exception):
 
     def _add_hint(self, msg):
         hint = self.hint
-        if hint is None or hint == "":
+        if hint is None:
             return msg
         return msg + f"\n  (hint: {self.hint})"
 
