e62e0a86181f9b41329f24659c6f4af55c78c6c8
Charles Cooper <cooper.charles.m@gmail.com>
2024-11-20 15:25:21 +0100

    style things from review

--- vyper/venom/passes/literals_codesize.py
+++ vyper/venom/passes/literals_codesize.py
@@ -2,6 +2,14 @@ from vyper.utils import evm_not
 from vyper.venom.basicblock import IRLiteral
 from vyper.venom.passes.base_pass import IRPass
 
+# not takes 1 byte1, so it makes sense to use it when we can save at least
+# 1 byte
+NOT_THRESHOLD = 1
+
+# shl takes 3 bytes, so it makes sense to use it when we can save at least
+# 3 bytes
+SHL_THRESHOLD = 3
+
 
 class ReduceLiteralsCodesize(IRPass):
     def run_pass(self):
@@ -23,19 +31,19 @@ class ReduceLiteralsCodesize(IRPass):
             val = op.value % (2**256)
 
             # transform things like 0xffff...01 to (not 0xfe)
-            if len(hex(val)) // 2 - len(hex(evm_not(val))) // 2 > 1:  # not is 1 byte
+            if len(hex(val)) // 2 - len(hex(evm_not(val))) // 2 > NOT_THRESHOLD:
                 inst.opcode = "not"
                 op.value = evm_not(val)
                 continue
 
             # transform things like 0x123400....000 to 0x1234 << ...
             binz = bin(val)[2:]
-            if (ix := len(binz) - binz.rfind("1")) > 24:  # shl is 3 bytes
+            if (ix := len(binz) - binz.rfind("1")) > SHL_THRESHOLD * 8:
                 ix -= 1
-                inst.opcode = "shl"
                 # sanity check
                 assert (val >> ix) << ix == val, val
                 assert (val >> ix) & 1 == 1, val
 
+                inst.opcode = "shl"
                 inst.operands = [IRLiteral(val >> ix), IRLiteral(ix)]
                 continue
