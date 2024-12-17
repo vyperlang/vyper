0cf9f44f2f6bc78eb9dae1e2a893fb1099736412
Harry Kalogirou <harkal@nlogn.eu>
2024-12-17 12:19:56 +0200

    merge in walker stuff

--- vyper/venom/analysis/cfg.py
+++ vyper/venom/analysis/cfg.py
@@ -1,4 +1,4 @@
-from typing import Iterator
+from typing import Callable, Iterator
 
 from vyper.utils import OrderedSet
 from vyper.venom.analysis import IRAnalysis
@@ -34,20 +34,35 @@ class CFGAnalysis(IRAnalysis):
                     bb.add_cfg_out(next_bb)
                     next_bb.add_cfg_in(bb)
 
-        self._compute_dfs_r(self.function.entry)
+        self._compute_dfs_post_r(self.function.entry)
 
-    def _compute_dfs_r(self, bb):
+    def _compute_dfs_post_r(self, bb):
         if bb.is_reachable:
             return
         bb.is_reachable = True
 
         for out_bb in bb.cfg_out:
-            self._compute_dfs_r(out_bb)
+            self._compute_dfs_post_r(out_bb)
 
         self._dfs.add(bb)
 
     @property
-    def dfs_walk(self) -> Iterator[IRBasicBlock]:
+    def dfs_pre_walk(self) -> Iterator[IRBasicBlock]:
+        visited = OrderedSet()
+        def _visit_dfs_pre_r(bb: IRBasicBlock):
+            if bb in visited:
+                return
+            visited.add(bb)
+
+            yield bb
+
+            for out_bb in bb.cfg_out:
+                yield from _visit_dfs_pre_r(out_bb)
+
+        yield from _visit_dfs_pre_r(self.function.entry)
+
+    @property
+    def dfs_post_walk(self) -> Iterator[IRBasicBlock]:
         return iter(self._dfs)
 
     def invalidate(self):
