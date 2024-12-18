192d82c9f0603e542f91a7731ddd8142559c96b8
Charles Cooper <cooper.charles.m@gmail.com>
2024-12-18 15:01:35 -0500

    fix rule for variables

--- vyper/venom/parser.py
+++ vyper/venom/parser.py
@@ -46,7 +46,7 @@ VENOM_GRAMMAR = """
 
     CONST: SIGNED_INT
     OPCODE: CNAME
-    VAR_IDENT: "%" NAME
+    VAR_IDENT: "%" (DIGIT|LETTER|"_"|":")+
 
     # handy for identifier to be an escaped string sometimes
     # (especially for machine-generated labels)
