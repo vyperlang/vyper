0171aab99abf534845ea49e5ea7f8fffbc3d4e99
Charles Cooper <cooper.charles.m@gmail.com>
2024-12-18 11:51:47 -0500

    move around tests, add test comments

--- tests/unit/compiler/venom/test_removeunused.py
+++ tests/unit/compiler/venom/test_removeunused.py
@@ -61,98 +61,10 @@ def test_removeunused_chain():
     _check_pre_post(pre, post)
 
 
-
-def test_removeunused_msize_basic():
-    pre = """
-    main:
-        %a = mload 32
-        %b = msize
-        %c_unused = mload 64  # safe to remove
-        return %b, %b
-    """
-    post = """
-    main:
-        %a = mload 32
-        %b = msize
-        return %b, %b
-    """
-    _check_pre_post(pre, post)
-
-
-def test_removeunused_msize_two_msizes():
-    pre = """
-    main:
-        %a = mload 32
-        %b = msize
-        %c = mload 64  # not safe to remove - has MSIZE effect
-        %d = msize
-        return %b, %d
-    """
-    _check_no_change(pre)
-
-
-def test_removeunused_msize_loop():
-    pre = """
-    main:
-        %msize = msize
-
-        # not safe to remove because the previous instruction is
-        # still reachable
-        %1 = mload %msize
-        jmp @main
-    """
-    _check_no_change(pre)
-
-
-def test_removeunused_msize_branches():
-    pre = """
-    function global {
-        main:
-            %1 = param
-            %2 = mload 10  ; looks unused, but has MSIZE effect
-            jnz %1, @branch1, @branch2
-        branch1:
-            %3 = msize  ; blocks removal of `%2 = mload 10`
-            mstore 10, %3
-            jmp @end
-        branch2:
-            jmp @end
-        end:
-            stop
-    }
-    """
-    _check_no_change(pre, scope="function")
-
-
-def test_removeunused_unused_msize_loop():
-    pre = """
-    main:
-        %1_unused = msize
-        %2_unused = mload 10
-        jmp @main
-    """
-    post = """
-    main:
-        jmp @main
-    """
-    _check_pre_post(pre, post)
-
-
-def test_removeunused_unused_msize():
-    pre = """
-    main:
-        %1_unused = mload 10
-        %2_unused = msize
-        stop
-    """
-    post = """
-    main:
-        stop
-    """
-    _check_pre_post(pre, post)
-
-
 def test_removeunused_loop():
+    """
+    Test unused variable removal in loop
+    """
     pre = """
     main:
         %1 = 10
@@ -173,5 +85,108 @@ def test_removeunused_loop():
         %2 = add %p, 1
         mstore 10, %2
         jmp @after
+    """
+    _check_pre_post(pre, post)
+
+
+def test_removeunused_mload_basic():
+    pre = """
+    main:
+        %a = mload 32
+        %b = msize
+        %c_unused = mload 64  # safe to remove
+        return %b, %b
+    """
+    post = """
+    main:
+        %a = mload 32
+        %b = msize
+        return %b, %b
+    """
+    _check_pre_post(pre, post)
+
+
+def test_removeunused_mload_two_msizes():
+    pre = """
+    main:
+        %a = mload 32
+        %b = msize
+        %c = mload 64  # not safe to remove - has MSIZE effect
+        %d = msize
+        return %b, %d
+    """
+    _check_no_change(pre)
+
+
+def test_removeunused_msize_loop():
+    pre = """
+    main:
+        %1 = msize
+
+        # not safe to remove because the previous instruction is
+        # still reachable
+        %2 = mload %1
+
+        jmp @main
+    """
+    _check_no_change(pre)
+
+
+def test_removeunused_msize_branches():
+    """
+    Test that mload removal is blocked by msize in a downstream basic
+    block
+    """
+    pre = """
+    function global {
+        main:
+            %1 = param
+            %2 = mload 10  ; looks unused, but has MSIZE effect
+            jnz %1, @branch1, @branch2
+        branch1:
+            %3 = msize  ; blocks removal of `%2 = mload 10`
+            mstore 10, %3
+            jmp @end
+        branch2:
+            jmp @end
+        end:
+            stop
+    }
+    """
+    _check_no_change(pre, scope="function")
+
+
+def test_remove_unused_mload_msize_chain():
+    """
+    Test effect chain removal - remove mload which is initially blocked by
+    an msize but is free to be removed after the msize is removed.
+    """
+    pre = """
+    main:
+        %1_unused = mload 10
+        %2_unused = msize
+        stop
+    """
+    post = """
+    main:
+        stop
+    """
+    _check_pre_post(pre, post)
+
+def test_remove_unused_mload_msize_chain_loop():
+    """
+    Test effect chain removal - remove mload which is initially blocked by
+    an msize but is free to be removed after the msize is removed.
+    Loop version.
+    """
+    pre = """
+    main:
+        %1_unused = msize
+        %2_unused = mload 10
+        jmp @main
+    """
+    post = """
+    main:
+        jmp @main
     """
     _check_pre_post(pre, post)
