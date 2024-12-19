5f58a303dab8f69dcc0a42fb4040f5e6b8286a4e
Charles Cooper <cooper.charles.m@gmail.com>
2024-12-18 20:32:42 -0500

    Revert "feat[venom]: disable legacy optimizer in the venom pipeline"
    This reverts commit f0df458bb41e887869ade22e5e7b3bc12bf54a39.
    
    don't always disable since it breaks tests and potentially other
    functionality. we will disable at the relevant call sites.

--- vyper/ir/optimizer.py
+++ vyper/ir/optimizer.py
@@ -2,7 +2,6 @@ import operator
 from typing import List, Optional, Tuple, Union
 
 from vyper.codegen.ir_node import IRnode
-from vyper.compiler.settings import get_global_settings
 from vyper.evm.opcodes import version_check
 from vyper.exceptions import CompilerPanic, StaticAssertionException
 from vyper.utils import (
@@ -418,9 +417,6 @@ def _check_symbols(symbols, ir_node):
 
 
 def optimize(node: IRnode) -> IRnode:
-    settings = get_global_settings()
-    if settings is not None and settings.experimental_codegen:
-        return node
     _, ret = _optimize(node, parent=None)
     return ret
 
