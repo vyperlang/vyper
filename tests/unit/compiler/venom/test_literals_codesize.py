109fce15413091836919988a06b1b487b219a026
Charles Cooper <cooper.charles.m@gmail.com>
2024-12-20 12:14:01 -0500

    add check that the optimization actually works

--- tests/unit/compiler/venom/test_literals_codesize.py
+++ tests/unit/compiler/venom/test_literals_codesize.py
@@ -6,11 +6,22 @@ from vyper.venom.basicblock import IRLiteral
 from vyper.venom.context import IRContext
 from vyper.venom.passes import ReduceLiteralsCodesize
 
+
+def _calc_push_size(val: int):
+    s = hex(val).removeprefix("0x")
+    if len(s) % 2 != 0:  # justify to multiple of 2
+        s = "0" + s
+    return 1 + len(s)
+
+
 should_invert = [2**256 - 1] + [((2**i) - 1) << (256 - i) for i in range(121, 256 + 1)]
 
 
 @pytest.mark.parametrize("orig_value", should_invert)
 def test_literal_codesize_ff_inversion(orig_value):
+    """
+    Test that literals like 0xfffffffffffabcd get inverted to `not 0x5432`
+    """
     ctx = IRContext()
     fn = ctx.create_function("_global")
     bb = fn.get_basic_block()
@@ -20,8 +31,13 @@ def test_literal_codesize_ff_inversion(orig_value):
     ac = IRAnalysesCache(fn)
     ReduceLiteralsCodesize(ac, fn).run_pass()
 
-    assert bb.instructions[0].opcode == "not"
-    assert evm_not(bb.instructions[0].operands[0].value) == orig_value
+    inst0 = bb.instructions[0]
+    assert inst0.opcode == "not"
+    op0 = inst0.operands[0]
+    assert evm_not(op0.value) == orig_value
+    # check the optimization actually improved codesize, after accounting
+    # for the addl NOT instruction
+    assert _calc_push_size(op0.value) + 1 < _calc_push_size(orig_value)
 
 
 should_not_invert = [1, 0xFE << 248 | (2**248 - 1)] + [
@@ -31,6 +47,9 @@ should_not_invert = [1, 0xFE << 248 | (2**248 - 1)] + [
 
 @pytest.mark.parametrize("orig_value", should_not_invert)
 def test_literal_codesize_no_inversion(orig_value):
+    """
+    Check funky cases where inversion would result in bytecode increase
+    """
     ctx = IRContext()
     fn = ctx.create_function("_global")
     bb = fn.get_basic_block()
@@ -53,6 +72,9 @@ should_shl = (
 
 @pytest.mark.parametrize("orig_value", should_shl)
 def test_literal_codesize_shl(orig_value):
+    """
+    Test that literals like 0xabcd00000000 get transformed to `shl 32 0xabcd`
+    """
     ctx = IRContext()
     fn = ctx.create_function("_global")
     bb = fn.get_basic_block()
@@ -66,6 +88,10 @@ def test_literal_codesize_shl(orig_value):
     op0, op1 = bb.instructions[0].operands
     assert op0.value << op1.value == orig_value
 
+    # check the optimization actually improved codesize, after accounting
+    # for the addl PUSH and SHL instructions
+    assert _calc_push_size(op0.value) + _calc_push_size(op1.value) + 1 < _calc_push_size(orig_value)
+
 
 should_not_shl = [1 << i for i in range(0, 3 * 8)] + [
     0x0,
@@ -75,6 +101,9 @@ should_not_shl = [1 << i for i in range(0, 3 * 8)] + [
 
 @pytest.mark.parametrize("orig_value", should_not_shl)
 def test_literal_codesize_no_shl(orig_value):
+    """
+    Check funky cases where shl transformation would result in bytecode increase
+    """
     ctx = IRContext()
     fn = ctx.create_function("_global")
     bb = fn.get_basic_block()
