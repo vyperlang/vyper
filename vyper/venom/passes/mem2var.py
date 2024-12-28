7c79be7a27a94a660b6dc23b56e150bafcd5d478
Charles Cooper <cooper.charles.m@gmail.com>
2024-12-28 08:25:20 -0500

    mem2var debug

--- vyper/venom/passes/mem2var.py
+++ vyper/venom/passes/mem2var.py
@@ -19,20 +19,20 @@ class Mem2Var(IRPass):
         self.var_name_count = 0
         for var, inst in dfg.outputs.items():
             if inst.opcode == "alloca":
-                self._process_alloca_var(dfg, var)
+                self._process_alloca_var(dfg, inst, var)
             elif inst.opcode == "palloca":
                 self._process_palloca_var(dfg, inst, var)
 
         self.analyses_cache.invalidate_analysis(DFGAnalysis)
         self.analyses_cache.invalidate_analysis(LivenessAnalysis)
 
-    def _mk_varname(self, varname: str):
+    def _mk_varname(self, varname: str, alloca_id: int):
         varname = varname.removeprefix("%")
-        varname = f"@var{varname}_{self.var_name_count}"
+        varname = f"@alloca_{alloca_id}_{varname}_{self.var_name_count}"
         self.var_name_count += 1
         return varname
 
-    def _process_alloca_var(self, dfg: DFGAnalysis, var: IRVariable):
+    def _process_alloca_var(self, dfg: DFGAnalysis, alloca_inst, var: IRVariable):
         """
         Process alloca allocated variable. If it is only used by
         mstore/mload/return instructions, it is promoted to a stack variable.
@@ -42,7 +42,8 @@ class Mem2Var(IRPass):
         if not all([inst.opcode in ["mstore", "mload", "return"] for inst in uses]):
             return
 
-        var_name = self._mk_varname(var.name)
+        alloca_id = alloca_inst.operands[2]
+        var_name = self._mk_varname(var.value, alloca_id.value)
         var = IRVariable(var_name)
         for inst in uses:
             if inst.opcode == "mstore":
@@ -68,12 +69,13 @@ class Mem2Var(IRPass):
         if not all(inst.opcode in ["mstore", "mload"] for inst in uses):
             return
 
-        var_name = self._mk_varname(var.name)
+        ofst, _size, alloca_id = palloca_inst.operands
+        var_name = self._mk_varname(var.value, alloca_id.value)
         var = IRVariable(var_name)
 
         # some value given to us by the calling convention
         palloca_inst.opcode = "mload"
-        palloca_inst.operands = [palloca_inst.operands[0]]
+        palloca_inst.operands = [ofst]
         palloca_inst.output = var
 
         for inst in uses:
