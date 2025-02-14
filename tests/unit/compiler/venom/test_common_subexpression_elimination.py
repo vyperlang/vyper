0e382aeec4c8c1f92bfc7b527a99e0f91dd6579b
Hodan <plodeada@fit.cvut.cz>
2025-02-14 14:42:11 +0100

    hevm tests for cse

--- tests/unit/compiler/venom/test_common_subexpression_elimination.py
+++ tests/unit/compiler/venom/test_common_subexpression_elimination.py
@@ -3,18 +3,26 @@ import pytest
 from tests.venom_utils import assert_ctx_eq, parse_from_basic_block
 from vyper.venom.analysis.analysis import IRAnalysesCache
 from vyper.venom.passes.common_subexpression_elimination import CSE
+from tests.hevm import hevm_check_venom
 
 
-def _check_pre_post(pre: str, post: str):
+pytestmark = pytest.mark.hevm
+
+def _check_pre_post(pre: str, post: str, hevm: bool = True):
     ctx = parse_from_basic_block(pre)
     for fn in ctx.functions.values():
         ac = IRAnalysesCache(fn)
         CSE(ac, fn).run_pass()
     assert_ctx_eq(ctx, parse_from_basic_block(post))
 
+    if not hevm:
+        return
 
-def _check_no_change(pre: str):
-    _check_pre_post(pre, pre)
+    hevm_check_venom(pre, post)
+
+
+def _check_no_change(pre: str, hevm: bool = True):
+    _check_pre_post(pre, pre, hevm)
 
 
 def test_cse():
@@ -25,7 +33,7 @@ def test_cse():
         %mul1 = mul %sum1, 10
         %sum2 = add %1, 10
         %mul2 = mul %sum2, 10
-        return %mul1, %mul2
+        sink %mul1, %mul2
     """
 
     post = """
@@ -35,7 +43,7 @@ def test_cse():
         %mul1 = mul %sum1, 10
         %sum2 = %sum1
         %mul2 = %mul1
-        return %mul1, %mul2
+        sink %mul1, %mul2
     """
 
     _check_pre_post(pre, post)
@@ -49,7 +57,7 @@ def test_cse_commutative():
         %mul1 = mul %sum1, 10
         %sum2 = add 10, %1
         %mul2 = mul 10, %sum2
-        return %mul1, %mul2
+        sink %mul1, %mul2
     """
 
     post = """
@@ -59,7 +67,7 @@ def test_cse_commutative():
         %mul1 = mul %sum1, 10
         %sum2 = %sum1
         %mul2 = %mul1
-        return %mul1, %mul2
+        sink %mul1, %mul2
     """
 
     _check_pre_post(pre, post)
@@ -74,7 +82,7 @@ def test_cse_no_commutative():
         %1 = param
         %sum1 = sub %1, 10
         %sum2 = sub 10, %1
-        return %sum1, %sum2
+        sink %sum1, %sum2
     """
 
     _check_no_change(pre)
@@ -97,7 +105,7 @@ def test_cse_effects_1():
         ; mloads are separated with barrier
         %1 = add %mload1, 10
         %2 = add %mload2, 10
-        return %1, %2
+        sink %1, %2
     """
 
     _check_no_change(pre)
@@ -123,7 +131,7 @@ def test_cse_effects_2():
         ; this cannot be substituted since mload1
         ; and mload2 are separated by barrier
         %3 = add %mload2, 10
-        return %1, %2
+        sink %1, %2
     """
 
     post = """
@@ -135,7 +143,7 @@ def test_cse_effects_2():
         %mload2 = mload 0
         %2 = %1
         %3 = add %mload2, 10
-        return %1, %2
+        sink %1, %2
     """
 
     _check_pre_post(pre, post)
@@ -150,7 +158,7 @@ def test_cse_logs_no_indepontent():
         stop
     """
 
-    _check_no_change(pre)
+    _check_no_change(pre, hevm=False)
 
 
 def test_cse_effects_3():
@@ -184,7 +192,7 @@ def test_cse_effect_mstore():
         mstore 0, %1
         %mload2 = mload 0
         %res = add %mload1, %mload2
-        return %res
+        sink %res
     """
 
     post = """
@@ -196,7 +204,7 @@ def test_cse_effect_mstore():
         nop
         %mload2 = %mload1
         %res = add %mload1, %mload2
-        return %res
+        sink %res
     """
 
     _check_pre_post(pre, post)
@@ -217,7 +225,7 @@ def test_cse_effect_mstore_with_msize():
         %msize = msize
         %res1 = add %mload1, %msize
         %res2 = add %mload2, %msize
-        return %res1, %res2
+        sink %res1, %res2
     """
 
     _check_no_change(pre)
@@ -253,10 +261,10 @@ def test_cse_different_branches_cannot_optimize():
         ; branch this expression came from
         ; so you cannot substitute
         {same(3)}
-        return %m1, %m2, %m3
+        sink %m1, %m2, %m3
     """
 
-    _check_no_change(pre)
+    _check_no_change(pre, hevm=False)
 
 
 def test_cse_different_branches_can_optimize():
@@ -292,7 +300,7 @@ def test_cse_different_branches_can_optimize():
         jmp @join
     join:
         {same(3)}
-        return %m0, %m1, %m2, %m3
+        sink %m0, %m1, %m2, %m3
     """
 
     post = f"""
@@ -309,11 +317,24 @@ def test_cse_different_branches_can_optimize():
         jmp @join
     join:
         {same_opt(3)}
-        return %m0, %m1, %m2, %m3
+        sink %m0, %m1, %m2, %m3
     """
 
-    _check_pre_post(pre, post)
+    _check_pre_post(pre, post, hevm=False)
 
+
+def test_cse_invoke_idempotent():
+    pre = f"""
+    main:
+        ; invoke
+        %invoke0 = invoke @f
+        %i0 = add 1, %invoke0
+        %invoke1 = invoke @f
+        %i1 = add 1, %invoke1
+        sink %i0, %i1
+    """
+
+    _check_no_change(pre, hevm=False)
 
 def test_cse_non_idempotent():
     """
@@ -334,12 +355,6 @@ def test_cse_non_idempotent():
 
     pre = f"""
     main:
-        ; invoke
-        %invoke0 = invoke @f
-        %i0 = add 1, %invoke0
-        %invoke1 = invoke @f
-        %i1 = add 1, %invoke1
-
         ; staticcall
         {call("staticcall", 0, "s")}
 
@@ -348,7 +363,7 @@ def test_cse_non_idempotent():
 
         ; call
         {call("call", 2, "c")}
-        return %i0, %i1, %s0, %s1, %d0, %d1, %c0, %c1
+        sink %s0, %s1, %d0, %d1, %c0, %c1
     """
 
     _check_no_change(pre)
@@ -433,7 +448,7 @@ def test_cse_immutable_queries(opcode):
         %res1 = add %1, 1
         %2 = {opcode}
         %res2 = add %1, 1
-        return %res1, %res2
+        sink %res1, %res2
     """
 
     post = f"""
@@ -442,10 +457,13 @@ def test_cse_immutable_queries(opcode):
         %res1 = add %1, 1
         %2 = {opcode}
         %res2 = %res1
-        return %res1, %res2
+        sink %res1, %res2
     """
 
-    _check_pre_post(pre, post)
+    # the codesize will be for sure different size
+    # result of code is dependant on code which will
+    # be different
+    _check_pre_post(pre, post, hevm=opcode != "codesize")
 
 
 @pytest.mark.parametrize(
