b3443b0c05849a94c193281b7f830a8612a5e854
Charles Cooper <cooper.charles.m@gmail.com>
2025-02-20 10:28:02 +0100

    lint

--- vyper/venom/passes/function_inliner.py
+++ vyper/venom/passes/function_inliner.py
@@ -6,14 +6,7 @@ from vyper.utils import OrderedSet
 from vyper.venom.analysis.cfg import CFGAnalysis
 from vyper.venom.analysis.dfg import DFGAnalysis
 from vyper.venom.analysis.fcg import FCGAnalysis
-from vyper.venom.basicblock import (
-    IRBasicBlock,
-    IRInstruction,
-    IRLabel,
-    IRLiteral,
-    IROperand,
-    IRVariable,
-)
+from vyper.venom.basicblock import IRBasicBlock, IRInstruction, IRLabel, IROperand, IRVariable
 from vyper.venom.function import IRFunction
 from vyper.venom.passes import FloatAllocas
 from vyper.venom.passes.base_pass import IRGlobalPass
