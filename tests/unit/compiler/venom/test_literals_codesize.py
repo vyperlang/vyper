7ed274b1977afa984514bfd1427461c1c343058b
plodeada <plodeada@fit.cvut.cz>
2025-02-21 11:34:18 +0100

    literal codesize tests

--- tests/unit/compiler/venom/test_literals_codesize.py
+++ tests/unit/compiler/venom/test_literals_codesize.py
@@ -1,5 +1,7 @@
 import pytest
 
+from tests.hevm import hevm_check_venom
+from tests.venom_utils import assert_ctx_eq, parse_from_basic_block
 from vyper.utils import evm_not
 from vyper.venom.analysis import IRAnalysesCache
 from vyper.venom.basicblock import IRLiteral
@@ -14,6 +16,24 @@ def _calc_push_size(val: int):
     return 1 + len(s)
 
 
+def _check_pre_post(pre: str, post: str, hevm: bool = True):
+    ctx = parse_from_basic_block(pre)
+    for fn in ctx.functions.values():
+        ac = IRAnalysesCache(fn)
+        ReduceLiteralsCodesize(ac, fn).run_pass()
+
+    assert_ctx_eq(ctx, parse_from_basic_block(post))
+
+    if not hevm:
+        return
+
+    hevm_check_venom(pre, post)
+
+
+def _check_no_change(pre):
+    _check_pre_post(pre, pre, hevm=False)
+
+
 should_invert = [2**256 - 1] + [((2**i) - 1) << (256 - i) for i in range(121, 256 + 1)]
 
 
@@ -22,22 +42,25 @@ def test_literal_codesize_ff_inversion(orig_value):
     """
     Test that literals like 0xfffffffffffabcd get inverted to `not 0x5432`
     """
-    ctx = IRContext()
-    fn = ctx.create_function("_global")
-    bb = fn.get_basic_block()
+    pre = f"""
+    main:
+        %1 = {orig_value}
+        sink %1
+    """
 
-    bb.append_instruction("store", IRLiteral(orig_value))
-    bb.append_instruction("stop")
-    ac = IRAnalysesCache(fn)
-    ReduceLiteralsCodesize(ac, fn).run_pass()
+    not_val = evm_not(orig_value)
 
-    inst0 = bb.instructions[0]
-    assert inst0.opcode == "not"
-    op0 = inst0.operands[0]
-    assert evm_not(op0.value) == orig_value
+    post = f"""
+    main:
+        %1 = not {not_val}
+        sink %1
+    """
+
+    _check_pre_post(pre, post)
+
     # check the optimization actually improved codesize, after accounting
     # for the addl NOT instruction
-    assert _calc_push_size(op0.value) + 1 < _calc_push_size(orig_value)
+    assert _calc_push_size(not_val) + 1 < _calc_push_size(orig_value)
 
 
 should_not_invert = [1, 0xFE << 248 | (2**248 - 1)] + [
@@ -50,17 +73,14 @@ def test_literal_codesize_no_inversion(orig_value):
     """
     Check funky cases where inversion would result in bytecode increase
     """
-    ctx = IRContext()
-    fn = ctx.create_function("_global")
-    bb = fn.get_basic_block()
 
-    bb.append_instruction("store", IRLiteral(orig_value))
-    bb.append_instruction("stop")
-    ac = IRAnalysesCache(fn)
-    ReduceLiteralsCodesize(ac, fn).run_pass()
+    pre = f"""
+    main:
+        %1 = {orig_value}
+        sink %1
+    """
 
-    assert bb.instructions[0].opcode == "store"
-    assert bb.instructions[0].operands[0].value == orig_value
+    _check_no_change(pre)
 
 
 should_shl = (
@@ -104,14 +124,10 @@ def test_literal_codesize_no_shl(orig_value):
     """
     Check funky cases where shl transformation would result in bytecode increase
     """
-    ctx = IRContext()
-    fn = ctx.create_function("_global")
-    bb = fn.get_basic_block()
+    pre = f"""
+    main:
+        %1 = {orig_value}
+        sink %1
+    """
 
-    bb.append_instruction("store", IRLiteral(orig_value))
-    bb.append_instruction("stop")
-    ac = IRAnalysesCache(fn)
-    ReduceLiteralsCodesize(ac, fn).run_pass()
-
-    assert bb.instructions[0].opcode == "store"
-    assert bb.instructions[0].operands[0].value == orig_value
+    _check_no_change(pre)
