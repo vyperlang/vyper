5a7c3147df7a8be1f09871c10149c6dc2e0887c3
Charles Cooper <cooper.charles.m@gmail.com>
2025-02-28 15:14:51 +0100

    fix lint

--- tests/functional/examples/thirdparty/test_thirdparty.py
+++ tests/functional/examples/thirdparty/test_thirdparty.py
@@ -1,15 +1,16 @@
-import pytest
 import glob
 from pathlib import Path
-import vyper.compiler as compiler
-import os
 
+import pytest
+
+import vyper.compiler as compiler
 
 dir_path = Path(__file__).parent
 
-def get_example_vy_filenames():
 
-    return glob.glob("**/*.vy", root_dir = dir_path, recursive=True)
+def get_example_vy_filenames():
+    return glob.glob("**/*.vy", root_dir=dir_path, recursive=True)
+
 
 @pytest.mark.parametrize("vy_filename", get_example_vy_filenames())
 def test_compile(vy_filename):
