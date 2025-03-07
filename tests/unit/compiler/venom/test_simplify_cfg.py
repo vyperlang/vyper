eeefbcb47edb36b2e561096342fdc6bc18a98840
Charles Cooper <cooper.charles.m@gmail.com>
2025-03-07 14:24:56 +0100

    add a cfg simplification test

--- tests/unit/compiler/venom/test_simplify_cfg.py
+++ tests/unit/compiler/venom/test_simplify_cfg.py
@@ -32,3 +32,39 @@ def test_phi_reduction_after_block_pruning():
     """
 
     _check_pre_post(pre, post)
+
+
+def test_block_merging():
+    """
+    demonstrate merging of basic blocks (even if they occur "out of order"
+    in the function)
+    """
+    pre = """
+    _global:
+        %1 = param
+        %2 = param
+        jmp @b
+    a:
+        ; demonstrate order of basic blocks
+        %3 = %1
+        sstore 0, %3
+        jmp @c
+    b:
+        %4 = %2
+        sstore 1, %4
+        jmp @a
+    c:
+        sink %3, %4
+    """
+    post = """
+    _global:
+        %1 = param
+        %2 = param
+        %4 = %2
+        sstore 1, %4
+        %3 = %1
+        sstore 0, %3
+        sink %3, %4
+    """
+
+    _check_pre_post(pre, post)
