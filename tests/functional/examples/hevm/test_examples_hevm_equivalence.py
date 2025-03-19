a97641fcb8421709ee51257f307a4e390e9004ac
cyberthirst <cyberthirst.eth@gmail.com>
2025-03-19 10:42:53 +0100

    refactor hevm example tests to passing and other xfail in 10s

--- tests/functional/examples/hevm/test_examples_hevm_equivalence.py
+++ tests/functional/examples/hevm/test_examples_hevm_equivalence.py
@@ -1,34 +1,27 @@
-import os
-import pytest
-import glob
 from pathlib import Path
 
+import pytest
 
-pytestmark = pytest.mark.hevm
-
-# List of contracts to exclude from testing
-EXCLUDED_CONTRACTS = [
-    "examples/wallet/wallet.vy", # CopySlice with a symbolically sized region not currently implemented
-    "examples/name_registry/name_registry.vy", # CopySlice with a symbolically sized region not currently implemented
-    "examples/auctions/blind_auction.vy", # 1x -> SMT result timeout/unknown for TIMEOUT=600
-    "examples/tokens/ERC1155ownable.vy", # long time to finish
-    "examples/voting/ballot.vy", # long time to finish
-    "examples/tokens/ERC721.vy" # long time to finish
-    "examples/crowdfund.vy"  # ??
-]
+PASSING = ["examples/storage/storage.vy", "examples/storage/advanced_storage.vy"]
 
 
 def get_example_contracts():
     root = Path(__file__).parent.parent.parent.parent.parent
     examples_dir = root / "examples"
-    vy_file_paths = [str(p) for p in examples_dir.rglob("*.vy")]
-    ignored_set = [str(root / path) for path in EXCLUDED_CONTRACTS]
-    filtered = [path for path in vy_file_paths if path not in ignored_set]
-    return filtered
+    contract_paths = [str(p) for p in examples_dir.rglob("*.vy")]
+    return contract_paths
 
+
+@pytest.mark.hevm("--smttimeout", "10")
 @pytest.mark.parametrize("contract_path", get_example_contracts())
-def test_compile_example_contract(get_contract, contract_path):
-    with open(contract_path, 'r') as f:
+def test_compile_example_contract(get_contract, contract_path, request):
+    for contract in PASSING:
+        if contract_path.endswith(contract):
+            break
+    else:
+        request.node.add_marker(pytest.mark.xfail(strict=True))
+
+    with open(contract_path, "r") as f:
         source_code = f.read()
 
     _ = get_contract(source_code)
