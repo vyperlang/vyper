cc7d27a60f6118fa819ab96a423b5a4ff0573c68
Harry Kalogirou <harkal@nlogn.eu>
2025-03-21 10:02:04 +0200

    test volatile

--- tests/unit/compiler/venom/test_dead_store_elimination.py
+++ tests/unit/compiler/venom/test_dead_store_elimination.py
@@ -1,12 +1,67 @@
 import pytest
 
-from tests.venom_utils import PrePostChecker
+from tests.venom_utils import PrePostChecker, parse_from_basic_block, assert_ctx_eq
 from vyper.venom.passes import DeadStoreElimination
+from vyper.venom.passes.base_pass import IRPass
+from vyper.venom.analysis.mem_ssa import MemSSA
+from vyper.venom.analysis import IRAnalysesCache
 
 pytestmark = pytest.mark.hevm
 
 _check_pre_post = PrePostChecker([DeadStoreElimination])
 
+
+class VolatilePrePostChecker(PrePostChecker):
+    def __init__(self, passes: list[type], volatile_locations=None, post=None, default_hevm=True):
+        super().__init__(passes, post, default_hevm)
+        if volatile_locations is None:
+            self.volatile_locations = []
+        else:
+            self.volatile_locations = volatile_locations
+
+    def __call__(self, pre: str, post: str, hevm: bool | None = None) -> list[IRPass]:
+        from vyper.venom.basicblock import MemoryLocation, IROperand
+        
+        self.pass_objects.clear()
+        
+        if hevm is None:
+            hevm = self.default_hevm
+            
+        pre_ctx = parse_from_basic_block(pre)
+        for fn in pre_ctx.functions.values():
+            ac = IRAnalysesCache(fn)
+            
+            mem_ssa = ac.request_analysis(MemSSA)
+            
+            for loc in self.volatile_locations:
+                volatile_loc = MemoryLocation(
+                    base=IROperand(loc),
+                    offset=0, 
+                    size=32,
+                    is_volatile=True
+                )
+                mem_ssa.mark_location_volatile(volatile_loc)
+            
+            for p in self.passes:
+                obj = p(ac, fn)
+                self.pass_objects.append(obj)
+                obj.run_pass()
+        
+        post_ctx = parse_from_basic_block(post)
+        for fn in post_ctx.functions.values():
+            ac = IRAnalysesCache(fn)
+            for p in self.post_passes:
+                obj = p(ac, fn)
+                self.pass_objects.append(obj)
+                obj.run_pass()
+        
+        assert_ctx_eq(pre_ctx, post_ctx)
+        
+        if hevm:
+            from tests.hevm import hevm_check_venom
+            hevm_check_venom(pre, post)
+        
+        return self.pass_objects
 
 def test_basic_dead_store():
     pre = """
@@ -185,3 +240,83 @@ def test_multiple_dead_stores():
             stop
     """
     _check_pre_post(pre, post)
+
+
+def test_volatile_location_store():
+    volatile_addr = 0xFFFF0000
+    pre = """
+        _global:
+            %val = 42
+            mstore 0xFFFF0000, %val  ; This store should not be eliminated because the location will be marked as volatile
+            stop
+    """
+    
+    # Should not change - volatile stores are preserved
+    checker = VolatilePrePostChecker([DeadStoreElimination], volatile_locations=[volatile_addr])
+    checker(pre, pre)
+
+
+def test_volatile_vs_non_volatile_store():
+    volatile_addr = 0xFFFF0000
+    non_volatile_addr = 0x1000
+    
+    pre = """
+        _global:
+            %val1 = 42
+            %val2 = 24
+            
+            mstore 0x1000, %val1
+            
+            mstore 0xFFFF0000, %val2
+            
+            stop
+    """
+    
+    post = """
+        _global:
+            %val1 = 42
+            %val2 = 24
+            
+            nop
+            
+            mstore 0xFFFF0000, %val2
+            
+            stop
+    """
+    
+    checker = VolatilePrePostChecker([DeadStoreElimination], volatile_locations=[volatile_addr])
+    checker(pre, post)
+
+def test_multiple_volatile_locations():
+    volatile_addrs = [0xFFFF0000, 0xFFFF1000]
+    
+    pre = """
+        _global:
+            %val1 = 42
+            %val2 = 24
+            %val3 = 84
+            
+            mstore 0xFFFF0000, %val1
+            mstore 0xFFFF1000, %val2
+            
+            mstore 0x2000, %val3
+            
+            stop
+    """
+    
+    post = """
+        _global:
+            %val1 = 42
+            %val2 = 24
+            %val3 = 84
+            
+            mstore 0xFFFF0000, %val1
+            mstore 0xFFFF1000, %val2
+            
+            nop
+            
+            stop
+    """
+
+    checker = VolatilePrePostChecker([DeadStoreElimination], volatile_locations=volatile_addrs)
+    checker(pre, post)
