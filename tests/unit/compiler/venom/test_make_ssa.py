7bb966c88595fc8ddde5ed4e56c2e96fdf057fdb
Hodan <plodeada@fit.cvut.cz>
2025-03-24 17:07:04 +0100

    start of the test

--- tests/unit/compiler/venom/test_make_ssa.py
+++ tests/unit/compiler/venom/test_make_ssa.py
@@ -1,6 +1,6 @@
-from tests.venom_utils import assert_ctx_eq, parse_venom
+from tests.venom_utils import assert_ctx_eq, parse_venom, parse_from_basic_block
 from vyper.venom.analysis import IRAnalysesCache
-from vyper.venom.passes import MakeSSA
+from vyper.venom.passes import MakeSSA, Mem2Var, RemoveUnusedVariablesPass
 
 
 def _check_pre_post(pre, post):
@@ -50,3 +50,34 @@ def test_phi_case():
     }
     """
     _check_pre_post(pre, post)
+
+def test_multiple_make_ssa_error():
+    pre = """
+    main:
+        %v = mload 64
+        jmp @test
+    test:
+        jnz %v, @then, @else
+    then:
+        %t = mload 96
+        assert %t
+        jmp @if_exit
+    else:
+        jmp @if_exit
+    if_exit:
+        %v = add %v, 1
+        jmp @test
+    """
+
+    ctx = parse_from_basic_block(pre)
+    print("hello")
+    print(ctx)
+    for fn in ctx.functions.values():
+        ac = IRAnalysesCache(fn)
+        MakeSSA(ac, fn).run_pass()
+        Mem2Var(ac, fn).run_pass()
+        MakeSSA(ac, fn).run_pass()
+        #RemoveUnusedVariablesPass(ac, fn).run_pass()
+
+    print(ctx)
+    assert False
