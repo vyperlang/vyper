9036106e7f719622c3d7e9e62d52479f774f0f65
Harry Kalogirou <harkal@nlogn.eu>
2025-03-27 14:34:38 +0200

    no longer scheduler regression

--- tests/functional/codegen/types/test_lists.py
+++ tests/functional/codegen/types/test_lists.py
@@ -193,7 +193,6 @@ def test_array(x: int128, y: int128, z: int128, w: int128) -> int128:
     assert c.test_array(2, 7, 1, 8) == -5454
 
 
-@pytest.mark.venom_xfail(raises=StackTooDeep, reason="stack scheduler regression")
 def test_four_d_array_accessor(get_contract):
     four_d_array_accessor = """
 @external
