c100d1b34ed4ec0b6798935fa7c9c3013df27f5f
Harry Kalogirou <harkal@nlogn.eu>
2025-03-30 22:12:48 +0300

    tests

---
+++ tests/unit/compiler/venom/test_redundant_load_elimination.py
@@ -0,0 +1,56 @@
+import pytest
+
+from tests.venom_utils import PrePostChecker
+from vyper.venom.passes import RedundantLoadElimination
+
+pytestmark = pytest.mark.hevm
+
+_check_pre_post = PrePostChecker([RedundantLoadElimination])
+
+
+def test_basic_redundant_load():
+    pre = """
+        _global:
+            %val = 42
+            mstore 0, %val
+            %loaded1 = mload 0
+            %loaded2 = mload 0
+            stop
+    """
+    post = """
+        _global:
+            %val = 42
+            mstore 0, %val
+            %loaded1 = mload 0
+            %loaded2 = store %loaded1
+            stop
+    """
+    _check_pre_post(pre, post)
+
+
+def test_redundant_load_after_store():
+    pre = """
+        _global:
+            %val1 = 42
+            %val2 = 24
+            mstore 0, %val1
+            %loaded1 = mload 0
+            mstore 0, %val2
+            %loaded2 = mload 0
+            stop
+    """
+    _check_pre_post(pre, pre)
+
+
+def test_redundant_load_different_locations():
+    pre = """
+        _global:
+            %val1 = 42
+            %val2 = 24
+            mstore 0, %val1
+            %loaded1 = mload 0
+            mstore 32, %val2
+            %loaded2 = mload 32
+            stop
+    """
+    _check_pre_post(pre, pre)
