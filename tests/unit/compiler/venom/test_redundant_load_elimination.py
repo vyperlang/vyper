7ddf2752b9c9bedd0b2463dce327bc6d6fc573e1
Harry Kalogirou <harkal@nlogn.eu>
2025-03-30 22:38:37 +0300

    branching tests

--- tests/unit/compiler/venom/test_redundant_load_elimination.py
+++ tests/unit/compiler/venom/test_redundant_load_elimination.py
@@ -54,3 +54,99 @@ def test_redundant_load_different_locations():
             stop
     """
     _check_pre_post(pre, pre)
+
+
+def test_redundant_load_in_branches():
+    pre = """
+        _global:
+            %cond = 1
+            %val = 42
+            mstore 0, %val
+            jnz %cond, @then, @else
+        then:
+            %loaded1 = mload 0
+            jmp @merge
+        else:
+            %loaded2 = mload 0
+            jmp @merge
+        merge:
+            %loaded3 = mload 0
+            stop
+    """
+    post = """
+        _global:
+            %cond = 1
+            %val = 42
+            mstore 0, %val
+            jnz %cond, @then, @else
+        then:
+            %loaded1 = mload 0
+            jmp @merge
+        else:
+            %loaded2 = store %loaded1
+            jmp @merge
+        merge:
+            %loaded3 = store %loaded1
+            stop
+    """
+    _check_pre_post(pre, post)
+
+
+def test_not_redundant_load_with_phi():
+    pre = """
+        _global:
+            %cond = 1
+            %val1 = 42
+            %val2 = 24
+            jnz %cond, @then, @else
+        then:
+            mstore 0, %val1
+            %loaded1 = mload 0
+            jmp @merge
+        else:
+            mstore 0, %val2
+            %loaded2 = mload 0
+            jmp @merge
+        merge:
+            %loaded3 = mload 0
+            stop
+    """
+    _check_pre_post(pre, pre)
+
+
+def test_redundant_load_in_loop():
+    pre = """
+        _global:
+            %val = 42
+            %i = 0
+            mstore 0, %val
+            %loaded1 = mload 0
+            jmp @loop
+        loop:
+            %cond = lt %i, 5
+            jnz %cond, @body, @exit
+        body:
+            %loaded2 = mload 0
+            %i = add %i, 1
+            jmp @loop
+        exit:
+            stop
+    """
+    post = """
+        _global:
+            %val = 42
+            %i = 0
+            mstore 0, %val
+            %loaded1 = mload 0
+            jmp @loop
+        loop:
+            %cond = lt %i, 5
+            jnz %cond, @body, @exit
+        body:
+            %loaded2 = store %loaded1
+            %i = add %i, 1
+            jmp @loop
+        exit:
+            stop
+    """
+    _check_pre_post(pre, post)
