d5677b167924197e44a64f89685b8377fd79ff4b
Charles Cooper <cooper.charles.m@gmail.com>
2025-04-08 10:21:36 +0200

    feat[lang]: enable bitwise ops for `bytesM` types (#4538)
    this commit adds bitwise ops (`&`, `|`, `<<`, `>>`, `^`, `~`) for
    `bytesM` types. for implementation simplicity, shift and invert
    operators are only implemented for `bytes32`.
    
    ---------
    
    Co-authored-by: cyberthirst <cyberthirst.eth@gmail.com>

--- tests/functional/builtins/codegen/test_bitwise.py
+++ tests/functional/builtins/codegen/test_bitwise.py
@@ -1,43 +1,181 @@
+import hypothesis.strategies as st
 import pytest
+from hypothesis import given, settings
 
 from vyper.compiler import compile_code
-from vyper.exceptions import InvalidLiteral, InvalidOperation, TypeMismatch
+from vyper.exceptions import InvalidLiteral, InvalidOperation, TypeMismatch, UnimplementedException
 from vyper.utils import unsigned_to_signed
 
-code = """
+
+def get_code_for_type(typ, use_shift=True):
+    code = f"""
 @external
-def _bitwise_and(x: uint256, y: uint256) -> uint256:
+def _bitwise_and(x: {typ}, y: {typ}) -> {typ}:
     return x & y
-
 @external
-def _bitwise_or(x: uint256, y: uint256) -> uint256:
+def _bitwise_or(x: {typ}, y: {typ}) -> {typ}:
     return x | y
-
 @external
-def _bitwise_xor(x: uint256, y: uint256) -> uint256:
+def _bitwise_xor(x: {typ}, y: {typ}) -> {typ}:
     return x ^ y
-
 @external
-def _bitwise_not(x: uint256) -> uint256:
+def _bitwise_not(x: {typ}) -> {typ}:
     return ~x
-
-@external
-def _shl(x: uint256, y: uint256) -> uint256:
-    return x << y
-
-@external
-def _shr(x: uint256, y: uint256) -> uint256:
-    return x >> y
     """
 
+    shift_code = f"""
+@external
+def _shl(x: {typ}, y: uint256) -> {typ}:
+    return x << y
+@external
+def _shr(x: {typ}, y: uint256) -> {typ}:
+    return x >> y
+    """
+    if use_shift:
+        return code + shift_code
 
-def test_bitwise_opcodes():
+    return code
+
+
+BYTESM_TYPES = ["bytes" + str(i + 1) for i in range(32)]
+UINT_TYPES = ["uint" + str(i + 8) for i in range(0, 256, 8)]
+ALL_TYPES = BYTESM_TYPES + UINT_TYPES
+
+
+@pytest.mark.parametrize("typ", ["uint256", "bytes32"])
+def test_bitwise_opcodes(typ):
+    code = get_code_for_type(typ)
     opcodes = compile_code(code, output_formats=["opcodes"])["opcodes"]
     assert "SHL" in opcodes
     assert "SHR" in opcodes
 
 
+@pytest.mark.parametrize("typ", ["uint256", "bytes32"])
+@pytest.mark.xfail  # fails due to bad vyper grammar
+def test_not_roundtrip(get_contract, typ):
+    code = f"""
+@external
+def round_trip() -> {typ}:
+        b: {typ} = empty({typ})
+        return ~~b
+    """
+    c = get_contract(code)
+    assert c.round_trip() == (0 if typ == "uint256" else b"\00" * 32)
+
+
+@pytest.mark.parametrize("typ", [typ for typ in ALL_TYPES if typ not in ["uint256", "bytes32"]])
+@pytest.mark.parametrize("shift_op", ["<<", ">>"])
+def test_invalid_shift(typ, shift_op):
+    code = f"""
+@external
+def do_shift(x: {typ}, y: uint256) -> {typ}:
+    return x {shift_op} y
+    """
+    with pytest.raises(InvalidOperation):
+        compile_code(code)
+
+
+@pytest.mark.parametrize("typ", [typ for typ in ALL_TYPES if typ not in ["uint256", "bytes32"]])
+def test_unimplemented_unary_not(typ):
+    code = f"""
+@external
+def do_not(x: {typ}) -> {typ}:
+    return ~x
+    """
+    with pytest.raises(UnimplementedException):
+        compile_code(code)
+
+
+@pytest.mark.fuzzing
+@settings(max_examples=50)
+@given(value=st.binary(min_size=32, max_size=32))
+def test_not_operator_bytes(get_contract, value):
+    source = """
+@external
+def foo(a: bytes32) -> bytes32:
+    return ~a
+     """
+    contract = get_contract(source)
+
+    foo_res = contract.foo(value)
+
+    expected = bytes(~b & 0xFF for b in value)
+
+    assert foo_res == expected
+
+
+@st.composite
+def get_bytes(draw, len):
+    return draw(
+        st.tuples(st.binary(min_size=len, max_size=len), st.binary(min_size=len, max_size=len))
+    )
+
+
+@pytest.mark.fuzzing
+@pytest.mark.parametrize("typ", BYTESM_TYPES)
+@pytest.mark.parametrize("op", ["&", "|", "^"])
+def test_bitwise_binary_bytes(get_contract, typ, op):
+    source = f"""
+@external
+def do_op(a: {typ}, b: {typ}) -> {typ}:
+    return a {op} b
+     """
+    contract = get_contract(source)
+    bytes_len = int(typ.removeprefix("bytes"))
+
+    @given(values=get_bytes(bytes_len))
+    @settings(max_examples=50)
+    def _fuzz(values):
+        val1, val2 = values
+        res = contract.do_op(val1, val2)
+        if op == "&":
+            expected = bytes(x & y for x, y in zip(val1, val2))
+        elif op == "|":
+            expected = bytes(x | y for x, y in zip(val1, val2))
+        else:
+            assert op == "^"
+            expected = bytes(x ^ y for x, y in zip(val1, val2))
+
+        assert res == expected
+
+    _fuzz()
+
+
+@pytest.mark.fuzzing
+@pytest.mark.parametrize("op", ["<<", ">>"])
+def test_bitwise_shift_bytes(get_contract, op):
+    source = f"""
+@external
+def do_op(a: bytes32, b: uint256) -> bytes32:
+   return a {op} b
+        """
+    contract = get_contract(source)
+
+    @given(
+        bytes_value=st.binary(min_size=32, max_size=32),
+        shift_by=st.integers(min_value=0, max_value=257),
+    )
+    @settings(max_examples=10000)
+    def _fuzz(bytes_value, shift_by):
+        res = contract.do_op(bytes_value, shift_by)
+        if op == ">>":
+            expected = int.from_bytes(bytes_value, "big")
+            expected = expected >> shift_by
+            expected = expected.to_bytes(32, "big")
+        else:
+            assert op == "<<"
+            expected = int.from_bytes(bytes_value, "big")
+            mask = (1 << 256) - 1
+            expected = (expected << shift_by) & mask
+            expected = expected.to_bytes(32, "big")
+
+        assert res == expected
+
+    _fuzz()
+
+
 def test_test_bitwise(get_contract):
+    code = get_code_for_type("uint256")
     c = get_contract(code)
     x = 126416208461208640982146408124
     y = 7128468721412412459
@@ -110,6 +248,14 @@ def _shl(x: uint256) -> uint256:
 
 
 fail_list = [
+    (
+        # cannot shift by non-uint bits
+        """
+def foo(a: bytes32, b: int256) -> bytes32:
+    return a << b
+    """,
+        TypeMismatch,
+    ),
     (
         # cannot shift non-uint256/int256 argument
         """
