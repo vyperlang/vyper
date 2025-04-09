4f1a5e13d5012b7c298e628d3ccfbe5e3a77d340
Charles Cooper <cooper.charles.m@gmail.com>
2025-04-09 14:11:08 +0200

    add suggestions from cyber

--- tests/functional/codegen/features/decorators/test_raw_return.py
+++ tests/functional/codegen/features/decorators/test_raw_return.py
@@ -13,45 +13,53 @@ def foo(x: Bytes[100]) -> Bytes[100]:
     moo_result = c.foo(abi.encode("(bytes)", (b"cow",)))
     assert moo_result == b"cow"
 
-
 def test_proxy_raw_return(env, get_contract):
     impl1 = """
 @external
-def greet() -> String[32]:
+def foo() -> String[32]:
     return "Hello"
     """
 
     impl2 = """
-# test delegate calling with a different type, but byte-compatible in abi
 @external
-def greet() -> Bytes[32]:
+def foo() -> Bytes[32]:
     return b"Goodbye"
+    """
+
+    impl3 = """
+@external
+def foo() -> DynArray[uint256, 2]:
+    #return [1, 2]
+    a: DynArray[uint256, 2] = [1, 2]
+    return a
     """
 
     proxy = """
 target: address
-
 @external
 def set_implementation(target: address):
     self.target = target
-
 @external
-@raw_return
-def greet() -> Bytes[128]:
-    # forward msg.data to the implementation contract
+def foo() -> ABIBuffer[128]:
     data: Bytes[128] = raw_call(self.target, msg.data, is_delegate_call=True, max_outsize=128)
-    return data
+    return convert(data, ABIBuffer[128])
     """
 
     impl_c1 = get_contract(impl1)
     impl_c2 = get_contract(impl2)
+    impl_c3 = get_contract(impl3)
 
     proxy_c = get_contract(proxy)
 
     proxy_c.set_implementation(impl_c1.address)
-    assert proxy_c.greet() == b"Hello"
-    assert impl_c1.greet() == "Hello"  # different type
+    assert proxy_c.foo() == b"Hello"
 
     proxy_c.set_implementation(impl_c2.address)
-    assert impl_c2.greet() == b"Goodbye"
-    assert proxy_c.greet() == b"Goodbye"  # identical
+    assert proxy_c.foo() == b"Goodbye"
+
+    proxy_c.set_implementation(impl_c3.address)
+    # need low-level call otherwise we fail due to bytes decoding
+    # because ABIBytes is represented as bytes in ABI
+    res = env.message_call(proxy_c.address, data=method_id("foo()"))
+    assert abi.decode("(uint256[])", res) == ([1, 2],)
+
