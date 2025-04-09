39941e4502165d80b74b1782935199a771a9919a
Charles Cooper <cooper.charles.m@gmail.com>
2025-04-09 14:13:01 +0200

    lint

--- tests/functional/codegen/features/decorators/test_raw_return.py
+++ tests/functional/codegen/features/decorators/test_raw_return.py
@@ -1,5 +1,7 @@
 from eth.codecs import abi
 
+from vyper.utils import method_id
+
 
 def test_raw_return(get_contract, tx_failed):
     test_bytes = """
@@ -12,6 +14,7 @@ def foo(x: Bytes[100]) -> Bytes[100]:
     c = get_contract(test_bytes)
     moo_result = c.foo(abi.encode("(bytes)", (b"cow",)))
     assert moo_result == b"cow"
+
 
 def test_proxy_raw_return(env, get_contract):
     impl1 = """
@@ -36,13 +39,21 @@ def foo() -> DynArray[uint256, 2]:
 
     proxy = """
 target: address
+
 @external
 def set_implementation(target: address):
     self.target = target
+
 @external
-def foo() -> ABIBuffer[128]:
-    data: Bytes[128] = raw_call(self.target, msg.data, is_delegate_call=True, max_outsize=128)
-    return convert(data, ABIBuffer[128])
+@raw_return
+def foo() -> Bytes[128]:
+    data: Bytes[128] = raw_call(
+        self.target,
+        msg.data,
+        is_delegate_call=True,
+        max_outsize=128
+    )
+    return data
     """
 
     impl_c1 = get_contract(impl1)
@@ -51,15 +62,17 @@ def foo() -> ABIBuffer[128]:
 
     proxy_c = get_contract(proxy)
 
+    assert impl_c1.foo() == "Hello"
     proxy_c.set_implementation(impl_c1.address)
     assert proxy_c.foo() == b"Hello"
 
+    assert impl_c2.foo() == b"Goodbye"
     proxy_c.set_implementation(impl_c2.address)
     assert proxy_c.foo() == b"Goodbye"
 
+    assert impl_c3.foo() == [1, 2]
     proxy_c.set_implementation(impl_c3.address)
     # need low-level call otherwise we fail due to bytes decoding
     # because ABIBytes is represented as bytes in ABI
     res = env.message_call(proxy_c.address, data=method_id("foo()"))
     assert abi.decode("(uint256[])", res) == ([1, 2],)
-
