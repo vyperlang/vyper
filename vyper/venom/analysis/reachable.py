88c6bdca061da94136c7c6e9d9cc7d97625b4199
Charles Cooper <cooper.charles.m@gmail.com>
2025-04-10 14:14:19 +0200

    remove ReachableAnalysis.invalidate()

--- vyper/venom/analysis/reachable.py
+++ vyper/venom/analysis/reachable.py
@@ -29,14 +29,3 @@ class ReachableAnalysis(IRAnalysis):
         for out_bb in bb.cfg_out:
             self._compute_reachable_r(out_bb)
             s.update(self.reachable[out_bb])
-
-    def invalidate(self):
-        from vyper.venom.analysis import DFGAnalysis, DominatorTreeAnalysis, LivenessAnalysis
-
-        self.analyses_cache.invalidate_analysis(DominatorTreeAnalysis)
-        self.analyses_cache.invalidate_analysis(LivenessAnalysis)
-
-        self._dfs = None
-
-        # be conservative - assume cfg invalidation invalidates dfg
-        self.analyses_cache.invalidate_analysis(DFGAnalysis)
