266b1dc81cbc6b1493c335717e4e8316ac96e2c1
Charles Cooper <cooper.charles.m@gmail.com>
2025-04-10 12:27:26 +0200

    remove dead code
    it's duplicated with vyper/ast/utils.py

--- vyper/ast/parse.py
+++ vyper/ast/parse.py
@@ -3,7 +3,7 @@ import pickle
 import tokenize
 from decimal import Decimal
 from functools import cached_property
-from typing import Dict, List, Optional, Union
+from typing import Optional
 
 from vyper.ast import nodes as vy_ast
 from vyper.ast.pre_parser import PreParser
@@ -136,31 +136,6 @@ def _parse_to_ast(
 
 
 LINE_INFO_FIELDS = ("lineno", "col_offset", "end_lineno", "end_col_offset")
-
-
-def ast_to_dict(ast_struct: Union[vy_ast.VyperNode, List]) -> Union[Dict, List]:
-    """
-    Converts a Vyper AST node, or list of nodes, into a dictionary suitable for
-    output to the user.
-    """
-    if isinstance(ast_struct, vy_ast.VyperNode):
-        return ast_struct.to_dict()
-
-    if isinstance(ast_struct, list):
-        return [i.to_dict() for i in ast_struct]
-
-    raise CompilerPanic(f'Unknown Vyper AST node provided: "{type(ast_struct)}".')
-
-
-def dict_to_ast(ast_struct: Union[Dict, List]) -> Union[vy_ast.VyperNode, List]:
-    """
-    Converts an AST dict, or list of dicts, into Vyper AST node objects.
-    """
-    if isinstance(ast_struct, dict):
-        return vy_ast.get_node(ast_struct)
-    if isinstance(ast_struct, list):
-        return [vy_ast.get_node(i) for i in ast_struct]
-    raise CompilerPanic(f'Unknown ast_struct provided: "{type(ast_struct)}".')
 
 
 def annotate_python_ast(
