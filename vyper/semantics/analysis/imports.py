vyperlang/vyper

commit 1591a1229cf837ccca6cfcc32aed601ce19fad44

feat[lang]!: move sqrt to new stdlib `math` module (#4520)
this commit moves the `sqrt()` builtin to a new, pure vyper stdlib
implementation.

it includes machinery to handle extension of the stdlib in the
future. the benefits of having machinery to handle code written in pure
vyper in the stdlib is that it will be easier for external contributors
to contribute and audit. it also uses the native import machinery,
rather than the existing ad-hoc mechanism for writing vyper from inside
the compiler, which simplifies the implementation. that mechanism for
writing pure vyper builtins is also removed in this commit (previously,
this machinery had been used specifically to handle "pure vyper"
builtins, the only one of which was `sqrt()`).

---------

Co-authored-by: Charles Cooper <cooper.charles.m@gmail.com>

vyper/semantics/analysis/imports.py
@@ -4,6 +4,7 @@ from pathlib import Path, PurePath
 from typing import Any, Iterator
 
 import vyper.builtins.interfaces
+import vyper.builtins.stdlib
 from vyper import ast as vy_ast
 from vyper.compiler.input_bundle import (
     ABIInput,
@@ -14,7 +15,6 @@ from vyper.compiler.input_bundle import (
     PathLike,
 )
 from vyper.exceptions import (
-    CompilerPanic,
     DuplicateImport,
     ImportCycle,
     ModuleNotFound,
@@ -271,40 +271,49 @@ def _import_to_path(level: int, module_str: str) -> PurePath:
     return PurePath(f"{base_path}{module_str.replace('.', '/')}/")
 
 
-# can add more, e.g. "vyper.builtins.interfaces", etc.
-BUILTIN_PREFIXES = ["ethereum.ercs"]
+_builtins_cache: dict[PathLike, tuple[CompilerInput, vy_ast.Module]] = {}
+
+# builtin import path -> (prefix for removal, package, suffix)
+BUILTIN_MODULE_RULES = {
+    "ethereum.ercs": ("ethereum.ercs", vyper.builtins.interfaces.__package__, ".vyi"),
+    "math": ("", vyper.builtins.stdlib.__package__, ".vy"),
+}
 
 
 # TODO: could move this to analysis/common.py or something
+def _get_builtin_prefix(module_str: str):
+    for prefix in BUILTIN_MODULE_RULES.keys():
+        if module_str.startswith(prefix):
+            return prefix
+    return None
+
+
 def _is_builtin(module_str):
-    return any(module_str.startswith(prefix) for prefix in BUILTIN_PREFIXES)
-
-
-_builtins_cache: dict[PathLike, tuple[CompilerInput, vy_ast.Module]] = {}
+    return _get_builtin_prefix(module_str) is not None
 
 
 def _load_builtin_import(level: int, module_str: str) -> tuple[CompilerInput, vy_ast.Module]:
-    if not _is_builtin(module_str):  # pragma: nocover
-        raise CompilerPanic("unreachable!")
+    module_prefix = _get_builtin_prefix(module_str)
+    assert module_prefix is not None, "unreachable"
 
-    builtins_path = vyper.builtins.interfaces.__path__[0]
+    assert level == 0, "builtin imports are absolute"
+
+    builtins_path = vyper.builtins.__path__[0]
     # hygiene: convert to relpath to avoid leaking user directory info
     # (note Path.relative_to cannot handle absolute to relative path
     # conversion, so we must use the `os` module).
     builtins_path = safe_relpath(builtins_path)
 
-    search_path = Path(builtins_path).parent.parent.parent
+    search_path = Path(builtins_path).parent.parent
     # generate an input bundle just because it knows how to build paths.
     input_bundle = FilesystemInputBundle([search_path])
 
-    # remap builtins directory --
-    # ethereum/ercs => vyper/builtins/interfaces
-    remapped_module = module_str
-    if remapped_module.startswith("ethereum.ercs"):
-        remapped_module = remapped_module.removeprefix("ethereum.ercs")
-        remapped_module = vyper.builtins.interfaces.__package__ + remapped_module
+    remove_prefix, target_package, suffix = BUILTIN_MODULE_RULES[module_prefix]
+    base_name = module_str.removeprefix(remove_prefix + ".")
+    remapped_module = f"{target_package}.{base_name}"
 
-    path = _import_to_path(level, remapped_module).with_suffix(".vyi")
+    path = _import_to_path(level, remapped_module)
+    path = path.with_suffix(suffix)
 
     # builtins are globally the same, so we can safely cache them
     # (it is also *correct* to cache them, so that types defined in builtins
@@ -326,12 +335,12 @@ def _load_builtin_import(level: int, module_str: str) -> tuple[CompilerInput, vy
             hint = f"try renaming `{module_prefix}` to `I{module_prefix}`"
         raise ModuleNotFound(module_str, hint=hint) from e
 
-    interface_ast = _parse_ast(file)
+    builtin_ast = _parse_ast(file)
 
     # no recursion needed since builtins don't have any imports
 
-    _builtins_cache[path] = file, interface_ast
-    return file, interface_ast
+    _builtins_cache[path] = file, builtin_ast
+    return file, builtin_ast
 
 
 def resolve_imports(module_ast: vy_ast.Module, input_bundle: InputBundle):

tests/functional/codegen/types/numbers/test_sqrt.py
@@ -4,6 +4,8 @@ import hypothesis
 import pytest
 
 from tests.utils import decimal_to_int
+from vyper.compiler import compile_code
+from vyper.exceptions import UnimplementedException
 from vyper.utils import SizeLimits
 
 DECIMAL_PLACES = 10
@@ -24,9 +26,11 @@ def decimal_sqrt(val):
 
 def test_sqrt_literal(get_contract):
     code = """
+import math
+
 @external
 def test() -> decimal:
-    return sqrt(2.0)
+    return math.sqrt(2.0)
     """
     c = get_contract(code)
     assert c.test() == decimal_sqrt(Decimal("2"))
@@ -35,14 +39,16 @@ def test() -> decimal:
 # TODO: use parametrization here
 def test_sqrt_variable(get_contract):
     code = """
+import math
+
 @external
 def test(a: decimal) -> decimal:
-    return sqrt(a)
+    return math.sqrt(a)
 
 @external
 def test2() -> decimal:
     a: decimal = 44.001
-    return sqrt(a)
+    return math.sqrt(a)
     """
 
     c = get_contract(code)
@@ -59,17 +65,19 @@ def test2() -> decimal:
 
 def test_sqrt_storage(get_contract):
     code = """
+import math
+
 s_var: decimal
 
 @external
 def test(a: decimal) -> decimal:
     self.s_var = a + 1.0
-    return sqrt(self.s_var)
+    return math.sqrt(self.s_var)
 
 @external
 def test2() -> decimal:
     self.s_var = 444.44
-    return sqrt(self.s_var)
+    return math.sqrt(self.s_var)
     """
 
     c = get_contract(code)
@@ -82,12 +90,14 @@ def test2() -> decimal:
 
 def test_sqrt_inline_memory_correct(get_contract):
     code = """
+import math
+
 @external
 def test(a: decimal) -> (decimal, decimal, decimal, decimal, decimal, String[100]):
     x: decimal = 1.0
     y: decimal = 2.0
     z: decimal = 3.0
-    e: decimal = sqrt(a)
+    e: decimal = math.sqrt(a)
     f: String[100] = 'hello world'
     return a, x, y, z, e, f
     """
@@ -108,9 +118,11 @@ def test(a: decimal) -> (decimal, decimal, decimal, decimal, decimal, String[100
 @pytest.mark.parametrize("value", DECIMAL_RANGE)
 def test_sqrt_sub_decimal_places(value, get_contract):
     code = """
+import math
+
 @external
 def test(a: decimal) -> decimal:
-    return sqrt(a)
+    return math.sqrt(a)
     """
 
     c = get_contract(code)
@@ -123,9 +135,11 @@ def test(a: decimal) -> decimal:
 @pytest.fixture(scope="module")
 def sqrt_contract(get_contract):
     code = """
+import math
+
 @external
 def test(a: decimal) -> decimal:
-    return sqrt(a)
+    return math.sqrt(a)
     """
     c = get_contract(code)
     return c
@@ -171,6 +185,8 @@ def test_sqrt_invalid_range(tx_failed, sqrt_contract, value):
 
 def test_sqrt_eval_once(get_contract):
     code = """
+import math
+
 c: uint256
 
 @internal
@@ -180,9 +196,22 @@ def some_decimal() -> decimal:
 
 @external
 def foo() -> uint256:
-    k: decimal = sqrt(self.some_decimal())
+    k: decimal = math.sqrt(self.some_decimal())
     return self.c
     """
 
     c = get_contract(code)
     assert c.foo() == 1
+
+
+def test_use_old_sqrt_builtin(get_contract):
+    code = """
+import math
+
+@external
+def foo() -> decimal:
+    return sqrt(2.0)
+    """
+    pattern = "The `sqrt` builtin was removed. Instead import module `math` and use `math.sqrt()"
+    with pytest.raises(UnimplementedException, match=pattern):
+        compile_code(code)

vyper/builtins/_utils.py
@@ -1,42 +1,0 @@
-from vyper.ast import parse_to_ast
-from vyper.codegen.context import Context
-from vyper.codegen.stmt import parse_body
-from vyper.semantics.analysis.local import FunctionAnalyzer
-from vyper.semantics.namespace import Namespace, override_global_namespace
-from vyper.semantics.types.function import ContractFunctionT, FunctionVisibility, StateMutability
-from vyper.semantics.types.module import ModuleT
-
-
-def _strip_ast_source(ir_node):
-    ir_node.ast_source = None
-    for x in ir_node.args:
-        _strip_ast_source(x)
-
-
-def generate_inline_function(code, variables, variables_2, memory_allocator):
-    ast_code = parse_to_ast(code, add_fn_node="dummy_fn")
-    # Annotate the AST with a temporary old (i.e. typecheck) namespace
-    namespace = Namespace()
-    namespace.update(variables_2)
-    with override_global_namespace(namespace):
-        # Initialise a placeholder `FunctionDef` AST node and corresponding
-        # `ContractFunctionT` type to rely on the annotation visitors in semantics
-        # module.
-        ast_code.body[0]._metadata["func_type"] = ContractFunctionT(
-            "sqrt_builtin", [], [], None, FunctionVisibility.INTERNAL, StateMutability.NONPAYABLE
-        )
-        analyzer = FunctionAnalyzer(ast_code, ast_code.body[0], namespace)
-        analyzer.analyze()
-
-    new_context = Context(
-        vars_=variables, module_ctx=ModuleT(ast_code), memory_allocator=memory_allocator
-    )
-    generated_ir = parse_body(ast_code.body[0].body, new_context)
-    # strip source position info from the generated_ir since
-    # it doesn't make any sense (e.g. the line numbers will start from 0
-    # instead of where we are in the code)
-    # NOTE if we ever use this for inlining user-code, it would make
-    # sense to fix the offsets of the source positions in the generated
-    # code instead of stripping them.
-    _strip_ast_source(generated_ir)
-    return new_context, generated_ir

vyper/builtins/functions.py
@@ -6,7 +6,7 @@ from vyper import ast as vy_ast
 from vyper.abi_types import ABI_Tuple
 from vyper.ast.validation import validate_call_args
 from vyper.codegen.abi_encoder import abi_encode
-from vyper.codegen.context import Context, VariableRecord
+from vyper.codegen.context import Context
 from vyper.codegen.core import (
     LOAD,
     STORE,
@@ -52,9 +52,10 @@ from vyper.exceptions import (
     StructureException,
     TypeMismatch,
     UnfoldableNode,
+    UnimplementedException,
     ZeroDivisionException,
 )
-from vyper.semantics.analysis.base import Modifiability, VarInfo
+from vyper.semantics.analysis.base import Modifiability
 from vyper.semantics.analysis.utils import (
     get_common_types,
     get_exact_type_from_node,
@@ -2053,59 +2054,10 @@ class Sqrt(BuiltinFunctionT):
     _inputs = [("d", DecimalT())]
     _return_type = DecimalT()
 
-    @process_inputs
-    def build_IR(self, expr, args, kwargs, context):
-        # TODO fix cyclic dependency with codegen/stmt.py
-        from ._utils import generate_inline_function
-
-        arg = args[0]
-        # TODO: reify decimal and integer sqrt paths (see isqrt)
-        with arg.cache_when_complex("x") as (b1, arg):
-            sqrt_code = """
-assert x >= 0.0
-z: decimal = 0.0
-
-if x == 0.0:
-    z = 0.0
-else:
-    z = x / 2.0 + 0.5
-    y: decimal = x
-
-    for i: uint256 in range(256):
-        if z == y:
-            break
-        y = z
-        z = (x / z + z) / 2.0
-
-    if y < z:
-        z = y
-            """
-
-            x_type = DecimalT()
-            placeholder_copy = ["pass"]
-            # Steal current position if variable is already allocated.
-            if arg.value == "mload":
-                new_var_pos = arg.args[0]
-            # Other locations need to be copied.
-            else:
-                new_var_pos = context.new_internal_variable(x_type)
-                placeholder_copy = ["mstore", new_var_pos, arg]
-            # Create input variables.
-            variables = {"x": VariableRecord(name="x", pos=new_var_pos, typ=x_type, mutable=False)}
-            # Dictionary to update new (i.e. typecheck) namespace
-            variables_2 = {"x": VarInfo(DecimalT())}
-            # Generate inline IR.
-            new_ctx, sqrt_ir = generate_inline_function(
-                code=sqrt_code,
-                variables=variables,
-                variables_2=variables_2,
-                memory_allocator=context.memory_allocator,
-            )
-            z_ir = new_ctx.vars["z"].as_ir_node()
-            ret = IRnode.from_list(
-                ["seq", placeholder_copy, sqrt_ir, z_ir], typ=DecimalT(), location=MEMORY
-            )
-            return b1.resolve(ret)
+    def fetch_call_return(self, node):
+        message = "The `sqrt` builtin was removed. Instead import module "
+        message += "`math` and use `math.sqrt()`"
+        raise UnimplementedException(message, node)
 
 
 class ISqrt(BuiltinFunctionT):
@@ -2584,8 +2536,8 @@ DISPATCH_TABLE = {
     "unsafe_div": UnsafeDiv(),
     "pow_mod256": PowMod256(),
     "uint2str": Uint2Str(),
-    "isqrt": ISqrt(),
     "sqrt": Sqrt(),
+    "isqrt": ISqrt(),
     "shift": Shift(),
     "create_minimal_proxy_to": CreateMinimalProxyTo(),
     "create_forwarder_to": CreateForwarderTo(),

vyper/builtins/stdlib/math.vy
@@ -1,0 +1,22 @@
+@internal
+@pure
+def sqrt(x: decimal) -> decimal:
+    assert x >= 0.0
+    z: decimal = 0.0
+
+    if x == 0.0:
+        z = 0.0
+    else:
+        z = x / 2.0 + 0.5
+        y: decimal = x
+
+        for i: uint256 in range(256):
+            if z == y:
+                break
+            y = z
+            z = (x / z + z) / 2.0
+
+        if y < z:
+            z = y
+
+    return z

commit a6c4a7f61a02eded5361e0a6c9a1144c123f2628

fix[lang]: only reserve builtins at the top level
commit 1591a1229cf837ccca introduced a regression in snekmate, which is
that imports of the form `from .. import math` are rejected, due to the
assertion that `level == 0` on L299 inside of _load_builtin_import.

this was already a latent bug for several versions, but was not
previously tripped, since the only builtin namespace was `ethereum.ercs`
(and users presumably didn't have modules named `ethereum`).

this commit fixes builtin detection by protecting the builtin check with
an additional requirement that `level == 0` (indicating an absolute
import).

vyper/semantics/analysis/imports.py
@@ -159,7 +159,7 @@ class ImportAnalyzer:
     def _load_import(
         self, node: vy_ast.VyperNode, level: int, module_str: str, alias: str
     ) -> tuple[CompilerInput, Any]:
-        if _is_builtin(module_str):
+        if level == 0 and _is_builtin(module_str):
             return _load_builtin_import(level, module_str)
 
         path = _import_to_path(level, module_str)