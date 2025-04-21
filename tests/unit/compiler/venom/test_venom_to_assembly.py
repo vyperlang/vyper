197f70ffe0d825606da563796febe1ae5f089177
Charles Cooper <cooper.charles.m@gmail.com>
2025-04-21 09:50:43 +0200

    add tests for params

---
+++ tests/unit/compiler/venom/test_venom_to_assembly.py
@@ -0,0 +1,34 @@
+from vyper.venom.venom_to_assembly import VenomCompiler
+from vyper.venom.parser import parse_venom
+
+def test_dead_params():
+    code = """
+    function foo {
+        main:
+            %1 = param  ; dead
+            %2 = param
+            ret %2
+    }
+    """
+    ctx = parse_venom(code)
+
+    asm = VenomCompiler([ctx]).generate_evm()
+    assert asm == ["SWAP1", "POP", "JUMP"]
+
+def test_optimistic_swap_params():
+    code = """
+    function foo {
+        main:
+            %1 = param
+            %2 = param  ; %2 is scheduled before %3
+            %3 = param
+            %4 = 117  ; show that %2 gets swapped "optimistically" before this instruction
+            mstore %1
+            mstore %2
+            ret %3
+    }
+    """
+    ctx = parse_venom(code)
+
+    asm = VenomCompiler([ctx]).generate_evm()
+    assert asm == ["SWAP2", "PUSH1", 117, "POP", "MSTORE", "MSTORE", "JUMP"]
