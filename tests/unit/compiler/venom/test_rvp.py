e4b268930e424d55276a0c1412f29d51186b4e71
Harry Kalogirou <harkal@nlogn.eu>
2025-05-06 14:55:41 +0200

    add branch test

--- tests/unit/compiler/venom/test_rvp.py
+++ tests/unit/compiler/venom/test_rvp.py
@@ -74,3 +74,47 @@ def test_branch_elimination_with_ranges():
     assert rvp.lattice[IRVariable("%1")] == Interval(10, 10)
     assert rvp.lattice[IRVariable("%2")] == Interval(20, 20)
     assert rvp.lattice[IRVariable("%3")] == Interval(30, 30)
+
+
+def test_branch_range_propagation():
+    pre = """
+    main:
+        %1 = param
+        %2 = 0
+        jnz %1, @then, @else
+    then:
+        %3 = add %1, %2  ; %1 is non-zero here
+        exit
+    else:
+        %4 = add %1, %2  ; %1 is zero here
+        exit
+    """
+
+    post = """
+    main:
+        %1 = param
+        %2 = 0
+        jnz %1, @then, @else
+    then:
+        %3 = add %1, 0  ; %1 is non-zero here
+        exit
+    else:
+        %4 = add 0, 0  ; %1 is zero here
+        exit
+    """
+
+    passes = _check_pre_post(pre, post)
+    rvp = passes[0]
+
+    assert rvp.lattice[IRVariable("%1")] == Interval(-_inf, _inf)
+    assert rvp.lattice[IRVariable("%2")] == Interval(0, 0)
+
+    then_ctx = rvp.get_context("@then")
+    assert then_ctx.lattice[IRVariable("%1")] == Interval(-_inf, -1, 1, _inf)
+    assert then_ctx.lattice[IRVariable("%2")] == Interval(0, 0)
+    assert then_ctx.lattice[IRVariable("%3")] == Interval(-_inf, -1, 1, _inf)
+
+    else_ctx = rvp.get_context("@else")
+    assert else_ctx.lattice[IRVariable("%1")] == Interval(0, 0)
+    assert else_ctx.lattice[IRVariable("%2")] == Interval(0, 0)
+    assert else_ctx.lattice[IRVariable("%4")] == Interval(0, 0)
