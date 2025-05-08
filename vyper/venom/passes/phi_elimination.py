05c7136f39e5df4a977015c1e21462d46d02d3f7
plodeada <plodeada@fit.cvut.cz>
2025-05-08 17:03:15 +0200

    more explicit implementation

--- vyper/venom/passes/phi_elimination.py
+++ vyper/venom/passes/phi_elimination.py
@@ -36,33 +36,48 @@ class PhiEliminationPass(IRPass):
         self.dom = self.analyses_cache.request_analysis(DominatorTreeAnalysis)
         self.phi_to_origins = dict()
 
+        fully_done: set[IRInstruction] = set()
+
         for bb in self.function.get_basic_blocks():
             for inst in bb.instructions:
                 if inst.opcode != "phi":
                     break
-                self._handle_phi(inst)
+                self._handle_phi(inst, fully_done)
 
-    def _handle_phi(self, inst: IRInstruction):
+    def _handle_phi(self, inst: IRInstruction, fully_done: set[IRInstruction]):
         assert inst.opcode == "phi"
-        self._handle_inst_r(inst)
+        done: set[IRInstruction] = fully_done.copy()
+        visited: set[IRInstruction] = set()
+        self._handle_inst_r(inst, done, visited)
+        fully_done.add(inst)
 
-    def _handle_inst_r(self, inst: IRInstruction) -> set[IRInstruction]:
+    def _handle_inst_r(self, inst: IRInstruction, done: set[IRInstruction], visited: set[IRInstruction]) -> set[IRInstruction]:
         if inst.opcode == "phi":
-            if inst in self.phi_to_origins:
+            if inst in visited or inst in done:
                 # phi is the only place where we can get dfg cycles.
                 # break the recursion.
+
+                # if it is only visited we stumbled on
+                # self reference and the sources are
+                # in the other branch
+                if inst not in done:
+                    return set()
+
+                # already computed result
                 srcs = self.phi_to_origins[inst].copy()
                 if len(srcs) > 1:
                     return set([inst])
                 return srcs
+            visited.add(inst)
 
             self.phi_to_origins[inst] = set()
 
             for _, var in inst.phi_operands:
                 next_inst = self.dfg.get_producing_instruction(var)
                 assert next_inst is not None, (inst, var)
-                self.phi_to_origins[inst] |= self._handle_inst_r(next_inst)
+                self.phi_to_origins[inst] |= self._handle_inst_r(next_inst, done, visited)
 
+            done.add(inst)
             if len(self.phi_to_origins[inst]) > 1:
                 return set([inst])
             return self.phi_to_origins[inst]
@@ -72,6 +87,6 @@ class PhiEliminationPass(IRPass):
             var = inst.operands[0]
             next_inst = self.dfg.get_producing_instruction(var)
             assert next_inst is not None
-            return self._handle_inst_r(next_inst)
+            return self._handle_inst_r(next_inst, done, visited)
 
         return set([inst])
