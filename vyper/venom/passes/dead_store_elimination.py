4310654e4ef80781529348a4b99b39f45a53973e
Harry Kalogirou <harkal@nlogn.eu>
2025-05-13 09:31:00 +0300

    no printing

--- vyper/venom/passes/dead_store_elimination.py
+++ vyper/venom/passes/dead_store_elimination.py
@@ -24,10 +24,6 @@ class DeadStoreElimination(IRPass):
         self.used_defs = OrderedSet[MemoryDef]()
         dead_defs = OrderedSet[MemoryDef]()
 
-        with self.mem_ssa.print_context():
-            print("------------------------")
-            print(self.function)
-
         for _, mem_uses in self.mem_ssa.memory_uses.items():
             for mem_use in mem_uses:
                 aliased_accesses = self.mem_ssa.get_aliased_memory_accesses(mem_use)
@@ -40,6 +36,11 @@ class DeadStoreElimination(IRPass):
 
         for def_ in dead_defs:
             self.updater.nop(def_.store_inst, annotation="[dead store elimination]")        
+
+        # with self.mem_ssa.print_context():
+        #     print("------------------------")
+        #     print(self.function)
+
 
         
 
