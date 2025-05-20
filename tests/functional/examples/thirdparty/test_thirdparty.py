d1f4226b9a9dc3fc11312a1deff755d41fa45a0c
Charles Cooper <cooper.charles.m@gmail.com>
2025-05-20 10:19:57 +0300

    xfail CurveStableSwapMeta

--- tests/functional/examples/thirdparty/test_thirdparty.py
+++ tests/functional/examples/thirdparty/test_thirdparty.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pytest
 
 import vyper.compiler as compiler
+from vyper.compiler.settings import OptimizationLevel
 
 dir_path = Path(__file__).parent
 
@@ -13,7 +14,15 @@ def get_example_vy_filenames():
 
 
 @pytest.mark.parametrize("vy_filename", get_example_vy_filenames())
-def test_compile(vy_filename):
+def test_compile(vy_filename, compiler_settings, request):
+    if (
+        vy_filename == "curvefi/CurveStableSwapMetaNG.vy"
+        and compiler_settings.experimental_codegen
+        and compiler_settings.optimize == OptimizationLevel.NONE
+    ):
+        # fails with StackTooDeep
+        request.node.add_marker(pytest.mark.xfail(strict=True))
+
     with open(dir_path / vy_filename) as f:
         source_code = f.read()
     compiler.compile_code(source_code)
