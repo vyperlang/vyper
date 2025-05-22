450cf2551c63b0d0f2a56f99c08487113dcddeff
cyberthirst <cyberthirst.eth@gmail.com>
2025-05-22 14:50:39 +0200

    refactor output to fs

--- tests/exports.py
+++ tests/exports.py
@@ -12,20 +12,19 @@ class TracedItem:
     deps: list[str]
     traces: list[dict[str, Any]]
 
-
 class TestExporter:
     def __init__(self, export_dir: Path, test_root: Path):
         self.export_dir: Path = export_dir
         self.test_root: Path = test_root
-        self.data: dict[Path, TracedItem] = {}
-        self._current_item: Optional[TracedItem] = None
+        # module_path -> all traced items in the module
+        self.data: dict[Path, list[TracedItem]] = {}
+        self._current_module: Optional[Path] = None
         self._executed_fixtures: list[str] = []
-        self.output_file: Optional[Path] = None
 
     def append_fixture(self, fixture):
         # some fixtures don't have traces (e.g. `tx_failed`)
         # append only if it has traces
-        if self._current_item.traces:
+        if self.current_item.traces:
             self._executed_fixtures.append(fixture)
 
     def _resolve_dependencies(self, item: Union[FixtureDef, Item]):
@@ -39,7 +38,14 @@ class TestExporter:
                 # TODO we need a fully qualified name here
                 deps_with_traces.append(f)
 
-        self._current_item.deps = deps_with_traces
+        self.current_item.deps = deps_with_traces
+
+    @property
+    def current_item(self) -> TracedItem:
+        path = self._current_module
+        assert  path is not None
+        assert len(self.data[path]) > 0
+        return self.data[path][-1]
 
     def set_item(self, item: Union[FixtureDef, Item]):
         if isinstance(item, Item):
@@ -52,13 +58,14 @@ class TestExporter:
 
         rel_module = module_file.relative_to(self.test_root)
 
-        item_name = item.name if isinstance(item, Item) else item.argname
-        key = self.export_dir / rel_module / item_name
+        path = self.export_dir / rel_module
 
-        if key not in self.data:
-            self.data[key] = TracedItem(item.name, [], [])
+        if path not in self.data:
+            item_name = item.name if isinstance(item, Item) else item.argname
+            # TODO we probably need to number the item names if they're fixtures
+            self.data[path] = [TracedItem(item.name, [], [])]
 
-        self._current_item = self.data[key]
+        self._current_module = self.data[path]
         self._resolve_dependencies(item)
 
     def trace_deployment(
@@ -93,7 +100,7 @@ class TestExporter:
             "raw_ir": raw_ir,
             "blueprint_initcode_prefix": blueprint_initcode_prefix,
         }
-        self._current_item.traces.append(deployment)
+        self.current_item.traces.append(deployment)
 
     def trace_call(self, output: Optional[bytes], **call_args):
         if "calldata" in call_args:
@@ -102,8 +109,23 @@ class TestExporter:
         out_hex = output.hex() if output is not None else None
 
         call = {"trace_type": "call", "output": out_hex, "call_args": call_args}
-        self._current_item.traces.append(call)
+        self.current_item.traces.append(call)
 
     def finalize_export(self):
-        with open(self.output_file, "w") as f:
-            json.dump(self.data, f, indent=2)
+        for module_path, traced_items in self.data.items():
+            json_path = module_path.with_suffix(".json")
+
+            json_path.parent.mkdir(parents=True, exist_ok=True)
+
+            serializable = [
+                {
+                    "name": ti.name,
+                    "deps": ti.deps,
+                    "traces": ti.traces,
+                }
+                for ti in traced_items
+            ]
+
+            with json_path.open("w", encoding="utf-8") as f:
+                json.dump(serializable, f, indent=2)
+
