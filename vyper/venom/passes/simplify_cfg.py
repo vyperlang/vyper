vyperlang/vyper

commit 2360d629d1607e09d4915bdfb4c4650133e98161

feat[venom]: allow labels to be assigned to variables (#4514)
this commit allows labels to be assigned to variables. it updates
assumptions in `SCCP` and the venom parser which blocked that behavior.

it also updates the text format for `djmp` to be like `jnz` (condition
is first). it also fixes a bug in SimplifyCFG where `merge_jump` did
not rename label operands.

---------

Co-authored-by: Charles Cooper <cooper.charles.m@gmail.com>

vyper/venom/passes/simplify_cfg.py
@@ -36,6 +36,8 @@ class SimplifyCFGPass(IRPass):
         assert b.label in jump_inst.operands, f"{b.label} {jump_inst.operands}"
         jump_inst.operands[jump_inst.operands.index(b.label)] = next_bb.label
 
+        self._replace_label(b.label, next_bb.label)
+
         # Update CFG
         a.remove_cfg_out(b)
         a.add_cfg_out(next_bb)
@@ -84,6 +86,8 @@ class SimplifyCFGPass(IRPass):
         """
         Remove empty basic blocks.
         """
+        # NOTE CMC 2025-03-12 this function is a no-op!
+
         fn = self.function
         worklist = list(fn.get_basic_blocks())
         i = count = 0
@@ -104,17 +108,18 @@ class SimplifyCFGPass(IRPass):
                 replaced_label, replacement_label = replacement_label, replaced_label
                 next_bb.label = replacement_label
 
-            for bb2 in fn.get_basic_blocks():
-                for inst in bb2.instructions:
-                    for op in inst.operands:
-                        if isinstance(op, IRLabel) and op.value == replaced_label.value:
-                            op.value = replacement_label.value
+            self._replace_label(replaced_label, replacement_label)
 
             fn.remove_basic_block(bb)
             i -= 1
             count += 1
 
         return count
+
+    def _replace_label(self, original_label: IRLabel, replacement_label: IRLabel):
+        for bb in self.function.get_basic_blocks():
+            for inst in bb.instructions:
+                inst.replace_operands({original_label: replacement_label})
 
     def run_pass(self):
         fn = self.function

tests/functional/venom/test_venom_label_variables.py
@@ -1,0 +1,85 @@
+from vyper.compiler.phases import generate_bytecode
+from vyper.compiler.settings import OptimizationLevel
+from vyper.venom import generate_assembly_experimental, run_passes_on
+from vyper.venom.check_venom import check_venom_ctx
+from vyper.venom.parser import parse_venom
+
+
+def test_labels_as_variables():
+    code = """
+    function global {
+      global:
+        mstore 64, 128
+        %2 = callvalue
+        %3 = iszero %2
+        jnz %3, @block_0xe, @block_0xb
+      block_0xe:
+        %6 = calldatasize
+        %7 = lt %6, 4
+        jnz %7, @block_0x2f, @block_0x17
+      block_0xb:
+        revert 0, 0
+      block_0x2f:
+        revert 0, 0
+      block_0x17:
+        %12 = calldataload 0
+        %14 = shr 224, %12
+        %16 = eq 10773267, %14
+        jnz %16, @block_0x33, @block_0x25
+      block_0x33:
+        %block_57 = @block_0x39
+        jmp @block_0x67
+      block_0x25:
+        %21 = eq 4171824493, %14
+        jnz %21, @block_0x4d, @block_0x2f
+      block_0x67:
+        %phi0 = phi @block_0x33, %block_57, @block_0x6f, %block_118
+        djmp %phi0, @block_0x76, @block_0x39
+      block_0x4d:
+        jmp @block_0x6f
+      block_0x39:
+        %28 = mload 64
+        %block_68 = @block_0x44
+        jmp @block_0x91
+      block_0x6f:
+        %block_118 = @block_0x76
+        jmp @block_0x67
+      block_0x91:
+        %phi1 = phi @block_0x39, %28, @block_0x53, %45
+        %phi2 = phi @block_0x39, %28, @block_0x53, %45
+        %phi3 = phi @block_0x39, %block_68, @block_0x53, %block_94
+        %36 = add %phi1, 32
+        %39 = add %phi2, 0
+        jmp @block_0x84
+      block_0x84:
+        jmp @block_0x7b
+      block_0x76:
+        jmp @block_0x53
+      block_0x7b:
+        jmp @block_0x8b
+      block_0x53:
+        %45 = mload 64
+        %block_94 = @block_0x5e
+        jmp @block_0x91
+      block_0x8b:
+        mstore %39, 1
+        jmp @block_0xa2
+      block_0xa2:
+        djmp %phi3, @block_0x5e, @block_0x44
+      block_0x44:
+        %49 = mload 64
+        %50 = sub %36, %49
+        return %49, %50
+      block_0x5e:
+        %52 = mload 64
+        %53 = sub %36, %52
+        return %52, %53
+    }
+    """
+    ctx = parse_venom(code)
+
+    check_venom_ctx(ctx)
+
+    run_passes_on(ctx, OptimizationLevel.default())
+    asm = generate_assembly_experimental(ctx)
+    generate_bytecode(asm, compiler_metadata=None)

vyper/venom/basicblock.py
@@ -386,10 +386,8 @@ class IRInstruction:
         for i in range(0, len(self.operands), 2):
             label = self.operands[i]
             var = self.operands[i + 1]
-            assert isinstance(label, IRLabel), "phi operand must be a label"
-            assert isinstance(
-                var, (IRVariable, IRLiteral)
-            ), "phi operand must be a variable or literal"
+            assert isinstance(label, IRLabel), f"not a label: {label} (at `{self}`)"
+            assert isinstance(var, IRVariable), f"not a variable: {var} (at `{self}`)"
             yield label, var
 
     def remove_phi_operand(self, label: IRLabel) -> None:
@@ -431,7 +429,7 @@ class IRInstruction:
         opcode = f"{self.opcode} " if self.opcode != "store" else ""
         s += opcode
         operands = self.operands
-        if opcode not in ["jmp", "jnz", "invoke"]:
+        if opcode not in ["jmp", "jnz", "djmp", "invoke"]:
             operands = list(reversed(operands))
         s += ", ".join([(f"@{op}" if isinstance(op, IRLabel) else str(op)) for op in operands])
         return s
@@ -445,7 +443,7 @@ class IRInstruction:
         operands = self.operands
         if self.opcode == "invoke":
             operands = [operands[0]] + list(reversed(operands[1:]))
-        elif self.opcode not in ("jmp", "jnz", "phi"):
+        elif self.opcode not in ("jmp", "jnz", "djmp", "phi"):
             operands = reversed(operands)  # type: ignore
         s += ", ".join([(f"@{op}" if isinstance(op, IRLabel) else str(op)) for op in operands])
 

vyper/venom/parser.py
@@ -178,7 +178,7 @@ class VenomTransformer(Transformer):
         if isinstance(value, IRInstruction):
             value.output = to
             return value
-        if isinstance(value, (IRLiteral, IRVariable)):
+        if isinstance(value, (IRLiteral, IRVariable, IRLabel)):
             return IRInstruction("store", [value], output=to)
         raise TypeError(f"Unexpected value {value} of type {type(value)}")
 
@@ -200,7 +200,7 @@ class VenomTransformer(Transformer):
             # invoke <target> <stack arguments>
             operands = [operands[0]] + list(reversed(operands[1:]))
         # special cases: operands with labels look better un-reversed
-        elif opcode not in ("jmp", "jnz", "phi"):
+        elif opcode not in ("jmp", "jnz", "djmp", "phi"):
             operands.reverse()
         return IRInstruction(opcode, operands)
 

vyper/venom/passes/sccp/sccp.py
@@ -36,7 +36,7 @@ class FlowWorkItem:
 
 
 WorkListItem = Union[FlowWorkItem, SSAWorkListItem]
-LatticeItem = Union[LatticeEnum, IRLiteral]
+LatticeItem = Union[LatticeEnum, IRLiteral, IRLabel]
 Lattice = dict[IRVariable, LatticeItem]
 
 
@@ -147,13 +147,14 @@ class SCCP(IRPass):
         return lat
 
     def _set_lattice(self, op: IROperand, value: LatticeItem):
-        assert isinstance(op, IRVariable), "Can't set lattice for non-variable"
+        assert isinstance(op, IRVariable), f"Not a variable: {op}"
         self.lattice[op] = value
 
-    def _eval_from_lattice(self, op: IROperand) -> IRLiteral | LatticeEnum:
-        if isinstance(op, IRLiteral):
+    def _eval_from_lattice(self, op: IROperand) -> LatticeItem:
+        if isinstance(op, (IRLiteral, IRLabel)):
             return op
 
+        assert isinstance(op, IRVariable), f"Not a variable: {op}"
         return self._lookup_from_lattice(op)
 
     def _visit_phi(self, inst: IRInstruction):
@@ -244,6 +245,7 @@ class SCCP(IRPass):
             elif isinstance(op, IRVariable):
                 eval_result = self.lattice[op]
             else:
+                assert isinstance(op, IRLiteral)  # clarity
                 eval_result = op
 
             # The value from the lattice should have evaluated to BOTTOM

commit 7731d626bfdf2c63a9c144eadd730cb335f59499

perf[venom]: optimize time spent in `SimplifyCFG` (#4658)
this commit optimizes the performance of the `SimplifyCFG`
pass by scheduling label renaming for batch processing after
`_collapse_chained_blocks()`.

this performance regression was introduced in 2360d629d1607e09d
and 18d0787a737e240d36e21, since `_replace_label()` was added to
`_merge_jump()`, and it is called in a loop.

vyper/venom/passes/simplify_cfg.py
@@ -37,7 +37,7 @@ class SimplifyCFGPass(IRPass):
         assert b.label in jump_inst.operands, f"{b.label} {jump_inst.operands}"
         jump_inst.operands[jump_inst.operands.index(b.label)] = next_bb.label
 
-        self._replace_label(b.label, next_bb.label)
+        self._schedule_label_replacement(b.label, next_bb.label)
 
         # Update CFG
         self.cfg.remove_cfg_out(a, b)
@@ -82,10 +82,14 @@ class SimplifyCFGPass(IRPass):
         self.visited = OrderedSet()
         self._collapse_chained_blocks_r(entry)
 
-    def _replace_label(self, original_label: IRLabel, replacement_label: IRLabel):
+    def _schedule_label_replacement(self, original_label: IRLabel, replacement_label: IRLabel):
+        assert original_label not in self.label_map
+        self.label_map[original_label] = replacement_label
+
+    def _replace_all_labels(self):
         for bb in self.function.get_basic_blocks():
             for inst in bb.instructions:
-                inst.replace_operands({original_label: replacement_label})
+                inst.replace_operands(self.label_map)
 
     def remove_unreachable_blocks(self) -> int:
         # Remove unreachable basic blocks
@@ -146,7 +150,9 @@ class SimplifyCFGPass(IRPass):
             self.cfg = self.analyses_cache.force_analysis(CFGAnalysis)
 
         for _ in range(fn.num_basic_blocks):  # essentially `while True`
+            self.label_map = {}
             self._collapse_chained_blocks(entry)
+            self._replace_all_labels()
             self.cfg = self.analyses_cache.force_analysis(CFGAnalysis)
             if self.remove_unreachable_blocks() == 0:
                 break