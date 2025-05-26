36ff4d01ea3b6f0a7a2b2c38b3a66721539307c9
Charles Cooper <cooper.charles.m@gmail.com>
2025-05-26 12:38:43 +0200

    update tests for when --experimental-codegen is turned on

--- tests/unit/cli/vyper_json/test_compile_json.py
+++ tests/unit/cli/vyper_json/test_compile_json.py
@@ -139,15 +139,11 @@ def test_keyerror_becomes_jsonerror(input_json):
         compile_json(input_json)
 
 
-def test_compile_json(input_json, input_bundle):
+def test_compile_json(input_json, input_bundle, experimental_codegen):
     foo_input = input_bundle.load_file("contracts/foo.vy")
     # remove venom related from output formats
     # because they require venom (experimental)
     output_formats = OUTPUT_FORMATS.copy()
-    del output_formats["bb"]
-    del output_formats["bb_runtime"]
-    del output_formats["cfg"]
-    del output_formats["cfg_runtime"]
     foo = compile_from_file_input(
         foo_input,
         output_formats=output_formats,
@@ -192,7 +188,7 @@ def test_compile_json(input_json, input_bundle):
             "ast": data["ast_dict"]["ast"],
             "annotated_ast": data["annotated_ast_dict"]["ast"],
         }
-        assert output_json["contracts"][path][contract_name] == {
+        expected = {
             "abi": data["abi"],
             "devdoc": data["devdoc"],
             "interface": data["interface"],
@@ -214,6 +210,14 @@ def test_compile_json(input_json, input_bundle):
                 "methodIdentifiers": data["method_identifiers"],
             },
         }
+        if experimental_codegen:
+            expected["venom"] = {
+                "bb": repr(data["bb"]),
+                "bb_runtime": repr(data["bb_runtime"]),
+                "cfg": data["cfg"],
+                "cfg_runtime": data["cfg_runtime"],
+            }
+        assert output_json["contracts"][path][contract_name] == expected
 
 
 def test_compilation_targets(input_json):
@@ -231,7 +235,7 @@ def test_compilation_targets(input_json):
     assert list(output_json["contracts"].keys()) == ["contracts/foo.vy", "contracts/bar.vy"]
 
 
-def test_different_outputs(input_bundle, input_json):
+def test_different_outputs(input_bundle, input_json, experimental_codegen):
     input_json["settings"]["outputSelection"] = {
         "contracts/bar.vy": "*",
         "contracts/foo.vy": ["evm.methodIdentifiers"],
@@ -246,16 +250,11 @@ def test_different_outputs(input_bundle, input_json):
 
     foo = contracts["contracts/foo.vy"]["foo"]
     bar = contracts["contracts/bar.vy"]["bar"]
-    assert sorted(bar.keys()) == [
-        "abi",
-        "devdoc",
-        "evm",
-        "interface",
-        "ir",
-        "layout",
-        "metadata",
-        "userdoc",
-    ]
+    expected_keys = ["abi", "devdoc", "evm", "interface", "ir", "layout", "metadata", "userdoc"]
+    if experimental_codegen:
+        expected_keys.append("venom")
+        expected_keys.sort()
+    assert sorted(bar.keys()) == expected_keys
 
     assert sorted(foo.keys()) == ["evm"]
 
