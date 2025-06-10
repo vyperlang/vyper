284211670b3c6a827ab056d69e7aec0335a93f6b
Charles Cooper <cooper.charles.m@gmail.com>
2025-06-11 00:54:39 +0200

    feat[test]: add code writing DSL to test suite
    this makes it easier to programmatically generate vyper source code,
    which will make it easier to write parametrized tests in the test suite.

---
+++ tests/dsl/__init__.py
@@ -0,0 +1,53 @@
+"""
+DSL for building Vyper contracts in tests.
+
+Example usage:
+    from tests.dsl import CodeModel
+    
+    # create a model
+    model = CodeModel()
+    
+    # define storage variables
+    balance = model.storage_var('balance: uint256')
+    owner = model.storage_var('owner: address')
+    
+    # build a simple contract
+    code = (model
+        .function('__init__()')
+        .deploy()
+        .body(f'{owner} = msg.sender')
+        .done()
+        .function('deposit()')
+        .external()
+        .payable()
+        .body(f'{balance} += msg.value')
+        .done()
+        .function('get_balance() -> uint256')
+        .external()
+        .view()
+        .body(f'return {balance}')
+        .done()
+        .build())
+    
+    # The generated code will be:
+    # balance: uint256
+    # owner: address
+    # 
+    # @deploy
+    # def __init__():
+    #     self.owner = msg.sender
+    # 
+    # @external
+    # @payable
+    # def deposit():
+    #     self.balance += msg.value
+    # 
+    # @external
+    # @view
+    # def get_balance() -> uint256:
+    #     return self.balance
+"""
+
+from tests.dsl.code_model import CodeModel, VarRef
+
+__all__ = [CodeModel, VarRef]
