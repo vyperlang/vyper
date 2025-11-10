631391755cbb3d5239462050e0c25ef772b2b586
Hodan <plodeada@fit.cvut.cz>
2025-11-10 11:45:50 +0100

    test for allocation

--- tests/unit/compiler/venom/test_concretize_mem.py
+++ tests/unit/compiler/venom/test_concretize_mem.py
@@ -1,7 +1,8 @@
 from tests.venom_utils import PrePostChecker
-from vyper.venom.passes import ConcretizeMemLocPass
+from vyper.venom.passes import ConcretizeMemLocPass, Mem2Var, AssignElimination
 
 _check_pre_post = PrePostChecker([ConcretizeMemLocPass], default_hevm=False)
+_check_pre_post_mem2var = PrePostChecker([Mem2Var, AssignElimination], default_hevm=False)
 
 
 def test_valid_overlap():
@@ -27,3 +28,78 @@ def test_valid_overlap():
     """
 
     _check_pre_post(pre, post)
+
+def test_venom_allocation():
+    pre = """
+    main:
+        %ptr = alloca 0, [3,256]
+        calldatacopy %ptr, 100, 256
+        %1 = mload %ptr
+        sink %1
+    """
+
+    post1 = """
+    main:
+        calldatacopy [3,256], 100, 256
+        %1 = mload [3,256]
+        sink %1
+    """
+
+    post2 = """
+    main:
+        calldatacopy 64, 100, 256
+        %1 = mload 64
+        sink %1
+    """
+
+    _check_pre_post_mem2var(pre, post1)
+    _check_pre_post(post1, post2)
+
+def test_venom_allocation_branches():
+    pre = """
+    main:
+        %ptr1 = alloca 0, [3,256]
+        %ptr2 = alloca 1, [4,128]
+        %cond = source
+        jnz %cond, @then, @else
+    then:
+        calldatacopy %ptr1, 100, 256
+        %1 = mload %ptr1
+        sink %1
+    else:
+        calldatacopy %ptr2, 1000, 64
+        %2 = mload %ptr2
+        sink %2
+    """
+
+    post1 = """
+    main:
+        %cond = source
+        jnz %cond, @then, @else
+    then:
+        calldatacopy [3,256], 100, 256
+        %1 = mload [3,256]
+        sink %1
+    else:
+        calldatacopy [4,128], 1000, 64
+        %2 = mload [4,128]
+        sink %2
+    """
+    
+    post2 = """
+    main:
+        %cond = source
+        jnz %cond, @then, @else
+    then:
+        calldatacopy 64, 100, 256
+        %1 = mload 64
+        sink %1
+    else:
+        calldatacopy 64, 1000, 64
+        %2 = mload 64
+        sink %2
+    """
+
+    _check_pre_post_mem2var(pre, post1)
+    _check_pre_post(post1, post2)
+
