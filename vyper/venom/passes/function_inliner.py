f771dbe277e1cce44c3d5686d60195cb0896fbc4
Charles Cooper <cooper.charles.m@gmail.com>
2025-11-27 07:32:46 -0500

    increase nestiness

--- vyper/venom/passes/function_inliner.py
+++ vyper/venom/passes/function_inliner.py
@@ -192,22 +192,21 @@ class FunctionInlinerPass(IRGlobalPass):
                     # all preceding operands (if any) are return values.
                     ret_values = [op for op in inst.operands[:-1] if not isinstance(op, IRLabel)]
 
-                    inst.opcode = "jmp"
-                    inst.operands = [call_site_return.label]
 
                     # Map each returned value to corresponding callsite outputs
-                    if len(ret_values) == 0:
-                        continue
+                    if len(ret_values) > 0:
+                        callsite_outs = call_site.get_outputs()
+                        assert len(ret_values) == len(
+                            callsite_outs
+                        ), f"Return arity mismatch: {len(ret_values)} vs {len(callsite_outs)}"
+                        for idx, ret_value in enumerate(ret_values):
+                            target_out = callsite_outs[idx]
+                            bb.insert_instruction(
+                                IRInstruction("assign", [ret_value], [target_out]), -1
+                            )
 
-                    callsite_outs = call_site.get_outputs()
-                    assert len(ret_values) == len(
-                        callsite_outs
-                    ), f"Return arity mismatch: {len(ret_values)} vs {len(callsite_outs)}"
-                    for idx, ret_value in enumerate(ret_values):
-                        target_out = callsite_outs[idx]
-                        bb.insert_instruction(
-                            IRInstruction("assign", [ret_value], [target_out]), -1
-                        )
+                    inst.opcode = "jmp"
+                    inst.operands = [call_site_return.label]
 
             for inst in bb.instructions:
                 if not inst.annotation:
