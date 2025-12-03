40e3716479f721636c53535c0bef61c560711341
Quentin Bernet <quentin.bernet@bluewin.ch>
2025-12-03 14:25:04 +0000

    Replace expression by local alias

--- vyper/semantics/analysis/module.py
+++ vyper/semantics/analysis/module.py
@@ -317,7 +317,7 @@ class ModuleAnalyzer(VyperNodeVisitorBase):
                 # refactor so that range() is properly tagged.
                 continue
 
-            call_t = call_node.func._expr_info.typ
+            call_t = expr_info.typ
 
             if not isinstance(call_t, ContractFunctionT):
                 continue
