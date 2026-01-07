56bb178d1767a3fdae4bb0855c7493aed8ed8246
Quentin Bernet <quentin.bernet@bluewin.ch>
2026-01-07 09:24:15 +0000

    Make test unlinearizable

--- tests/functional/codegen/abstract/test_simple.py
+++ tests/functional/codegen/abstract/test_simple.py
@@ -1,28 +1,30 @@
 def test_basic_default_default_param_function(env, get_logs, get_contract, make_input_bundle):
+    # Both modules "call" at least one method from the other one
     contract = """
-import foo
+import abstract_m
 
-initializes: foo
+initializes: abstract_m
 
 @external
 def my_method() -> uint256:
-    return foo.foo()
+    return abstract_m.foo()
 
-def baz() -> uint256:
-    return 101
-
-@override(foo)
+@override(abstract_m)
 def bar() -> uint256:
-    return self.baz()
+    return abstract_m.const()
     """
-    foo = """
+
+    abstract_m = """
 def foo() -> uint256:
     return self.bar()
 
 @abstract
 def bar() -> uint256: ...
+
+def const() -> uint256:
+    return 101
     """
-    input_bundle = make_input_bundle({"foo.vy": foo})
+    input_bundle = make_input_bundle({"abstract_m.vy": abstract_m})
     c = get_contract(contract, input_bundle=input_bundle)
 
     assert c.my_method() == 101
