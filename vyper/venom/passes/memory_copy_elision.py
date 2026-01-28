87f33162bd64369ddc6a63ce61a7456ec5a0ce5a
Hodan <plodeada@fit.cvut.cz>
2026-01-28 15:57:02 +0100

    allowed load stores in different address spaces

--- vyper/venom/passes/memory_copy_elision.py
+++ vyper/venom/passes/memory_copy_elision.py
@@ -3,42 +3,54 @@ from vyper.venom.analysis import BasePtrAnalysis, MemoryAliasAnalysis, DFGAnalys
 from vyper.venom.basicblock import IRBasicBlock, IRInstruction, IRVariable
 from vyper.venom.memory_location import MemoryLocation
 import vyper.evm.address_space as addr_space
-from vyper.venom.effects import Effects
+from vyper.venom.effects import Effects, to_addr_space
 from vyper.venom.passes.machinery.inst_updater import InstUpdater
 
 _NONMEM_COPY_OPCODES = ("calldatacopy", "codecopy", "dloadbytes", "returndatacopy")
 _COPIES_OPCODES = ("mcopy", *_NONMEM_COPY_OPCODES)
 
+_LOADS = {"mload": Effects.MEMORY, "sload": Effects.STORAGE, "tload": Effects.TRANSIENT}
+_STORES = {"mstore": Effects.MEMORY, "sstore": Effects.STORAGE, "tstore": Effects.TRANSIENT}
+
 class MemoryCopyElisionPass(IRPass):
     base_ptr: BasePtrAnalysis
     copies: dict[MemoryLocation, IRInstruction]
-    loads: dict[IRVariable, tuple[MemoryLocation, IRInstruction]]
+    loads: dict[Effects, dict[IRVariable, tuple[MemoryLocation, IRInstruction]]]
 
     def run_pass(self):
         self.base_ptr = self.analyses_cache.request_analysis(BasePtrAnalysis)
         self.mem_alias = self.analyses_cache.request_analysis(MemoryAliasAnalysis)
         self.dfg = self.analyses_cache.request_analysis(DFGAnalysis)
         self.updater = InstUpdater(self.dfg)
+        self.loads = {Effects.MEMORY: dict(), Effects.STORAGE: dict(), Effects.TRANSIENT: dict()}
 
         for bb in self.function.get_basic_blocks():
             self._process_bb(bb)
 
     def _process_bb(self, bb: IRBasicBlock):
         self.copies = dict()
-        self.loads = dict()
+        for e in self.loads.values():
+            e.clear()
         for inst in bb.instructions:
-            if inst.opcode == "mload":
-                read_loc = self.base_ptr.get_read_location(inst, addr_space=addr_space.MEMORY)
+            if inst.opcode in _LOADS:
+                eff = _LOADS[inst.opcode]
+                space = to_addr_space(eff)
+                assert space is not None
+                read_loc = self.base_ptr.get_read_location(inst, addr_space=space)
                 if read_loc.is_fixed:
-                    self.loads[inst.output] = (read_loc, inst)
-            elif inst.opcode == "mstore":
-                write_loc = self.base_ptr.get_write_location(inst, addr_space=addr_space.MEMORY)
-                self._try_elide_load_store(inst, write_loc)
-                self._invalidate(write_loc)
+                    self.loads[eff][inst.output] = (read_loc, inst)
+
+            elif inst.opcode in _STORES:
+                eff = _STORES[inst.opcode]
+                space = to_addr_space(eff)
+                assert space is not None
+                write_loc = self.base_ptr.get_write_location(inst, addr_space=space)
+                self._try_elide_load_store(inst, write_loc, eff)
+                self._invalidate(write_loc, eff)
 
             elif inst.opcode in _NONMEM_COPY_OPCODES:
                 write_loc = self.base_ptr.get_write_location(inst, addr_space.MEMORY)
-                self._invalidate(write_loc)
+                self._invalidate(write_loc, Effects.MEMORY)
                 if write_loc.is_fixed:
                     self.copies[write_loc] =inst
 
@@ -46,34 +58,36 @@ class MemoryCopyElisionPass(IRPass):
                 self._try_elide_copy(inst)
 
                 write_loc = self.base_ptr.get_write_location(inst, addr_space.MEMORY)
-                self._invalidate(write_loc)
+                self._invalidate(write_loc, Effects.MEMORY)
                 if write_loc.is_fixed:
                     self.copies[write_loc] =inst
 
             elif _volatile_memory(inst):
                 self.copies.clear()
-                self.loads.clear()
+                self.loads[Effects.MEMORY].clear()
 
-    def _invalidate(self, write_loc: MemoryLocation):
-        if not write_loc.is_fixed:
+    def _invalidate(self, write_loc: MemoryLocation, eff: Effects):
+        if not write_loc.is_fixed and Effects.MEMORY in eff:
             self.copies.clear()
-            self.loads.clear()
+        if not write_loc.is_fixed:
+            self.loads[eff].clear()
 
-        to_remove = []
-        for mem_loc in self.copies.keys():
-            if self.mem_alias.may_alias(mem_loc, write_loc):
-                to_remove.append(mem_loc)
+        if Effects.MEMORY in eff:
+            to_remove = []
+            for mem_loc in self.copies.keys():
+                if self.mem_alias.may_alias(mem_loc, write_loc):
+                    to_remove.append(mem_loc)
 
-        for mem_loc in to_remove:
-            del self.copies[mem_loc]
+            for mem_loc in to_remove:
+                del self.copies[mem_loc]
         
         to_remove = []
-        for var, (mem_loc, _) in self.loads.items():
+        for var, (mem_loc, _) in self.loads[eff].items():
             if self.mem_alias.may_alias(mem_loc, write_loc):
                 to_remove.append(var)
 
         for var in to_remove:
-            del self.loads[var]
+            del self.loads[eff][var]
 
 
     def _try_elide_copy(self, inst: IRInstruction):
@@ -95,15 +109,15 @@ class MemoryCopyElisionPass(IRPass):
         inst.opcode = previous.opcode
         inst.operands[1] = src
 
-    def _try_elide_load_store(self, inst: IRInstruction, write_loc: MemoryLocation):
+    def _try_elide_load_store(self, inst: IRInstruction, write_loc: MemoryLocation, eff: Effects):
         val = inst.operands[0]
         if not isinstance(val, IRVariable):
             return
-        if val not in self.loads:
+        if val not in self.loads[eff]:
             return
-        if self.loads[val][0] != write_loc:
+        if self.loads[eff][val][0] != write_loc:
             return
-        _, load_inst = self.loads[val]
+        _, load_inst = self.loads[eff][val]
         uses = self.dfg.get_uses(load_inst.output)
         if len(uses) > 1:
             return
