74c7631a744a738b3449d969b7f7edb7b588e3e0
Hodan <plodeada@fit.cvut.cz>
2026-02-03 16:23:54 +0100

    added proper support for globals

--- vyper/venom/memory_allocator.py
+++ vyper/venom/memory_allocator.py
@@ -8,6 +8,7 @@ from vyper.venom.memory_location import Allocation
 
 
 class MemoryAllocator:
+    global_allocation: set[tuple[int, int]]
     # global state:
     #   all allocated mems, alloca => (ptr, size)
     allocated: dict[Allocation, int]
@@ -31,6 +32,7 @@ class MemoryAllocator:
     def __init__(self):
         self.reserved = set()
 
+        self.global_allocation = set()
         self.allocated = dict()
         self.mems_used = dict()
         self.fn_eom = dict()
@@ -38,6 +40,11 @@ class MemoryAllocator:
 
     def set_position(self, alloca: Allocation, position: int):
         self.allocated[alloca] = position
+
+    def add_global(self, alloca: Allocation):
+        assert alloca in self.allocated
+        ptr = self.allocated[alloca]
+        self.global_allocation.add((ptr, alloca.alloca_size))
 
     def allocate(self, alloca: Allocation) -> int:
         assert alloca not in self.allocated
@@ -71,7 +78,7 @@ class MemoryAllocator:
         return IRLiteral(self.allocated[ptr.base_alloca] + ptr.offset)
 
     def start_fn_allocation(self, fn):
-        self.reserved = set()
+        self.reserved = self.global_allocation.copy()
         self.current_function = fn
         self.allocated_fn = OrderedSet()
 
@@ -92,7 +99,7 @@ class MemoryAllocator:
         return eom
 
     def reset(self):
-        self.reserved = set()
+        self.reserved = self.global_allocation.copy()
 
     def reserve(self, alloca: Allocation):
         assert alloca in self.allocated
