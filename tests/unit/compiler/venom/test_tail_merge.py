0c60c464372c9336ebd434356297e6e4f4c55a18
Harry Kalogirou <harkal@nlogn.eu>
2026-02-09 14:02:13 +0200

    claude added tests

--- tests/unit/compiler/venom/test_tail_merge.py
+++ tests/unit/compiler/venom/test_tail_merge.py
@@ -94,3 +94,126 @@ def test_no_merge_for_non_halting_blocks():
     """
 
     _check_no_change(pre)
+
+
+def test_merge_identical_stop_blocks():
+    pre = """
+    main:
+        %cond = source
+        jnz %cond, @a, @b
+    a:
+        stop
+    b:
+        stop
+    """
+    post = """
+    main:
+        %cond = source
+        jnz %cond, @a, @a
+    a:
+        stop
+    """
+
+    _check_pre_post(pre, post)
+
+
+def test_merge_identical_return_blocks():
+    pre = """
+    main:
+        %cond = source
+        jnz %cond, @a, @b
+    a:
+        %a0 = 0
+        %a1 = 32
+        return %a0, %a1
+    b:
+        %b0 = 0
+        %b1 = 32
+        return %b0, %b1
+    """
+    post = """
+    main:
+        %cond = source
+        jnz %cond, @a, @a
+    a:
+        %a0 = 0
+        %a1 = 32
+        return %a0, %a1
+    """
+
+    _check_pre_post(pre, post)
+
+
+def test_no_merge_different_halting_opcodes():
+    pre = """
+    main:
+        %cond = source
+        jnz %cond, @a, @b
+    a:
+        revert 0, 0
+    b:
+        stop
+    """
+
+    _check_no_change(pre)
+
+
+def test_merge_three_identical_blocks():
+    pre = """
+    main:
+        %c1 = source
+        jnz %c1, @block_a, @dispatch
+    dispatch:
+        %c2 = source
+        jnz %c2, @block_b, @block_c
+    block_a:
+        revert 0, 0
+    block_b:
+        revert 0, 0
+    block_c:
+        revert 0, 0
+    """
+    post = """
+    main:
+        %c1 = source
+        jnz %c1, @block_a, @dispatch
+    dispatch:
+        %c2 = source
+        jnz %c2, @block_a, @block_a
+    block_a:
+        revert 0, 0
+    """
+
+    _check_pre_post(pre, post)
+
+
+def test_no_merge_entry_block():
+    pre = """
+    main:
+        stop
+    other:
+        stop
+    """
+
+    _check_no_change(pre)
+
+
+def test_no_merge_with_phi_nodes():
+    pre = """
+    main:
+        %cond = source
+        jnz %cond, @left, @right
+    left:
+        %y = 2
+        jnz %cond, @phi_revert, @plain_revert
+    right:
+        %z = 3
+        jnz %cond, @phi_revert, @plain_revert
+    phi_revert:
+        %p = phi @left, %y, @right, %z
+        revert 0, 0
+    plain_revert:
+        revert 0, 0
+    """
+
+    _check_no_change(pre)
