df238ac820121c5981fadc25c7fe7caf81503823
Quentin Bernet <quentin.bernet@bluewin.ch>
2026-02-11 11:04:04 +0000

    Make Namespace not a dict anymore

--- vyper/semantics/namespace.py
+++ vyper/semantics/namespace.py
@@ -3,14 +3,14 @@ import contextvars
 
 from vyper.ast.identifiers import validate_identifier
 from vyper.builtins.functions import get_builtin_functions
-from vyper.exceptions import CompilerPanic, NamespaceCollision, UndeclaredDefinition
+from vyper.exceptions import NamespaceCollision, UndeclaredDefinition
 from vyper.semantics import environment
 from vyper.semantics.analysis.base import VarInfo
 from vyper.semantics.analysis.levenshtein_utils import get_levenshtein_error_suggestions
 from vyper.semantics.types import PRIMITIVE_TYPES
 
 
-class Namespace(dict):
+class Namespace:
     """
     Immutable namespace object representing a contract's resolved names.
     Do note however that values stored in the namespace are not necessarily immutable !
@@ -22,29 +22,34 @@ class Namespace(dict):
       ModuleInfo      - imported module bindings
     """
 
+    _dict: dict  # [str, VarInfo | VyperType | type[VyperType] | ModuleInfo]
+
+    def __init__(self, data: dict):
+        self._dict = data
+
     def __eq__(self, other):
         return self is other
 
+    def __contains__(self, key):
+        return key in self._dict
+
+    def __iter__(self):
+        return iter(self._dict)
+
+    def __len__(self):
+        return len(self._dict)
+
     def __getitem__(self, key):
-        if key not in self:
-            hint = get_levenshtein_error_suggestions(key, self, 0.2)
+        if key not in self._dict:
+            hint = get_levenshtein_error_suggestions(key, self._dict, 0.2)
             raise UndeclaredDefinition(f"'{key}' has not been declared.", hint=hint)
-        return super().__getitem__(key)
+        return self._dict[key]
 
-    def __setitem__(self, attr, obj):
-        raise CompilerPanic("Cannot modify an immutable Namespace")
-
-    def __delitem__(self, key):
-        raise CompilerPanic("Cannot modify an immutable Namespace")
-
-    def update(self, other):
-        raise CompilerPanic("Cannot modify an immutable Namespace")
-
-    def clear(self):
-        raise CompilerPanic("Cannot modify an immutable Namespace")
+    def items(self):
+        return self._dict.items()
 
     def __reduce__(self):
-        return (Namespace, (dict(self),))
+        return (Namespace, (self._dict,))
 
     # -- immutable update methods --
 
@@ -53,7 +58,7 @@ class Namespace(dict):
         validate_identifier(attr)
 
         if attr in self:
-            prev = super().__getitem__(attr)
+            prev = self._dict[attr]
             prev_decl = getattr(prev, "decl_node", None)
             msg = f"'{attr}' has already been declared"
             if prev_decl is None:
@@ -64,16 +69,18 @@ class Namespace(dict):
         """Return a new Namespace with the key-value pair added."""
         self._validate_assignment(key)
         assert isinstance(key, str), f"not a string: {key}"
-        new_data = dict(self)
+        new_data = self._dict.copy()
         new_data[key] = value
         return Namespace(new_data)
 
     def with_items(self, items: dict) -> "Namespace":
         """Return a new Namespace with multiple items added."""
-        new_ns = self
+        new_data = self._dict.copy()
         for key, value in items.items():
-            new_ns = new_ns.with_item(key, value)
-        return new_ns
+            self._validate_assignment(key)
+            assert isinstance(key, str), f"not a string: {key}"
+            new_data[key] = value
+        return Namespace(new_data)
 
     # -- static namespace infrastructure --
 
