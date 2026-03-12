b12f1c163dfdedc82d415b055c9190ced6e7729a
Harry Kalogirou <harkal@nlogn.eu>
2026-03-12 14:06:43 +0200

    cleanup

--- vyper/venom/passes/float_allocas.py
+++ vyper/venom/passes/float_allocas.py
@@ -1,4 +1,3 @@
-from vyper.venom.basicblock import IRInstruction, IRLiteral
 from vyper.venom.passes.base_pass import IRPass
 
 
@@ -33,7 +32,7 @@ class FloatAllocas(IRPass):
         while i < len(insts):
             inst = insts[i]
 
-            if inst.opcode not in ("alloca", "palloca", "calloca"):
+            if inst.opcode != "alloca":
                 non_alloca.append(inst)
                 i += 1
                 continue
@@ -42,46 +41,6 @@ class FloatAllocas(IRPass):
             # TODO: investigate.
             entry_bb.insert_instruction(inst)
 
-            if inst.opcode == "palloca" and self._move_palloca_init_store(entry_bb, insts, i, inst):
-                i += 2  # skip the moved init store
-                continue
-
             i += 1
 
         return non_alloca
-
-    def _move_palloca_init_store(
-        self, entry_bb, insts: list[IRInstruction], idx: int, palloca_inst: IRInstruction
-    ) -> bool:
-        """
-        Move the synthetic palloca init store (stack-passed params only) to entry.
-        Returns True if an init store was moved and should be skipped in the caller.
-        """
-        if idx + 1 >= len(insts):
-            return False
-
-        next_inst = insts[idx + 1]
-        if not self._is_palloca_init_store(palloca_inst, next_inst):
-            return False
-
-        entry_bb.insert_instruction(next_inst)
-        return True
-
-    def _is_palloca_init_store(
-        self, palloca_inst: IRInstruction, mstore_inst: IRInstruction
-    ) -> bool:
-        if mstore_inst.opcode != "mstore" or len(mstore_inst.operands) < 2:
-            return False
-
-        if mstore_inst.operands[1] != palloca_inst.output:
-            return False
-
-        alloca_id = palloca_inst.operands[1]
-        if not isinstance(alloca_id, IRLiteral):
-            return False
-
-        param = self.function.get_param_by_id(alloca_id.value)
-        if param is None:
-            return False
-
-        return mstore_inst.operands[0] == param.func_var
