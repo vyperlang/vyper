180b72a82bf8adfab14034cccba8f8e1f6f357b5
Hodan <plodeada@fit.cvut.cz>
2026-03-25 14:34:39 +0100

    just assert in fix mem locs

--- vyper/venom/passes/fix_mem_locations.py
+++ vyper/venom/passes/fix_mem_locations.py
@@ -37,9 +37,6 @@ class FixMemLocationsPass(IRPass):
         # needed here it should be correct
         self.dfg = self.analyses_cache.request_analysis(DFGAnalysis)
         self.updater = InstUpdater(self.dfg)
-        inst = self.function.entry.instructions[0]
-        self.free_ptr1 = self._create_pinned_alloca(inst, MemoryPositions.FREE_VAR_SPACE)
-        self.free_ptr2 = self._create_pinned_alloca(inst, MemoryPositions.FREE_VAR_SPACE2)
 
         for bb in self.function.get_basic_blocks():
             self._process_bb(bb)
@@ -51,45 +48,9 @@ class FixMemLocationsPass(IRPass):
             if write_op is not None:
                 size = get_write_max_size(inst)
                 if size is not None and isinstance(write_op, IRLiteral):
-                    if in_free_var(MemoryPositions.FREE_VAR_SPACE, write_op.value):
-                        offset = write_op.value - MemoryPositions.FREE_VAR_SPACE
-                        ptr = self.updater.add_before(
-                            inst, "gep", [self.free_ptr1, IRLiteral(offset)]
-                        )
-                        assert ptr is not None
-                        update_write_location(inst, ptr)
-                    elif in_free_var(MemoryPositions.FREE_VAR_SPACE2, write_op.value):
-                        offset = write_op.value - MemoryPositions.FREE_VAR_SPACE2
-                        ptr = self.updater.add_before(
-                            inst, "gep", [self.free_ptr2, IRLiteral(offset)]
-                        )
-                        assert ptr is not None
-                        update_write_location(inst, ptr)
+                    assert False, (bb, inst)
             if read_op is not None:
                 size = get_read_size(inst)
                 if size is None or not isinstance(read_op, IRLiteral):
                     continue
-
-                if in_free_var(MemoryPositions.FREE_VAR_SPACE, read_op.value):
-                    offset = read_op.value - MemoryPositions.FREE_VAR_SPACE
-                    ptr = self.updater.add_before(inst, "gep", [self.free_ptr1, IRLiteral(offset)])
-                    assert ptr is not None
-                    update_read_location(inst, ptr)
-                elif in_free_var(MemoryPositions.FREE_VAR_SPACE2, read_op.value):
-                    offset = read_op.value - MemoryPositions.FREE_VAR_SPACE2
-                    ptr = self.updater.add_before(inst, "gep", [self.free_ptr2, IRLiteral(offset)])
-                    assert ptr is not None
-                    update_read_location(inst, ptr)
-
-    def _create_pinned_alloca(self, inst: IRInstruction, mem_position: int) -> IRVariable:
-        """
-        Creates alloca and sets its concrete position to
-        expected memory position
-        """
-        ptr = self.updater.add_before(inst, "alloca", [IRLiteral(32)])
-        assert ptr is not None
-        alloca_inst = self.dfg.get_producing_instruction(ptr)
-        alloca_inst.annotation = f"free var {mem_position}"
-        assert alloca_inst is not None
-        self.function.ctx.mem_allocator.set_position(Allocation(alloca_inst), mem_position)
-        return ptr
+                assert False, (bb, inst)
