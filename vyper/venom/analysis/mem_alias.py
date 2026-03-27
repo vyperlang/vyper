3dd92c8d5b18096a61ef2244be26c411c1f93c7c
Hodan <plodeada@fit.cvut.cz>
2026-03-27 16:18:55 +0100

    bisect in the memalias

--- vyper/venom/analysis/mem_alias.py
+++ vyper/venom/analysis/mem_alias.py
@@ -1,10 +1,11 @@
+import bisect
 from typing import Optional
 
 from vyper.evm.address_space import MEMORY, STORAGE, TRANSIENT, AddrSpace
 from vyper.utils import OrderedSet
 from vyper.venom.analysis import BasePtrAnalysis, CFGAnalysis, DFGAnalysis, IRAnalysis
 from vyper.venom.basicblock import IRInstruction
-from vyper.venom.memory_location import MemoryLocation
+from vyper.venom.memory_location import MemoryLocation, Allocation
 
 
 class MemoryAliasAnalysisAbstract(IRAnalysis):
@@ -23,6 +24,8 @@ class MemoryAliasAnalysisAbstract(IRAnalysis):
 
         # Map from memory locations to sets of potentially aliasing locations
         self.alias_sets: dict[MemoryLocation, OrderedSet[MemoryLocation]] = {}
+        self.concrete_locs: set[MemoryLocation] = set()
+        self.abstract_locs: dict[Allocation, list[MemoryLocation]] = dict()
 
         # Analyze all memory operations
         for bb in self.function.get_basic_blocks():
@@ -46,6 +49,12 @@ class MemoryAliasAnalysisAbstract(IRAnalysis):
         if loc not in self.alias_sets:
             self.alias_sets[loc] = OrderedSet()
 
+        if not loc.is_concrete:
+            self._analyze_abstract_mem_location(loc)
+            return
+
+        self.concrete_locs.add(loc)
+
         # Check for aliasing with existing locations
         # NOTE: This is O(n) per location, resulting in O(n^2) total for n locations.
         # For large contracts, consider using an interval tree or immutable set
@@ -54,6 +63,39 @@ class MemoryAliasAnalysisAbstract(IRAnalysis):
             if MemoryLocation.may_overlap(loc, other_loc):
                 self.alias_sets[loc].add(other_loc)
                 self.alias_sets[other_loc].add(loc)
+    
+    def insert_memloc(self, loc: MemoryLocation):
+        assert loc.alloca is not None
+        if loc.alloca not in self.abstract_locs:
+            self.abstract_locs[loc.alloca] = []
+        
+        def key(item: MemoryLocation) -> int:
+            if item.size is None or item.offset is None:
+                return 2**256
+            return item.size + item.offset
+
+        alloca_list = self.abstract_locs[loc.alloca]
+        index  = bisect.bisect_left(alloca_list, key(loc), key=key)
+        if len(alloca_list) <= index or alloca_list[index] != loc:
+            alloca_list.insert(index, loc)
+        offset = 0 if loc.offset is None else loc.offset
+        return bisect.bisect_left(alloca_list, offset, key=key)
+        
+
+    def _analyze_abstract_mem_location(self, loc: MemoryLocation):
+        assert loc.alloca is not None
+        index = self.insert_memloc(loc)
+
+        for concrete_loc in self.concrete_locs:
+            if MemoryLocation.may_overlap(loc, concrete_loc):
+                self.alias_sets[loc].add(concrete_loc)
+                self.alias_sets[concrete_loc].add(loc)
+
+        for other_loc in self.abstract_locs[loc.alloca][index:]:
+            if MemoryLocation.may_overlap(loc, other_loc):
+                self.alias_sets[loc].add(other_loc)
+                self.alias_sets[other_loc].add(loc)
+
 
     def may_alias(self, loc1: MemoryLocation, loc2: MemoryLocation) -> bool:
         """
