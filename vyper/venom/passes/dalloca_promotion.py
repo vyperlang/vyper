18324e1ba4be8d15f4a810dbf266c1f14059e782
Harry Kalogirou <harkal@nlogn.eu>
2026-04-17 09:50:03 +0300

    invalidate analyses

--- vyper/venom/passes/dalloca_promotion.py
+++ vyper/venom/passes/dalloca_promotion.py
@@ -1,3 +1,4 @@
+from vyper.venom.analysis import BasePtrAnalysis, MemLivenessAnalysis
 from vyper.venom.basicblock import IRLiteral
 from vyper.venom.passes.base_pass import IRPass
 
@@ -11,9 +12,15 @@ class DallocaPromotion(IRPass):
     required_successors = ("ConcretizeMemLocPass",)
 
     def run_pass(self):
+        promoted = False
         for bb in self.function.get_basic_blocks():
             for inst in bb.instructions:
                 if inst.opcode != "dalloca":
                     continue
                 if isinstance(inst.operands[0], IRLiteral):
                     inst.opcode = "alloca"
+                    promoted = True
+
+        if promoted:
+            self.analyses_cache.invalidate_analysis(MemLivenessAnalysis)
+            self.analyses_cache.invalidate_analysis(BasePtrAnalysis)
