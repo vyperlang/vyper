47cbd247a33f9f38be2738664eb3d2645855f5dc
Quentin Bernet <quentin.bernet@bluewin.ch>
2026-04-23 09:39:49 +0000

    Revert "Simplify version computation"
    This reverts commit f376a25b63d57ea86b7245a0da8f9dbacd676a3d.

--- setup.py
+++ setup.py
@@ -1,24 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import os
 import re
+import subprocess
 
 from setuptools import setup
 
 
 # strip local version
-def _local_version_suffix(version):
-    """
-    Return +commit.{_hash} for non-tagged versions.
-    Returns empty string for exact tag matches.
-    """
-    if version.exact:
-        return ""
-    # version.node is like "g073df5f1", strip the 'g' prefix
-    node = version.node
-    _hash = node.removeprefix("g")
-    assert node != _hash, "There should always be a prefix"
-    return f"+commit.{_hash}"
-
+def _local_version(version):
+    return ""
 
 def _global_version(version):
     from setuptools_scm.version import guess_next_dev_version
@@ -29,9 +20,25 @@ def _global_version(version):
     return re.sub(r"\.dev\d+", "", version_str)
 
 
+hash_file_rel_path = os.path.join("vyper", "vyper_git_commithash.txt")
+hashfile = os.path.relpath(hash_file_rel_path)
+
+# there is no way in setuptools-scm to get metadata besides the package
+# version into version.py. (and we need that version to be PEP440 compliant
+# in order to get it into pypi). so, add the commit hash to the package
+# separately, in order so that we can add it to `vyper --version`.
+try:
+    commithash = subprocess.check_output("git rev-parse --short HEAD".split())
+    commithash_str = commithash.decode("utf-8").strip()
+    with open(hashfile, "w") as fh:
+        fh.write(commithash_str)
+except subprocess.CalledProcessError:
+    pass
+
+
 setup(
     use_scm_version={
-        "local_scheme": _local_version_suffix,
+        "local_scheme": _local_version,
         "version_scheme": _global_version,
         "write_to": "vyper/version.py",
     }
--- vyper/__init__.py
+++ vyper/__init__.py
@@ -1,16 +1,24 @@
 from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as _version
+from pathlib import Path as _Path
 
 from vyper.compiler import compile_code, compile_from_file_input
 
-# pep440 version with commit hash
-__long_version__: str
+_commit_hash_file = _Path(__file__).parent.joinpath("vyper_git_commithash.txt")
+
+if _commit_hash_file.exists():
+    with _commit_hash_file.open() as fp:
+        __commit__ = fp.read()
+else:
+    __commit__ = "unknown"
+
+__version__: str
 try:
-    __long_version__ = _version(__name__)
+    __version__ = _version(__name__)
 except PackageNotFoundError:
     from vyper.version import version
 
-    __long_version__ = version
+    __version__ = version
 
-# clean version
-__version__ = __long_version__.split("+")[0]
+# pep440 version with commit hash
+__long_version__ = f"{__version__}+commit.{__commit__}"
