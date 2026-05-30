e1d3b49f594e0f2dd051b5eff21dafa7ef0bb41c
Charles Cooper <cooper.charles.m@gmail.com>
2026-05-30 19:24:45 +0000

    refactor: remove inline ABI utility imports

--- vyper/codegen_shared/abi_utils.py
+++ vyper/codegen_shared/abi_utils.py
@@ -5,10 +5,7 @@ If codegen_legacy is removed, consider moving these to vyper/abi.py
 or into methods on VyperType / ABIType.
 """
 
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from vyper.semantics.types import VyperType
+from vyper.semantics.types import StructT, TupleT, VyperType
 
 # In storage, dyn arrays use 1 slot for the length.
 # In memory, dyn arrays use 32 bytes (1 word) for the length.
@@ -16,10 +13,8 @@ if TYPE_CHECKING:
 DYNAMIC_ARRAY_OVERHEAD = 1
 
 
-def is_tuple_like(typ: "VyperType") -> bool:
+def is_tuple_like(typ: VyperType) -> bool:
     """Check if type is tuple-like (tuple or struct)."""
-    from vyper.semantics.types import StructT, TupleT
-
     # A lot of code paths treat tuples and structs similarly
     # so we have a convenience function to detect it
     ret = isinstance(typ, (TupleT, StructT))
@@ -27,27 +22,23 @@ def is_tuple_like(typ: "VyperType") -> bool:
     return ret
 
 
-def needs_external_call_wrap(typ: "VyperType") -> bool:
+def needs_external_call_wrap(typ: VyperType) -> bool:
     """
     For calls to ABI conforming contracts, return types are ALWAYS tuples
     even if only one element is being returned.
     https://docs.soliditylang.org/en/latest/abi-spec.html#function-selector-and-argument-encoding
     """
-    from vyper.semantics.types import TupleT
-
     return not (isinstance(typ, TupleT) and typ.length > 1)
 
 
-def calculate_type_for_external_return(typ: "VyperType") -> "VyperType":
+def calculate_type_for_external_return(typ: VyperType) -> VyperType:
     """Wrap type in a tuple if needed for ABI return encoding."""
-    from vyper.semantics.types import TupleT
-
     if needs_external_call_wrap(typ):
         return TupleT((typ,))
     return typ
 
 
-def abi_encoding_matches_vyper(typ: "VyperType") -> bool:
+def abi_encoding_matches_vyper(typ: VyperType) -> bool:
     """
     Returns True if the ABI encoding matches vyper's memory encoding
     of a type, otherwise False.
