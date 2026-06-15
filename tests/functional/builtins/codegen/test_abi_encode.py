d50e880e106772f7fddeaaa0410c9b5cfaf0a562
banteg <4562643+banteg@users.noreply.github.com>
2026-06-15 21:28:18 +0400

    test[venom]: rely on pipeline-level abi_encode coverage

--- tests/functional/builtins/codegen/test_abi_encode.py
+++ tests/functional/builtins/codegen/test_abi_encode.py
@@ -3,7 +3,6 @@ from eth.codecs import abi
 
 from tests.utils import decimal_to_int
 from vyper.compiler import compile_code
-from vyper.compiler.settings import Settings
 from vyper.exceptions import StructureException
 
 
@@ -397,13 +396,11 @@ def foo(ensure_tuple: bool) -> Bytes[96]:
     assert c.foo(True) == expected_output
 
 
-@pytest.mark.parametrize("use_venom", [False, True])
-def test_abi_encode_no_args(use_venom):
+def test_abi_encode_no_args():
     code = """
 @external
 def foo() -> Bytes[32]:
     return abi_encode()
     """
-    settings = Settings(experimental_codegen=use_venom)
     with pytest.raises(StructureException, match="abi_encode expects at least one argument"):
-        compile_code(code, settings=settings)
+        compile_code(code)
