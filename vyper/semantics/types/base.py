b21d489a98b1a3c6f93784dd7a20729f5b46307a
Quentin Bernet <quentin.bernet@bluewin.ch>
2026-07-27 15:58:46 +0200

    Cleanup

--- vyper/semantics/types/base.py
+++ vyper/semantics/types/base.py
@@ -489,7 +489,7 @@ class KwargSettings:
 class BottomT(VyperType):
     """
     Bottom type, the ultimate subtype: is a subtype of every other type.
-    It is unhabited: no value has this type.
+    It is uninhabited: no value has this type.
 
     It is for example the element type for empty lists: `[]: DynArray[Never, 1]`
     """
