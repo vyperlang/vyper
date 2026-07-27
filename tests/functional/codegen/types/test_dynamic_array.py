7106325d22af3db36ff9d9a5f82245929b171f38
Quentin Bernet <quentin.bernet@bluewin.ch>
2026-07-27 13:42:14 +0200

    Make codegen test check output

--- tests/functional/codegen/types/test_dynamic_array.py
+++ tests/functional/codegen/types/test_dynamic_array.py
@@ -241,10 +241,12 @@ flag Foo:
     Member1
 
 @external
-def foo():
+def foo() -> DynArray[DynArray[Foo, 5], 5]:
     tmp: DynArray[DynArray[Foo, 5], 5] = [[], [Foo.Member1]]
+    return tmp
     """
-    get_contract(code)
+    c = get_contract(code)
+    assert c.foo() == [[], [1]]
 
 
 def test_array_accessor(get_contract):
