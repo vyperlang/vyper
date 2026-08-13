e3c8523d8746ba0c3aea2b01e4486ddd04e990e6
Hodan <plodeada@fit.cvut.cz>
2026-08-13 15:09:01 +0200

    test that disables optional passes

--- tests/functional/examples/thirdparty/test_thirdparty.py
+++ tests/functional/examples/thirdparty/test_thirdparty.py
@@ -3,6 +3,34 @@ from pathlib import Path
 
 import pytest
 
+from vyper.venom.passes import (
+    AffineFoldingPass,
+    AlgebraicOptimizationPass,
+    AssertCombinerPass,
+    AssertEliminationPass,
+    AssignElimination,
+    BranchOptimizationPass,
+    CSE,
+    DeadStoreElimination,
+    DFTPass,
+    FunctionInlinerPass,
+    InternalReturnCopyForwardingPass,
+    ReduceLiteralsCodesize,
+    LoadElimination,
+    Mem2Var,
+    MemMergePass,
+    MemoryCopyElisionPass,
+    OverflowEliminationPass,
+    PhiEliminationPass,
+    ReadonlyInvokeArgCopyForwardingPass,
+    RemoveUnusedVariablesPass,
+    RevertToAssert,
+    SCCP,
+    TailMergePass,
+)
+
+from vyper.venom import OPTIMIZATION_PASSES
+
 import vyper.compiler as compiler
 
 dir_path = Path(__file__).parent
@@ -14,9 +42,52 @@ def get_example_vy_filenames():
 
 @pytest.mark.parametrize("vy_filename", get_example_vy_filenames())
 def test_compile(vy_filename):
-    if vy_filename == "curvefi/amm/stableswap/implementation/implementation_v_700.vy":
-        pytest.xfail("StackTooDeep: Unsupported dup depth 17")
-
     with open(dir_path / vy_filename) as f:
         source_code = f.read()
     compiler.compile_code(source_code)
+
+@pytest.mark.parametrize("vy_filename", get_example_vy_filenames())
+@pytest.mark.parametrize("pass_to_disable", [
+    AffineFoldingPass,
+    AlgebraicOptimizationPass,
+    AssertCombinerPass,
+    AssertEliminationPass,
+    AssignElimination,
+    BranchOptimizationPass,
+    CSE,
+    DeadStoreElimination,
+    DFTPass,
+    FunctionInlinerPass,
+    InternalReturnCopyForwardingPass,
+    ReduceLiteralsCodesize,
+    LoadElimination,
+    Mem2Var,
+    MemMergePass,
+    MemoryCopyElisionPass,
+    OverflowEliminationPass,
+    PhiEliminationPass,
+    ReadonlyInvokeArgCopyForwardingPass,
+    RemoveUnusedVariablesPass,
+    RevertToAssert,
+    SCCP,
+    TailMergePass,
+])
+def test_compile_pass_fuzz(vy_filename, pass_to_disable, compiler_settings, monkeypatch):
+    if not compiler_settings.experimental_codegen:
+        pytest.skip()
+
+    if pass_to_disable not in OPTIMIZATION_PASSES[compiler_settings.optimize] and pass_to_disable != FunctionInlinerPass:
+        pytest.skip()
+
+    with open(dir_path / vy_filename) as f:
+        source_code = f.read()
+    
+    run = []
+    def temp(*args, **kwargs):
+        run.append(True)
+
+    monkeypatch.setattr(pass_to_disable, "run_pass", temp)
+
+    compiler.compile_code(source_code)
+
+    assert len(run) != 0 and all(run)
