be155aa20691ac0798fab2caa9de6ebeca77ac7a
tserg <8017125+tserg@users.noreply.github.com>
2026-08-15 21:48:46 +0800

    reorder

--- vyper/codegen/core.py
+++ vyper/codegen/core.py
@@ -517,6 +517,30 @@ def append_dyn_array(darray_node, elem_node):
             return IRnode.from_list(b1.resolve(b2.resolve(ret)))
 
 
+def pop_dyn_array(darray_node, return_popped_item):
+    assert isinstance(darray_node.typ, DArrayT)
+    assert darray_node.encoding == Encoding.VYPER
+    ret = ["seq"]
+    with darray_node.cache_when_complex("darray") as (b1, darray_node):
+        old_len = clamp("gt", get_dyn_array_count(darray_node), 0)
+        new_len = IRnode.from_list(["sub", old_len, 1], typ=UINT256_T)
+
+        with new_len.cache_when_complex("new_len") as (b2, new_len):
+            # store new length
+            ret.append(ensure_eval_once("pop_dynarray", STORE(darray_node, new_len)))
+
+            # NOTE skip array bounds check bc we already asserted len two lines up
+            if return_popped_item:
+                popped_item = get_element_ptr(darray_node, new_len, array_bounds_check=False)
+                ret.append(popped_item)
+                typ = popped_item.typ
+                location = popped_item.location
+            else:
+                typ, location = None, None
+
+            return IRnode.from_list(b1.resolve(b2.resolve(ret)), typ=typ, location=location)
+
+
 def extend_dyn_array(dst, src, context):
     assert isinstance(dst.typ, DArrayT)
 
@@ -556,30 +580,6 @@ def extend_dyn_array(dst, src, context):
             ret.append(ensure_eval_once("extend_dynarray", STORE(dst, new_len)))
 
             return IRnode.from_list(b1.resolve(b2.resolve(b3.resolve(ret))))
-
-
-def pop_dyn_array(darray_node, return_popped_item):
-    assert isinstance(darray_node.typ, DArrayT)
-    assert darray_node.encoding == Encoding.VYPER
-    ret = ["seq"]
-    with darray_node.cache_when_complex("darray") as (b1, darray_node):
-        old_len = clamp("gt", get_dyn_array_count(darray_node), 0)
-        new_len = IRnode.from_list(["sub", old_len, 1], typ=UINT256_T)
-
-        with new_len.cache_when_complex("new_len") as (b2, new_len):
-            # store new length
-            ret.append(ensure_eval_once("pop_dynarray", STORE(darray_node, new_len)))
-
-            # NOTE skip array bounds check bc we already asserted len two lines up
-            if return_popped_item:
-                popped_item = get_element_ptr(darray_node, new_len, array_bounds_check=False)
-                ret.append(popped_item)
-                typ = popped_item.typ
-                location = popped_item.location
-            else:
-                typ, location = None, None
-
-            return IRnode.from_list(b1.resolve(b2.resolve(ret)), typ=typ, location=location)
 
 
 # add an offset to a pointer, keeping location and encoding info
