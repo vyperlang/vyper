6b176339294d00da4839dda9afbbc8847129e0a5
Hodan <plodeada@fit.cvut.cz>
2026-09-20 09:35:15 +0200

    lint

--- vyper/venom/passes/loop_invariant_hosting.py
+++ vyper/venom/passes/loop_invariant_hosting.py
@@ -58,7 +58,7 @@ class LoopInvariantHoisting(IRPass):
     dfg: DFGAnalysis
     gas_heuristic: int
 
-    def run_pass(self, / , gas_heuristic: int = 2):
+    def run_pass(self, /, gas_heuristic: int = 2):
         self.gas_heuristic = gas_heuristic
         self.cfg = self.analyses_cache.request_analysis(CFGAnalysis)
         self.dfg = self.analyses_cache.request_analysis(DFGAnalysis)  # type: ignore
@@ -133,7 +133,7 @@ class LoopInvariantHoisting(IRPass):
         cannot_hoists_insts: set[IRInstruction],
         loop: OrderedSet[IRBasicBlock],
     ) -> OrderedSet[IRInstruction]:
-        res = OrderedSet()
+        res: OrderedSet[IRInstruction] = OrderedSet()
         for op in inst.operands:
             if not isinstance(op, IRVariable):
                 continue
